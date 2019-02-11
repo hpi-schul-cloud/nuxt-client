11b3eac9e564ed8d734766fa4cb0de3e52729195
Christian Tjerk Weißmann <christian.t.weissmann@campus.tu-berlin.de>
2019-02-11 15:25:23 +0100

    Adjusted links for storybook

--- stories/base.stories.js
+++ stories/base.stories.js
@@ -2,11 +2,11 @@ import { storiesOf } from "@storybook/vue";
 import { withMarkdownNotes } from "@storybook/addon-notes";
 import baseDoc from "../docs/components/base.md";
 
-import BaseButton from "../components/ui/BaseButton.vue";
-import BaseCard from "../components/ui/BaseCard.vue";
-import BaseIcon from "../components/ui/BaseIcon.vue";
-import BaseInput from "../components/ui/BaseInput.vue";
-import BaseLink from "../components/ui/BaseLink.vue";
+import BaseButton from "../src/components/ui/BaseButton.vue";
+import BaseCard from "../src/components/ui/BaseCard.vue";
+import BaseIcon from "../src/components/ui/BaseIcon.vue";
+import BaseInput from "../src/components/ui/BaseInput.vue";
+import BaseLink from "../src/components/ui/BaseLink.vue";
 
 storiesOf("Base Components", module)
 	.addDecorator(withMarkdownNotes(baseDoc))
