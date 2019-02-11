a5f8242bd46fe5b679ce66333ef3312ef1cf666d
Adrian Jost <adrianjost@users.noreply.github.com>
2019-02-11 17:49:19 +0100

    fix hygen templates (#19)
    * move to src folder
    
    * update dependencies
    
    * fix travis & run lint
    
    * removed husky
    
    * fix test config
    
    * Adjusted links for storybook
    
    * fix babel config
    
    * add font download
    
    * redo storybook build
    
    * Adjusted alisases
    
    * another try to fix travis
    
    * and another try to fix travis
    
    * and one more try
    
    * and the next one
    
    * update documentation
    
    * try without cache
    
    * fix alias
    
    * update hygen docs
    
    * yarn -> yarn install
    
    * hopefully it's now fixed! :)
    
    * run build synchronous
    
    * update package.json
    
    * added some tests for BaseLink component
    
    * removed build from travis

--- src/components/ui/BaseLink.unit.js
+++ src/components/ui/BaseLink.unit.js
@@ -17,4 +17,28 @@ describe("@components/BaseLink", () => {
 		});
 		expect(element.innerHTML).toContain(slotContent);
 	});
+
+	it("renders a-tag for external links", () => {
+		const { element } = shallowMount(BaseLink, {
+			propsData: {
+				href: "https://schul-cloud.org",
+			},
+		});
+		expect(element.outerHTML).toContain("https://schul-cloud.org");
+		expect(element.tagName).toEqual("A");
+	});
+
+	it("renders NuxtLink-tag for external links", () => {
+		createComponentMocks("NuxtLink");
+		const { element } = shallowMount(BaseLink, {
+			...createComponentMocks({
+				stubs: ["NuxtLink"],
+			}),
+			propsData: {
+				to: "/news",
+			},
+		});
+		expect(element.getAttribute("to")).toEqual("/news");
+		expect(element.tagName).not.toEqual("A");
+	});
 });
