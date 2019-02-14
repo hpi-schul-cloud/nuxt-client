1ecf5a20626d43c505bbaff4c92bb9f195745244
Falko Benthin <Falko.Benthin@hpi.de>
2019-02-14 15:36:19 +0100

    deployment zerflext in story* und nuxt*, um storybook und nuxtclient getrennt zu deployen

--- deploy.sh
+++ deploy.sh
@@ -14,18 +14,24 @@ docker push schulcloud/schulcloud-nuxtclient:$DOCKERTAG
 docker push schulcloud/schulcloud-nuxtclient:$GIT_SHA
 docker push schulcloud/schulcloud-nuxtclient:latest
 
-# screw together config file for docker swarm
-eval "echo \"$( cat compose-nuxt-test.dummy )\"" > docker-compose-nuxtclient.yml
-
-
-# copy config-file to server and execute mit travis_rsa
-pwd
-ls
 openssl aes-256-cbc -K $encrypted_b7461320c5f4_key -iv $encrypted_b7461320c5f4_iv -in travis_rsa.enc -out travis_rsa -d
 chmod 600 travis_rsa
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-nuxtclient.yml linux@test.schul-cloud.org:~
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxtclient.yml test-schul-cloud
-# ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker service update --force test-schul-cloud_server
+
+if [[ $DOCKERTAG == story* ]] 
+then
+  # screw together config file for docker swarm
+  eval "echo \"$( cat compose-storybook-test.dummy )\"" > docker-compose-storybook.yml
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-storybook.yml linux@test.schul-cloud.org:~
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-storybook.yml test-schul-cloud
+elif [[ $DOCKERTAG == nuxt* ]]
+then
+  # screw together config file for docker swarm
+  eval "echo \"$( cat compose-nuxt-test.dummy )\"" > docker-compose-nuxtclient.yml
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-nuxtclient.yml linux@test.schul-cloud.org:~
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxtclient.yml test-schul-cloud
+else
+  echo "Branch wird nicht deployt"
+fi
 
 exit 0
 
