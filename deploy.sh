fa5d6126694279331a076959bde11171cc476d30
Falko Benthin <Falko.Benthin@hpi.de>
2019-02-14 14:51:09 +0100

    wo ist die travis_rsa? Teil 2

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ eval "echo \"$( cat compose-nuxt-test.dummy )\"" > docker-compose-nuxtclient.yml
 # copy config-file to server and execute mit travis_rsa
 pwd
 ls
+openssl aes-256-cbc -K $encrypted_b7461320c5f4_key -iv $encrypted_b7461320c5f4_iv -in travis_rsa.enc -out travis_rsa -d
 chmod 600 travis_rsa
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-nuxtclient.yml linux@test.schul-cloud.org:~
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxtclient.yml test-schul-cloud
