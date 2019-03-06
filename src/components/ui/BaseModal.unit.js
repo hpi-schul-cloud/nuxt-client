25bef8a45a9714f1a38c6c640fc9c5ce5607a09e
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-06 13:47:54 +0100

    Update BaseModal.unit.js

--- src/components/ui/BaseModal.unit.js
+++ src/components/ui/BaseModal.unit.js
@@ -4,23 +4,23 @@ import BaseModal from "./BaseModal";
 const modal = {
 	data: () => ({ active: false }),
 	template: `
-	<BaseModal id="modal" ref="modal" :active.sync="active" @close="active = false">
-		<div>
-			<div class="modal-header">
-				<h3>custom header</h3>
-			</div>
+		<BaseModal id="modal" ref="modal" :active.sync="active" @close="active = false">
+			<div>
+				<div class="modal-header">
+					<h3>custom header</h3>
+				</div>
 
-			<div class="modal-body">
-				Hello I'm a modal, do you like to close me? Then just click outside of my box or the button below.
-			</div>
+				<div class="modal-body">
+					Hello I'm a modal, do you like to close me? Then just click outside of my box or the button below.
+				</div>
 
-			<div class="modal-footer">
-				<BaseButton id="button" class="is-light" @click="$refs.modal.close()">
-					OK
-				</BaseButton>
+				<div class="modal-footer">
+					<BaseButton id="button" class="is-light" @click="$refs.modal.close()">
+						OK
+					</BaseButton>
+				</div>
 			</div>
-		</div>
-	</BaseModal>				
+		</BaseModal>
 	`,
 	components: { BaseModal },
 };
