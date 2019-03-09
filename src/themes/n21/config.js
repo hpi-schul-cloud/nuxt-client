05d4c836ef63e4dafd14f510e3fddc151db1e929
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-09 08:57:40 +0100

    update images

--- src/themes/n21/config.js
+++ src/themes/n21/config.js
@@ -1,10 +1,8 @@
-const images = "/images/";
-
 export default {
 	name: "Niedersächsische Bildungscloud",
 	short_name: "Bildungscloud",
 	logo: {
-		app: images + "logo/app-logo-512.png",
-		white: images + "logo/logo-white.svg",
+		app: "/images/logo/app-logo-512.png",
+		white: "/images/logo/logo-white.svg",
 	},
 };
