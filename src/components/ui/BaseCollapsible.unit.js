797389193433b62c21f6fff2bde8dd5cf6ebe97c
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-11 11:31:34 +0100

    + BaseCollapsible tests

--- src/components/ui/BaseCollapsible.unit.js
+++ src/components/ui/BaseCollapsible.unit.js
@@ -1,5 +1,25 @@
 import BaseCollapsible from "./BaseCollapsible";
 
+const collapsible = {
+	data: () => ({ active: false }),
+	template: `
+		<BaseCollapsible label="Label for Toggle Button">
+			<p id="slot-content">Slot Content</p>
+		</BaseCollapsible>
+	`,
+	components: { BaseCollapsible },
+};
+
 describe("@components/BaseCollapsible", () => {
 	it(...isValidComponent(BaseCollapsible));
+
+	it("pressing the label should toggle slot visibility", () => {
+		const wrapper = mount(collapsible);
+
+		expect(wrapper.find("#slot-content").exists()).toBe(false);
+		wrapper.find(".collapsible").trigger("click");
+		expect(wrapper.find("#slot-content").exists()).toBe(true);
+		wrapper.find(".collapsible").trigger("click");
+		expect(wrapper.find("#slot-content").exists()).toBe(false);
+	});
 });
