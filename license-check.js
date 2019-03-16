df9b6e55e32a687b12293dcee09a6fa0407346f9
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-16 14:45:07 +0100

    undo "force travis to fail"

--- license-check.js
+++ license-check.js
@@ -30,14 +30,12 @@ crawler.dumpLicenses(options, function(error, dependencies) {
 	const incompatibleDependencies = Object.keys(dependencies).filter(
 		(dependency) => {
 			const licenses = dependencies[dependency].licenses;
-			/*
 			if (licenses.includes("AND")) {
 				return !licenses.split(/AND/).every(isCompatible);
 			}
 			if (licenses.includes("OR")) {
 				return !licenses.split(/OR/).some(isCompatible);
 			}
-			*/
 			return !isCompatible(licenses);
 		}
 	);
@@ -59,5 +57,6 @@ crawler.dumpLicenses(options, function(error, dependencies) {
 				`All dependency licenses seem to be compatible with ${pkg.license}.`
 			)
 		);
+		process.exit(0);
 	}
 });
