16bf8bdc9225bbf9f726f94ebb7cae26f4565e9d
AngelikaWieck <32275343+AngelikaWieck@users.noreply.github.com>
2019-03-20 15:18:42 +0100

    Component/dropdown menu (#66)
    * implemented DropdownMenu with MenuLinks
    
    * added aria attributes
    
    * added mixins directory and improved MenuLink
    
    * changed DropdownMenu links to internal links

---
+++ src/components/MenuLink.vue
@@ -0,0 +1,28 @@
+<template>
+	<BaseLink class="link" v-bind="$attrs">
+		<slot />
+	</BaseLink>
+</template>
+
+<script>
+import BaseLink from "@components/ui/BaseLink.vue";
+
+export default {
+	name: "MenuLink",
+	components: { BaseLink },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@variables";
+
+.link {
+	padding: 12px 16px;
+	color: black;
+	text-decoration: none;
+
+	&:hover {
+		background-color: #f1f1f1;
+	}
+}
+</style>
