6f59e631bd2c707a4c1e51b383f4676da1d177aa
Angelika Wieck <angelika.wieck@student.hpi.de>
2019-03-20 21:55:13 +0100

    implemented basic quote

---
+++ src/components/Quote.unit.js
@@ -0,0 +1,7 @@
+import Quote from "./Quote";
+
+describe("@components/Quote", () => {
+	it("exports a valid component", () => {
+		expect(Quote).toBeAComponent();
+	});
+});
---
+++ src/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+	<blockquote :cite="cite">
+		<slot />
+	</blockquote>
+</template>
+
+<script>
+export default {
+	name: "Quote",
+	props: {
+		cite: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@variables";
+
+blockquote::after {
+	display: block;
+	margin-top: 1em;
+	font-size: 0.8em;
+	content: "Von: " attr(cite);
+}
+</style>
