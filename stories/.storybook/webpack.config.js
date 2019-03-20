55fd6ac6b46b2e126bf1f437c8f1349ab9c2b5c7
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-20 14:26:31 +0100

    upgrade to 5.0.3 done

--- stories/.storybook/webpack.config.js
+++ stories/.storybook/webpack.config.js
@@ -1,37 +1,33 @@
 // filename = webpack.config.js
 const path = require("path");
 
-module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: "storybook-addon-vue-info/loader",
-				enforce: "post",
-			},
-			{
-				test: /\.scss$/,
-				loaders: ["style-loader", "css-loader", "sass-loader"],
-				include: path.resolve(__dirname, "../../"),
-			},
-			/* // we are not using it in our nuxt config, so why here.
-			{
-				test: /\.svg$/,
-				loader: "vue-svg-loader",
-			},
-			*/
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {},
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		alias: require("../../aliases.config").webpack,
-	},
+module.exports = ({ config }) => {
+	const rules = [
+		{
+			test: /\.vue$/,
+			loader: "storybook-addon-vue-info/loader",
+			enforce: "post",
+		},
+		{
+			test: /\.scss$/,
+			loaders: ["style-loader", "css-loader", "sass-loader"],
+			include: path.resolve(__dirname, "../../"),
+		},
+		{
+			test: /\.(png|jpg|gif)$/,
+			use: [
+				{
+					loader: "file-loader",
+					options: {},
+				},
+			],
+		},
+	];
+	const alias = require("../../aliases.config").webpack;
+
+	config.module.rules = config.module.rules.concat(rules);
+	Object.assign(config.resolve.alias, alias);
+
+	console.log(config.module.rules);
+	return config;
 };
