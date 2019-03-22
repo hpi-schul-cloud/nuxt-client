0a012b75234b5298a914e517e960d9609c828a1b
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-22 11:14:13 +0100

    reorder addons to order tabs

--- stories/.storybook/addons.js
+++ stories/.storybook/addons.js
@@ -1,7 +1,7 @@
-import "@storybook/addon-notes/register";
+import "storybook-addon-vue-info/lib/register";
+import "@storybook/addon-a11y/register";
 import "@storybook/addon-knobs/register";
+import "@storybook/addon-notes/register";
 import "@storybook/addon-actions/register";
 import "@storybook/addon-links/register";
 import "@storybook/addon-viewport/register";
-import "@storybook/addon-a11y/register";
-import "storybook-addon-vue-info/lib/register";
