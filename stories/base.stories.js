d5cb483e4e9720b541e631104fb122bfb4df7ada
Adrian Jost <adrianjost@users.noreply.github.com>
2019-04-06 13:22:44 +0200

    Update base.stories.js

--- stories/base.stories.js
+++ stories/base.stories.js
@@ -270,9 +270,10 @@ storiesOf("Base Components", module)
 				"quote",
 				`For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.`
 			),
+			srcText: text("src-text", ""),
 		}),
 		template: outdent`
-			<BaseBlockquote :cite="cite">
+			<BaseBlockquote :cite="cite" :src-text="srcText || undefined">
 				{{quote}}
 			</BaseBlockquote>
 		`,
