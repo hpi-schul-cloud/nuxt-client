9f795fef3244e95954931d48ffe0fc4c7b73e658
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-05-02 12:40:40 +0200

    reorganized courses grid, added tests

---
+++ src/components/CoursesGrid.unit.js
@@ -0,0 +1,29 @@
+import { shallowMount } from "@vue/test-utils";
+import CoursesGrid from "./CoursesGrid";
+
+const course1 = {
+	_id: "0000dcfbfb5c7a3f00bf21abc",
+	name: "Deutsch",
+};
+
+const course2 = {
+	_id: "0000dcfbfb5c7a3f00bf21abd",
+	name: "Mathe",
+};
+
+describe("@components/CoursesGrid", () => {
+	it("exports a valid component", () => {
+		expect(CoursesGrid).toBeAComponent();
+	});
+	it("generates course card grid with 2 cards", () => {
+		const courses = [course1, course2];
+		const wrapper = shallowMount(CoursesGrid, {
+			propsData: { courses: courses },
+		});
+		expect(wrapper.findAll("course-card-stub").length).toBe(2);
+	});
+	it("generates course card grid with no courses", () => {
+		const wrapper = shallowMount(CoursesGrid);
+		expect(wrapper.findAll("course-card-stub").length).toBe(0);
+	});
+});
---
+++ src/components/CoursesGrid.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="app">
+		<div class="grid">
+			<div v-for="(course, i) of courses" :key="i" class="tile">
+				<course-card :course="course" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import CourseCard from "./CourseCard.vue";
+
+export default {
+	name: "CoursesGrid",
+	components: {
+		CourseCard,
+	},
+	props: {
+		courses: {
+			type: Array,
+			default: () => [],
+		},
+	},
+	computed: {},
+};
+</script>
+
+<style lang="scss">
+@import "@variables";
+
+.grid {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+	min-width: $size-content-width-min;
+	max-width: $size-content-width-max;
+	margin: 0 auto;
+}
+
+.tile {
+	display: flex;
+	margin: 10px;
+}
+</style>
