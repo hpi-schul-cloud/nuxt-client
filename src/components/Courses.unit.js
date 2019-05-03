2ff2b727ebcf23a27edd14146021dae82aacc884
Adrian Jost <adrianjost@users.noreply.github.com>
2019-05-03 13:12:29 +0200

    Delete Courses.unit.js

--- src/components/Courses.unit.js
+++
@@ -1,7 +0,0 @@
-import Courses from "./Courses";
-
-describe("@components/Courses", () => {
-	it(...isValidComponent(Courses));
-
-	//TODO missing test with set courses prop need to find out what format the server sends courses json
-});
