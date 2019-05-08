42d04e39abf5af52503214ca48238c8b2cdd6409
Michael Smith <smith64fx@gmail.com>
2019-05-08 14:37:50 +0800

    revert color coding

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -7,7 +7,7 @@
 		>
 		<section v-if="news && news.length > 0" class="section">
 			<news-card
-				v-for="article of newsColored"
+				v-for="article of news"
 				:key="article._id"
 				:article="article"
 				class="mb-2"
@@ -33,27 +33,6 @@ export default {
 		...mapGetters("news", {
 			news: "list",
 		}),
-		newsColored() {
-			const colors = [
-				"#F44336",
-				"#E91E63",
-				"#3F51B5",
-				"#2196F3",
-				"#03A9F4",
-				"#00BCD4",
-				"#009688",
-				"#4CAF50",
-				"#CDDC39",
-				"#FFC107",
-				"#FF9800",
-				"#FF5722",
-			];
-
-			return this.news.map((article) => {
-				article.color = colors[Math.floor(Math.random() * colors.length)];
-				return article;
-			});
-		},
 	},
 	created(ctx) {
 		this.find();
