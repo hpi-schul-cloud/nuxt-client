12c8a4ba62abafd8bd71cb3b7bb20e60bdf40a04
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-05-22 10:50:39 +0200

    BaseButton & BaseIcon

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -16,44 +16,119 @@ export default {};
 @import "@styles";
 
 .button {
-	display: flex;
+	display: inline-block;
 	align-items: center;
+	min-width: var(--space-xxxl);
 	padding: var(--space-xs) var(--space-sm);
-	color: var(--color-gray);
-	text-transform: uppercase;
+	font-family: var(--font-accent);
+	font-size: var(--text-md);
+	font-weight: var(--button-font-weight);
+	line-height: var(--button-line-height);
+	color: var(--color-gray-dark);
+	text-align: center;
+	white-space: nowrap;
 	cursor: pointer;
 	border: 1px solid var(--color-gray);
 	border-radius: var(--radius-md);
-	transition: background-color var(--duration-transition-medium) ease;
+	transition: all var(--duration-transition-medium)
+		cubic-bezier(0.23, 1, 0.32, 1) 0ms;
 	&:hover,
 	&:focus {
-		filter: brightness(0.7);
+		background-color: var(--color-gray-light);
+	}
+
+	&:focus {
+		background-color: var(--color-gray-light);
+		outline: none;
+	}
+
+	@include breakpoint(md) {
+		font-size: var(--button-font-large);
 	}
 }
 
-.is-light {
-	color: var(--color-gray);
-	background-color: transparent;
-	border-width: 1px;
-}
-.is-primary {
-	color: var(--color-white);
-	background-color: var(--color-primary);
+.is-primary.is-outline {
+	color: var(--color-primary);
+	background-color: var(--color-white);
+	border: 1px solid var(--color-primary);
 }
 
-.is-accent {
+.is-primary,
+.is-hero-cta,
+.is-primary.is-outline:hover {
+	color: var(--color-white);
+	background-color: var(--color-primary);
+	border: 1px solid transparent;
+}
+
+.is-hero-cta {
+	box-shadow: 0 12px 17px 2px rgba(0, 0, 0, 0.14),
+		0 5px 22px 4px rgba(0, 0, 0, 0.12), 0 7px 8px -4px rgba(0, 0, 0, 0.2);
+}
+
+.is-primary:hover,
+.is-primary:focus,
+.is-hero-cta:hover {
+	background-color: var(--color-primary-dark);
+}
+
+.is-secondary.is-outline {
+	color: var(--color-accent);
+	background-color: var(--color-white);
+	border: 1px solid var(--color-accent);
+}
+
+.is-secondary,
+.is-secondary.is-outline:hover {
+	color: var(--color-white);
 	background-color: var(--color-accent);
+	border: 1px solid transparent;
+}
+
+.is-secondary:hover,
+.is-secondary:focus {
+	background-color: var(--color-accent-dark);
+}
+
+.is-success {
+	color: var(--color-white);
+	background-color: var(--color-success);
+	border: 1px solid transparent;
+	&:hover,
+	&:focus {
+		background-color: var(--color-success);
+		filter: brightness(80%);
+	}
+}
+
+.is-danger {
+	color: var(--color-white);
+	background-color: var(--color-danger);
+	border: 1px solid transparent;
+	&:hover,
+	&:focus {
+		background-color: var(--color-danger);
+		filter: brightness(80%);
+	}
 }
 
 .is-small {
-	font-size: var(--text-sm);
+	padding: var(--space-xxxs) var(--space-sm) var(--space-xxxxs) var(--space-sm);
 }
 
 .is-medium {
-	font-size: var(--text-md);
+	padding: var(--space-xs) var(--space-sm) var(--space-xxs) var(--space-sm);
+
+	@include breakpoint(md) {
+		padding: var(--space-sm) var(--space-sm) var(--space-xs) var(--space-sm);
+	}
 }
 
 .is-large {
-	font-size: var(--text-lg);
+	padding: var(--space-sm) var(--space-sm) var(--space-sm) var(--space-sm);
+
+	@include breakpoint(md) {
+		padding: var(--space-md) var(--space-sm);
+	}
 }
 </style>
