8ff41b6920a4483e63df1fe2a3acc0e85184f659
Adrian Jost <adrianjost@users.noreply.github.com>
2019-05-23 18:14:29 +0200

    add more logs to deployment for easier debugging

--- deploy/pull-deploy.sh
+++ deploy/pull-deploy.sh
@@ -15,14 +15,18 @@ then
 fi
 
 # deploy docs
+echo "Deploy docs to surge.sh"
 surge --project ./dist/docs --domain docs.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh
 # deploy storybook
+echo "Deploy storybook to surge.sh"
 surge --project ./dist/storybook --domain stories.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh
 
 # and there is not already a comment
 if curl -s "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments" | grep "We deployed this pull-request for you."
 then
 		curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \ -d "{\"body\": \"We deployed this pull-request for you. Check it out:\n\n**Docs:** https://docs.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh \n**Storybook:** https://stories.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh\"}" \ "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+		echo "Commented URLs to github"
+
 else
 		echo "URL Comment already exists. => Skip"
 fi
