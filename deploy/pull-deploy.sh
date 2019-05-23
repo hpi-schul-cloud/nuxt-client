9c29f978f589065afff11129b2de38bea223003a
Adrian Jost <adrianjost@users.noreply.github.com>
2019-05-23 18:00:07 +0200

    fix deploy script

---
+++ deploy/pull-deploy.sh
@@ -0,0 +1,28 @@
+# Deploy Docs & Storybook to surge.sh
+# and comment deployed URLs to pull requests
+
+# Only run if it is a pull request
+if [ "$TRAVIS_PULL_REQUEST" == "false" ];
+then
+	echo "Do not deploy to test system. This is not a pull request."
+	exit 0;
+fi
+# and are not in master
+if [ "$TRAVIS_BRANCH" == "master" ];
+then
+	echo "Do not deploy to test system. We are on the master branch."
+	exit 0;
+fi
+
+# deploy docs
+surge --project ./dist/docs --domain docs.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh
+# deploy storybook
+surge --project ./dist/storybook --domain stories.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh
+
+# and there is not already a comment
+if curl -s "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments" | grep "We deployed this pull-request for you."
+then
+		curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \ -d "{\"body\": \"We deployed this pull-request for you. Check it out:\n\n**Docs:** https://docs.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh \n**Storybook:** https://stories.${TRAVIS_PULL_REQUEST}.nuxt.schul-cloud.surge.sh\"}" \ "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+else
+		echo "URL Comment already exists. => Skip"
+fi
