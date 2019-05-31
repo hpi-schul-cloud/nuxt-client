c1acedbb407715425ef19b3889036e6cf9932b82
Adrian Jost <adrianjost@users.noreply.github.com>
2019-05-31 11:09:21 +0200

    test deploy vuepress

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -50,6 +50,8 @@ chmod 600 travis_rsa
 # Log in to the docker CLI
 echo "$MY_DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
+vuepress
+
 if [[ $DOCKERTAG == master ]]
 then
   storybook
