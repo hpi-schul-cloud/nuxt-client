a14c52cf6cf8ebee688a4912d8e5febcdb8060b6
Adrian Jost <adrianjost@users.noreply.github.com>
2019-05-31 16:51:30 +0200

    store bugfix

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,10 +3,10 @@ import VueI18n from "vue-i18n";
 
 Vue.use(VueI18n);
 
-export const i18n = () =>
+export const i18n = (store) =>
 	new VueI18n({
-		locale: store.state.i18n.locale,
-		fallbackLocale: "en",
+		locale: store ? store.state.i18n.locale : "de",
+		fallbackLocale: "de",
 		messages: {
 			en: require("@@/locales/en.json"),
 			de: require("@@/locales/de.json"),
@@ -16,5 +16,5 @@ export const i18n = () =>
 export default ({ app, store }) => {
 	// Set i18n instance on app
 	// This way we can use it in middleware and pages asyncData/fetch
-	app.i18n = i18n();
+	app.i18n = i18n(store);
 };
