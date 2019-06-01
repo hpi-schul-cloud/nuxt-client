591e96fb50cefbf27afd841ad2a0267966bf09e9
Adrian Jost <adrianjost@users.noreply.github.com>
2019-06-01 14:53:33 +0200

    Ooops, updated wrong docker command :grin:

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,14 +6,28 @@ echo GITSHA $GIT_SHA
 
 DOCKERFILE_VERSION=${DOCKERFILE_VERSION:="Dockerfile"}
 
+# nuxt client bauen und deployen
+function nuxtclient {
+	docker build \
+		-t schulcloud/schulcloud-nuxt-client:latest \
+		-t schulcloud/schulcloud-nuxt-client:$GIT_SHA \
+		-f $DOCKERFILE_VERSION.nuxt \
+		../
+	docker push schulcloud/schulcloud-nuxt-client:$GIT_SHA
+	docker push schulcloud/schulcloud-nuxt-client:latest
+
+	eval "echo \"$( cat compose-nuxt.dummy )\"" > docker-compose-nuxt-client.yml
+
+	scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-nuxt-client.yml linux@test.schul-cloud.org:~
+	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxt-client.yml test-schul-cloud
+}
+
 # storybook doku bauen und deployen
 function storybook {
 	docker build \
 		-t schulcloud/schulcloud-nuxt-storybook:latest \
 		-t schulcloud/schulcloud-nuxt-storybook:$GIT_SHA \
 		-f $DOCKERFILE_VERSION.storybook \
-		--build-arg ALGOLIA_NAME \
-		--build-arg ALGOLIA_API_KEY \
 		../
 	docker push schulcloud/schulcloud-nuxt-storybook:$GIT_SHA
 	docker push schulcloud/schulcloud-nuxt-storybook:latest
@@ -24,21 +38,15 @@ function storybook {
 	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxt-storybook.yml test-schul-cloud
 }
 
-# client doku bauen und deployen
-function nuxtclient {
-	docker build -t schulcloud/schulcloud-nuxt-client:latest -t schulcloud/schulcloud-nuxt-client:$GIT_SHA -f $DOCKERFILE_VERSION.nuxt ../
-	docker push schulcloud/schulcloud-nuxt-client:$GIT_SHA
-	docker push schulcloud/schulcloud-nuxt-client:latest
-
-	eval "echo \"$( cat compose-nuxt.dummy )\"" > docker-compose-nuxt-client.yml
-
-	scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-nuxt-client.yml linux@test.schul-cloud.org:~
-	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxt-client.yml test-schul-cloud
-}
-
 # vuepress doku bauen und deployen
 function vuepress {
-	docker build -t schulcloud/schulcloud-nuxt-vuepress:latest -t schulcloud/schulcloud-nuxt-vuepress:$GIT_SHA -f $DOCKERFILE_VERSION.vuepress ../
+	docker build \
+		-t schulcloud/schulcloud-nuxt-vuepress:latest \
+		-t schulcloud/schulcloud-nuxt-vuepress:$GIT_SHA \
+		-f $DOCKERFILE_VERSION.vuepress \
+		--build-arg ALGOLIA_NAME \
+		--build-arg ALGOLIA_API_KEY \
+		../
 	docker push schulcloud/schulcloud-nuxt-vuepress:$GIT_SHA
 	docker push schulcloud/schulcloud-nuxt-vuepress:latest
 
@@ -48,6 +56,9 @@ function vuepress {
 	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@test.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-nuxt-vuepress.yml test-schul-cloud
 }
 
+# ----------------
+# MAIN SCRIPT
+# ----------------
 cd deploy
 
 openssl aes-256-cbc -K $encrypted_b7461320c5f4_key -iv $encrypted_b7461320c5f4_iv -in travis_rsa.enc -out travis_rsa -d
