aa3b066c909cadc0f236e6be57f475661a6bb203
Adrian Jost <adrianjost@users.noreply.github.com>
2019-06-01 15:24:41 +0200

    fix nuxt client deployment to use env variables

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,6 +12,10 @@ function nuxtclient {
 		-t schulcloud/schulcloud-nuxt-client:latest \
 		-t schulcloud/schulcloud-nuxt-client:$GIT_SHA \
 		-f $DOCKERFILE_VERSION.nuxt \
+		--build-arg API_HOST \
+		--build-arg SC_THEME \
+		--build-arg SC_TITLE \
+		--build-arg SC_SHORT_TITLE \
 		../
 	docker push schulcloud/schulcloud-nuxt-client:$GIT_SHA
 	docker push schulcloud/schulcloud-nuxt-client:latest
