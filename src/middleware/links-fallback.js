4f7284ad1bc4af0f03c716b69116ea7b423d22b8
Adrian Jost <adrianjost@users.noreply.github.com>
2019-07-05 17:15:57 +0200

    revert links-fallback changes

--- src/middleware/links-fallback.js
+++ src/middleware/links-fallback.js
@@ -5,24 +5,27 @@ export default async function(ctx, cb) {
 
 	const segments = route.path.split("/");
 	const controllerName = segments[1];
-	const legacyRouteConfig =
-		routes.find((r) => {
-			return typeof r === "object"
-				? r.route.includes(controllerName)
-				: r.includes(controllerName);
-		}) || {};
-
-	// current url doesn't match any excluded url
-	const isLegacy = (legacyRouteConfig.routesExcluded || []).every(
-		(excludedRoute) => {
-			const fullPath = "/" + legacyRouteConfig.route + excludedRoute;
-			return !fullPath.match(route.matched[0].regex);
+	const legacyRoute = routes.find((r) => {
+		if (typeof r === "object") {
+			return r.route.includes(controllerName);
+		} else {
+			return r.includes(controllerName);
 		}
-	);
+	});
 
-	if (isLegacy) {
-		window.location = route.path;
-	} else {
-		cb();
+	if (legacyRoute) {
+		if (!legacyRoute.routesExcluded) {
+			window.location = route.path;
+		} else {
+			const isLegacy = legacyRoute.routesExcluded.some((excludedRoute) => {
+				const fullPath = "/" + legacyRoute.route + excludedRoute;
+				return !fullPath.match(route.matched[0].regex);
+			});
+			if (isLegacy) {
+				window.location = route.path;
+			} else {
+				cb();
+			}
+		}
 	}
 }
