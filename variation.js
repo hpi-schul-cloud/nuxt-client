7d592810038dbea6aa878a8020770390e221ff50
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-07-24 11:44:36 +0200

    added error block if theme directory does not exist

--- variation.js
+++ variation.js
@@ -1,25 +1,30 @@
 const fs = require("fs");
 
 const walkSync = function(dir, filelist, base) {
-	const files = fs.readdirSync(dir);
-	const extension = /\.(vue)$/i;
-	myfilelist = filelist || {};
-	files.forEach(function(file) {
-		const dirname = dir.replace(base, "").substr(1);
-		const fullname = dir + "/" + file;
-		const filename = file.replace(/(.*)\.[^.]+$/, "$1");
+	try {
+		const files = fs.readdirSync(dir);
+		const extension = /\.(vue)$/i;
+		myfilelist = filelist || {};
+		files.forEach(function(file) {
+			const dirname = dir.replace(base, "").substr(1);
+			const fullname = dir + "/" + file;
+			const filename = file.replace(/(.*)\.[^.]+$/, "$1");
 
-		if (fs.statSync(dir + "/" + file).isDirectory()) {
-			myfilelist = walkSync(dir + "/" + file, myfilelist, base);
-		} else {
-			if (extension.test(file)) {
-				myfilelist["@" + dirname + "/" + filename] = fullname;
+			if (fs.statSync(dir + "/" + file).isDirectory()) {
+				myfilelist = walkSync(dir + "/" + file, myfilelist, base);
 			} else {
-				myfilelist["@" + dirname + "/" + file] = fullname;
+				if (extension.test(file)) {
+					myfilelist["@" + dirname + "/" + filename] = fullname;
+				} else {
+					myfilelist["@" + dirname + "/" + file] = fullname;
+				}
 			}
-		}
-	});
-	return myfilelist;
+		});
+		return myfilelist;
+	} catch (err) {
+		//directory does not exist
+		return {};
+	}
 };
 
 module.exports = function(variation) {
