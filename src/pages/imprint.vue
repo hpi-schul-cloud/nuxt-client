0a25473d9e055fdd02317a1fee2d02b6e2bfb4b8
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-08-01 17:21:35 +0200

    removed example component

--- src/pages/imprint.vue
+++ src/pages/imprint.vue
@@ -3,12 +3,10 @@
 		<div class="columns is-mobile">
 			<h1>Imprint - {{ $theme.short_name }}</h1>
 		</div>
-		<pulsating-dot class="alert-dot"></pulsating-dot>
 	</section>
 </template>
 <script>
-import PulsatingDot from "@components/PulsatingDot";
 export default {
-	components: { PulsatingDot },
+	components: {},
 };
 </script>
