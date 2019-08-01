691661017e194c6508b6edb43eebe820a7319bd7
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-08-01 12:15:38 +0200

    updated default config var name

--- src/themes/default/config.js
+++ src/themes/default/config.js
@@ -2,6 +2,6 @@ export default {
 	name: "HPI Schul-Cloud",
 	short_name: "Schul-Cloud",
 	logo: {
-		logo_top: require("@assets/img/logo/cloud-transparent-mono.svg"),
+		logo_image_mono: require("@assets/img/logo/cloud-transparent-mono.svg"),
 	},
 };
