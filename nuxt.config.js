5416091faf60c9aedbaf48d592ee47d064d6ae19
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-01 14:54:00 +0200

    fix API_URL
    you do not need to set API_URL anymore

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,6 @@ module.exports = {
 	theme: "default",
 	env: {
 		FALLBACK_DISABLED: process.env.FALLBACK_DISABLED || false,
-		API_URL: API_URL || "http://localhost:3030",
 	},
 	/*
 	 ** Headers of the page
@@ -90,7 +89,7 @@ module.exports = {
 	 */
 	axios: {
 		// See https://github.com/nuxt-community/axios-module#options
-		baseUrl: API_URL || "http://localhost:3030",
+		baseURL: API_URL,
 	},
 
 	/*
