68740dbfdc9bbb94b84ccb3dea2e6bdf58cb99cb
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-10-21 13:10:31 +0200

    add proxy error page

--- src/plugins/authenticate.js
+++ src/plugins/authenticate.js
@@ -1,10 +1,14 @@
 export default async ({ app, store }) => {
 	try {
+		if (location.pathname.startsWith("/error/proxy")) {
+			// Do not populate store on error pages
+			// otherwise we may get into a loop.
+			return;
+		}
 		const jwt = app.$cookies.get("jwt");
 		if (!jwt) {
 			store.dispatch("auth/logout");
 		}
-
 		store.commit("auth/setAccessToken", jwt);
 		await store.dispatch("auth/authenticate");
 	} catch (e) {
