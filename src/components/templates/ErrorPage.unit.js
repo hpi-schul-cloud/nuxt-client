d3c747a0e8693802b94613138bd32313ac5c72ef
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-10-21 13:18:18 +0200

    add one more testcase

--- src/components/templates/ErrorPage.unit.js
+++ src/components/templates/ErrorPage.unit.js
@@ -15,4 +15,28 @@ describe("@components/templates/ErrorPage", () => {
 		});
 		expect(wrapper.text()).toContain(testMessage);
 	});
+
+	it("Show error-message if defined", () => {
+		const testMessage = "testmessage";
+		const wrapper = shallowMount(ErrorPage, {
+			propsData: {
+				error: {
+					statusCode: "500",
+					message: testMessage,
+				},
+			},
+		});
+		expect(wrapper.text()).toContain(testMessage);
+	});
+
+	it("Show generic error message if no message is defined", () => {
+		const wrapper = shallowMount(ErrorPage, {
+			propsData: {
+				error: {
+					statusCode: "500",
+				},
+			},
+		});
+		expect(wrapper.text()).toContain("Ein Fehler ist aufgetreten");
+	});
 });
