ef25a36f57baf72675756418c3a0e243909fdc0e
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-10-29 14:36:36 +0100

    alias locale update

--- aliases.config.js
+++ aliases.config.js
@@ -9,7 +9,7 @@ const aliases = {
 	...variation,
 	"@": "src",
 	"@@": ".",
-	"@locales": "locale",
+	"@locale": "locale",
 	"@assets": "src/assets",
 	"@components": "src/components",
 	"@storyComponents": "stories/components",
