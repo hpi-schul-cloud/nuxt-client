cb05c734ea6f5372477541f62007b1756381eb03
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-10-29 16:28:28 +0100

    refactor utils import

--- src/utils/sidebarBaseItems.js
+++ src/utils/sidebarBaseItems.js
@@ -144,6 +144,11 @@ export default [
 				icon: "building",
 				href: "/administration/school/",
 			},
+			{
+				title: "Datenquellen",
+				icon: "database",
+				href: "/administration/datasources/",
+			},
 		],
 	},
 	{
