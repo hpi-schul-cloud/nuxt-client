f315a9493bf10a4e19d72d177848be2ca45e3787
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-06 11:12:23 +0100

    fix tests

--- tests/middleware/permission-check.unit.js
+++ tests/middleware/permission-check.unit.js
@@ -7,7 +7,7 @@ const mockApp = {
 };
 
 const getMockStore = ({ permissions = [], user } = {}) => {
-	const mockUser = user ? user : { permissions };
+	const mockUser = user !== undefined ? user : { permissions };
 	return {
 		state: {
 			auth: {
@@ -21,110 +21,107 @@ const getMockRoute = (requiredPermissions) => ({
 	meta: [{ requiredPermissions }],
 });
 
+const getMockContext = ({
+	app = mockApp,
+	store = getMockStore(),
+	route = getMockRoute(),
+} = {}) => ({
+	app,
+	store,
+	route,
+});
+
 describe("@middleware/permission-check", () => {
 	it("exports a function", () => {
 		expect(typeof permissionCheck).toBe("function");
 	});
 
-	it("grants access using simple syntax", () => {
-		const mockContext = {
-			app: mockApp,
+	it("grants access using simple syntax", async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({ permissions: ["PERMISSION_A", "PERMISSION_B"] }),
 			route: getMockRoute(["PERMISSION_A"]),
-		};
-		expect(permissionCheck(mockContext)).resolves.toBe(true);
+		});
+		await expect(permissionCheck(mockContext)).resolves.toBe(true);
 	});
 
-	it("grants access using advanced AND syntax", () => {
-		const mockContext = {
-			app: mockApp,
+	it(`grants access using advanced "AND" syntax`, async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({ permissions: ["PERMISSION_A", "PERMISSION_B"] }),
 			route: getMockRoute({
 				operator: "AND",
 				permissions: ["PERMISSION_A", "PERMISSION_B"],
 			}),
-		};
-		expect(permissionCheck(mockContext)).resolves.toBe(true);
+		});
+		await expect(permissionCheck(mockContext)).resolves.toBe(true);
 	});
 
-	it("grants access using advanced OR syntax", () => {
-		const mockContext = {
-			app: mockApp,
+	it(`grants access using advanced "OR" syntax`, async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({ permissions: ["PERMISSION_A", "PERMISSION_B"] }),
 			route: getMockRoute({
 				operator: "OR",
 				permissions: ["PERMISSION_B"],
 			}),
-		};
-		expect(permissionCheck(mockContext)).resolves.toBe(true);
+		});
+		await expect(permissionCheck(mockContext)).resolves.toBe(true);
 	});
 
-	it("grants Access if no permissions are required AND user exists", () => {
-		const mockContext = {
-			app: mockApp,
+	it("grants Access if no permissions are required AND user exists", async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({ permissions: ["PERMISSION_A"] }),
-			route: getMockRoute(),
-		};
-		expect(permissionCheck(mockContext)).resolves.toBe(true);
+		});
+		await expect(permissionCheck(mockContext)).resolves.toBe(true);
 	});
 
-	it("grants Access if no permissions are required AND user is missing", () => {
-		const mockContext = {
-			app: mockApp,
+	it("grants Access if no permissions are required AND user is missing", async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({ user: null }),
-			route: getMockRoute(),
-		};
-		expect(permissionCheck(mockContext)).resolves.toBe(true);
+		});
+		await expect(permissionCheck(mockContext)).resolves.toBe(true);
 	});
 
-	it("throws error.401 on missing permission using simple syntax", () => {
-		const mockContext = {
-			app: mockApp,
-			store: getMockStore({}),
+	it("throws error.401 on missing permission using simple syntax", async () => {
+		const mockContext = getMockContext({
 			route: getMockRoute(["MISSING_PERMISSION"]),
-		};
-		expect(permissionCheck(mockContext)).rejects.toThrow(
+		});
+		await expect(permissionCheck(mockContext)).rejects.toThrow(
 			new Error("error.401")
 		);
 	});
 
-	it("throws error.401 on missing permission using advanced AND syntax", () => {
-		const mockContext = {
-			app: mockApp,
+	it("throws error.401 on missing permission using advanced AND syntax", async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({
 				permissions: ["PERMISSION_A"],
 			}),
 			route: getMockRoute({
 				operator: "OR",
-				permissions: ["PERMISSION_A", "MISSING_PERMISSION"],
+				permissions: ["PERMISSION_B", "MISSING_PERMISSION"],
 			}),
-		};
-		expect(permissionCheck(mockContext)).rejects.toThrow(
+		});
+		await expect(permissionCheck(mockContext)).rejects.toThrow(
 			new Error("error.401")
 		);
 	});
 
-	it("throws error.401 on missing permission using advanced OR syntax", () => {
-		const mockContext = {
-			app: mockApp,
-			store: getMockStore({}),
+	it("throws error.401 on missing permission using advanced OR syntax", async () => {
+		const mockContext = getMockContext({
 			route: getMockRoute({
 				operator: "OR",
 				permissions: ["MISSING_PERMISSION"],
 			}),
-		};
-		expect(permissionCheck(mockContext)).rejects.toThrow(
+		});
+		await expect(permissionCheck(mockContext)).rejects.toThrow(
 			new Error("error.401")
 		);
 	});
 
-	it("throws error.401 on missing user", () => {
-		const mockContext = {
-			app: mockApp,
+	it("throws error.401 on missing user", async () => {
+		const mockContext = getMockContext({
 			store: getMockStore({ user: null }),
 			route: getMockRoute(["MISSING_PERMISSION"]),
-		};
-		expect(permissionCheck(mockContext)).rejects.toThrow(
+		});
+		await expect(permissionCheck(mockContext)).rejects.toThrow(
 			new Error("error.401")
 		);
 	});
