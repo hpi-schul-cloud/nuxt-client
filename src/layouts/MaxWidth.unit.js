72b5b8a67db6bf7fa88f4ba5f8c3c0654d61d207
Luisa <luisa.schrott@gmail.com>
2019-11-06 14:00:58 +0100

    fix

--- src/layouts/MaxWidth.unit.js
+++ src/layouts/MaxWidth.unit.js
@@ -2,7 +2,6 @@ import MaxWidth from "@layouts/MaxWidth";
 
 describe("@components/base/MaxWidth", () => {
 	it(...isValidComponent(MaxWidth));
-	it(...rendersSlotContent(MaxWidth, ["default"]));
 
 	it("contains class container-max-width", () => {
 		const wrapper = mount(MaxWidth);
