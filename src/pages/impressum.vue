a9d988e3890fcbeaceb3162736ee4bf9dc282998
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-11-06 17:16:04 +0100

    added maxwidth

--- src/pages/impressum.vue
+++ src/pages/impressum.vue
@@ -1,12 +1,15 @@
 <template>
-	<impressum />
+	<max-width>
+		<impressum />
+	</max-width>
 </template>
 
 <script>
 import Impressum from "@components/templates/impressum";
+import MaxWidth from "@components/atoms/MaxWidth";
 
 export default {
-	components: { Impressum },
+	components: { Impressum, MaxWidth },
 	meta: {
 		isPublic: true,
 	},
