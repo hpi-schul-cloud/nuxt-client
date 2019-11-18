929d8c53a9fdd05773b0f499f1b4542b63fdee05
Luisa <luisa.schrott@gmail.com>
2019-11-18 11:18:33 +0100

    solved conflicts

--- src/pages/courses/create.vue
+++ src/pages/courses/create.vue
@@ -18,6 +18,26 @@ import CourseWizard from "@components/organisms/CourseWizard";
 
 export default {
 	components: { CourseWizard },
+	data() {
+		return {
+			stepList: [
+				{ name: "Kursdaten" },
+				{ name: "Kurs-Mitglieder" },
+				{ name: "Abschließen" },
+			],
+			course: {
+				name: "",
+				description: "",
+				startDate: "",
+				untilDate: "",
+				times: [],
+				teacherIds: [],
+				substitutionIds: [],
+				userIds: [],
+				classIds: [],
+			},
+		};
+	},
 	async asyncData({ store }) {
 		try {
 			const teacherRole = (await store.dispatch("roles/find", {
@@ -41,26 +61,6 @@ export default {
 				students,
 			};
 		} catch (err) {}
-	},
-	data() {
-		return {
-			stepList: [
-				{ name: "Kursdaten" },
-				{ name: "Kurs-Mitglieder" },
-				{ name: "Abschließen" },
-			],
-			course: {
-				name: "",
-				description: "",
-				startDate: "",
-				untilDate: "",
-				times: [],
-				teacherIds: [],
-				substitutionIds: [],
-				userIds: [],
-				classIds: [],
-			},
-		};
 	},
 	computed: {
 		...mapGetters("classes", {
