67c489f17ec67c96467fb28287a65e99d016ee69
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-18 10:24:22 +0100

    moved news detail page to #359

--- src/pages/news/_id/index.vue
+++ src/pages/news/_id/index.vue
@@ -13,45 +13,36 @@
 					},
 				]"
 			/>
-			<i>
-				{{ dayjs(news.displayAt).fromNow() }} von {{ news.creator.firstName }}
-				{{ news.creator.lastName }}
-			</i>
-			<h1> {{ news.title }} </h1>
-
 			<!-- eslint-disable-next-line vue/no-v-html -->
 			<div v-html="news.content"></div>
 			<hr />
-			<base-link :to="{ name: 'news-id-edit' }">
-				<base-button>
-					{{ $t("pages.news._id.index.edit") }}
-				</base-button>
-			</base-link>
+			<base-button @click="$router.push({ name: 'news-id-edit' })">
+				Artikel bearbeiten
+			</base-button>
 		</section>
 	</div>
 </template>
 
 <script>
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
-import "dayjs/locale/de";
-dayjs.locale("de");
+import { mapGetters } from "vuex";
 
 export default {
+	computed: {
+		...mapGetters("news", {
+			news: "current",
+		}),
+	},
+	created(ctx) {
+		this.get(this.$route.params.id);
+	},
+	methods: {
+		get(id) {
+			this.$store.dispatch("news/get", id);
+		},
+	},
 	head() {
 		return {
 			title: (this.news || {}).title || "News",
-		};
-	},
-	data() {
-		return {
-			dayjs,
-		};
-	},
-	async asyncData({ store, params }) {
-		return {
-			news: await store.dispatch("news/get", params.id),
 		};
 	},
 };
