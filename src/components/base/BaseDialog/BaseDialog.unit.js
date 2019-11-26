fa6b3137c0831821a2189caf32eccc44cdfbb90c
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-11-26 19:18:24 +0100

    suggesting tests based on michaels work

--- src/components/base/BaseDialog/BaseDialog.unit.js
+++ src/components/base/BaseDialog/BaseDialog.unit.js
@@ -1,5 +1,43 @@
+import { createLocalVue } from "@vue/test-utils";
 import BaseDialog from "./BaseDialog";
 
 describe("@components/BaseDialog", () => {
 	it(...isValidComponent(BaseDialog));
+
+	it("Renders correctly without arguments and can be closed", () => {
+		const localVue = createLocalVue();
+		localVue.use(BaseDialog);
+		expect(typeof localVue.prototype.$dialog.confirm).toBe("function");
+
+		const wrapper = mount(BaseDialog, {
+			attachToDocument: true,
+			localVue,
+		});
+
+		const dialogue = wrapper.vm.$dialog.confirm();
+		expect(dialogue.isActive).toBe(true);
+		expect(dialogue.currentIconColor).toBe("var(--color-primary)");
+		dialogue.close();
+		expect(dialogue.isActive).toBe(false);
+	});
+
+	it("Renders correctly with arguments and can be closed", () => {
+		const localVue = createLocalVue();
+		localVue.use(BaseDialog);
+		expect(typeof localVue.prototype.$dialog.confirm).toBe("function");
+
+		const wrapper = mount(BaseDialog, {
+			attachToDocument: true,
+			localVue,
+		});
+
+		const dialogue = wrapper.vm.$dialog.confirm({
+			actionDesign: "danger",
+		});
+		expect(dialogue.isActive).toBe(true);
+		expect(dialogue.currentIconColor).toBe("var(--color-danger)");
+		//TODO Validate more parameters
+		dialogue.close();
+		expect(dialogue.isActive).toBe(false);
+	});
 });
