21c00acb4ad00b7ba7959eaa8c0b08909816d903
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-12-04 14:50:31 +0100

    add storybook entry for ListGridViewToggle

--- src/components/molecules/ListGridViewToggle.stories.js
+++ src/components/molecules/ListGridViewToggle.stories.js
@@ -4,6 +4,9 @@ import ListGridViewToggle from "./ListGridViewToggle";
 
 storiesOf("Molecules|ListGridViewToggle", module).add("default", () => ({
 	components: { ListGridViewToggle },
-	template: `<ListGridViewToggle />`,
-	data: () => ({}),
+	template: `<div>
+		<ListGridViewToggle v-model="viewType"/>
+		{{viewType}}
+	</div>`,
+	data: () => ({ viewType: "grid" }),
 }));
