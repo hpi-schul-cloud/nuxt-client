1be7a37e1343bf54d4be23940c2f01b86ebb852a
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-12-20 10:25:59 +0100

    removed unnecessary test

--- src/components/molecules/TitleInput.unit.js
+++ src/components/molecules/TitleInput.unit.js
@@ -2,16 +2,4 @@ import TitleInput from "./TitleInput";
 
 describe("@components/TitleInput", () => {
 	it(...isValidComponent(TitleInput));
-	it("contains an input", () => {
-		const wrapper = mount(TitleInput, {
-			propsData: {
-				vmodel: "",
-				label: "test",
-				name: "test",
-				value: "test",
-				type: "text",
-			},
-		});
-		expect(wrapper.find("input").exists()).toBe(true);
-	});
 });
