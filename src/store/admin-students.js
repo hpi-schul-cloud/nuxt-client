9c04d90dcad41d0b313136c9873574c6cb019894
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.uni-potsdam.de>
2020-01-11 19:08:58 +0100

    removed ununsed store 'admin-students'

--- src/store/admin-students.js
+++
@@ -1,7 +0,0 @@
-import mergeDeep from "../utils/merge-deep";
-import serviceTemplate from "../utils/service-template";
-const base = serviceTemplate("users/admin/students");
-
-const module = mergeDeep(base, {});
-
-export default module;
