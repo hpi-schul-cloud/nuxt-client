c9f0ef92fc4bccccc3182f3f2ef99e096618929c
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-13 15:03:14 +0100

    add error logging for missing jwt in auth plugins

--- src/plugins/authenticate.js
+++ src/plugins/authenticate.js
@@ -2,13 +2,14 @@ export default async ({ app, store, route }) => {
 	const isPublic = route.meta.some((meta) => meta.isPublic);
 	try {
 		const jwt = app.$cookies.get("jwt");
-		if (!jwt & !isPublic) {
+		if (!jwt && !isPublic) {
 			await store.dispatch("auth/logout");
 			window.location = "/";
 		}
 		store.commit("auth/setAccessToken", jwt);
 		await store.dispatch("auth/authenticate");
-	} catch (e) {
+	} catch (error) {
+		console.error(error);
 		store.dispatch("auth/logout");
 	}
 };
