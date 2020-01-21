0eaa016a387e198161018a35d4675d4e556cb4fa
Luisa <luisa.schrott@gmail.com>
2020-01-21 11:39:22 +0100

    renamed svg

--- src/components/molecules/EmptyState.stories.js
+++ src/components/molecules/EmptyState.stories.js
@@ -1,13 +1,10 @@
 import { storiesOf } from "@storybook/vue";
-
-
 import EmptyState from "./EmptyState";
-import ExampleImage from "@assets/icons/emptystate.svg";
+import ExampleImage from "@assets/img/emptystate-graph.svg";
 
 storiesOf("Molecules|EmptyState", module)
 	.add("default", () => ({
 		components: { EmptyState },
 		data: () => ({ imgsrc: ExampleImage }),
 		template: `<EmptyState :image="imgsrc" description="Noch keine Datenquellen vorhanden. Mit dem Plus unten rechts kannst du Datenquellen hinzufügen." />`,
-
 	}));
