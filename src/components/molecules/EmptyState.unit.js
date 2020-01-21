b95a1b73e9aebc878487ce3b4b26bb464e362518
Luisa <luisa.schrott@gmail.com>
2020-01-21 11:33:36 +0100

    added tests

--- src/components/molecules/EmptyState.unit.js
+++ src/components/molecules/EmptyState.unit.js
@@ -2,4 +2,27 @@ import EmptyState from './EmptyState'
 
 describe('@components/EmptyState', () => {
 	it(...isValidComponent(EmptyState));
+
+
+	it("renders its classes", () => {
+		const wrapper = mount(EmptyState,{
+			propsData: {
+				image: "@assets/icons/emptystate.svg",
+				description: "description"
+			},
+		});
+	  expect(wrapper.find(".base").exists()).toBe(true);
+		expect(wrapper.find(".container").exists()).toBe(true);
+		expect(wrapper.find(".img-container").exists()).toBe(true);
+	});
+
+	it("Renders image", () => {
+		const wrapper = shallowMount(EmptyState, {
+			propsData: {
+				image: "@assets/icons/emptystate.svg",
+				description: "description"
+			},
+		});
+		expect(wrapper.find("img").classes("image")).toBe(true);
+	});
 })
