387cda54888c595ec9c6ed7a7064b4fcc24945a9
Luisa <luisa.schrott@gmail.com>
2020-01-22 11:12:20 +0100

    added test

--- src/components/molecules/LoadingModal.unit.js
+++ src/components/molecules/LoadingModal.unit.js
@@ -1,5 +1,27 @@
-import LoadingModal from './LoadingModal'
+import LoadingModal from "./LoadingModal";
 
-describe('@components/LoadingModal', () => {
+describe("@components/LoadingModal", () => {
 	it(...isValidComponent(LoadingModal));
-})
+	it(`check props are set correctly `, () => {
+		const wrapper = mount(LoadingModal, {
+			propsData: {
+				title: "title",
+				description: "description",
+				active: true,
+			},
+		});
+		expect(wrapper.text()).toContain("title");
+		expect(wrapper.text()).toContain("description");
+	});
+
+	it(`button is centered`, () => {
+		const wrapper = mount(LoadingModal, {
+			propsData: {
+				title: "title",
+				description: "description",
+				active: true,
+			},
+		});
+		expect(wrapper.find(".mb--md").exists()).toBe(true);
+	});
+});
