a22c429c4dcc6619930c9792b2bcee9951f72ce1
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-01-23 10:41:12 +0100

    syntax err story

--- src/components/molecules/EmptyState.stories.js
+++ src/components/molecules/EmptyState.stories.js
@@ -5,5 +5,5 @@ import ExampleImage from "@assets/img/emptystate-graph.svg";
 storiesOf("Molecules|EmptyState", module).add("default", () => ({
 	components: { EmptyState },
 	data: () => ({ imgsrc: ExampleImage }),
-	template: `<EmptyState :image="imgsrc" :alt="Platzhalter Bild für nicht vorhandene Datenquellen"> <template v-slot:description>Noch keine Datenquellen vorhanden. Mit dem Plus unten rechts kannst du eine Datenquelle hinzufügen.</template> </EmptyState>`,
+	template: `<EmptyState :image="imgsrc" alt="Platzhalter Bild für nicht vorhandene Datenquellen"> <template v-slot:description>Noch keine Datenquellen vorhanden. Mit dem Plus unten rechts kannst du eine Datenquelle hinzufügen.</template> </EmptyState>`,
 }));
