7917600587b3c06dac26155e829a9f4c458b3ac1
CeEv <cedric@it-4d.de>
2020-01-24 13:10:22 +0100

    init with toggle

--- src/pages/administration/datasources/index.vue
+++ src/pages/administration/datasources/index.vue
@@ -3,12 +3,32 @@
 		<user-has-permission permission="DATASOURCES_VIEW">
 			<base-breadcrumb :inputs="inputs" />
 			<h3>Datenquellen</h3>
-			<empty-state :image="imgsrc">
-				<template v-slot:description
-					>Noch keine Datenquellen vorhanden. Mit dem Plus unten rechts kannst
-					du eine Datenquelle hinzufügen.
-				</template>
-			</empty-state>
+
+			<section v-if="!datasources || datasources.length === 0" class="section">
+				<empty-state :image="imgsrc">
+					<template v-slot:description
+						>Noch keine Datenquellen vorhanden. Mit dem Plus unten rechts kannst
+						du eine Datenquelle hinzufügen.
+					</template>
+				</empty-state>
+			</section>
+
+			<section v-if="datasources && datasources.length > 0" class="section">
+				<datasource-card
+					:image="require('@assets/img/logo/logo-webuntis.svg')"
+					title="WebUnits"
+				>
+					<template v-slot:actions>
+						<BaseButton design="primary text">
+							<BaseIcon source="custom" icon="datasource-import" :fill="color" />
+							Datenquelle importieren
+						</BaseButton>
+						<!-- lastStatus: { type: String, enum: ['Success', 'Warning', 'Error'] } -->
+						<BaseIcon source="custom" icon="success" :fill="green" />
+					</template>
+				</datasource-card>
+			</section>
+
 			<floating-fab
 				:position="position"
 				:icon="icon"
@@ -24,6 +44,7 @@ import EmptyState from "@components/molecules/EmptyState";
 import FloatingFab from "@components/molecules/FloatingFab";
 import ExampleImage from "@assets/img/emptystate-graph.svg";
 import UserHasPermission from "@components/helpers/UserHasPermission";
+import DatasourceCard from "@components/molecules/DatasourceCard";
 import { mapGetters } from "vuex";
 
 export default {
@@ -32,6 +53,7 @@ export default {
 		EmptyState,
 		FloatingFab,
 		UserHasPermission,
+		DatasourceCard,
 	},
 	data() {
 		return {
@@ -65,6 +87,19 @@ export default {
 		find() {
 			this.$store.dispatch("datasources/find");
 		},
+		/*
+		methods: {
+		find() {
+			this.$store.dispatch("news/find", {
+				query: {
+					$sort: {
+						createdAt: -1,
+					},
+				},
+			});
+		},
+	},
+	*/
 	},
 	head() {
 		return {
