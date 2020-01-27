96c860944eff8823e5ff0f91db58297e9b33d451
Luisa <luisa.schrott@gmail.com>
2020-01-27 14:12:24 +0100

    added test for BaseContentContainer

--- src/components/base/BaseContentContainer.unit.js
+++ src/components/base/BaseContentContainer.unit.js
@@ -1,6 +1,21 @@
 import BaseContentContainer from "./BaseContentContainer";
 
+const smallContainer = {
+	components: { BaseContentContainer },
+	template: `<base-content-container size="small"></base-content-container>`,
+};
+
 describe("@components/BaseContentContainer", () => {
 	it(...isValidComponent(BaseContentContainer));
 	it(...rendersSlotContent(BaseContentContainer));
+
+	it("renders prop size default", () => {
+		const wrapper = mount(BaseContentContainer);
+		expect(wrapper.classes()).toContain("medium");
+	});
+
+	it("renders prop size small", () => {
+		const wrapper = mount(smallContainer);
+		expect(wrapper.find(".small").exists()).toBe(true);
+	});
 });
