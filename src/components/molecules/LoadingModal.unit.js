b780a61050e6afe2a99d887d5535d77a53af2315
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-27 08:35:46 +0100

    remove useless test

--- src/components/molecules/LoadingModal.unit.js
+++ src/components/molecules/LoadingModal.unit.js
@@ -13,15 +13,4 @@ describe("@components/LoadingModal", () => {
 		expect(wrapper.text()).toContain("title");
 		expect(wrapper.text()).toContain("description");
 	});
-
-	it(`button is centered`, () => {
-		const wrapper = mount(LoadingModal, {
-			propsData: {
-				title: "title",
-				description: "description",
-				active: true,
-			},
-		});
-		expect(wrapper.find(".mb--md").exists()).toBe(true);
-	});
 });
