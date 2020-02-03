e88b05f8c863547f2f3074b63922b31d39f9fdf3
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-02-03 11:14:05 +0100

    refactored baseIcon story

--- stories/base.stories.js
+++ stories/base.stories.js
@@ -178,35 +178,6 @@ storiesOf("3 Base UI Components/Base UI", module)
 		template: "<base-card>Card</base-card>",
 		methods: {},
 	}))
-	.add("Base Icon", () => ({
-		components: { BaseIcon },
-		data: () => ({
-			icon: text("icon", "home"),
-			source: select(
-				"source",
-				{ material: "material", fa: "fa", custom: "custom" },
-				"material"
-			),
-			size: text("size", "1em"),
-			color: color("color", "#f8a41b"),
-		}),
-		template: `<div>
-			<p>
-				Icon usage is simple: <base-icon :source="source" :icon="icon" :style="{'font-size': size, fill: color}"/>
-			</p>
-			<p>
-				You can als use icons from fontawesome: <base-icon source="fa" icon="solid/address-book" :style="{'font-size': size}"/>
-			</p>
-			<p>
-				The Color can be also be set using fill:
-				<base-icon source="material" icon="add" :fill="color"/>
-			</p>
-			<p>
-				Scaling works, by setting the font-size attribute:
-				<base-icon source="custom" icon="tasks" style="font-size: 2em" />
-			</p>
-		</div>`,
-	}))
 	.add("Base Select", () => ({
 		components: { BaseSelect },
 		data: () => ({
