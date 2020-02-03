f869a201bd7a1f50cc9eefd03a4741bb1381fb96
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-02-03 14:29:07 +0100

    removed header image because of screenshot tests

--- src/components/molecules/MintEcPageHeader.stories.js
+++ src/components/molecules/MintEcPageHeader.stories.js
@@ -4,6 +4,6 @@ import MintEcPageHeader from "./MintEcPageHeader";
 
 storiesOf("4 Molecules/MintEcPageHeader", module).add("default", () => ({
 	components: { MintEcPageHeader },
-	template: `<MintEcPageHeader image="https://headless.schul-cloud.org/content/images/2019/12/01_schule-informieren-6.png" heading="Schule informieren" teaser="Ein Teaser" />`,
+	template: `<MintEcPageHeader image="" heading="Schule informieren" teaser="Ein Teaser" />`,
 	data: () => ({}),
 }));
