65d517fc3828710bb65bb46a16db17fa0563ae41
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-02-18 15:47:50 +0100

    Redirect unauthenticated requests back to original page after login

--- src/plugins/authenticate.js
+++ src/plugins/authenticate.js
@@ -4,12 +4,14 @@ export default async ({ app, store, route }) => {
 		const jwt = app.$cookies.get("jwt");
 		if (!jwt && !isPublic) {
 			await store.dispatch("auth/logout");
-			window.location = "/";
+			window.location = `${process.env.NOT_AUTHENTICATED_REDIRECT_URL}?redirect=${window.location}`;
+		} else {
+			store.commit("auth/setAccessToken", jwt);
+			await store.dispatch("auth/authenticate");
 		}
-		store.commit("auth/setAccessToken", jwt);
-		await store.dispatch("auth/authenticate");
 	} catch (error) {
 		console.error(error);
 		store.dispatch("auth/logout");
+		window.location = process.env.NOT_AUTHENTICATED_REDIRECT_URL;
 	}
 };
