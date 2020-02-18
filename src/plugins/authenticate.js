a468995fff7f47ca9e9a9218cb4cd77bc059e832
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-02-18 16:48:50 +0100

    Don't require authentication for public pages

--- src/plugins/authenticate.js
+++ src/plugins/authenticate.js
@@ -1,8 +1,9 @@
 export default async ({ app, store, route }) => {
 	const isPublic = route.meta.some((meta) => meta.isPublic);
+	if (isPublic) return; // we don't need authentication for this page.
 	try {
 		const jwt = app.$cookies.get("jwt");
-		if (!jwt && !isPublic) {
+		if (!jwt) {
 			await store.dispatch("auth/logout");
 			window.location = `${process.env.NOT_AUTHENTICATED_REDIRECT_URL}?redirect=${window.location}`;
 		} else {
