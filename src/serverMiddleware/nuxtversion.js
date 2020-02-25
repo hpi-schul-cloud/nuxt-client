77d0e1e2e331d5b21151c4d8366ac213416bdbd2
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-02-25 12:00:22 +0100

    set CORS header to allow requests from everywhere

--- src/serverMiddleware/nuxtversion.js
+++ src/serverMiddleware/nuxtversion.js
@@ -12,6 +12,7 @@ const GIT_INFO = {
 
 export default async function(req, res, next) {
 	if (req.url === "/nuxtversion") {
+		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.setHeader("Content-Type", "application/json; charset=utf-8");
 		res.write(JSON.stringify(GIT_INFO, null, "\t"));
 		res.end();
