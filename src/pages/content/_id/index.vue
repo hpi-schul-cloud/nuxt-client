9814831a12cd1d7444f2f0f5b9b132232b71ffe5
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-03-11 10:39:06 +0100

    Use grid layout for detail page

--- src/pages/content/_id/index.vue
+++ src/pages/content/_id/index.vue
@@ -1,35 +1,19 @@
 <template>
 	<div class="resource">
-		<div class="resource__img-container">
-			<div
-				class="resource__img-container--background"
-				:style="{
-					backgroundImage: `url(${resource.preview.url})`,
-				}"
-			/>
-			<div class="resource__img-container--icons">
-				<base-button design="none" @click="$router.go(-1)">
-					<base-icon
-						class="resource__img-container--icon"
-						source="material"
-						icon="close"
-					/>
-				</base-button>
+		<div class="header">
+			<div class="header-content">
+				<div class="navigation" @click="$router.go(-1)">
+					<base-icon source="material" icon="arrow_back" />
+					<span>{{ $t("pages.content._id.header.back-navigation") }}</span>
+				</div>
+				<h1 class="h2">{{ resource.title || resource.name }}</h1>
 
-				<!--
-				<base-button design="none" @click="bookmarkHandler">
-					<base-icon
-						class="resource__img-container--icon"
-						source="material"
-						:icon="bookmarkIconSelector"
-					/>
-				</base-button>
-				-->
-
-				<div>
-					<base-button design="none">
+				<div class="actions">
+					<base-button design="hero-cta icon">
+						<base-icon source="material" icon="file_copy" />
+					</base-button>
+					<base-button design="icon">
 						<base-icon
-							class="resource__img-container--icon"
 							source="material"
 							icon="more_vert"
 							@click="menuActive = !menuActive"
@@ -42,77 +26,93 @@
 					/>
 				</div>
 			</div>
-
-			<img :src="resource.preview.url" alt="content-preview-img" />
 		</div>
-		<div class="resource__content">
-			<div class="resource__header">
-				<div class="resource__header--title">{{
-					resource.title || resource.name
-				}}</div>
-				<div class="resource__header--menu"></div>
-				<div class="resource__header--subtitle">
-					{{ resource.properties["cm:creator"][0] || "" }} (Autor),
-					{{
-						(resource.properties["ccm:metadatacontributer_provider"] &&
-							resource.properties["ccm:metadatacontributer_provider"][0]) ||
-							"provider"
-					}}
-					(Herausgeber)
+		<div class="content" :class="description ? '' : 'no-description'">
+			<div class="metadata">
+				<div>
+					<span v-if="type">{{ type }}</span>
+					<span v-if="type && createdAt"> - </span>
+					<span v-if="createdAt">
+						{{ $t("pages.content._id.metadata.createdAt") }}
+						{{ createdAt }}
+					</span>
+				</div>
+				<div>
+					<span v-if="author">
+						{{ $t("pages.content._id.metadata.author") }}:
+						{{ author }}
+					</span>
+					<span v-if="provider">
+						{{ $t("pages.content._id.metadata.provider") }}:
+						{{ provider }}
+					</span>
 				</div>
 			</div>
-
-			<div class="resource__body">
-				{{ resource.description }}
+			<div class="preview">
+				<div
+					class="preview-background"
+					:style="{
+						backgroundImage: `url(${resource.preview.url})`,
+					}"
+				/>
+				<img :src="resource.preview.url" alt="content preview image" />
 			</div>
-
-			<div class="resource__footer">
-				<div class="resource__footer--subcontainer">
-					<div class="resource__footer--subcontainer_icon">
-						<base-icon source="material" icon="file_copy" />
-					</div>
-					<div class="resource__footer--subcontainer_text">{{
-						resource.name
-					}}</div>
-				</div>
-				<div class="resource__footer--subcontainer">
-					<div class="resource__footer--subcontainer_icon">
-						<base-icon source="material" icon="calendar_today" />
-					</div>
-					<div class="resource__footer--subcontainer_text">
-						<div
-							>erstellt am
-							{{ dayjs(resource.properties["cm:createdISO8601"][0]) }}</div
-						>
-						<div
-							>zuletzt geändert am
-							{{ dayjs(resource.properties["cm:modifiedISO8601"][0]) }}</div
-						>
-					</div>
-				</div>
+			<div class="description">
+				{{ description }}
 			</div>
-		</div>
-		<div class="resource__floating">
-			<floating-fab icon="add" aria-label="floating button" />
 		</div>
 	</div>
 </template>
 
 <script>
-import FloatingFab from "@components/molecules/FloatingFab";
 import ContextMenu from "@components/molecules/ContextMenu";
 import dayjs from "dayjs";
 
+const getMetadataAttribute = (properties, key) => {
+	if (Array.isArray(properties[key])) {
+		return properties[key][0];
+	}
+	return null;
+};
+
+const getType = (i18n, mimetype) => {
+	return {
+		"text/html": i18n.t("pages.content._id.meta.html"),
+		video: i18n.t("pages.content._id.meta.video"),
+		image: i18n.t("pages.content._id.meta.image"),
+		"image/jpeg": i18n.t("pages.content._id.meta.image"),
+	}[mimetype];
+};
+
 export default {
 	components: {
-		FloatingFab,
 		ContextMenu,
 	},
 
 	layout: "loggedInFull",
-	async asyncData({ store, params }) {
+	async asyncData({ store, params, app: { i18n } }) {
+		const resource = await store.dispatch(
+			"content/getResourceMetadata",
+			params.id
+		);
+		const provider = getMetadataAttribute(
+			resource.properties,
+			"ccm:metadatacontributer_provider"
+		);
+		const author = getMetadataAttribute(resource.properties, "cm:creator");
+		const createdAt = dayjs(
+			getMetadataAttribute(resource.properties, "cm:created")
+		);
+		const type = getType(i18n, resource.mimetype);
+		const { description } = resource;
+
 		return {
-			resource: await store.dispatch("content/getResourceMetadata", params.id),
+			resource,
+			provider,
+			author,
+			type,
+			createdAt,
+			description,
 		};
 	},
 	data() {
@@ -158,92 +158,107 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.resource {
-	display: flex;
-	width: 100%;
-	height: 90vh;
-	&__img-container {
-		position: relative;
-		width: 70%;
+@import "@styles";
+
+.header {
+	position: relative;
+	min-height: var(--space-xl-5);
+	margin-bottom: var(--space-md);
+	background-color: var(--color-white);
+
+	&::after {
+		position: absolute;
+		top: 10px;
+		z-index: var(--layer-behind);
+		width: 100%;
 		height: 100%;
-		background-color: var(--color-tertiary-dark);
-		&--background {
+		content: "";
+		box-shadow: 0 0 10px var(--color-overlay);
+	}
+
+	.header-content {
+		padding: 0 var(--space-xl) var(--space-md);
+
+		.navigation {
+			display: flex;
+			align-items: center;
+			font-weight: var(--font-weight-bold);
+			cursor: pointer;
+		}
+
+		.actions {
 			position: absolute;
-			top: 50%;
-			left: 50%;
-			width: 100%;
-			height: 100%;
+			top: calc(100% - var(--space-md));
+			right: 0;
+			margin-right: var(--space-xl);
+		}
+	}
+}
+
+.content {
+	display: grid;
+	grid-template-areas: "meta" "preview" "description";
+	grid-template-rows: auto auto 1fr;
+	grid-template-columns: 1fr;
+	grid-gap: var(--space-md);
+	min-height: var(--size-content-width-min);
+	padding: 0 var(--space-xl);
+
+	@include breakpoint(tablet) {
+		grid-template-areas: "meta meta" "preview description";
+		grid-template-rows: auto 1fr;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	&.no-description {
+		grid-template-areas: "meta" "preview";
+		grid-template-rows: auto auto;
+
+		@include breakpoint(tablet) {
+			grid-template-areas: "meta" "preview";
+			grid-template-rows: auto;
+			grid-template-columns: auto;
+		}
+	}
+
+	.metadata {
+		grid-area: meta;
+		padding: var(--space-xl) 0;
+	}
+
+	.preview {
+		position: relative;
+		grid-area: preview;
+
+		.preview-background {
+			position: absolute;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			z-index: var(--layer-behind);
 			filter: blur(7px);
 			background-repeat: no-repeat;
 			background-position: center;
 			background-size: cover;
 			opacity: 0.7;
-			transform: translate(-50%, -50%);
 		}
-		&--icons {
-			position: absolute;
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			width: 100%;
-			padding: var(--space-lg);
-			color: var(--color-white);
-		}
-		&--icon {
-			padding: var(--space-xs-3);
-			font-size: var(--heading-4);
-			color: var(--color-white);
-			background-color: var(--color-gray-dark);
-			border-radius: var(--radius-round);
-			opacity: 0.6;
-		}
-		img {
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			max-width: 100%;
-			transform: translate(-50%, -50%);
-			// height: 100%;
-		}
-	}
-	&__content {
-		width: 30%;
-		padding: var(--space-lg);
-	}
-	&__header {
-		&--title {
-			margin-bottom: var(--space-xs-3);
-			font-size: var(--heading-6);
-			font-weight: var(--font-weight-bold);
-			line-height: var(--line-height-md);
-		}
-		&--subtitle {
-			margin-bottom: var(--space-xl);
-			font-size: var(--text-xs);
-			font-weight: var(--font-weight-bold);
-			line-height: var(--line-height-md);
-		}
-	}
-	&__body {
-		margin-bottom: var(--space-xl);
-		font-size: var(--text-sm);
-		line-height: var(--line-height-md);
-	}
-	&__footer {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		font-size: var(--text-xs);
-		line-height: var(--line-height-md);
 
-		&--subcontainer {
-			display: flex;
+		img {
+			object-position: center;
+			object-fit: contain;
 			width: 100%;
-			margin-bottom: var(--space-xs-2);
-			&_icon {
-				margin-right: var(--space-xs);
+			height: 100%;
+			max-height: 50vh;
+
+			@include breakpoint(tablet) {
+				max-height: auto;
 			}
 		}
+	}
+
+	.description {
+		grid-area: description;
 	}
 }
 </style>
