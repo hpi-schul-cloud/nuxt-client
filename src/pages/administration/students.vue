ed301ccd5eb9fb67dfb2833c9cd7b4f5eaea2887
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-03-16 16:39:19 +0100

    added commented out birthday column

--- src/pages/administration/students.vue
+++ src/pages/administration/students.vue
@@ -81,6 +81,10 @@ export default {
 					field: "email",
 					label: "E-Mail",
 				},
+				// {
+				// 	field: "birthday",
+				// 	label: "Geburtsdatum",
+				// },
 				{
 					field: "consent.consentStatus",
 					label: "Consent",
