9e7249d3eecd7b3409c7054c977229fb5a0f8aa3
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-03-18 15:54:36 +0100

    fix problem when consent is not set in the current users state
    while the async store function to fetch data is executed, there might be a inconsistent store after a user is created.
    here the consent object is not set.
    this fixes it, by checking if the consent object is set

--- src/pages/administration/students/index.vue
+++ src/pages/administration/students/index.vue
@@ -18,8 +18,8 @@
 			<template v-slot:datacolumn-createdAt="{ data }">
 				{{ dayjs(data).format("DD.MM.YYYY") }}
 			</template>
-			<template v-slot:datacolumn-consent-consentStatus="{ data }">
-				<span v-if="data === 'ok'">
+			<template v-slot:datacolumn-consent="{ data }">
+				<span v-if="data && data.consentStatus === 'ok'">
 					<base-icon
 						source="material"
 						icon="check"
@@ -32,20 +32,21 @@
 						color="var(--color-success)"
 					/>
 				</span>
-				<span v-else-if="data === 'parentsAgreed'">
+				<span v-else-if="data && data.consentStatus === 'parentsAgreed'">
 					<base-icon
 						source="material"
 						icon="check"
 						color="var(--color-warning)"
 					/>
 				</span>
-				<span v-else-if="data === 'missing'">
+				<span v-else-if="data && data.consentStatus === 'missing'">
 					<base-icon
 						source="material"
 						icon="close"
 						color="var(--color-danger)"
 					/>
 				</span>
+				<span v-else />
 			</template>
 		</backend-data-table>
 		<fab-floating
@@ -109,7 +110,7 @@ export default {
 				// 	label: this.$t("common.labels.birthday"),
 				// },
 				{
-					field: "consent.consentStatus",
+					field: "consent",
 					label: this.$t("common.labels.consent"),
 				},
 				{
