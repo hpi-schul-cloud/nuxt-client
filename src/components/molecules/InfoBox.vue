bd2243b178ced027ad35342250c6444812ee0abe
adrianjost <adrianjost@users.noreply.github.com>
2020-03-25 19:07:39 +0000

    npm run lint

--- src/components/molecules/InfoBox.vue
+++ src/components/molecules/InfoBox.vue
@@ -1,45 +1,44 @@
 <template>
-<div  v-if="active" class="wrapper">
-	<div class="content">
-		<h4>
-			<slot name="header" />
-		</h4>
+	<div v-if="active" class="wrapper">
+		<div class="content">
+			<h4>
+				<slot name="header" />
+			</h4>
 			<p class="body">
 				<slot name="body" />
 			</p>
-		<div class="actions">
-			<slot name="actions" />
-			<base-button design="info" @click.self="close">Schließen</base-button>
+			<div class="actions">
+				<slot name="actions" />
+				<base-button design="info" @click.self="close">Schließen</base-button>
+			</div>
 		</div>
 	</div>
-</div>
-
 </template>
 
 <script>
 export default {
-		props: {
-			active: {
-				type: Boolean,
-			},
+	props: {
+		active: {
+			type: Boolean,
 		},
+	},
 	methods: {
 		close() {
 			this.$emit("update:active", false);
 		},
 	},
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "@styles";
 
-.wrapper{
+.wrapper {
 	position: relative;
 	max-width: 460px;
 	color: var(--color-white);
-	background-color:var(--color-info);
-	border-radius:var(--radius-lg);
+	background-color: var(--color-info);
+	border-radius: var(--radius-lg);
 
 	h4 {
 		color: var(--color-white);
@@ -48,12 +47,11 @@ export default {
 .content {
 	padding: var(--space-md);
 }
-.actions{
+.actions {
 	text-align: right;
 
 	> label {
 		margin-bottom: var(--space-md);
 	}
 }
-
 </style>
