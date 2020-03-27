344b370c43fb9c35015e5cb6cb4b8cbc9b4b0765
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-03-27 17:24:33 +0100

    add tests to infiniteScrolling mixin

--- src/mixins/infiniteScrolling.js
+++ src/mixins/infiniteScrolling.js
@@ -1,30 +1,30 @@
 export default {
 	data() {
 		return {
-			bottom: false,
-			scrollY: 0,
+			$_bottom: false,
+			$_scrollY: 0,
 		};
 	},
 	created() {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 		window.addEventListener("scroll", () => {
-			this.bottom = this.$_bottomVisible();
-			this.scrollY = window.scrollY;
+			this.$_bottom = this.$_bottomVisible();
+			this.$_scrollY = window.scrollY;
 		});
 	},
 	beforeDestroy() {
 		window.removeEventListener("scroll", () => {
-			this.bottom = this.$_bottomVisible();
-			this.scrollY = window.scrollY;
+			this.$_bottom = this.$_bottomVisible();
+			this.$_scrollY = window.scrollY;
 		});
 	},
 	methods: {
 		$_bottomVisible() {
 			const { scrollY } = window;
-			const visible = document.documentElement.clientHeight;
+			const visibleHeight = document.documentElement.clientHeight;
 			const pageHeight = document.documentElement.scrollHeight;
-			const bottomOfPage = visible + scrollY >= pageHeight - 2;
-			return bottomOfPage || pageHeight < visible;
+			const bottomOfPage = visibleHeight + scrollY >= pageHeight - 2;
+			return bottomOfPage || pageHeight < visibleHeight;
 		},
 		$_backToTop() {
 			window.scrollTo({ top: 0, behavior: "smooth" });
---
+++ src/mixins/infiniteScrolling.unit.js
@@ -0,0 +1,82 @@
+import infiniteScrolling from "./infiniteScrolling";
+
+const getInstance = () => {
+	jest.spyOn(window, "scrollTo").mockImplementation();
+
+	return mount(
+		{
+			template: "<div/>",
+		},
+		{
+			mixins: [infiniteScrolling],
+		}
+	);
+};
+describe("@mixins/infiniteScrolling", () => {
+	it("can be used as a mixin", () => {
+		expect(getInstance).not.toThrow();
+	});
+	it("exposes $_bottom data variable", () => {
+		const wrapper = getInstance();
+		expect(wrapper.vm.$data.$_bottom).not.toBeUndefined();
+	});
+	it("exposes scrollY position as $_scrollY data variable", () => {
+		const wrapper = getInstance();
+		expect(wrapper.vm.$data.$_scrollY).not.toBeUndefined();
+	});
+	it("$_backToTop scroll to the top", () => {
+		const scrollToMock = jest.spyOn(window, "scrollTo");
+		const wrapper = getInstance();
+		wrapper.vm.$_backToTop();
+		expect(scrollToMock.mock.calls.pop()[0]).toStrictEqual(
+			expect.objectContaining({
+				top: 0,
+			})
+		);
+	});
+	it.each([
+		// [scrollY, viewportHeight, pageHeight, result]
+		[0, 100, 200, false], // not scrolled on normal page
+		[0, 500, 200, true], // page smaller than viewport
+		[300, 300, 500, true], // scrolled to bottom
+	])(
+		"calculates bottom intersection correctly for scrollY %p, viewportHeight %p and pageHeight %p",
+		(scrollY, clientHeight, scrollHeight, result) => {
+			jest
+				.spyOn(document.documentElement, "clientHeight", "get")
+				.mockImplementation(() => clientHeight);
+			jest
+				.spyOn(document.documentElement, "scrollHeight", "get")
+				.mockImplementation(() => scrollHeight);
+			window.scrollY = scrollY;
+			const wrapper = getInstance();
+			expect(wrapper.vm.$_bottomVisible()).toBe(result);
+		}
+	);
+	it("removes window event listeners on destroy", () => {
+		jest
+			.spyOn(window, "removeEventListener")
+			.mockImplementation()
+			.mockImplementation();
+		const wrapper = getInstance();
+		wrapper.destroy();
+		expect(window.removeEventListener.mock.calls[0]).toContain("scroll");
+	});
+	it("updates data on scroll event", () => {
+		jest
+			.spyOn(document.documentElement, "clientHeight", "get")
+			.mockImplementation(() => 500);
+		jest
+			.spyOn(document.documentElement, "scrollHeight", "get")
+			.mockImplementation(() => 200);
+		window.scrollY = scrollY;
+
+		jest
+			.spyOn(window, "addEventListener")
+			.mockImplementation((event, cb) => cb());
+		const wrapper = getInstance();
+
+		expect(wrapper.vm.$_bottom).toBe(true);
+		expect(wrapper.vm.$_scrollY).toBe(300);
+	});
+});
