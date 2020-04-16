d6a97e5a62b2e759e49107356452b0a24c4c759d
Adrian Jost <22987140+adrianjost@users.noreply.github.com>
2020-04-16 14:07:11 +0000

    Update src/components/organisms/DataFilter/DataFilterLayout.unit.js

--- src/components/organisms/DataFilter/DataFilterLayout.unit.js
+++ src/components/organisms/DataFilter/DataFilterLayout.unit.js
@@ -3,17 +3,5 @@ import DataFilterLayout from "./DataFilterLayout";
 describe("@components/organisms/DataFilter/DataFilterLayout", () => {
 	it(...isValidComponent(DataFilterLayout));
 
-	it("renders its slots", () => {
-		const wrapper = mount(DataFilterLayout, {
-			slots: {
-				select: "select",
-				chips: "chips",
-				modal: "modal",
-			},
-		});
-		console.log("wrapperhtml", wrapper.html());
-		expect(wrapper.find(".select").text()).toBe("select");
-		expect(wrapper.find(".chips").text()).toBe("chips");
-		expect(wrapper.find(".modal").text()).toBe("modal");
-	});
+	it(...rendersSlotContent(DataFilterLayout, ["select", "chips", "modal"]));
 });
