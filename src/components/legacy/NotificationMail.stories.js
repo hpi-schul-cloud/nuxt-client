58c449a4da90a6be982ce8a19cfbda23b53a32b3
ABialoskorski <ABialoskorski@users.noreply.github.com>
2020-04-24 11:20:58 +0000

    npm run lint

--- src/components/legacy/NotificationMail.stories.js
+++ src/components/legacy/NotificationMail.stories.js
@@ -1,11 +1,9 @@
 import { storiesOf } from "@storybook/vue";
 
-
 import NotificationMail from "./NotificationMail";
 
-storiesOf("Legacy/NotificationMail", module)
-	.add("default", () => ({
-		components: { NotificationMail },
-		template: `<NotificationMail />`,
-		data: () => ({}),
-	}));
+storiesOf("Legacy/NotificationMail", module).add("default", () => ({
+	components: { NotificationMail },
+	template: `<NotificationMail />`,
+	data: () => ({}),
+}));
