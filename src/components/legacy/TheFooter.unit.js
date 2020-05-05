04bc1d1dd418d63f5343cde9a4939eec6be7543d
Kamil Nowak <kamil.nowak@capgemini.com>
2020-05-05 08:45:25 +0200

    Removed root to '/datenschutz' from footer'

--- src/components/legacy/TheFooter.unit.js
+++ src/components/legacy/TheFooter.unit.js
@@ -3,7 +3,9 @@ import TheFooter from "./TheFooter";
 describe("@components/legacy/TheFooter", () => {
 	it(...isValidComponent(TheFooter));
 
-	const $theme = { name: "test" };
+	const $theme = {
+		name: "test"
+	};
 	it("check that all links are rendered in the footer", () => {
 		const wrapper = shallowMount(TheFooter, {
 			...createComponentMocks({
@@ -11,8 +13,7 @@ describe("@components/legacy/TheFooter", () => {
 					auth: {
 						state: () => ({
 							school: {
-								documentBaseDir:
-									"https://s3.hidrive.strato.com/schul-cloud-hpi/default/",
+								documentBaseDir: "https://s3.hidrive.strato.com/schul-cloud-hpi/default/",
 							},
 						}),
 					},
@@ -23,7 +24,7 @@ describe("@components/legacy/TheFooter", () => {
 				i18n: true,
 			}),
 		});
-		expect(wrapper.findAll("base-link-stub")).toHaveLength(11);
+		expect(wrapper.findAll("base-link-stub")).toHaveLength(10);
 		expect(wrapper.find("span").text()).toBe(
 			"© " + new Date().getFullYear() + " " + $theme.name
 		);
