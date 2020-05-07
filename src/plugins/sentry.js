d33824bdeb6bf07dce435b44fff6e03fdd0617e6
Artur Białoskórski <bialoskorski.artur@gmail.com>
2020-05-07 12:41:38 +0200

    add sentry to env

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -1,4 +1,5 @@
 import Theme from "@theme/config";
+const env = require("../../nuxt.config")
 
 export default ({ app, store }) => {
 	const { user } = store.state.auth;
@@ -6,12 +7,24 @@ export default ({ app, store }) => {
 		scope.setLevel("warn");
 		scope.setTag("theme", Theme.name);
 		scope.setTag("domain", window.location.host);
+		scope.setTag('sha', sha);
+
 
 		// populate userdata
 		if (user && user.schoolId) {
 			scope.setTag({ schoolId: user.schoolId });
 		}
 	});
+	if (env.SENTRY_DSN) {
+	Sentry.init({
+		dsn: env.SENTRY_DSN,
+		environment: app.get('env'),
+		release: version,
+		sampleRate: env.SENTRY_SAMPLE_RATE,
+	});
+
+	app.use(Sentry.Handlers.requestHandler());
+}
 	// eslint-disable-next-line no-console
 	console.log("sentry error logging initialized");
 };
