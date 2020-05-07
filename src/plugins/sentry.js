e8c67c8baf1ce964da9e8ed054b8e24bdbfe5c69
ABialoskorski <ABialoskorski@users.noreply.github.com>
2020-05-07 10:46:26 +0000

    npm run lint

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -1,5 +1,5 @@
 import Theme from "@theme/config";
-const env = require("../../nuxt.config")
+const env = require("../../nuxt.config");
 
 export default ({ app, store }) => {
 	const { user } = store.state.auth;
@@ -7,8 +7,7 @@ export default ({ app, store }) => {
 		scope.setLevel("warn");
 		scope.setTag("theme", Theme.name);
 		scope.setTag("domain", window.location.host);
-		scope.setTag('sha', sha);
-
+		scope.setTag("sha", sha);
 
 		// populate userdata
 		if (user && user.schoolId) {
@@ -16,15 +15,15 @@ export default ({ app, store }) => {
 		}
 	});
 	if (env.SENTRY_DSN) {
-	Sentry.init({
-		dsn: env.SENTRY_DSN,
-		environment: app.get('env'),
-		release: version,
-		sampleRate: env.SENTRY_SAMPLE_RATE,
-	});
+		Sentry.init({
+			dsn: env.SENTRY_DSN,
+			environment: app.get("env"),
+			release: version,
+			sampleRate: env.SENTRY_SAMPLE_RATE,
+		});
 
-	app.use(Sentry.Handlers.requestHandler());
-}
+		app.use(Sentry.Handlers.requestHandler());
+	}
 	// eslint-disable-next-line no-console
 	console.log("sentry error logging initialized");
 };
