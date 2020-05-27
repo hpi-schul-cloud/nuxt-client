23b8824ab296b8236be045d521ba6d047e601493
Artur <bialoskorski.artur@gmail.com>
2020-05-27 08:32:16 +0200

    removed Configuration

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -1,6 +1,5 @@
 import Theme from "@theme/config";
 const env = require("../../nuxt.config");
-const { Configuration } = require("@schul-cloud/commons");
 
 export default ({ app, store }) => {
 	const { user } = store.state.auth;
@@ -8,20 +7,15 @@ export default ({ app, store }) => {
 		scope.setLevel("warn");
 		scope.setTag("theme", Theme.name);
 		scope.setTag("domain", window.location.host);
+		scope.setTag("sampleRate", env.SENTRY_SAMPLE_RATE);
 
 		// populate userdata
 		if (user && user.schoolId) {
 			scope.setTag({ schoolId: user.schoolId });
 		}
 	});
-	if (Configuration.has("SENTRY_DSN")) {
-		Sentry.init({
-			dsn: Configuration.get("SENTRY_DSN"),
-			sampleRate: env.SENTRY_SAMPLE_RATE,
-		});
 
 		app.use(Sentry.Handlers.requestHandler());
-	}
 	// eslint-disable-next-line no-console
 	console.log("sentry error logging initialized");
-};
+	}
