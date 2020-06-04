b2d3fa955f63259b6b99825e2e757e18f1cea260
robertstrz <robertstrz@users.noreply.github.com>
2020-06-04 20:29:43 +0000

    npm run lint

--- tests/unit/jestMatchers.js
+++ tests/unit/jestMatchers.js
@@ -1,7 +1,11 @@
-const { toBeVisible, toBeEmptyDOMElement, toBeInTheDocument } = require("@testing-library/jest-dom/matchers");
+const {
+	toBeVisible,
+	toBeEmptyDOMElement,
+	toBeInTheDocument,
+} = require("@testing-library/jest-dom/matchers");
 
 global.expect.extend({
 	toBeVisible,
 	toBeEmptyDOMElement,
-	toBeInTheDocument
+	toBeInTheDocument,
 });
