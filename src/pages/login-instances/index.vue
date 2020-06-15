4db71d0819c774f8cfa7fc450632fcf03f7fc8d9
ABialoskorski <ABialoskorski@users.noreply.github.com>
2020-06-15 07:11:09 +0000

    npm run lint

--- src/pages/login-instances/index.vue
+++ src/pages/login-instances/index.vue
@@ -17,88 +17,88 @@
 </template>
 
 <script>
-	import InstanceTile from "@/components/molecules/InstanceTile";
+import InstanceTile from "@/components/molecules/InstanceTile";
 
-	export default {
-		components: {
-			InstanceTile,
+export default {
+	components: {
+		InstanceTile,
+	},
+	layout: "loggedoutFullLayout",
+	meta: {
+		isPublic: true,
+	},
+	data: function () {
+		return {
+			tiles: [
+				{
+					icon: "brb",
+					url: this.$t("pages.loginInstances.brb_link"),
+				},
+				{
+					icon: "n21",
+					url: this.$t("pages.loginInstances.n21_link"),
+				},
+				{
+					icon: "thr",
+					url: this.$t("pages.loginInstances.thr_link"),
+				},
+				{
+					icon: "hpi",
+					url: this.$t("pages.loginInstances.hpi_link"),
+				},
+				{
+					icon: "open_HPI",
+					url: this.$t("pages.loginInstances.open_hpi_link"),
+				},
+			],
+		};
+	},
+	computed: {
+		isMobile() {
+			return this.$mq === "mobile";
 		},
-		layout: "loggedoutFullLayout",
-		meta: {
-			isPublic: true,
-		},
-		data: function () {
-			return {
-				tiles: [
-					{
-						icon: "brb",
-						url: this.$t("pages.loginInstances.brb_link"),
-					},
-					{
-						icon: "n21",
-						url: this.$t("pages.loginInstances.n21_link"),
-					},
-					{
-						icon: "thr",
-						url: this.$t("pages.loginInstances.thr_link"),
-					},
-					{
-						icon: "hpi",
-						url: this.$t("pages.loginInstances.hpi_link"),
-					},
-					{
-						icon: "open_HPI",
-						url: this.$t("pages.loginInstances.open_hpi_link"),
-					},
-				],
-			};
-		},
-		computed: {
-			isMobile() {
-				return this.$mq === "mobile";
-			},
-		},
-	};
+	},
+};
 </script>
 
 <style lang="scss" scoped>
-	.login-container {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		margin: 0;
-	}
-	.box-container {
-		display: grid;
-		justify-content: center;
-		padding: 0 calc(2.5 * var(--sidebar-width-tablet));
-	}
-	.box-container-mobile {
-		padding: 0;
-	}
-	.boxes {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr;
-		grid-gap: var(--space-lg);
-	}
-	.boxes.mobile {
-		grid-template-columns: 1fr 1fr;
-		grid-gap: var(--space-sm);
-	}
-	.h6 {
-		margin-top: var(--space-xl-4) !important;
-		margin-bottom: var(--space-md);
-	}
-	.h1 {
-		margin-top: 0 !important;
-		margin-bottom: var(--space-xl-4) !important;
-		font-size: var(--sidebar-sub-item-height);
-	}
-	.h6.margin {
-		margin-top: var(--space-xs) !important;
-		margin-bottom: var(--border-width-bold);
-	}
-	.h1.margin {
-		margin-bottom: var(--space-lg) !important;
-	}
+.login-container {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	margin: 0;
+}
+.box-container {
+	display: grid;
+	justify-content: center;
+	padding: 0 calc(2.5 * var(--sidebar-width-tablet));
+}
+.box-container-mobile {
+	padding: 0;
+}
+.boxes {
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-gap: var(--space-lg);
+}
+.boxes.mobile {
+	grid-template-columns: 1fr 1fr;
+	grid-gap: var(--space-sm);
+}
+.h6 {
+	margin-top: var(--space-xl-4) !important;
+	margin-bottom: var(--space-md);
+}
+.h1 {
+	margin-top: 0 !important;
+	margin-bottom: var(--space-xl-4) !important;
+	font-size: var(--sidebar-sub-item-height);
+}
+.h6.margin {
+	margin-top: var(--space-xs) !important;
+	margin-bottom: var(--border-width-bold);
+}
+.h1.margin {
+	margin-bottom: var(--space-lg) !important;
+}
 </style>
