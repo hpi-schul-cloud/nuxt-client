8f9433a625d0dca5e1649d21a07455d86baca7e4
ABialoskorski <ABialoskorski@users.noreply.github.com>
2020-06-16 11:25:16 +0000

    npm run lint

--- src/components/legacy/NavigationBar.vue
+++ src/components/legacy/NavigationBar.vue
@@ -8,19 +8,19 @@
 			</div>
 			<div class="link-container">
 				<base-link
-						v-for="(route, idx) in links"
-						:key="route.href"
-						:class="{ li: true, active: activeLink === route.href }"
-						:to="route.to"
-						:href="route.href"
-						:no-styles="true"
-						@click="setActive(idx)"
+					v-for="(route, idx) in links"
+					:key="route.href"
+					:class="{ li: true, active: activeLink === route.href }"
+					:to="route.to"
+					:href="route.href"
+					:no-styles="true"
+					@click="setActive(idx)"
 				>
 					{{ route.title }}
 				</base-link>
 				<div class="buttons-container">
 					<base-button design="secondary outline">
-						<base-icon source="fa" icon="sign-in" class="icon"/>
+						<base-icon source="fa" icon="sign-in" class="icon" />
 						{{ $t("common.labels.login") }}
 					</base-button>
 					<base-button design="secondary">
@@ -33,158 +33,158 @@
 	</div>
 </template>
 <script>
-	import BaseButton from "../base/BaseButton";
-	export default {
-		components: { BaseButton },
-		props: {
-			img: {
-				type: String,
-				required: true,
-			},
-			links: {
-				type: Array,
-				reqired: true,
-				default: () => {},
-			},
+import BaseButton from "../base/BaseButton";
+export default {
+	components: { BaseButton },
+	props: {
+		img: {
+			type: String,
+			required: true,
 		},
-		data() {
-			return {
-				activeLink: window.location.pathname,
-			};
+		links: {
+			type: Array,
+			reqired: true,
+			default: () => {},
 		},
-		methods: {
-			setActive(idx) {
-				this.activeLink = idx;
-			},
+	},
+	data() {
+		return {
+			activeLink: window.location.pathname,
+		};
+	},
+	methods: {
+		setActive(idx) {
+			this.activeLink = idx;
 		},
-	};
+	},
+};
 </script>
 
 <style lang="scss" scoped>
-	@import "@styles";
-	.header {
-		position: sticky;
-		position: -webkit-sticky;
-		top: 0;
-		left: 0;
-		z-index: var(--layer-fab);
+@import "@styles";
+.header {
+	position: sticky;
+	position: -webkit-sticky;
+	top: 0;
+	left: 0;
+	z-index: var(--layer-fab);
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	width: 100%;
+	line-height: var(--line-height-lg);
+	color: var(--color-black);
+	text-align: center;
+	background-color: var(--color-overlay-light);
+	-webkit-backdrop-filter: blur(5px);
+	backdrop-filter: blur(5px);
+	box-shadow: var(--shadow-sm);
+}
+.nav-container {
+	padding: 0 calc(5 * var(--border-width));
+	margin: auto;
+}
+.logo-container {
+	width: calc(7 * var(--sidebar-sub-item-height));
+	height: var(--topbar-height);
+}
+.link-container {
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	padding-bottom: var(--space-xs);
+
+	@include breakpoint(tablet) {
 		display: flex;
-		flex-flow: row wrap;
-		justify-content: center;
-		width: 100%;
-		line-height: var(--line-height-lg);
-		color: var(--color-black);
-		text-align: center;
-		background-color: var(--color-overlay-light);
-		-webkit-backdrop-filter: blur(5px);
-		backdrop-filter: blur(5px);
-		box-shadow: var(--shadow-sm);
-	}
-	.nav-container {
-		padding: 0 calc(5 * var(--border-width));
-		margin: auto;
-	}
-	.logo-container {
-		width: calc(7 * var(--sidebar-sub-item-height));
-		height: var(--topbar-height);
-	}
-	.link-container {
-		display: flex;
-		flex-flow: row wrap;
-		justify-content: center;
-		padding-bottom: var(--space-xs);
-
-		@include breakpoint(tablet) {
-			display: flex;
-			align-items: center;
-			justify-content: center;
-		}
-
-		@include breakpoint(desktop) {
-			justify-content: right;
-			padding-bottom: 0;
-		}
-		> a {
-			padding: calc(9 * var(--border-width));
-			margin-right: 0;
-			margin-bottom: var(--space-xs);
-
-			@include breakpoint(tablet) {
-				margin-bottom: 0;
-			}
-		}
-	}
-	.icon {
-		font-size: var(--radius-lg);
-	}
-
-	@media (min-width: 576px) {
-		.nav-container {
-			width: 540px;
-			max-width: 100%;
-		}
-		.link-container > a,
-		.link-container > button {
-			margin-right: var(--space-xs);
-		}
-	}
-
-	@media (min-width: 768px) {
-		.nav-container {
-			display: flex;
-			justify-content: space-between;
-			width: 720px;
-			max-width: 100%;
-		}
-	}
-
-	@media (max-width: 992px) {
-		.logo-container {
-			margin: auto;
-		}
-	}
-
-	@media (min-width: 992px) {
-		.nav-container {
-			width: 960px;
-			max-width: 100%;
-		}
-	}
-
-	@media (min-width: 1200px) {
-		.nav-container {
-			width: 1140px;
-			max-width: 100%;
-		}
-	}
-	.li {
 		align-items: center;
 		justify-content: center;
-		padding: var(--space-xs) var(--space-md);
-		font-family: var(--font-primary);
-		font-size: var(--text-md);
-		color: var(--color-black);
+	}
+
+	@include breakpoint(desktop) {
+		justify-content: right;
+		padding-bottom: 0;
+	}
+	> a {
+		padding: calc(9 * var(--border-width));
+		margin-right: 0;
+		margin-bottom: var(--space-xs);
 
 		@include breakpoint(tablet) {
-			display: flex;
-		}
-
-		@include breakpoint(desktop) {
-			font-size: var(--text-md);
-		}
-		&:hover {
-			background-color: var(--color-gray-light);
-			border-radius: var(--radius-sm);
+			margin-bottom: 0;
 		}
 	}
-	a.active {
+}
+.icon {
+	font-size: var(--radius-lg);
+}
+
+@media (min-width: 576px) {
+	.nav-container {
+		width: 540px;
+		max-width: 100%;
+	}
+	.link-container > a,
+	.link-container > button {
+		margin-right: var(--space-xs);
+	}
+}
+
+@media (min-width: 768px) {
+	.nav-container {
+		display: flex;
+		justify-content: space-between;
+		width: 720px;
+		max-width: 100%;
+	}
+}
+
+@media (max-width: 992px) {
+	.logo-container {
+		margin: auto;
+	}
+}
+
+@media (min-width: 992px) {
+	.nav-container {
+		width: 960px;
+		max-width: 100%;
+	}
+}
+
+@media (min-width: 1200px) {
+	.nav-container {
+		width: 1140px;
+		max-width: 100%;
+	}
+}
+.li {
+	align-items: center;
+	justify-content: center;
+	padding: var(--space-xs) var(--space-md);
+	font-family: var(--font-primary);
+	font-size: var(--text-md);
+	color: var(--color-black);
+
+	@include breakpoint(tablet) {
+		display: flex;
+	}
+
+	@include breakpoint(desktop) {
+		font-size: var(--text-md);
+	}
+	&:hover {
+		background-color: var(--color-gray-light);
+		border-radius: var(--radius-sm);
+	}
+}
+a.active {
+	color: var(--color-white);
+	background-color: var(--color-primary);
+	border-radius: var(--radius-sm);
+	&:hover {
 		color: var(--color-white);
 		background-color: var(--color-primary);
 		border-radius: var(--radius-sm);
-		&:hover {
-			color: var(--color-white);
-			background-color: var(--color-primary);
-			border-radius: var(--radius-sm);
-		}
 	}
+}
 </style>
