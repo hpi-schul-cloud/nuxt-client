cdc08805c9b846690bbc0efc04b143746665051b
ABialoskorski <ABialoskorski@users.noreply.github.com>
2020-06-25 14:41:27 +0000

    npm run lint

--- src/components/molecules/ContentCard.vue
+++ src/components/molecules/ContentCard.vue
@@ -121,7 +121,6 @@ export default {
 			this.menuActive = true;
 		},
 	},
-
 };
 </script>
 
--- stories/mockData/Resource.js
+++ stories/mockData/Resource.js
@@ -1,44 +1,44 @@
-const $route= {
-        name: "news-id",
-        params: {
-            id: "randomId",
-        },
-        query: {},
-    };
+const $route = {
+	name: "news-id",
+	params: {
+		id: "randomId",
+	},
+	query: {},
+};
 export const Resource = {
-    params: {
-        id: "95436bf3-430c-4797-bd9b-a4c897414dce",
-    },
-    query: {
-        course: $route.query.course,
-        topic: $route.query.topic,
-    },
-    ref: {
-        archived: false,
-        id: "95436bf3-430c-4797-bd9b-a4c897414dce",
-        isHomeRepo: true,
-        repo: "mv-repo.schul-cloud.org",
-    },
-    content: {
-        url:
-            "https://mv-repo.schul-cloud.org/edu-sharing/components/render/70eedbd1-a4e9-47ee-ac00-3848863e5295",
-    },
-    name: "Mathematische Ausdrücke sortieren",
-    downloadUrl:
-        "https://mv-repo.schul-cloud.org/edu-sharing/eduservlet/download?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295",
-    preview: {
-        isIcon: false,
-        isGenerated: true,
-        url:
-            "https://mv-repo.schul-cloud.org/edu-sharing/preview?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295&storeProtocol=workspace&storeId=SpacesStore&dontcache=1593071828302",
-    },
-    properties: {
-        "cm:creator": ["admin"],
-        "cm:created": ["1580304014402"],
-        "cm:modified": ["1580304014402"],
-        "ccm:metadatacontributer_provider": ["Khan Academy"],
-        "cclom:general_description": [
-            "Lass uns etwas das Addieren und Subtrahieren von Variablen üben, die positive und negative Zahlen auf dem Zahlenstrahl darstellen.",
-        ],
-    },
+	params: {
+		id: "95436bf3-430c-4797-bd9b-a4c897414dce",
+	},
+	query: {
+		course: $route.query.course,
+		topic: $route.query.topic,
+	},
+	ref: {
+		archived: false,
+		id: "95436bf3-430c-4797-bd9b-a4c897414dce",
+		isHomeRepo: true,
+		repo: "mv-repo.schul-cloud.org",
+	},
+	content: {
+		url:
+			"https://mv-repo.schul-cloud.org/edu-sharing/components/render/70eedbd1-a4e9-47ee-ac00-3848863e5295",
+	},
+	name: "Mathematische Ausdrücke sortieren",
+	downloadUrl:
+		"https://mv-repo.schul-cloud.org/edu-sharing/eduservlet/download?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295",
+	preview: {
+		isIcon: false,
+		isGenerated: true,
+		url:
+			"https://mv-repo.schul-cloud.org/edu-sharing/preview?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295&storeProtocol=workspace&storeId=SpacesStore&dontcache=1593071828302",
+	},
+	properties: {
+		"cm:creator": ["admin"],
+		"cm:created": ["1580304014402"],
+		"cm:modified": ["1580304014402"],
+		"ccm:metadatacontributer_provider": ["Khan Academy"],
+		"cclom:general_description": [
+			"Lass uns etwas das Addieren und Subtrahieren von Variablen üben, die positive und negative Zahlen auf dem Zahlenstrahl darstellen.",
+		],
+	},
 };
