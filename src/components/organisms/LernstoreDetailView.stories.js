e7878dbb4be33d0ad764ce5f1122c476b7472670
Artur <bialoskorski.artur@gmail.com>
2020-06-26 08:29:41 +0200

    using resource in lernstore story

--- src/components/organisms/LernstoreDetailView.stories.js
+++ src/components/organisms/LernstoreDetailView.stories.js
@@ -1,4 +1,5 @@
 import { storiesOf } from "@storybook/vue";
+import { Resource } from "../../../stories/mockData/Resource";
 
 import LernstoreDetailView from "@components/organisms/LernstoreDetailView";
 
@@ -8,35 +9,8 @@ storiesOf("6 Organisms/LernstoreDetailView", module).add(
 		components: { LernstoreDetailView },
 		data: () => ({
 			resource: {
-				content: {
-					url:
-						"https://mv-repo.schul-cloud.org/edu-sharing/components/render/70eedbd1-a4e9-47ee-ac00-3848863e5295",
-				},
-				name: "Mathematische Ausdrücke sortieren",
-				downloadUrl:
-					"https://mv-repo.schul-cloud.org/edu-sharing/eduservlet/download?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295",
-				preview: {
-					isIcon: false,
-					isGenerated: true,
-					url:
-						"https://mv-repo.schul-cloud.org/edu-sharing/preview?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295&storeProtocol=workspace&storeId=SpacesStore&dontcache=1593071828302",
-				},
-				properties: {
-					"cm:creator": ["admin"],
-					"cm:created": ["1580304014402"],
-					"cm:modified": ["1580304014402"],
-					"ccm:metadatacontributer_provider": ["Khan Academy"],
-					"cclom:general_description": [
-						"Lass uns etwas das Addieren und Subtrahieren von Variablen üben, die positive und negative Zahlen auf dem Zahlenstrahl darstellen.",
-					],
-				},
-				ref: {
-					repo: "mv-repo.schul-cloud.org",
-					id: "70eedbd1-a4e9-47ee-ac00-3848863e5295",
-					archived: false,
-					isHomeRepo: true,
-				},
-			},
+				...Resource
+			}
 		}),
 
 		template: `<lernstore-detail-view :resource="resource"/>`,
--- stories/mockData/Resource.js
+++ stories/mockData/Resource.js
@@ -29,8 +29,7 @@ export const Resource = {
     preview: {
         isIcon: false,
         isGenerated: true,
-        url:
-            "https://mv-repo.schul-cloud.org/edu-sharing/preview?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295&storeProtocol=workspace&storeId=SpacesStore&dontcache=1593071828302",
+        url: "https://mv-repo.schul-cloud.org/edu-sharing/preview?nodeId=70eedbd1-a4e9-47ee-ac00-3848863e5295&amp;storeProtocol=workspace&amp;storeId=SpacesStore&amp;dontcache=1593152667345",
     },
     properties: {
         "cm:creator": ["admin"],
