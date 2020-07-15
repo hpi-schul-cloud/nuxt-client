5a7bc8bbed8336f3c8a8d90754d31087be1a34a7
Artur <bialoskorski.artur@gmail.com>
2020-07-15 10:26:02 +0200

    initial

---
+++ src/pages/colors/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        colors Page
+    </div>
+</template>
+
+<script>
+
+    export default {
+        components: {},
+        data: function () {
+            return {};
+        }
+    }
+</script>
