218f745f59ddfc99f60b3b198b404fa7135a5cfb
Artur Białoskórski <bialoskorski.artur@gmail.com>
2020-08-31 13:40:34 +0200

    added fallback

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -7,6 +7,8 @@ export const i18n = (store) =>
 	new VueI18n({
 		// fallback for storybook
 		locale: store.state.auth?.locale,
+		fallbackLocale: "de",	
+		
 		messages: {
 			en: require("@locale/en.json"),
 			de: require("@locale/de.json"),
