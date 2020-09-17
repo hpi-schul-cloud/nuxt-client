04faacb0ab91c430a9b78e289d5c27a1215617d7
Matti Schmidt <contact@schmidtchen.me>
2020-09-17 14:33:28 +0100

    updated role of pc repair image
    Co-authored-by: Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>

--- src/components/templates/ErrorPage.vue
+++ src/components/templates/ErrorPage.vue
@@ -7,7 +7,7 @@
 			fill="var(--color-primary)"
 			role="presentation"
 		/>
-		<img v-else alt="PC-Reparatur" src="@assets/img/pc_repair.png" />
+		<img v-else role="presentation src="@assets/img/pc_repair.png" />
 
 		<h1 class="error-msg">
 			<template v-if="error.message">
