5359c7f3ebbb7f041a53954266ea44122a66cc70
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2020-10-02 16:46:56 +0200

    SC-6278 disable admin routes again. undo linter config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,6 @@ module.exports = {
 	extends: ["@schul-cloud/eslint-config/javascriptVue"],
 	overrides: [
 		{
-			files: ["**/*.vue"],
-			rules: {
-				"max-lines": [
-					"error",
-					{ max: 400, skipBlankLines: true, skipComments: true },
-				],
-			},
-		},
-		{
 			files: ["**/*.stories.js"],
 			rules: {
 				"max-lines": [
