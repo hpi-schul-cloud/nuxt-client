4db63a1b3ad8a22134ecfcc540e97872854967fd
Lukasz Sielski <lukasz.sielski@capgemini.com>
2020-10-09 13:53:05 +0200

    some work

--- src/pages/courses/_id/index.vue
+++ src/pages/courses/_id/index.vue
@@ -1,8 +1,13 @@
 <template>
 	<div v-if="course">
 		<section class="section">
-			<base-breadcrumb :inputs="breadcrumbs" />
+			<!-- <base-breadcrumb :inputs="breadcrumbs" /> -->
 			<h1 class="h2">{{ course.name }}</h1>
+			<p>Amount of homeworks found: {{ homeworks.length }}</p>
+			<div v-for="(homework, index) in homeworks" :key="index">
+				{{ homework.name }}
+			</div>
+			<p>Amount of lessons found: {{ lessons.length }}</p>
 		</section>
 	</div>
 </template>
@@ -11,45 +16,45 @@
 import { mapGetters } from "vuex";
 
 export default {
-	async asyncData({ store, params }) {
-		return {
-			lessons: await store.dispatch("lessons/find", {
-				query: {
-					courseId: params.id,
-				},
-			}),
-			homeworks: await store.dispatch("homeworks/find", {
-				query: {
-					courseId: params.id,
-				},
-			}),
-			courseGroups: await store.dispatch("course-groups/find", {
-				query: {
-					courseId: params.id,
-				},
-			}),
-		};
-	},
 	computed: {
 		...mapGetters("courses", {
 			course: "current",
 		}),
-		breadcrumbs() {
-			return [
-				{ text: "Kurse", to: { name: "courses" } },
-				{
-					text: this.course.name,
-					to: { name: "courses-id", params: { id: this.course._id } },
-				},
-			];
-		},
+		...mapGetters("lessons", {
+			lessons: "list",
+		}),
+		...mapGetters("homeworks", {
+			homeworks: "list",
+		}),
+		// breadcrumbs() {
+		// 	return [
+		// 		{ text: "Kurse", to: { name: "courses" } },
+		// 		{
+		// 			text: this.course.name,
+		// 			to: { name: "courses-id", params: { id: this.course._id } },
+		// 		},
+		// 	];
+		// },
 	},
 	created(ctx) {
 		this.getCourse(this.$route.params.id);
+		this.getCourseContent(this.$route.params.id);
 	},
 	methods: {
 		getCourse(id) {
 			this.$store.dispatch("courses/get", id);
+		},
+		getCourseContent(id) {
+			this.$store.dispatch("homeworks/find", {
+				query: {
+					courseId: id,
+				},
+			});
+			this.$store.dispatch("lessons/find", {
+				query: {
+					courseId: id,
+				},
+			});
 		},
 	},
 };
