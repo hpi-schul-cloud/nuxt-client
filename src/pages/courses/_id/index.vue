d306b4a4b60284328e7cdd9f6b51126c26a20ea5
Lukasz Sielski <lukasz.sielski@capgemini.com>
2020-10-13 12:21:15 +0200

    base layout, waiting for components to diplay data in better way, still need for stylings

--- src/pages/courses/_id/index.vue
+++ src/pages/courses/_id/index.vue
@@ -1,21 +1,51 @@
 <template>
 	<div v-if="course">
 		<section class="section">
-			<!-- <base-breadcrumb :inputs="breadcrumbs" /> -->
+			<base-breadcrumb :inputs="breadcrumbs" />
 			<h1 class="h2">{{ course.name }}</h1>
-			<p>Amount of homeworks found: {{ homeworks.length }}</p>
-			<div v-for="(homework, index) in homeworks" :key="index">
-				{{ homework.name }}
-			</div>
-			<p>Amount of lessons found: {{ lessons.length }}</p>
+			<tabs>
+				<tab name="Kursinhalte" :selected="true">
+					<div v-for="(content, idx) in courseContents" :key="idx">
+						{{ idx }}. {{ content.type }} {{ content.name }}
+					</div>
+					<fab-floating
+						:show-label="true"
+						:actions="[
+							{
+								label: 'Thema',
+								icon: 'create',
+							},
+							{
+								label: 'Editor-Document',
+								icon: 'create',
+							},
+							{
+								label: 'Aufgabe',
+								icon: 'create',
+							},
+						]"
+					></fab-floating>
+				</tab>
+				<tab name="Tools">Tools</tab>
+				<tab name="Groups">Groups</tab>
+			</tabs>
 		</section>
 	</div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import Tabs from "@components/organisms/Tabs/Tabs";
+import Tab from "@components/atoms/Tab";
+import FabFloating from "@components/molecules/FabFloating";
 
 export default {
+	// layout: "loggedInFull",
+	components: {
+		Tabs,
+		Tab,
+		FabFloating,
+	},
 	computed: {
 		...mapGetters("courses", {
 			course: "current",
@@ -26,15 +56,24 @@ export default {
 		...mapGetters("homeworks", {
 			homeworks: "list",
 		}),
-		// breadcrumbs() {
-		// 	return [
-		// 		{ text: "Kurse", to: { name: "courses" } },
-		// 		{
-		// 			text: this.course.name,
-		// 			to: { name: "courses-id", params: { id: this.course._id } },
-		// 		},
-		// 	];
-		// },
+		courseContents() {
+			const store = this.$store;
+			const lessons = store.getters["lessons/list"];
+			lessons.forEach((lesson) => (lesson.type = "lesson"));
+			const homeworks = store.getters["homeworks/list"];
+			homeworks.forEach((homework) => (homework.type = "homework"));
+			console.log(lessons);
+			return [...lessons, ...homeworks];
+		},
+		breadcrumbs() {
+			return [
+				{ text: "Kurse", to: { name: "courses" } },
+				{
+					text: this.course.name,
+					to: { name: "courses-id", params: { id: this.course._id } },
+				},
+			];
+		},
 	},
 	created(ctx) {
 		this.getCourse(this.$route.params.id);
