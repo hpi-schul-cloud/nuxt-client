943f0ea3c7c96aba3b31b27e664bbd0bba711371
hpalac <hanna.palac@capgemini.com>
2020-10-15 10:01:10 +0200

    SC-7020: added a new molecule: BaseListItem.vue, BaseListItem.stories.js and an example svg

---
+++ src/components/molecules/BaseListItem.stories.js
@@ -0,0 +1,64 @@
+import { storiesOf } from "@storybook/vue";
+import BaseListItem from "@components/molecules/BaseListItem";
+import ExampleImage from "@assets/img/courses/draft.svg";
+import { boolean, text } from "@storybook/addon-knobs";
+
+storiesOf("5 Molecules/BaseListItem", module)
+	.add("with knobs", () => ({
+		components: { BaseListItem },
+		data: () => ({
+			image: ExampleImage,
+			title: text("title", "Das Nervensystem"),
+			subtitle: text("subtitle", "Editor-Dokument"),
+			status: text("status", "Entwurf"),
+			actionNeeded: boolean("actionNeeded", false),
+			actions: [
+				{ text: "Review", event: "event1" },
+				{ text: "Send back", event: "event2" },
+			],
+			contextOpen: false,
+		}),
+		template: `<div>
+		<BaseListItem :image="image" :title="title" :subtitle="subtitle" :status="status" :action-needed="actionNeeded" :actions="actions">
+		</BaseListItem>
+	</div>`,
+	}))
+	.add("with image (triple)", () => ({
+		components: { BaseListItem },
+		data: () => ({
+			image: ExampleImage,
+			title: "Das Nervensystem",
+			subtitle: "Editor-Dokument",
+			status: "Entwurf",
+			actions: [
+				{ text: "Review", event: "event1" },
+				{ text: "Send back", event: "event2" },
+			],
+		}),
+		template: `<div>
+		<BaseListItem :image="image" :title="title" :subtitle="subtitle" :status="status" :action-needed="actionNeeded" :actions="actions">
+		</BaseListItem>
+        <BaseListItem :image="image" :title="title" :subtitle="subtitle" :status="status" :action-needed="actionNeeded" :actions="actions">
+        </BaseListItem>
+        <BaseListItem :image="image" :title="title" :subtitle="subtitle" :status="status" :action-needed="actionNeeded" :actions="actions">
+        </BaseListItem>
+	</div>`,
+	}))
+	.add("with pulsating dot", () => ({
+		components: { BaseListItem },
+		data: () => ({
+			image: ExampleImage,
+			title: "Das Nervensystem",
+			subtitle: "Editor-Dokument",
+			status: "Entwurf",
+			actionNeeded: true,
+			actions: [
+				{ text: "Review", event: "event1" },
+				{ text: "Send back", event: "event2" },
+			],
+		}),
+		template: `<div>
+    <BaseListItem :image="image" :title="title" :subtitle="subtitle" :status="status" :action-needed="actionNeeded" :actions="actions">
+    </BaseListItem>
+    </div>`,
+	}));
