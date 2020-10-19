376a283e49268d5c265faf4db8ebd04213192e28
Lukasz Sielski <lukasz.sielski@capgemini.com>
2020-10-19 10:18:24 +0200

    added empty state, missing button, try of aligning fab button

--- src/pages/courses/_id/index.vue
+++ src/pages/courses/_id/index.vue
@@ -1,39 +1,60 @@
 <template>
 	<div v-if="course">
-		<section class="section">
-			<h1 class="h2">{{ course.name }}</h1>
-			<tabs>
+		<base-grid>
+			<h1 class="h2" style="grid-row: 1; grid-column: 1 / -1;">
+				{{ course.name }}
+			</h1>
+			<tabs style="grid-row: 2; grid-column: 1 / -1;">
 				<tab
 					:name="$t('pages.courses._id.educational_content')"
-					icon-name="fa fa-book"
+					icon-name="lerninhalte"
 					:selected="true"
 				>
-					<div v-for="(content, idx) in courseContents" :key="idx">
-						{{ idx }}. {{ content.type }} {{ content.name }}
-					</div>
-					<fab-floating
-						:show-label="true"
-						:actions="[
-							{
-								label: 'Thema',
-								icon: 'create',
-							},
-							{
-								label: 'Editor-Document',
-								icon: 'create',
-							},
-							{
-								label: 'Aufgabe',
-								icon: 'create',
-							},
-						]"
-					>
-					</fab-floating>
+					<template v-if="courseIsEmpty">
+						<empty-state
+							image="@assets/img/empty-state/course-empty-state.svg"
+							:title="$t('pages.courses._id.empty_course_title')"
+						>
+							<template v-slot:description>
+								<!-- eslint-disable vue/no-v-html -->
+								<span
+									v-html="$t('pages.courses._id.empty_course_description')"
+								/>
+							</template>
+						</empty-state>
+					</template>
+					<template v-else>
+						<div v-for="(content, idx) in courseContents" :key="idx">
+							{{ idx }}. {{ content.type }} {{ content.name }}
+						</div>
+					</template>
 				</tab>
-				<tab name="Tools">Tools</tab>
-				<tab name="Groups">Groups</tab>
+				<tab name="Groups" icon-name="gruppen">Groups</tab>
+				<tab name="Tools" icon-name="tools">Tools</tab>
 			</tabs>
-		</section>
+			<fab-icon
+				style="grid-row: 2; grid-column: -1;"
+				:show-label="true"
+				:actions="[
+					{
+						label: 'Thema',
+						icon: 'create',
+						iconSource: 'custom',
+					},
+					{
+						label: 'Editor-Document',
+						icon: 'create',
+						iconSource: 'custom',
+					},
+					{
+						label: 'Aufgabe',
+						icon: 'create',
+						iconSource: 'custom',
+					},
+				]"
+			>
+			</fab-icon>
+		</base-grid>
 	</div>
 </template>
 
@@ -41,14 +62,18 @@
 import { mapGetters } from "vuex";
 import Tabs from "@components/organisms/Tabs/Tabs";
 import Tab from "@components/atoms/Tab";
-import FabFloating from "@components/molecules/FabFloating";
+import FabIcon from "@components/molecules/FabIcon";
+import BaseGrid from "@components/base/BaseGrid";
+import EmptyState from "@components/molecules/EmptyState";
 
 export default {
 	layout: "loggedInFull",
 	components: {
 		Tabs,
 		Tab,
-		FabFloating,
+		FabIcon,
+		BaseGrid,
+		EmptyState,
 	},
 	computed: {
 		...mapGetters("courses", {
@@ -61,12 +86,16 @@ export default {
 			homeworks: "list",
 		}),
 		courseContents() {
-			const store = this.$store;
-			const lessons = store.getters["lessons/list"];
-			lessons.forEach((lesson) => (lesson.type = "lesson"));
-			const homeworks = store.getters["homeworks/list"];
-			homeworks.forEach((homework) => (homework.type = "homework"));
-			return [...lessons, ...homeworks];
+			// const store = this.$store;
+			// const lessons = store.getters["lessons/list"];
+			// lessons.forEach((lesson) => (lesson.type = "lesson"));
+			// const homeworks = store.getters["homeworks/list"];
+			// homeworks.forEach((homework) => (homework.type = "homework"));
+			// return [...lessons, ...homeworks];
+			return [];
+		},
+		courseIsEmpty() {
+			return this.courseContents.length === 0;
 		},
 	},
 	created(ctx) {
