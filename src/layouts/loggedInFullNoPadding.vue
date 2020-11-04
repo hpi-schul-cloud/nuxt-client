132b0aa68b096f46e2e7a0b4d744c781d764c8da
Lukasz Sielski <lukasz.sielski@capgemini.com>
2020-11-04 10:28:11 +0100

    adding id for main element in layouts (linkSkips)

--- src/layouts/loggedInFullNoPadding.vue
+++ src/layouts/loggedInFullNoPadding.vue
@@ -1,6 +1,6 @@
 <template>
 	<legacy-logged-in>
-		<main class="content" id="main-content" :class="{ inline: isInline }">
+		<main id="main-content" class="content" :class="{ inline: isInline }">
 			<Nuxt />
 		</main>
 	</legacy-logged-in>
