7475e22c4b962709956de473a36e3e24e5962c7e
murat-merdoglu-hpi <64781656+murat-merdoglu-hpi@users.noreply.github.com>
2020-11-25 12:37:43 +0100

    changing validation regex and corrections some bugs

--- src/components/organisms/Ldap/LdapRolesSection.vue
+++ src/components/organisms/Ldap/LdapRolesSection.vue
@@ -18,7 +18,7 @@
 					this.$t('pages.administration.ldapEdit.roles.labels.radio.ldapGroup')
 				"
 				name="group"
-				style="margin-right: var(--space-sm)"
+				style="margin-right: var(--space-sm);"
 				value="ldap_group"
 				@update:vmodel="$emit('input', { ...value, groupOption: $event })"
 			/>
@@ -34,7 +34,7 @@
 				value="user_attribute"
 				@update:vmodel="$emit('input', { ...value, groupOption: $event })"
 			/>
-			<p class="text-sm" style="margin-top: var(--space-xs)">
+			<p class="text-sm" style="margin-top: var(--space-xs);">
 				{{
 					this.$t(
 						"pages.administration.ldapEdit.roles.labels.radio.description"
@@ -50,7 +50,7 @@
 			:placeholder="
 				this.$t('pages.administration.ldapEdit.roles.placeholder.member')
 			"
-			style="margin-bottom: var(--space-xl)"
+			style="margin-bottom: var(--space-xl);"
 			:validation-model="$v.value.member"
 			:validation-messages="memberValidationMessages"
 			data-testid="ldapDataRolesMember"
