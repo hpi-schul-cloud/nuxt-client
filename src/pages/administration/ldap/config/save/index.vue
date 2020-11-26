4513a0acb92f8b2e6b4039bec2fb6b24f1cd802a
j-jmnz <j-jmnz@users.noreply.github.com>
2020-11-26 10:25:02 +0000

    npm run lint

--- src/pages/administration/ldap/config/save/index.vue
+++ src/pages/administration/ldap/config/save/index.vue
@@ -85,7 +85,7 @@
 						<base-icon
 							source="material"
 							icon="check_circle"
-							style="color: var(--color-success);"
+							style="color: var(--color-success)"
 						/>
 					</template>
 				</modal-body-info>
