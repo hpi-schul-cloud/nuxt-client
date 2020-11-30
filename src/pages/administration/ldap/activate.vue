2026d22ee3834bf9d3f319db58939b79baa53eed
j-jmnz <j-jmnz@users.noreply.github.com>
2020-11-30 09:29:45 +0000

    npm run lint

--- src/pages/administration/ldap/activate.vue
+++ src/pages/administration/ldap/activate.vue
@@ -95,7 +95,7 @@
 						<base-icon
 							source="material"
 							icon="check_circle"
-							style="color: var(--color-success);"
+							style="color: var(--color-success)"
 						/>
 					</template>
 				</modal-body-info>
