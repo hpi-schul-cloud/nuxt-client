e2989e549665bbecdf365ff616eee6250db37f44
ABialoskorski <bialoskorski.artur@gmail.com>
2020-12-04 10:11:46 +0100

    first version of delete Modal, courses page supports eng now

--- src/components/base/BaseModal.stories.js
+++ src/components/base/BaseModal.stories.js
@@ -14,6 +14,8 @@ import ModalFooter from "@components/molecules/ModalFooter";
 import LoadingModal from "@components/molecules/LoadingModal";
 import AutoLogoutWarning from "@components/organisms/AutoLogoutWarning";
 
+import DeleteModal from "@components/molecules/DeleteModal";
+
 storiesOf("4 Base UI Components/Modals", module)
 	.addParameters({
 		notes,
@@ -181,6 +183,10 @@ storiesOf("4 Base UI Components/Modals", module)
 			</template>
 		</base-modal>
 		</div>`,
+	}))
+	.add("Delete Dialog", () => ({
+		components: { DeleteModal },
+		template: `<delete-modal></delete-modal>`
 	}))
 	.add("Loading", () => ({
 		components: { LoadingModal },
---
+++ src/components/molecules/DeleteModal.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <base-button @click="showModal = true">
+            Open Modal
+        </base-button>
+        <base-modal
+            :active.sync="showModal"
+            @onBackdropClick="closeModal"
+        >
+            <template v-slot:header></template>
+            <template v-slot:body>
+                <modal-body-info :title="title" :description="description">
+                    <template v-slot:icon>
+                        <base-icon source="material" icon="report_problem" style="color: var(--color-danger)"/>
+                    </template>
+                </modal-body-info>
+            </template>
+            <template v-slot:footerRight>
+                <base-button design="text" @click="showModal = false">
+                    {{ $t("common.actions.cancel") }}
+                </base-button>
+                <base-button class="delete-btn" @click="confirmDelete">
+                    <base-icon source="material" icon="delete"/>
+                    {{ $t("common.actions.remove") }}
+                </base-button>
+            </template>
+        </base-modal>
+    </div>
+</template>
+
+<script>
+    import BaseButton from "@basecomponents/BaseButton";
+    import BaseIcon from "@basecomponents/BaseIcon";
+
+    import BaseModal from "@basecomponents/BaseModal";
+    import ModalBodyInfo from "@components/molecules/ModalBodyInfo";
+    export default {
+        components: {
+            BaseModal,
+            ModalBodyInfo,
+            BaseIcon,
+            BaseButton,
+        },
+        props: {
+            title: { type: String, default: "Willst du die Aufgabe wirklich löschen?" },
+            description: { type: String, default: "Der Kursinhalt wird unwiederruflich gelöscht" },
+        },
+        data() {
+            return {
+                showModal: false,
+            };
+        },
+        methods: {
+            closeModal() {
+                this.$emit("update:show-modal", false);
+                this.showModal = false;
+            },
+            confirmDelete() {
+                this.$emit("close");
+                // logic here
+                this.closeModal();
+            },
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+.delete-btn {
+    background-color: var(--color-danger) !important;
+    &:hover {
+        background-color: var(--color-danger-dark) !important;
+    }
+}
+</style>
