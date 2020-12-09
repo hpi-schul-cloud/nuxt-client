7b9323486313005f11a426dd5e570e68a233d292
Lukasz Sielski <lukasz.sielski@capgemini.com>
2020-12-09 12:56:22 +0100

    removed deleting logic from the DeleteModal, passing id and type of course item to delete in actions arguments

--- src/store/courses.js
+++ src/store/courses.js
@@ -1,6 +1,11 @@
 import serviceTemplate from "@utils/service-template";
 const base = serviceTemplate("courses");
 
+// base.state = () => {
+// 	let defaultState = base.state();
+// 	defaultState.
+// }
+
 const module = {
 	...base,
 };
