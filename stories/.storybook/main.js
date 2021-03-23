3c19327dd2c37077b435a5b983476d2c81bfff18
Fabian Mahling <FabianMahling@web.de>
2021-03-23 01:01:32 +0100

    bump version, uninstall deprecated packages

--- stories/.storybook/main.js
+++ stories/.storybook/main.js
@@ -1,14 +1,14 @@
 const path = require("path");
 module.exports = {
 	addons: [
-		"@storybook/addon-notes/register",
-		"@storybook/addon-viewport/register",
-		"storybook-addon-vue-info/lib/register",
-		"@storybook/addon-knobs/register",
-		"@storybook/addon-actions/register",
-		"@storybook/addon-a11y/register",
-		"@storybook/addon-backgrounds/register",
-		"@storybook/addon-storysource/register",
+		"@storybook/addon-viewport",
+		"storybook-addon-vue-info/lib",
+		"@storybook/addon-knobs",
+		"@storybook/addon-actions",
+		"@storybook/addon-a11y",
+		"@storybook/addon-backgrounds",
+		"@storybook/addon-storysource",
+		"@storybook/addon-docs",
 	],
 	webpackFinal: async (config) => {
 		config.module.rules.push({
