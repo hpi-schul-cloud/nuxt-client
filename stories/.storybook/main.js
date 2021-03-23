200bf86a350c47c3901845f79ff815ee6d531657
Fabian Mahling <FabianMahling@web.de>
2021-03-23 19:27:07 +0100

    move webpack config to one file

--- stories/.storybook/main.js
+++ stories/.storybook/main.js
@@ -9,21 +9,4 @@ module.exports = {
 		"@storybook/addon-storysource",
 		"@storybook/addon-docs",
 	],
-	webpackFinal: async (config) => {
-		config.module.rules.push({
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [
-				{
-					loader: "ts-loader",
-					options: {
-						appendTsSuffixTo: [/\.vue$/],
-						transpileOnly: true,
-					},
-				},
-			],
-		});
-		config.resolve.extensions.push(".ts");
-		return config;
-	},
 };
--- stories/.storybook/webpack.config.js
+++ stories/.storybook/webpack.config.js
@@ -3,6 +3,19 @@ const path = require("path");
 module.exports = ({ config }) => {
 	const rules = [
 		{
+			test: /\.ts$/,
+			exclude: /node_modules/,
+			use: [
+				{
+					loader: "ts-loader",
+					options: {
+						appendTsSuffixTo: [/\.vue$/],
+						transpileOnly: true,
+					},
+				},
+			],
+		},
+		{
 			test: /\.scss$/,
 			loaders: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
 			include: path.resolve(__dirname, "../../"),
@@ -27,6 +40,7 @@ module.exports = ({ config }) => {
 		},
 	];
 	config.module.rules.push(...rules);
+	config.resolve.extensions.push(".ts");
 
 	const alias = require("../../aliases.config").webpack;
 	Object.assign(config.resolve.alias, alias);
