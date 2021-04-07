57dfaee71a024599eb32a9623ac76b48647bd1c3
j_jmnz <jmnz.jesus@gmail.com>
2021-04-07 12:49:09 +0200

    add beforeEach

--- src/pages/administration/students/index.unit.js
+++ src/pages/administration/students/index.unit.js
@@ -1,46 +1,50 @@
 import { default as StudentPage } from "./index.vue";
 
 describe("students/index", () => {
-	const mockStore = {
-		auth: {
-			state: () => ({
-				user: {
-					roles: [
-						{
-							name: "administrator",
-						},
-					],
-					permissions: ["STUDENT_CREATE"],
-				},
-				school: {
-					isExternal: false,
-				},
-			}),
-		},
-		users: {
-			actions: {
-				handleUsers: jest.fn(),
-			},
-			getters: {
-				list: () => [],
-			},
-			state: () => ({
-				pagination: {
-					default: {
-						limit: 25,
-						skip: 0,
-						total: 2,
-						query: "__vue_devtool_nan__",
+	let mockStore;
+
+	beforeEach(() => {
+		mockStore = {
+			auth: {
+				state: () => ({
+					user: {
+						roles: [
+							{
+								name: "administrator",
+							},
+						],
+						permissions: ["STUDENT_CREATE"],
 					},
-				},
-			}),
-		},
-		uiState: {
-			getters: {
-				get: () => () => ({ page: 1 }),
+					school: {
+						isExternal: false,
+					},
+				}),
 			},
-		},
-	};
+			users: {
+				actions: {
+					handleUsers: jest.fn(),
+				},
+				getters: {
+					list: () => [],
+				},
+				state: () => ({
+					pagination: {
+						default: {
+							limit: 25,
+							skip: 0,
+							total: 2,
+							query: "__vue_devtool_nan__",
+						},
+					},
+				}),
+			},
+			uiState: {
+				getters: {
+					get: () => () => ({ page: 1 }),
+				},
+			},
+		};
+	});
 
 	it(...isValidComponent(StudentPage));
 
