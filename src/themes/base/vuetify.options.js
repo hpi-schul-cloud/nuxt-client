f56935a065175eed8a3697713f714f6d19c913dd
Dominik Brüchner <dominik.bruechner@gmail.com>
2021-04-28 15:19:58 +0200

    addded more default colors

--- src/themes/base/vuetify.options.js
+++ src/themes/base/vuetify.options.js
@@ -2,8 +2,12 @@ export default {
 	theme: {
 		themes: {
 			light: {
-				error: "#ff1134",
+				error: "#FF1134",
+				info: "#0A7AC9",
+				success: "#13BA98",
+				warning: "#FFD611",
 			},
 		},
 	},
+	iconfont: "fa4",
 };
