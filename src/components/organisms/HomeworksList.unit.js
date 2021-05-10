ff529f0090fd49b8f6c19cc37a95328d4bddf5f2
Dominik Brüchner <dominik.bruechner@gmail.com>
2021-05-10 12:01:54 +0200

    added unit test for homework item list

--- src/components/organisms/HomeworksList.unit.js
+++ src/components/organisms/HomeworksList.unit.js
@@ -1,5 +1,41 @@
 import HomeworksList from "./HomeworksList";
+import { homeworks } from "@@/stories/mockData/Homeworks";
+import Vuetify from "vuetify";
 
 describe("@components/organisms/HomeworksList", () => {
+	const mockStore = {
+		homeworks: {
+			getters: {
+				list: () => homeworks,
+			},
+			state: () => ({
+				list: homeworks,
+			}),
+		},
+	};
+
+	let vuetify;
+
+	beforeEach(() => {
+		vuetify = new Vuetify();
+	});
+
 	it(...isValidComponent(HomeworksList));
+
+	it("Renders complete homework items list", async () => {
+		const wrapper = mount(HomeworksList, {
+			...createComponentMocks(
+				{
+					i18n: true,
+					vuetify: true,
+					store: mockStore,
+				},
+				vuetify
+			),
+		});
+
+		expect(wrapper.findAllComponents({ name: "VListItem" })).toHaveLength(
+			homeworks.length
+		);
+	});
 });
