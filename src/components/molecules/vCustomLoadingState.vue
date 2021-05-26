d3a89c0299d63acfcde1a34fdf9717e77dad16f1
Martyna <mlecharka@gmail.com>
2021-05-26 08:35:23 +0200

    Created custom loading state component.

---
+++ src/components/molecules/vCustomLoadingState.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-container>
+		<v-skeleton-loader ref="skeleton" :type="type"></v-skeleton-loader>
+	</v-container>
+</template>
+
+<script>
+export default {
+	components: {},
+	layout: "defaultVuetify",
+	props: {
+		type: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
