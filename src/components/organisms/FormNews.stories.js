efb25c1cb6ca0f1c3e7d7b996db866f404a22534
Michael Wolowyk <michael.wolowyk@capgemini.com>
2021-06-23 10:50:13 +0200

    introduced news.saveAll function for persisting multiple news at once

--- src/components/organisms/FormNews.stories.js
+++ src/components/organisms/FormNews.stories.js
@@ -3,8 +3,10 @@ import { action } from "@storybook/addon-actions";
 
 import FormNews from "./FormNews";
 import FormActions from "@components/molecules/FormActions";
+import NewsDetailView from "@components/organisms/NewsDetailView";
+import NewsEditView from "@components/organisms/NewsEditView";
 
-storiesOf("6 Organisms/FormNews", module)
+storiesOf("6 Organisms/News", module)
 	.add("/new", () => ({
 		components: { FormNews, FormActions },
 		template: `
@@ -25,30 +27,38 @@ storiesOf("6 Organisms/FormNews", module)
 		},
 	}))
 	.add("/edit", () => ({
-		components: { FormNews, FormActions },
-		template: `
-	<form-news #actions="{ remove, cancel }" action="patch" @update:news="onNewsChange">
-	<form-actions>
-	<template v-slot:secondary>
-		<base-button
-			design="danger text"
-			type="button"
-			@click.prevent="remove"
-		>
-			{{ $t("common.actions.remove") }}
-		</base-button>
-	</template>
-	<template v-slot:primary>
-		<base-button design="primary" type="submit">
-			{{ $t("common.actions.save") }}
-		</base-button>
-		<base-button design="text" @click.prevent="cancel">
-			{{ $t("common.actions.cancel") }}
-		</base-button>
-	</template>
-</form-actions>
-	</form-news>`,
-		methods: {
-			onNewsChange: action("@update:news"),
-		},
+		components: { NewsEditView },
+		data: () => ({
+			current: {
+				id: "some_id",
+				title: "test news",
+				content:
+					"<div><h1>this is my news content</h1><p>News content</p></div>",
+				creator: {
+					firstName: "Test",
+					lastName: "User",
+				},
+				displayAt: "2021-03-02",
+			},
+		}),
+
+		template: `<news-edit-view :current="current"/>`,
+	}))
+	.add("/details", () => ({
+		components: { NewsDetailView },
+		data: () => ({
+			current: {
+				id: "some_id",
+				title: "test news",
+				content:
+					"<div><h1>this is my news content</h1><p>News content</p></div>",
+				creator: {
+					firstName: "Test",
+					lastName: "User",
+				},
+				displayAt: "2021-03-02",
+			},
+		}),
+
+		template: `<news-detail-view :current="current"/>`,
 	}));
