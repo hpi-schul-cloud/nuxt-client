8d819623a0e1b789c308e7fe265c680ea35c8ae4
Martyna <mlecharka@gmail.com>
2021-07-15 12:46:56 +0200

    Cleaned up open.vue from redundant code.

--- src/pages/tasks/open.vue
+++ src/pages/tasks/open.vue
@@ -3,30 +3,12 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import tasksEmptyState from "@assets/img/empty-state/Task_Empty_State.svg";
 import HomeworksDashboardMain from "@components/organisms/HomeworksDashboardMain";
 
 export default {
 	components: { HomeworksDashboardMain },
 	layout: "defaultVuetify",
-	data() {
-		return {
-			image: tasksEmptyState,
-		};
-	},
-	computed: {
-		...mapGetters("homeworks", {
-			homeworks: "getList",
-			isListEmpty: "isListEmpty",
-			isListFilled: "isListFilled",
-			openHomeworks: "getOpenHomeworks",
-			overDueHomeworks: "getOverDueHomeworks",
-		}),
-	},
-	mounted() {
-		this.$store.dispatch("homeworks/getHomeworksDashboard");
-	},
+
 	head() {
 		return {
 			title: this.$t("pages.homeworks.student.title"),
@@ -34,11 +16,3 @@ export default {
 	},
 };
 </script>
-
-<style lang="scss" scoped>
-@import "@variables";
-
-.v-container {
-	max-width: var(--size-content-width-max);
-}
-</style>
