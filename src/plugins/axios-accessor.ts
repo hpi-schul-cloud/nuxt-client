57d0856749050dcc090c3419067ab8856641a2f8
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2021-09-07 01:04:02 +0200

    cleanup unused code

--- src/plugins/axios-accessor.ts
+++ src/plugins/axios-accessor.ts
@@ -3,7 +3,6 @@ import { serverAPI } from "../utils/api";
 
 const accessor: Plugin = ({ $axios }) => {
 	serverAPI.setup($axios);
-	// initializeAxios($axios);
 };
 
 export default accessor;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,5 @@
 import { NuxtAxiosInstance } from "@nuxtjs/axios";
 
-/*
-let $axios: NuxtAxiosInstance;
-
-export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
-	$axios = axiosInstance;
-}
-
-export { $axios }; */
-
 enum APINames {
 	server,
 }
@@ -23,11 +14,8 @@ class API {
 			"It exist no axios instance, please pass it over constructor or setup().",
 	};
 
-	constructor(name: APINames, axios?: NuxtAxiosInstance) {
+	constructor(name: APINames) {
 		this.name = name;
-		if (axios !== undefined) {
-			this.setup(axios);
-		}
 	}
 
 	public setup(axios: NuxtAxiosInstance) {
@@ -51,25 +39,10 @@ class APIHandler {
 	private apis: API[];
 	private err = {
 		nameConflict: "A instance with this name already exist. It can not added.",
-		noMatch: "A instance with this name do not exist",
 	};
 
 	constructor() {
 		this.apis = [];
-	}
-
-	private getAPI(name: APINames): API {
-		const api = this.apis.find((api) => api.name === name);
-		if (api === undefined) {
-			throw new Error(this.err.noMatch);
-		}
-		return api;
-	}
-
-	public getAxios(name: APINames): NuxtAxiosInstance {
-		const api = this.getAPI(name);
-		const axios = api.get();
-		return axios;
 	}
 
 	private validateIfNameExist(name: APINames): void {
@@ -83,8 +56,8 @@ class APIHandler {
 		this.apis.push(api);
 	}
 
-	public create(name: APINames, axios?: NuxtAxiosInstance): API {
-		const api = new API(name, axios);
+	public create(name: APINames): API {
+		const api = new API(name);
 		this.add(api);
 		return api;
 	}
