bb95f20ef5e2a0faf5a6570f0943b19db26e90ba
Alexander Riese <alexander.riese@capgemini.com>
2021-09-16 17:43:10 +0200

    BC-295 update storybook-vue

--- stories/.storybook/main.js
+++ stories/.storybook/main.js
@@ -3,11 +3,11 @@ module.exports = {
 	addons: [
 		"@storybook/addon-notes/register",
 		"@storybook/addon-viewport/register",
-		"storybook-addon-vue-info/lib/register",
 		"@storybook/addon-knobs/register",
 		"@storybook/addon-actions/register",
 		"@storybook/addon-a11y/register",
 		"@storybook/addon-storysource/register",
+		"@storybook/addon-docs",
 	],
 	webpackFinal: async (config) => {
 		config.module.rules.push({
