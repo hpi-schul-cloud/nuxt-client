d9ca0aec687909c82b2736ef1c593e639912d4ab
j-jmnz <jmnz.jesus@gmail.com>
2021-09-29 09:21:55 +0200

    BC-296 - fix storybook again

--- stories/.storybook/main.js
+++ stories/.storybook/main.js
@@ -3,7 +3,6 @@ module.exports = {
 	addons: [
 		"@storybook/addon-notes/register",
 		"@storybook/addon-viewport/register",
-		"storybook-addon-vue-info/lib/register",
 		"@storybook/addon-knobs/register",
 		"@storybook/addon-actions/register",
 		"@storybook/addon-a11y/register",
