df1f3e032820645dfc5236ee4bd4bb316ffcc11b
odalys-hpi <odalys.adam@dataport.de>
2021-10-18 17:10:31 +0200

    BC-392 - basic skip link for main content

---
+++ src/components/molecules/SkipLinks.vue
@@ -0,0 +1,28 @@
+<template>
+	<a id="skiplink" tabindex="0" class="skip-link" href="#main-content">
+		Zum Hauptinhalt wechseln
+	</a>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@styles";
+
+.skip-link {
+	position: absolute;
+	top: -100px;
+	left: 50%;
+	padding: var(--space-xs);
+	color: var(--color-primary);
+	background-color: var(--color-white);
+	border: 1px solid #555;
+	border-radius: var(--radius-xs);
+}
+
+.skip-link:focus {
+	top: 10px;
+}
+</style>
