837dd5a3374bf74271a86fcaffe969cfd43d799e
odalys-hpi <odalys.adam@dataport.de>
2021-10-20 13:37:31 +0200

    BC-392 - added test for correct tabindex

--- src/components/molecules/SkipLinks.unit.js
+++ src/components/molecules/SkipLinks.unit.js
@@ -3,7 +3,7 @@ import SkipLinks from "./SkipLinks";
 describe("@components/molecules/SkipLinks", () => {
 	it(...isValidComponent(SkipLinks));
 
-	it("Should render its skip link", () => {
+	it("Should render its skip link", async () => {
 		const wrapper = mount(SkipLinks, {
 			...createComponentMocks({
 				i18n: true,
@@ -11,5 +11,15 @@ describe("@components/molecules/SkipLinks", () => {
 		});
 
 		expect(wrapper.find("#skip-link").exists()).toBe(true);
+	});
+
+	it("Should have tabindex 0", async () => {
+		const wrapper = mount(SkipLinks, {
+			...createComponentMocks({
+				i18n: true,
+			}),
+		});
+
+		expect(wrapper.attributes("tabindex")).toBe("0");
 	});
 });
