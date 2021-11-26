f32c03e0726453dd5b1dbdd2f50cb59f32cbad34
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2021-11-26 10:54:46 +0100

    disable storybook pages should be resolved with BC-868

--- src/components/templates/TasksDashboardMain.stories.js
+++ src/components/templates/TasksDashboardMain.stories.js
@@ -1,36 +1,39 @@
-import { storiesOf } from "@storybook/vue";
+// import { storiesOf } from "@storybook/vue";
 
-import TasksDashboardMain from "@components/templates/TasksDashboardMain";
-import mock from "@@/stories/mockData/Tasks";
+// import TasksDashboardMain from "@components/templates/TasksDashboardMain";
+// import mock from "@@/stories/mockData/Tasks";
 
-import TaskModule from "../../store/tasks";
+// import { rootStore } from "../../store/index";
+// import TaskModule from "../../store/tasks";
 
-const { tasks } = mock;
+// const { tasks } = mock;
 
-TaskModule.setTasks(tasks);
+// TaskModule.setTasks(tasks);
 
-storiesOf("0 Vuetify/Templates/TasksDashboard", module)
-	.add("Tasks Dashboard Student", () => ({
-		components: {
-			TasksDashboardMain,
-		},
-		data: () => ({
-			role: "student",
-		}),
-		template: `
-		<v-app>
-			<tasks-dashboard-main :role='role' />
-		</v-app>`,
-	}))
-	.add("Tasks Dashboard Teacher", () => ({
-		components: {
-			TasksDashboardMain,
-		},
-		data: () => ({
-			role: "teacher",
-		}),
-		template: `
-			<v-app>
-				<tasks-dashboard-main :role='role' />
-			</v-app>`,
-	}));
+// storiesOf("0 Vuetify/Templates/TasksDashboard", module)
+// 	.add("Tasks Dashboard Student", () => ({
+// 		components: {
+// 			TasksDashboardMain,
+// 		},
+// 		data: () => ({
+// 			role: "student",
+// 		}),
+// 		store: rootStore,
+// 		template: `
+// 		<v-app>
+// 			<tasks-dashboard-main :role='role' />
+// 		</v-app>`,
+// 	}))
+// 	.add("Tasks Dashboard Teacher", () => ({
+// 		components: {
+// 			TasksDashboardMain,
+// 		},
+// 		data: () => ({
+// 			role: "teacher",
+// 		}),
+// 		store: rootStore,
+// 		template: `
+// 			<v-app>
+// 				<tasks-dashboard-main :role='role' />
+// 			</v-app>`,
+// 	}));
