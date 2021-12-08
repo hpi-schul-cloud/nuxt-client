6f794b4bc07a7d27c91e303275b9ce0ec82579d4
odalys-hpi <odalys.adam@dataport.de>
2021-12-08 16:23:39 +0100

    BC-643 - second proposal for task overview rerouting

--- src/middleware/redirects.js
+++ src/middleware/redirects.js
@@ -1,12 +1,37 @@
 export default function (req, res, next) {
 	const redirects = [
 		{
-			from: /homework\/*/,
-			to: "/homework/",
+			from: "/homework/",
+			to: "/homework/overview",
+		},
+		{
+			from: "/homework/asked/",
+			to: "/homework/overview",
+		},
+		{
+			from: "/homework/private/",
+			to: "/homework/overview",
+		},
+		{
+			from: "/homework/archive/",
+			to: "/homework/overview",
+		},
+		{
+			from: "/tasks/assigned",
+			to: "/homework/overview",
+		},
+		{
+			from: "/tasks/open",
+			to: "/homework/overview",
 		},
 	];
-	
-	const redirect = redirects.find((r) => req.url.match(r.from) && req.url !== r.to);
+
+	console.log(req.url);
+	const redirect = redirects.find((r) => r.from === req.url);
+	console.log(redirect);
+	/* const redirect = redirects.find(
+		(r) => req.url.match(r.from) && req.url !== r.to
+	); */
 	if (redirect) {
 		res.writeHead(301, { Location: redirect.to });
 		res.end();
---
+++ src/pages/homework/overview.vue
@@ -0,0 +1,21 @@
+<template>
+	<tasks-dashboard-main :role="userRoles[0]" />
+</template>
+
+<script>
+import authModule from "@/store/auth";
+import TasksDashboardMain from "@components/templates/TasksDashboardMain";
+
+export default {
+	components: { TasksDashboardMain },
+	layout: "defaultVuetify",
+	computed: {
+		userRoles: () => authModule.getUserRoles, // TODO - how to handle multiple roles & is not teacher or student
+	},
+	head() {
+		return {
+			title: this.$t("pages.tasks.title"),
+		};
+	},
+};
+</script>
