9fdd90900dcffb82ca2b0a1561d1bb645c2fccb3
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-02-17 10:20:41 +0100

    BC-877 - Spike: Research mocking of typescript stores in Nuxt (#2043)
    * BC-877 - add ability to mock typescript store module
    
    * BC-877 - update tests for main task dashboard

---
+++ src/utils/mock-store-module.ts
@@ -0,0 +1,44 @@
+import { ActionTree, Module as Mod, MutationTree } from "vuex";
+import { VuexModule } from "vuex-module-decorators";
+
+type ConstructorOf<C> = { new (...args: any[]): C };
+type StaticsType = Record<string, any>;
+
+const mockMutations = <M>(
+	module: Function & Mod<M, any>,
+	statics: StaticsType
+) => {
+	Object.keys(module.mutations as MutationTree<M>).forEach((key) => {
+		// eslint-disable-next-line jest/prefer-spy-on
+		statics[key] = jest.fn();
+	});
+};
+
+const mockActions = <M>(
+	module: Function & Mod<M, any>,
+	statics: StaticsType
+) => {
+	Object.keys(module.actions as ActionTree<M, any>).forEach((key) => {
+		// eslint-disable-next-line jest/prefer-spy-on
+		statics[key] = jest.fn();
+	});
+};
+
+export function createModuleMocks<M extends VuexModule>(
+	moduleClass: ConstructorOf<M>
+): M {
+	const module: Function & Mod<M, any> = moduleClass;
+
+	const statics: StaticsType = {};
+
+	// -------- mutations --------
+	if (module.mutations) {
+		mockMutations(module, statics);
+	}
+	// -------- actions ---------
+	if (module.actions) {
+		mockActions(module, statics);
+	}
+
+	return statics as M;
+}
