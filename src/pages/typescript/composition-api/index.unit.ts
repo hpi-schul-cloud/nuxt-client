6a345af6e4a0bb0d8c1cf78f91cbadf6f98000ed
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-02-28 21:17:22 +0100

    BC-637 - fix tests

--- src/pages/typescript/composition-api/index.unit.ts
+++ src/pages/typescript/composition-api/index.unit.ts
@@ -4,59 +4,33 @@ import Index from "./index.vue";
 import { Auth } from "@/store/auth";
 import { createModuleMocks } from "@/utils/mock-store-module";
 
-const mockAuthModule = jest.fn();
 jest.mock("@store/auth", () => ({
 	...jest.requireActual("@store/auth"),
 	__esModule: true,
 	get default() {
-		return mockAuthModule();
+		return {
+			getSchool: {
+				name: "Test-Schule",
+			},
+		};
 	},
 }));
-
-const mockTaskModule = jest.fn();
-jest.mock("@store/tasks", () => ({
-	...jest.requireActual("@store/tasks"),
-	__esModule: true,
-	get default() {
-		return mockTaskModule();
-	},
-}));
-
-// describe("bla", () => {
-// 	it("blabla", () => {
-// 		expect(mockAuthModule).toBeDefined();
-// 	});
-// });
 
 describe("@index", () => {
 	let authModuleMock: Auth;
 	let wrapper: Wrapper<Index>;
 
 	beforeEach(() => {
-		authModuleMock = {
-			...createModuleMocks(Auth),
-			// TODO - solve when factories are implemented
-			// @ts-ignore
-			getSchool: {
-				name: "Test-Schule",
-			},
-		};
-		//	mockAuthModule.mockReturnValue(authModuleMock);
-
-		wrapper = mount(Index, {
-			...createComponentMocks({
-				i18n: true,
-			}),
-		});
+		wrapper = mount(Index);
 	});
 
-	it("index", async () => {
+	it("index", () => {
 		expect(wrapper.text()).toContain("Murat");
 	});
 
-	// describe("store", () => {
-	// 	it("should display school name", () => {
-	// 		expect(wrapper.find(".school").text()).toBe("Test-Schule");
-	// 	});
-	// });
+	describe("store", () => {
+		it("should display school name", () => {
+			expect(wrapper.find(".school").text()).toBe("Test-Schule");
+		});
+	});
 });
