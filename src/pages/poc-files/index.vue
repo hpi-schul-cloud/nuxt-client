69fceec8154ae4dede31bdbf7717c1d3189b048e
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-03-11 13:23:43 +0100

    BC-1326 - add basic file name editing

--- src/pages/poc-files/index.vue
+++ src/pages/poc-files/index.vue
@@ -14,8 +14,32 @@
 		<v-list>
 			<h2 class="h4">Uploaded files</h2>
 			<template v-for="f in files">
-				<v-list-item :key="f.id" @click="download(f)">
-					<v-list-item-title>{{ f.name }}</v-list-item-title>
+				<v-list-item :key="f.id">
+					<template v-if="isEditing(f)"
+						><v-text-field
+							v-model="editing[f.id]"
+							type="string"
+							label="Dateiname"
+							@keyup.enter.prevent="commitEdit(f)"
+						></v-text-field
+						><v-btn
+							icon
+							:loading="isLoading"
+							:disabled="isLoading"
+							@click.once.prevent="commitEdit(f)"
+							><v-icon>{{ mdiCheck }}</v-icon></v-btn
+						><v-btn icon @click="endEdit(f)"
+							><v-icon>{{ mdiClose }}</v-icon></v-btn
+						></template
+					>
+					<template v-else>
+						<v-list-item-title @click="download(f)">{{
+							f.name
+						}}</v-list-item-title
+						><v-btn icon @click.prevent="beginEdit(f)"
+							><v-icon>{{ mdiPencil }}</v-icon></v-btn
+						>
+					</template>
 				</v-list-item>
 			</template>
 		</v-list>
@@ -26,6 +50,8 @@
 import FilesPOCModule from "@/store/files-poc";
 import DefaultWireframe from "@/components/templates/DefaultWireframe.vue";
 
+import { mdiPencil, mdiCheck, mdiClose } from "@mdi/js";
+
 export default {
 	components: {
 		DefaultWireframe,
@@ -33,7 +59,11 @@ export default {
 	layout: "defaultVuetify",
 	data() {
 		return {
+			mdiPencil,
+			mdiCheck,
+			mdiClose,
 			file: null,
+			editing: {},
 		};
 	},
 	computed: {
@@ -54,14 +84,30 @@ export default {
 		FilesPOCModule.fetchFiles();
 	},
 	methods: {
-		upload() {
+		async upload() {
 			const { file } = this;
 			if (file) {
-				FilesPOCModule.upload(file);
+				await FilesPOCModule.upload(file);
 			}
 		},
-		download(file) {
-			FilesPOCModule.download(file);
+		async download(file) {
+			await FilesPOCModule.download(file);
+		},
+		beginEdit(file) {
+			this.editing = Object.assign({}, this.editing, { [file.id]: file.name });
+		},
+		endEdit(file) {
+			this.editing = Object.assign({}, this.editing, { [file.id]: null });
+		},
+		async commitEdit(file) {
+			await FilesPOCModule.rename({
+				fileId: file.id,
+				fileName: this.editing[file.id],
+			});
+			this.endEdit(file);
+		},
+		isEditing(file) {
+			return !!this.editing[file.id];
 		},
 	},
 	head() {
