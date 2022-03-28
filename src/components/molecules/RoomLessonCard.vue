0bdccdea1fd8812ead0680875cb9b4ac6272913b
Murat Merdoglu <64781656+murat-merdoglu-hpi@users.noreply.github.com>
2022-03-28 12:00:35 +0200

    Bc 1462 delete lesson (#2102)
    * BC-1462: input field error message corrected
    
    * BC-1462: refactoring modal component
    
    * BC-1462: added more action menu item
    
    * BC-1462: implemented confirm modal view
    
    * BC-1462: implemented store module for delete lesson action
    
    * BC-1462: refactored import lesson modals
    
    * BC-1462: implemented close button unit test
    
    * BC-1462: refactored import course modal
    
    * BC-1462: correction of typo
    
    Co-authored-by: Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>

--- src/components/molecules/RoomLessonCard.vue
+++ src/components/molecules/RoomLessonCard.vue
@@ -43,7 +43,12 @@
 </template>
 
 <script>
-import { mdiPencilOutline, mdiUndoVariant, mdiShareVariant } from "@mdi/js";
+import {
+	mdiPencilOutline,
+	mdiUndoVariant,
+	mdiShareVariant,
+	mdiTrashCanOutline,
+} from "@mdi/js";
 import MoreItemMenu from "./MoreItemMenu";
 import { ImportUserResponseRoleNamesEnum as Roles } from "@/serverApi/v3";
 import EnvConfigModule from "@/store/env-config";
@@ -75,6 +80,7 @@ export default {
 				mdiPencilOutline,
 				mdiUndoVariant,
 				mdiShareVariant,
+				mdiTrashCanOutline,
 			},
 			defaultTitleColor: "--color-secondary",
 		};
@@ -138,6 +144,12 @@ export default {
 						name: this.$t("pages.room.lessonCard.label.share"),
 					});
 				}
+
+				roleBasedMoreActions[Roles.Teacher].push({
+					icon: this.icons.mdiTrashCanOutline,
+					action: () => this.$emit("delete-lesson"),
+					name: this.$t("common.actions.remove"),
+				});
 			}
 
 			if (this.role === Roles.Student) {
