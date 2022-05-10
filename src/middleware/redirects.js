08687cedc9ef9da27bb599e5c5e304900c3ca3f8
Odalys Adam <odalys.adam@dataport.de>
2022-05-10 14:48:31 +0200

    BC-1743 - removed redirects

--- src/middleware/redirects.js
+++ src/middleware/redirects.js
@@ -1,30 +1,5 @@
 export default function (req, res, next) {
-	const redirects = [
-		{
-			from: /\/homework\/?/,
-			to: "/tasks",
-		},
-		{
-			from: /\/homework\/asked\/?/,
-			to: "/tasks",
-		},
-		{
-			from: /\/homework\/private\/?/,
-			to: "/tasks",
-		},
-		{
-			from: /\/homework\/archive\/?/,
-			to: "/tasks",
-		},
-		{
-			from: /\/tasks\/assigned\/?/,
-			to: "/tasks",
-		},
-		{
-			from: /\/tasks\/open\/?/,
-			to: "/tasks",
-		},
-	];
+	const redirects = [];
 
 	const redirect = redirects.find((r) => req.url.match(r.from));
 
