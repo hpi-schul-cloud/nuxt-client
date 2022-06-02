2fecb0cab421a664428a761bba20bdb32835085e
Murat Merdoglu <64781656+murat-merdoglu-hpi@users.noreply.github.com>
2022-06-02 16:08:24 +0200

    feature/BC-1713 - show skeleton on task copy progress (#2187)
    * BC-1713: implementing skeleton
    
    * BC-1713: correction of unit test
    
    * BC-1713: css styling and using dense property
    
    * BC-1713: removed unnecessary comments
    
    Co-authored-by: Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>

--- src/components/molecules/CopyResult.vue
+++ src/components/molecules/CopyResult.vue
@@ -1,13 +1,5 @@
 <template>
 	<div>
-		<v-progress-circular
-			v-if="showSpinner"
-			indeterminate
-			size="72"
-			width="6"
-			color="secondary"
-			class="spinner"
-		></v-progress-circular>
 		<v-treeview
 			:expand-icon="icons.mdiChevronDown"
 			:items="items"
@@ -16,6 +8,7 @@
 			:open="expandedNodes"
 			item-children="elements"
 			item-key="index"
+			dense
 			@keydown.space="onSpacePress"
 		>
 			<template v-slot:prepend="{ item }">
@@ -52,9 +45,6 @@ export default {
 			type: Array,
 			required: true,
 			default: () => [],
-		},
-		showSpinner: {
-			type: Boolean,
 		},
 	},
 	data() {
@@ -133,7 +123,7 @@ export default {
 @import "@styles";
 .treeview-item-failure {
 	color: var(--color-danger-dark);
-	white-space: pre-line;
+	white-space: normal;
 }
 .not-finished {
 	color: var(--color-danger-dark);
@@ -143,12 +133,5 @@ export default {
 }
 .partial {
 	color: var(--color-accent);
-}
-.spinner {
-	position: absolute;
-	right: 0;
-	left: 0;
-	margin-right: auto;
-	margin-left: auto;
 }
 </style>
