6fe5eb5528d10af717f86b593ad498b7bc3eb90e
Murat Merdoglu <murat.merdoglu@hpi.de>
2022-06-17 14:57:22 +0200

    correction of default object

--- src/store/room.ts
+++ src/store/room.ts
@@ -342,7 +342,7 @@ export default class RoomModule extends VuexModule {
 				courseId
 			);
 
-			this.setCopyResult(copyResult.data || {});
+			this.setCopyResult(copyResult.data);
 			this.setLoading(false);
 		} catch (error: any) {
 			this.setError(error);
