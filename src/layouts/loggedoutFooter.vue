9651cb82ba8405f520d5fdfe59cb495c026a138d
Odalys Adam <odalys.adam@dataport.de>
2022-06-29 12:49:45 +0200

    BC-168 - applied footer out of viewport logic

--- src/layouts/loggedoutFooter.vue
+++ src/layouts/loggedoutFooter.vue
@@ -1,14 +1,16 @@
 <template>
-	<div>
-		<navigation-bar
-			:buttons="true"
-			:img="require('@assets/img/logo/logo-image-mono.svg')"
-			:links="navbarItems"
-		/>
+	<div class="page">
+		<div class="topbar">
+			<navigation-bar
+				:buttons="true"
+				:img="require('@assets/img/logo/logo-image-mono.svg')"
+				:links="navbarItems"
+			/>
+		</div>
 		<div :class="isMobile ? 'small-wrapper' : 'wrapper'">
 			<Nuxt />
 		</div>
-		<the-footer />
+		<the-footer class="footer" />
 	</div>
 </template>
 
@@ -46,13 +48,35 @@ export default {
 <style lang="scss" scoped>
 @import "@styles";
 
+.page {
+	display: grid;
+	grid-template-areas:
+		"side top"
+		"side content"
+		"side footer";
+	grid-template-rows: auto 1fr auto;
+	grid-template-columns: 0 1fr;
+	max-width: 100%;
+	min-height: 100vh;
+}
+
+.topbar {
+	grid-area: top;
+}
+
+.footer {
+	grid-area: footer;
+}
+
 .small-wrapper {
 	grid-area: content;
+	min-height: 100vh;
 	padding: var(--space-md);
 }
 
 .wrapper {
 	grid-area: content;
+	min-height: calc(100vh - var(--topbar-height));
 	padding-right: calc(15 * var(--border-width));
 	padding-left: calc(15 * var(--border-width));
 	margin: var(--space-lg) auto;
