3cb8e634ba5413ea59562581d2680cb518cafc5e
Odalys Adam <odalys.adam@dataport.de>
2022-06-29 15:43:35 +0200

    BC-168 - adjusted imprint & error page layout

--- src/themes/thr/components/templates/impressum.vue
+++ src/themes/thr/components/templates/impressum.vue
@@ -1,43 +1,45 @@
 <template>
 	<div class="impressum-container">
-		<h1 class="h2">Impressum Thüringer Schulcloud</h1>
-		<h2 class="h4">
-			Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien
-			(Thillm)
-		</h2>
-		<p>
-			Heinrich-Heine-Allee 2-4 <br />
-			99438 Bad Berka <br />
-			vertreten durch den Direktor Dr. Andreas Jantowski <br />
-			Fon: +49 (36458) 56 0 <br />
-			Fax: +49 (36458) 56 300 <br />
-			E-Mail: Institut[at]thillm.de
-		</p>
-		<p>
-			<i>verantwortlich:</i> <br />
-			Jörg Becker <br />
-			Arbeitsbereichsleiter <br />
-			Arbeitsbereich 4 | Medien und Informationstechnologie <br />
-			Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien
-			(Thillm) <br />
-			Heinrich-Heine-Allee 2-4 <br />
-			99438 Bad Berka<br />
-			Fon: +49 (36458) 56 319 <br />
-			Fax: +49 (36458) 56 300 <br />
-			E-Mail: joerg.becker[at]thillm.de
-		</p>
+		<div class="impressum-max-width">
+			<h1 class="h2">Impressum Thüringer Schulcloud</h1>
+			<h2 class="h4">
+				Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien
+				(Thillm)
+			</h2>
+			<p>
+				Heinrich-Heine-Allee 2-4 <br />
+				99438 Bad Berka <br />
+				vertreten durch den Direktor Dr. Andreas Jantowski <br />
+				Fon: +49 (36458) 56 0 <br />
+				Fax: +49 (36458) 56 300 <br />
+				E-Mail: Institut[at]thillm.de
+			</p>
+			<p>
+				<i>verantwortlich:</i> <br />
+				Jörg Becker <br />
+				Arbeitsbereichsleiter <br />
+				Arbeitsbereich 4 | Medien und Informationstechnologie <br />
+				Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien
+				(Thillm) <br />
+				Heinrich-Heine-Allee 2-4 <br />
+				99438 Bad Berka<br />
+				Fon: +49 (36458) 56 319 <br />
+				Fax: +49 (36458) 56 300 <br />
+				E-Mail: joerg.becker[at]thillm.de
+			</p>
 
-		<h2 class="h4">Datenschutzbeauftrager</h2>
-		<p>
-			Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien
-			(Thillm) <br />
-			Datenschutzbeauftragter <br />
-			Heinrich-Heine-Allee 2-4 <br />
-			99438 Bad Berka <br />
-			Fon: +49 (36458) 56 288 <br />
-			Fax: +49 (36458) 56 300 <br />
-			E-Mail: datenschutzbeauftragter[at]thillm.de
-		</p>
+			<h2 class="h4">Datenschutzbeauftrager</h2>
+			<p>
+				Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien
+				(Thillm) <br />
+				Datenschutzbeauftragter <br />
+				Heinrich-Heine-Allee 2-4 <br />
+				99438 Bad Berka <br />
+				Fon: +49 (36458) 56 288 <br />
+				Fax: +49 (36458) 56 300 <br />
+				E-Mail: datenschutzbeauftragter[at]thillm.de
+			</p>
+		</div>
 	</div>
 </template>
 
@@ -47,4 +49,13 @@ export default {};
 
 <style lang="scss" scoped>
 @import "@styles";
+
+.impressum-container {
+	display: flex;
+	justify-content: center;
+}
+
+.impressum-max-width {
+	max-width: 720px;
+}
 </style>
