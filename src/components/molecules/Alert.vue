313ba71ed8722ac74b52e13a3a26627436323790
Oliver Happe <oliver.happe@dataport.de>
2022-07-27 16:15:19 +0200

    fix stylelint errors

--- src/components/molecules/Alert.vue
+++ src/components/molecules/Alert.vue
@@ -1,81 +1,81 @@
 <template>
-	<div :class="{ 'alert_wrapper-mobile': isMobile, alert_wrapper: !isMobile }">
-		<v-alert
-			v-model="show"
-			:type="status"
-			text
-			dismissible
-			class="alert"
-			:icon="icon"
-			max-width="400"
-			min-width="200"
-			:transition="
+  <div :class="{ 'alert_wrapper-mobile': isMobile, alert_wrapper: !isMobile }">
+    <v-alert
+        v-model="show"
+        :icon="icon"
+        :transition="
 				isMobile ? 'scale-transition' : 'scroll-x-reverse-transition'
 			"
-		>
-			<div class="alert_accent" :class="[status]"></div>
-			<div class="alert_text mr-2">
-				{{ text }}
-			</div>
-		</v-alert>
-	</div>
+        :type="status"
+        class="alert"
+        dismissible
+        max-width="400"
+        min-width="200"
+        text
+    >
+      <div :class="[status]" class="alert_accent"></div>
+      <div class="alert_text mr-2">
+        {{ text }}
+      </div>
+    </v-alert>
+  </div>
 </template>
 
 <script>
 import { notifierModule } from "@/store";
-import { mdiClose, mdiAlert, mdiCheckCircle, mdiInformation } from "@mdi/js";
-export default {
-	data() {
-		return {
-			show: false,
-			mdiClose,
-			mdiAlert,
-			mdiCheckCircle,
-			mdiInformation,
-		};
-	},
-	computed: {
-		notifierData() {
-			return notifierModule.getNotifier;
-		},
-		status() {
-			return this.notifierData?.status || "info";
-		},
-		isMobile() {
-			return this.$mq === "mobile";
-		},
-		text() {
-			return (
-				this.notifierData?.text || `Ich bin ein Alert mit sehr langer message `
-			);
-		},
-		icon() {
-			if (this.status === "success") return mdiCheckCircle;
-			if (this.status === "warning") return mdiAlert;
-			if (this.status === "danger") return mdiAlert;
-			if (this.status === "info") return mdiInformation;
-			return undefined;
-		},
-	},
-	watch: {
-		notifierData() {
-			this.show = true;
+import { mdiAlert, mdiCheckCircle, mdiClose, mdiInformation } from "@mdi/js";
 
-			// todo improve timeout to cancel old one
-			setTimeout(() => {
-				this.show = false;
-			}, this.timeout || 5000);
-		},
-	},
-	created() {
-		setTimeout(() => {
-			notifierModule.show({
-				text: "Test aus created",
-				status: "success",
-				timeout: 500,
-			});
-		}, 500);
-	},
+export default {
+  data() {
+    return {
+      show: false,
+      mdiClose,
+      mdiAlert,
+      mdiCheckCircle,
+      mdiInformation,
+    };
+  },
+  computed: {
+    notifierData() {
+      return notifierModule.getNotifier;
+    },
+    status() {
+      return this.notifierData?.status || "info";
+    },
+    isMobile() {
+      return this.$mq === "mobile";
+    },
+    text() {
+      return (
+          this.notifierData?.text || `Ich bin ein Alert mit sehr langer message `
+      );
+    },
+    icon() {
+      if (this.status === "success") return mdiCheckCircle;
+      if (this.status === "warning") return mdiAlert;
+      if (this.status === "danger") return mdiAlert;
+      if (this.status === "info") return mdiInformation;
+      return undefined;
+    },
+  },
+  watch: {
+    notifierData() {
+      this.show = true;
+
+      // todo improve timeout to cancel old one
+      setTimeout(() => {
+        this.show = false;
+      }, this.timeout || 5000);
+    },
+  },
+  created() {
+    setTimeout(() => {
+      notifierModule.show({
+        text: "Test aus created",
+        status: "success",
+      });
+    }, 500);
+  },
 };
 </script>
 
@@ -83,49 +83,50 @@ export default {
 @import "@variables";
 
 .alert_wrapper {
-	position: absolute;
-	top: 0;
-	right: 0;
-	overflow: visible;
-	z-index: 50; // TODO where are z-index vars?
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: var(--layer-tooltip);
+  overflow: visible;
 }
+
 .alert_wrapper-mobile {
-	position: fixed;
-	bottom: 5vh;
-	right: 0;
-	left: 0;
-	overflow: visible;
-	z-index: 50;
+  position: fixed;
+  right: 0;
+  bottom: 5vh;
+  left: 0;
+  z-index: var(--layer-tooltip);
+  overflow: visible;
 }
 
 .alert {
-	margin: 0 12px 0 12px;
-	overflow: hidden;
-	background-color: white !important;
+  margin: 0 var(--space-sm);
+  overflow: hidden;
+  background-color: var(--color-white) !important;
 }
 
 .alert_text {
-	color: var(--color-black);
+  color: var(--color-black);
 }
 
 .alert_accent {
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	width: 5px;
-	background-color: transparent;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 5px;
+  background-color: transparent;
 
-	&.success {
-		background-color: var(--color-success-dark);
-	}
+  &.success {
+    background-color: var(--color-success-dark);
+  }
 
-	&.warning {
-		background-color: var(--color-warning-dark);
-	}
+  &.warning {
+    background-color: var(--color-warning-dark);
+  }
 
-	&.danger {
-		background-color: var(--color-danger-dark);
-	}
+  &.danger {
+    background-color: var(--color-danger-dark);
+  }
 }
 </style>
