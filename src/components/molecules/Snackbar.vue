6faee4b977521091f1166045ba1f193be82cc789
Christian Darsow <christian.darsow@dataport.de>
2022-07-27 12:23:18 +0200

    WIP styling notification

--- src/components/molecules/Snackbar.vue
+++ src/components/molecules/Snackbar.vue
@@ -12,9 +12,13 @@
 			:left="position.includes('left')"
 			class="mt-12"
 			max-width="200"
+			min-width="320"
 		>
-			{{ notifierData.text }}
-
+			<div class="snackbar_content">
+				<div class="extra-border"></div>
+				<v-icon color="var(--color-success-dark)">{{ mdiCheckCircle }}</v-icon>
+				Aufgabe erfolgreich kopiert
+			</div>
 			<template #action="{ attrs }">
 				<v-btn
 					:color="closeButtonColor"
@@ -22,7 +26,7 @@
 					v-bind="attrs"
 					@click="show = false"
 				>
-					<v-icon color="var(--color-white)">{{ mdiClose }}</v-icon>
+					<v-icon color="var(--color-black)">{{ mdiClose }}</v-icon>
 				</v-btn>
 			</template>
 		</v-snackbar>
@@ -31,12 +35,14 @@
 
 <script>
 import { notifierModule } from "@/store";
-import { mdiClose } from "@mdi/js";
+import { mdiClose, mdiAlert, mdiCheckCircle } from "@mdi/js";
 export default {
 	data() {
 		return {
-			show: false,
+			show: true,
 			mdiClose,
+			mdiAlert,
+			mdiCheckCircle,
 		};
 	},
 	computed: {
@@ -51,14 +57,15 @@ export default {
 				danger: "var(--color-danger)",
 				info: "var(--color-info)",
 			};
-			return statusObj[this.notifierData.status] || "var(--color-primary)";
+			//return statusObj[this.notifierData.status] || "var(--color-primary)";
+			return statusObj[this.notifierData.status] || "warning";
 		},
 		position() {
 			if (this.$mq === "mobile") return ["bottom", "center"];
 			return this.notifierData.position || ["top", "right"];
 		},
 		timeout() {
-			return this.notifierData.timeout || 5000;
+			return this.notifierData.timeout || 5000 * 1000;
 		},
 		vertical() {
 			return this.notifierData.vertical || false;
@@ -81,4 +88,19 @@ export default {
 <style lang="scss" scoped>
 @import "~vuetify/src/styles/styles.sass";
 @import "@variables";
+
+.snackbar_content {
+	color: var(--color-black);
+
+	.extra-border {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 5px;
+		height: 100%;
+		background-color: var(--color-success-dark);
+		border-top-left-radius: 4px;
+		border-bottom-left-radius: 4px;
+	}
+}
 </style>
