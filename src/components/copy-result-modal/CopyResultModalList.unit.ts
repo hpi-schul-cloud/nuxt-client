b2cc5963e11b5c083548f3222b3ca08cf8a6fc80
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-08-16 16:05:09 +0200

    BC-2161 add unit test for copy result modal

---
+++ src/components/copy-result-modal/CopyResultModalList.unit.ts
@@ -0,0 +1,68 @@
+import { mount } from "@vue/test-utils";
+import CopyResultModalList from "./CopyResultModalList.vue";
+import CopyResultModalListItem from "./CopyResultModalListItem.vue";
+import { CopyResultItem } from "./types/CopyResultItem";
+import { CopyApiResponseTypeEnum } from "@/serverApi/v3";
+import createComponentMocks from "@@/tests/test-utils/componentMocks";
+
+const mockItems: CopyResultItem[] = [
+	{
+		type: CopyApiResponseTypeEnum.Task,
+		title: "Task Title",
+		elementId: "mockId",
+		elements: [
+			{
+				title: "Geogebra Element Title",
+				type: CopyApiResponseTypeEnum.LessonContentGeogebra,
+			},
+		],
+		url: "/tasks/taskId/topics/elementId/edit?returnUrl=rooms/courseId",
+	},
+	{
+		type: CopyApiResponseTypeEnum.Lesson,
+		title: "Lesson Title",
+		elementId: "mockId",
+		elements: [
+			{
+				title: "Geogebra Element Title",
+				type: CopyApiResponseTypeEnum.LessonContentGeogebra,
+			},
+		],
+		url: "/courses/courseId/topics/elementId/edit?returnUrl=rooms/taskId",
+	},
+];
+
+const getWrapper = (opts = {}) => {
+	return mount<any>(CopyResultModalList, {
+		...createComponentMocks({
+			i18n: true,
+		}),
+		stubs: {
+			CopyResultModalListItem: true,
+		},
+		...opts,
+	});
+};
+
+describe("@components/copy-result-modal/CopyResultModalList", () => {
+	it("Should render component", () => {
+		const wrapper = getWrapper({ propsData: { items: mockItems } });
+
+		expect(wrapper.findComponent(CopyResultModalList).exists()).toBe(true);
+	});
+
+	it("Should render the correct number of items", () => {
+		const wrapper = getWrapper({ propsData: { items: mockItems } });
+
+		expect(wrapper.findAllComponents(CopyResultModalListItem).length).toBe(2);
+	});
+
+	it("Should pass item props to the child components", () => {
+		const wrapper = getWrapper({ propsData: { items: mockItems } });
+
+		const itemWrappers = wrapper.findAllComponents(CopyResultModalListItem);
+
+		expect(itemWrappers.at(0).props()).toEqual({ item: mockItems[0] });
+		expect(itemWrappers.at(1).props()).toEqual({ item: mockItems[1] });
+	});
+});
