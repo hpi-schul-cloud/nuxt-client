9d2fd03d1bcbda62ccee24e4e4e70f18782e7b0a
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-08-18 10:55:42 +0200

    BC-2094 - add legacy dialog component

---
+++ vue3_poc/src/components/vCustomDialog.vue
@@ -0,0 +1,144 @@
+<template>
+	<v-dialog
+		ref="vDialog"
+		:value="isOpen"
+		:max-width="size"
+		@click:outside="$emit('dialog-closed', false)"
+		@keydown.esc="$emit('dialog-closed', false)"
+	>
+		<v-card :ripple="false">
+			<v-card-title>
+				<slot name="title"></slot>
+			</v-card-title>
+			<v-card-text>
+				<slot name="content"></slot>
+			</v-card-text>
+			<v-card-actions v-if="hasButtons">
+				<v-spacer></v-spacer>
+
+				<div class="button-section button-left">
+					<v-btn
+						v-if="checkButtons('back')"
+						data-testId="dialog-back"
+						depressed
+						outlined
+						@click="$emit('back')"
+					>
+						Back
+					</v-btn>
+					<v-btn
+						v-if="checkButtons('edit')"
+						data-testId="dialog-edit"
+						depressed
+						@click="$emit('dialog-edit')"
+					>
+						Edit
+					</v-btn>
+				</div>
+				<div class="button-section button-right">
+					<v-btn
+						v-if="checkButtons('cancel')"
+						data-testId="dialog-cancel"
+						class="dialog-closed"
+						depressed
+						text
+						@click="$emit('dialog-closed', false)"
+					>
+						Cancel
+					</v-btn>
+					<v-btn
+						v-if="checkButtons('confirm')"
+						data-testId="dialog-confirm"
+						class="dialog-confirmed px-6"
+						color="primary"
+						depressed
+						:disabled="confirmBtnDisabled"
+						@click="confirmDialog"
+					>
+						Confirm
+					</v-btn>
+					<v-btn
+						v-if="checkButtons('close')"
+						data-testId="dialog-close"
+						depressed
+						outlined
+						@click="$emit('dialog-closed', false)"
+					>
+						Close
+					</v-btn>
+					<v-btn
+						v-if="checkButtons('next')"
+						data-testId="dialog-next"
+						color="primary"
+						depressed
+						:disabled="nextBtnDisabled"
+						@click="$emit('next')"
+					>
+						Next
+					</v-btn>
+				</div>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+	model: {
+		prop: "isOpen",
+		event: "dialog-closed",
+	},
+	props: {
+		isOpen: {
+			type: Boolean,
+			required: true,
+		},
+		size: {
+			type: Number,
+			default: 480,
+		},
+		hasButtons: {
+			type: Boolean,
+		},
+		confirmBtnTitleKey: {
+			type: String,
+			default: "common.actions.confirm",
+		},
+		confirmBtnDisabled: {
+			type: Boolean,
+		},
+		nextBtnTitleKey: {
+			type: String,
+			default: "common.actions.continue",
+		},
+		nextBtnDisabled: {
+			type: Boolean,
+		},
+		buttons: {
+			type: Array,
+			default: () => ["cancel", "confirm"],
+		},
+	},
+	methods: {
+		confirmDialog() {
+			this.$emit("dialog-confirmed");
+			this.$emit("dialog-closed", false);
+		},
+		checkButtons(buttonName) {
+			return this.buttons.some((button) => button == buttonName);
+		},
+	},
+};
+</script>
+<style scoped>
+.button-left {
+	width: 25%;
+	text-align: left;
+}
+
+.button-right {
+	display: inline-block;
+	width: 75%;
+	text-align: right;
+}
+</style>
--- vue3_poc/src/pages/HomePage.vue
+++ vue3_poc/src/pages/HomePage.vue
@@ -24,15 +24,30 @@
 				</v-btn>
 			</div>
 			<br />
+			<div>
+				<v-btn @click="dialogIsOpen = true">Open Dialog</v-btn>
+			</div>
 		</div>
 	</div>
+	<v-custom-dialog
+		v-model="dialogIsOpen"
+		hasButtons
+		:buttons="['close']"
+		@dialog-closed="onCloseDialog"
+	>
+		<template v-slot:title><h4>Dialog Title</h4></template>
+	</v-custom-dialog>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 import { useCounterStore } from "@/store/counter";
 import { computed } from "@vue/reactivity";
+import vCustomDialog from "@/components/vCustomDialog.vue";
 export default {
+	components: {
+		vCustomDialog,
+	},
 	setup() {
 		const store = useCounterStore();
 		const persistentValue = "Composition api page";
@@ -47,6 +62,11 @@ export default {
 			store.increment();
 		};
 
+		const dialogIsOpen = ref(true);
+		const onCloseDialog = () => {
+			dialogIsOpen.value = false;
+		};
+
 		return {
 			persistentValue,
 			reactiveValue,
@@ -54,6 +74,8 @@ export default {
 			counter,
 			storeCounter,
 			onIncreaseStoreCounter,
+			dialogIsOpen,
+			onCloseDialog,
 		};
 	},
 };
