6f3a1d39337b5041ca6a524be42061c2372d8060
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-08-24 11:29:02 +0200

    BC-2358 - add store provider

--- src/plugins/store-accessor.ts
+++ src/plugins/store-accessor.ts
@@ -9,6 +9,7 @@ import FilesPOCModule from "@/store/files-poc";
 import FinishedTaskModule from "@/store/finished-tasks";
 import ImportUsersModule from "@/store/import-users";
 import NewsModule from "@/store/news";
+import NotifierModule from "@/store/notifier";
 import RoomModule from "@/store/room";
 import RoomsModule from "@/store/rooms";
 import SchoolsModule from "@/store/schools";
@@ -31,6 +32,7 @@ export default () => {
 		provide("finishedTaskModule", getModule(FinishedTaskModule, store));
 		provide("importUsersModule", getModule(ImportUsersModule, store));
 		provide("newsModule", getModule(NewsModule, store));
+		provide("notifierModule", getModule(NotifierModule, store));
 		provide("roomModule", getModule(RoomModule, store));
 		provide("roomsModule", getModule(RoomsModule, store));
 		provide("schoolsModule", getModule(SchoolsModule, store));
