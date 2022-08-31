a7876b69e9b62e9aa463889d1a672cd9eed4c4f6
Christian Darsow <christian.darsow@dataport.de>
2022-08-31 11:48:12 +0200

    Add editor component

--- src/pages/POCEditor.page.vue
+++ src/pages/POCEditor.page.vue
@@ -1,16 +1,35 @@
 <template>
 	<div>
 		<default-wireframe headline="POC - Editor CKEditor 5" :full-width="false">
-			add some content
+			<ckeditor
+				v-model="editorData"
+				:editor="editor"
+				:config="editorConfig"
+			></ckeditor>
 		</default-wireframe>
 	</div>
 </template>
 <script>
+import DefaultWireframe from "@/components/templates/DefaultWireframe.vue";
+import CKEditor from "@ckeditor/ckeditor5-vue2";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+// https://ckeditor.com/docs/ckeditor5/latest/installation/getting-started/frameworks/vuejs-v2.html#using-the-component-locally
+
 export default {
-	components: {},
+	components: {
+		DefaultWireframe,
+		ckeditor: CKEditor.component,
+	},
 	layout: "defaultVuetify",
 	data() {
-		return {};
+		return {
+			editor: ClassicEditor,
+			editorData: "<p>Content of the editor.</p>",
+			editorConfig: {
+				// The configuration of the editor.
+			},
+		};
 	},
 	mounted() {},
 	methods: {},
