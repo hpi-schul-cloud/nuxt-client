8ee85193e1f1e388a65a2755ff4273665d22a492
Christian Darsow <christian.darsow@dataport.de>
2022-09-09 13:56:52 +0200

    Update test content

--- vue3_poc/src/components/CK5ProofOfConcept.vue
+++ vue3_poc/src/components/CK5ProofOfConcept.vue
@@ -28,9 +28,70 @@ export default defineComponent({
 	data() {
 		return {
 			editor: Editor,
-			editorData: `<h2>Some headliner</h2>
-			<iframe src="https://www.google.com" title="I am dangourous">I will no longer be an iframe</iframe>
-			<p><math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mn>9</mn></msqrt></math></p>`,
+			editorData: `<iframe src="https://www.google.com" style="width: 300px; height: 100px">This iframe should be converted into safe text</iframe>
+<h2>h1</h2>
+<h3>h2</h3>
+<p>h3</p>
+<p>some
+	<strong>bold</strong>
+	<i>italic</i>
+	<u>underline</u>
+	<s>strike-through</s>
+	<sub>sub</sub> and
+	<sup>sup</sup>
+</p>
+<p>checking span
+	<span style="color:#F44336">color</span> and
+	<span style="background-color:#9C27B0">background color</span>
+</p>
+<blockquote>
+	<p>and some blockquote</p>
+</blockquote>
+<p>come code comes here:
+	<code>while (editor) { alert('does it render?') }</code>
+</p>
+<p>Line:<hr />
+</p>
+<p>
+	<ol>
+		<li>ordered list</li>
+	</ol>
+	<ul>
+		<li>unordered list</li>
+	</ul>
+</p>
+<p>symbols $₹₻≡⇒</p>
+<p>Some <a href="http://www.google.com">Link</a></p>
+<p>
+	<figure class="table">
+<table>
+	<tbody>
+	<tr>
+			<th>Col 1</th>
+			<th>Col 2</th>
+			<th>Col 3</th>
+		</tr>
+		<tr>
+			<td>table td 1 1</td>
+			<td>table td 1 2</td>
+			<td rowspan="2">merged table rows</td>
+		</tr>
+		<tr>
+			<td>table td 2 1</td>
+			<td>table td 2 2</td>
+		</tr>
+	</tbody>
+</table>
+	</figure>
+</p>
+<p>Math formula: <math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mn>9</mn></msqrt></math></p>
+<p>IMAGE:
+	<figure class="image"><img src="https://icatcare.org/app/uploads/2018/10/What-is-a-cat.png" alt="alt image attr"></figure>
+</p>
+<p>VIDEO:</p>
+	<video src="http://127.0.0.1:4000/video.mp4" controls="true" controlslist="nodownload"></video>
+<p>AUDIO:</p>
+<audio src="http://192.168.1.136/musik.mp3" controls="true" controlslist="nodownload"></audio>`,
 			editorConfig: {
 				language: "de",
 				//language: "en",
