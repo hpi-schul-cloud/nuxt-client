42296452abe662e9336416e84f4e3dbfa7f949e7
Christian Darsow <christian.darsow@dataport.de>
2022-09-19 15:45:28 +0200

    Add new CK build

---
+++ src/static/scripts/ckeditor5/translations/tt.js
@@ -0,0 +1,1 @@
+!function(t){const n=t.tt=t.tt||{};n.dictionary=Object.assign(n.dictionary||{},{"%0 of %1":"",Bold:"Калын",Cancel:"",Code:"Код",Italic:"",Redo:"Кабатла",Save:"Сакла","Show more items":"",Subscript:"",Superscript:"",Underline:"",Undo:""}),n.getPluralForm=function(t){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
