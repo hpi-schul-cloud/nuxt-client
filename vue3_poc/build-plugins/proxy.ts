70473a9f1b9dc3668c84cdcd04639f18880e5a2e
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-09-24 21:51:50 +0200

    add server api proxy

--- vue3_poc/build-plugins/proxy.ts
+++ vue3_poc/build-plugins/proxy.ts
@@ -6,8 +6,17 @@ const isLegacyClient = (path: string): boolean => {
 	return path.startsWith("/login") || path.startsWith("/logout");
 };
 
+const isServer = (path: string): boolean => {
+	return path.startsWith("/api/v");
+};
+
 const legacyClientProxy = createProxyMiddleware({
 	target: "http://localhost:3100",
+	changeOrigin: true,
+});
+
+const serverProxy = createProxyMiddleware({
+	target: "http://localhost:3030",
 	changeOrigin: true,
 });
 
@@ -15,8 +24,13 @@ const proxyPlugin = (): Plugin => ({
 	name: "dev-server-proxy",
 	configureServer(server) {
 		server.middlewares.use((req, res, next) => {
-			if (req.originalUrl && isLegacyClient(req.originalUrl)) {
-				// console.log("--- legacyUrl: ", req.originalUrl);
+			const path = req.originalUrl;
+
+			if (path && isServer(path)) {
+				// console.log("--- serverUrl: ", path);
+				serverProxy(req as Request, res as Response, next);
+			} else if (path && isLegacyClient(path)) {
+				// console.log("--- legacyUrl: ", path);
 				legacyClientProxy(req as Request, res as Response, next);
 			} else {
 				next();
