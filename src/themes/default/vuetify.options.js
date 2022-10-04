717cee0ad45d7e43b4de15f765abc0d2848b8171
Christian Darsow <christian.darsow@dataport.de>
2022-10-04 12:02:09 +0200

    Set colors in default theme

--- src/themes/default/vuetify.options.js
+++ src/themes/default/vuetify.options.js
@@ -5,7 +5,10 @@ const custom = {
 	theme: {
 		themes: {
 			light: {
-				primary: "#9e292b",
+				primary: {
+					base: "#9e292b",
+					darken1: "#800416",
+				},
 				//primary: {
 				// base: string
 				// lighten5: string
@@ -18,10 +21,41 @@ const custom = {
 				// darken3: string
 				// darken4: string
 				//},
-				secondary: "#54616e",
-				accent: "#E98404",
-				black: "#1b1b1b",
-				gray: "#aaaaaa",
+				secondary: {
+					base: "#54616e",
+					light1: "#8A9199",
+					darken1: "#3A424B",
+				},
+				black: {
+					base: "#1b1b1b",
+				},
+				white: {
+					base: "#ffffff",
+				},
+				gray: {
+					base: "#aaaaaa",
+					lighten1: "#eeeeee",
+					lighten2: "#E0E0E0",
+					darken1: "#444444",
+				},
+				info: {
+					base: "#0A7AC9",
+					darken1: "#085C96",
+				},
+				success: {
+					base: "#13BA98",
+					darken1: "#0E8C71",
+				},
+				warning: {
+					base: "#FF8311",
+				},
+				danger: {
+					base: "#ff1134",
+					darken1: "#BF0D26",
+				},
+				accent: {
+					base: "#E98404",
+				},
 			},
 		},
 	},
