4dfc294d674416e50359f0c1a01edc5626e12da8
Christian Darsow <christian.darsow@dataport.de>
2022-10-04 08:25:47 +0200

    Add colors to default theme

--- src/themes/default/vuetify.options.js
+++ src/themes/default/vuetify.options.js
@@ -6,8 +6,22 @@ const custom = {
 		themes: {
 			light: {
 				primary: "#9e292b",
+				//primary: {
+				// base: string
+				// lighten5: string
+				// lighten4: string
+				// lighten3: string
+				// lighten2: string
+				// lighten1: string
+				// darken1: string
+				// darken2: string
+				// darken3: string
+				// darken4: string
+				//},
 				secondary: "#54616e",
 				accent: "#E98404",
+				black: "#1b1b1b",
+				gray: "#aaaaaa",
 			},
 		},
 	},
