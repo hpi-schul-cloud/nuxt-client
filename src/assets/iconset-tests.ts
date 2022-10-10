c72eea63ffa07371e989178fea6e29c3d56bc04a
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2022-10-10 14:37:02 +0200

    remove unintended file

--- src/assets/iconset-tests.ts
+++
@@ -1,8 +0,0 @@
-import { h } from "vue";
-import type { IconSet, IconAliases, IconProps } from "vuetify";
-
-const custom: IconSet = {
-	component: (props: IconProps) => h(),
-};
-
-export { aliases, custom };
