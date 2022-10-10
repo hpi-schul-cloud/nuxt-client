12046c15782daf5a308741d299788487637d9eb9
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2022-10-10 12:28:54 +0200

    add trivy vulnerability scanning

---
+++ src/assets/iconset-tests.ts
@@ -0,0 +1,8 @@
+import { h } from "vue";
+import type { IconSet, IconAliases, IconProps } from "vuetify";
+
+const custom: IconSet = {
+	component: (props: IconProps) => h(),
+};
+
+export { aliases, custom };
