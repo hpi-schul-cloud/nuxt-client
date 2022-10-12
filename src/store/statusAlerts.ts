d1642803cd73a591f7bc03aa8fc8fa41549cf23e
Christian Darsow <christian.darsow@dataport.de>
2022-10-12 19:25:31 +0200

    Remove test data from store

--- src/store/statusAlerts.ts
+++ src/store/statusAlerts.ts
@@ -50,41 +50,6 @@ export default class StatusAlertsModule extends VuexModule {
 			this.setStatus("pending");
 			const response = await $axios.$get("/v1/alert");
 			this.setStatusAlerts(response as StatusAlert[]);
-			this.setStatusAlerts([
-				{
-					title: "Important info",
-					text: "Description of the alert",
-					status: "info",
-					origin: {
-						page: "status",
-						message_id: 1,
-					},
-					timestamp: "2022-08-25 10:33:38",
-					url: "https://status.test.cloud",
-				},
-				{
-					title: "Problem resolved",
-					text: "Description of the alert 2",
-					status: "done",
-					origin: {
-						page: "status",
-						message_id: 2,
-					},
-					timestamp: "2022-08-25 10:33:38",
-					url: "https://status.test.cloud",
-				},
-				{
-					title: "Critical problem in progress info2",
-					text: "Description of the alert 3",
-					status: "danger",
-					origin: {
-						page: "status",
-						message_id: 3,
-					},
-					timestamp: "2022-08-25 10:33:38",
-					url: "https://status.test.cloud",
-				},
-			]);
 			this.setStatus("completed");
 		} catch (error) {
 			this.setBusinessError(error as BusinessError);
