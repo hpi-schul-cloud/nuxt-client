488031b909965c28f95fad73dd6ff1c96d098cca
Murat Merdoglu <murat.merdoglu@hpi.de>
2022-10-14 14:55:12 +0200

    chore: resolve conflicts

--- src/pages/RoomDetails.page.vue
+++ src/pages/RoomDetails.page.vue
@@ -120,8 +120,7 @@ import CopyResultModal from "@components/copy-result-modal/CopyResultModal";
 import ImportLessonModal from "@components/molecules/ImportLessonModal";
 import MoreItemMenu from "@components/molecules/MoreItemMenu";
 import vCustomDialog from "@components/organisms/vCustomDialog.vue";
-import ShareCourseModal from "@components/share-course/ShareModal.vue";
-import ShareModal from "@components/share-modal/ShareModal.vue";
+import ShareModal from "@components/share-course/ShareModal.vue";
 import DefaultWireframe from "@components/templates/DefaultWireframe";
 import RoomDashboard from "@components/templates/RoomDashboard";
 import {
