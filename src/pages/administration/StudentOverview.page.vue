fbee0eba47b4d3467deacc8f8f0777f054377b42
Odalys Adam <odalys.adam@dataport.de>
2022-10-17 22:37:46 +0200

    remove base btn from admin tables

--- src/pages/administration/StudentOverview.page.vue
+++ src/pages/administration/StudentOverview.page.vue
@@ -91,19 +91,18 @@
 				</span>
 			</template>
 			<template #datacolumn-_id="{ data, selected, highlighted }">
-				<base-button
+				<v-btn
+					icon
 					:class="{
 						'action-button': true,
 						'row-selected': selected,
 						'row-highlighted': highlighted,
 					}"
-					design="text icon"
-					size="small"
 					:to="`/administration/students/${data}/edit`"
 					data-testid="edit_student_button"
 				>
-					<base-icon source="material" icon="edit" />
-				</base-button>
+					<v-icon size="20">{{ mdiPencil }}</v-icon>
+				</v-btn>
 			</template>
 		</backend-data-table>
 		<admin-table-legend
@@ -127,7 +126,7 @@ import print from "@mixins/print";
 import UserHasPermission from "@/mixins/UserHasPermission";
 import { printDateFromDeUTC, printDate } from "@plugins/datetime";
 import ProgressModal from "@components/molecules/ProgressModal";
-import { mdiPlus, mdiAccountPlus, mdiCloudDownload } from "@mdi/js";
+import { mdiPlus, mdiAccountPlus, mdiCloudDownload, mdiPencil } from "@mdi/js";
 
 export default {
 	components: {
@@ -148,7 +147,8 @@ export default {
 			mdiPlus,
 			mdiAccountPlus,
 			mdiCloudDownload,
-			something: [],
+			mdiPencil,
+			something: [], // ?? xD
 			currentFilterQuery: this.$uiState.get(
 				"filter",
 				"pages.administration.students.index"
@@ -612,6 +612,10 @@ export default {
 
 <style lang="scss" scoped>
 @import "@styles";
+
+::v-deep .row-highlighted.theme--light.v-btn:hover::before {
+	opacity: 0;
+}
 
 a.action-button {
 	&.row-highlighted:hover {
