0e5bf57a3fae0109405a4d09b33c06b59389602b
Odalys Adam <odalys.adam@dataport.de>
2022-10-17 14:41:00 +0200

    added test for empty props

--- src/components/molecules/StatusAlerts.unit.ts
+++ src/components/molecules/StatusAlerts.unit.ts
@@ -31,31 +31,37 @@ describe("@components/molecules/StatusAlerts", () => {
 		expect(wrapper.vm.statusAlerts).toStrictEqual(testProps.statusAlerts);
 	});
 
+	it("should not render status alert items, when none are given", () => {
+		const wrapper = getWrapper();
+
+		expect(wrapper.findAll("v-list-item")).toHaveLength(0);
+	});
+
 	it("should show alert title", () => {
 		const wrapper = getWrapper(testProps);
-		const title = wrapper.find("[data-test-id=alert-title-0]");
+		const title = wrapper.find("[data-test-id='alert-title-0']");
 		expect(title.element.textContent).toContain(mockStatusAlerts[0].title);
 	});
 
 	it("should show alert text", () => {
 		const wrapper = getWrapper(testProps);
-		const title = wrapper.find("[data-test-id=alert-text-0]");
+		const title = wrapper.find("[data-test-id='alert-text-0']");
 		expect(title.element.textContent).toContain(mockStatusAlerts[0].text);
 	});
 
 	it("should show the alert url", () => {
 		const wrapper = getWrapper(testProps);
-		const title = wrapper.find("[data-test-id=alert-link-0]");
+		const title = wrapper.find("[data-test-id='alert-link-0']");
 		expect(title.element.href).toContain(mockStatusAlerts[0].url);
 	});
 
 	it("should show multiple alerts", () => {
 		const wrapper = getWrapper(testProps);
-		const title0 = wrapper.find("[data-test-id=alert-title-0]");
+		const title0 = wrapper.find("[data-test-id='alert-title-0']");
 		expect(title0.element.textContent).toContain(mockStatusAlerts[0].title);
-		const title1 = wrapper.find("[data-test-id=alert-title-1]");
+		const title1 = wrapper.find("[data-test-id='alert-title-1']");
 		expect(title1.element.textContent).toContain(mockStatusAlerts[1].title);
-		const title2 = wrapper.find("[data-test-id=alert-title-2]");
+		const title2 = wrapper.find("[data-test-id='alert-title-2']");
 		expect(title2.element.textContent).toContain(mockStatusAlerts[2].title);
 	});
 });
