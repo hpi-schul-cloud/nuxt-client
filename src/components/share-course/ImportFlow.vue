1f2dc1ee676a06e1d7e0cf1f41ed2c50950cb455
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-10-18 12:24:53 +0200

    add success and error handling

--- src/components/share-course/ImportFlow.vue
+++ src/components/share-course/ImportFlow.vue
@@ -17,7 +17,7 @@
 <script type="ts">
 import ImportModal from "@/components/share-course/ImportModal.vue";
 import { useLoadingState } from "@/composables/loadingState";
-import { computed, defineComponent, inject, ref, watch } from "@vue/composition-api";
+import { computed, defineComponent, inject, ref } from "@vue/composition-api";
 import CopyResultModal from "../copy-result-modal/CopyResultModal.vue";
 
 // eslint-disable-next-line vue/require-direct-export
@@ -42,7 +42,6 @@ export default defineComponent({
 		const copyModule = inject("copyModule");
 		const notifier = inject("notifierModule");
 
-		const step = ref(0);
 		const parentName = ref("");
 
 		const isImportModalOpen = ref(false);
@@ -56,77 +55,85 @@ export default defineComponent({
 
 		const { isLoadingDialogOpen } = useLoadingState(i18n?.t("components.molecules.importCourse.options.loadingMessage")) // wip
 
-		const onImport = (courseName) => {
-			step.value = 2;
-			parentName.value = courseName;
-		};
-
-		const onCancel = () => {
-			step.value = 0;
-		};
-
-		watch(step, (newValue) => {
-			if (newValue === 1) {
-				isImportModalOpen.value = true
-				return;
-			}
-			if (newValue === 2) {
-				copyCourse();
-				return;
-			}
-			if (newValue === 3) {
-				isLoadingDialogOpen.value = false;
-				isCopyResultModalOpen.value = true;
-				return;
-			}
-
-			isImportModalOpen.value = false;
-			isLoadingDialogOpen.value = false;
-			isCopyResultModalOpen.value = false;
-		})
-
-		if (props.isActive === true) {
-			validateShareToken();
-			// copyModule.validateShareToken(props.token).then((result) => {
-			// 	parentName.value = result.parentName;
-			// 	step.value = 1;
-			// })
-
-			// check error for the validation
-			// 404 result would come after failing
-
-		}
-
-		const copyCourse = async() => {
-			isImportModalOpen.value = false;
-			isLoadingDialogOpen.value = true;
-			const copyResult = await copyModule.copyByShareToken({ token: props.token, type:'course', newName: parentName.value });
-			step.value = 3;
-
-			// check errors for copying
-			// check if 404 would be returned
-			console.log(copyResult);
-		}
-
+		const onImport = (courseName) => startImport(courseName);
+		const onCancel = () => resetFlow();
 		const onCopyResultModalClosed = () => {
 			emit('success');
 			copyModule.reset();
 		}
 
+		if (props.isActive === true) {
+			validateShareToken();
+		}
+
+		const showImportModal = () => {
+			isImportModalOpen.value = true;
+		}
+
+		const startImport = async (newName) => {
+			isImportModalOpen.value = false;
+			isLoadingDialogOpen.value = true;
+			await copyCourse(newName);
+		}
+
+		const showImportSuccess = () => {
+			notifier?.show({
+				text: "great it worked", // wip
+				status: "success",
+				timeout: 10000,
+			})
+			resetFlow();
+		}
+
+		const showResultModal = () => {
+			isLoadingDialogOpen.value = false;
+			isCopyResultModalOpen.value = true;
+		}
+
+		const showImportFailure = () => {
+			notifier?.show({
+				text: "Importing the course failed", // wip
+				status: "error",
+				timeout: 10000,
+			})
+			resetFlow();
+		}
+
+		const resetFlow = () => {
+			isImportModalOpen.value = false;
+			isLoadingDialogOpen.value = false;
+			isCopyResultModalOpen.value = false;
+		}
+
 		async function validateShareToken() {
-			const validateResult = await copyModule.validateShareToken(props.token);
-			debugger;
-			if (validateResult.code === 404) {
+			try {
+				const validateResult = await copyModule.validateShareToken(props.token);
+				parentName.value = validateResult.parentName;
+				showImportModal();
+			} catch (error) {
 				isImportModalOpen.value = false;
 				notifier?.show({
-					text: "The token has expired or never existed",
-					status: "success",
+					text: "The token has expired or never existed", // wip
+					status: "error",
 					timeout: 10000,
 				})
 				return;
 			}
-			parentName.value = validateResult.parentName;
-			step.value = 1;
+		}
+
+		const copyCourse = async(newName) => {
+			try {
+				const copyResultFailedItems = await copyModule.copyByShareToken({ token: props.token, type:'course', newName });
+				if (copyResultFailedItems.length === 0) {
+					isImportModalOpen.value = false;
+					showImportSuccess();
+				} else {
+					showResultModal();
+				}
+			} catch (error) {
+				isImportModalOpen.value = false;
+				showImportFailure();
+			}
 		}
 
 		return {
@@ -134,7 +141,6 @@ export default defineComponent({
 			isCopyResultModalOpen,
 			copyResultModalItems,
 			parentName,
-			step,
 			onCopyResultModalClosed,
 			onImport,
 			onCancel
