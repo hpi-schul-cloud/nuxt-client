60d95f32c0eb6d89e0c105591780b06f645e6598
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-10-19 12:23:15 +0200

    chore: removed introduced but unused import-course-store

--- src/plugins/store-accessor.ts
+++ src/plugins/store-accessor.ts
@@ -8,17 +8,16 @@ import FilePathsModule from "@/store/filePaths";
 import FilesPOCModule from "@/store/files-poc";
 import FinishedTaskModule from "@/store/finished-tasks";
 import ImportUsersModule from "@/store/import-users";
-import LoadingStateModule from "@store/loading-state";
 import NewsModule from "@/store/news";
 import NotifierModule from "@/store/notifier";
 import RoomModule from "@/store/room";
 import RoomsModule from "@/store/rooms";
 import SchoolsModule from "@/store/schools";
-import ShareCourseModule from "@store/share-course";
 import TaskModule from "@/store/tasks";
 import { onGlobalSetup, provide, useContext } from "@nuxtjs/composition-api";
+import LoadingStateModule from "@store/loading-state";
+import ShareCourseModule from "@store/share-course";
 import { getModule } from "vuex-module-decorators";
-import ImportCourseModule from "@store/import-course";
 
 export default () => {
 	onGlobalSetup(() => {
@@ -33,7 +32,6 @@ export default () => {
 		provide("filePathsModule", getModule(FilePathsModule, store));
 		provide("filesPOCModule", getModule(FilesPOCModule, store));
 		provide("finishedTaskModule", getModule(FinishedTaskModule, store));
-		provide("importCourseModule", getModule(ImportCourseModule, store));
 		provide("importUsersModule", getModule(ImportUsersModule, store));
 		provide("loadingStateModule", getModule(LoadingStateModule, store));
 		provide("newsModule", getModule(NewsModule, store));
