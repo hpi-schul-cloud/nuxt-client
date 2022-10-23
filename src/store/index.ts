a20c9a0727f2090da59ad92f9e0e4d8e7fbc057e
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-10-23 17:53:22 +0200

    fix stores

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,27 @@
-import { Store } from "vuex";
-import { initializeStores } from "@/utils/store-accessor";
+// idea from: https://github.com/garyo/vuex-module-decorators-example/blob/master/src/store/index.ts
 
+import Vue from "vue";
+import Vuex, { Store } from "vuex";
+// Note: you shouldn't need to import store modules here.
+import { initializeStores, modules } from "@/store/store-accessor";
+
+Vue.use(Vuex);
+
+// Initialize the modules using a Vuex plugin that runs when the root store is
+// first initialized. This is vital to using static modules because the
+// modules don't know the root store when they are loaded. Initializing them
+// when the root store is created allows them to be hooked up properly.
 const initializer = (store: Store<any>) => initializeStores(store);
 export const plugins = [initializer];
-export * from "@/utils/store-accessor";
+export * from "@/store/store-accessor"; // re-export the modules
+
+// Export the root store. You can add mutations & actions here as well.
+// Note that this is a standard Vuex store, not a vuex-module-decorator one.
+// (Perhaps could be, but I put everything in modules)
+export default new Store({
+	plugins, // important!
+	modules, // important!
+	state: {},
+	mutations: {},
+	actions: {},
+});
---
+++ src/store/store-accessor.ts
@@ -0,0 +1,99 @@
+// idea from: https://github.com/garyo/vuex-module-decorators-example/blob/master/src/store/store-accessor.ts
+
+// This is the "store accessor":
+// It initializes all the modules using a Vuex plugin (see store/index.ts)
+// In here you import all your modules, call getModule on them to turn them
+// into the actual stores, and then re-export them.
+
+import { Store } from "vuex";
+import { getModule } from "vuex-module-decorators";
+import AccountsModule from "@/store/accounts";
+import AuthModule from "@/store/auth";
+import AutoLogoutModule from "@/store/autoLogout";
+import ContentModule from "@/store/content";
+import CopyModule from "@/store/copy";
+import EnvConfigModule from "@/store/env-config";
+import FilePaths from "@/store/filePaths";
+import FilesPOCModule from "@/store/files-poc";
+import FinishedTaskModule from "@/store/finished-tasks";
+import ImportUsersModule from "@/store/import-users";
+import LoadingStateModule from "@/store/loading-state";
+import NewsModule from "@/store/news";
+import NotifierModule from "@/store/notifier";
+import RoomModule from "@/store/room";
+import RoomsModule from "@/store/rooms";
+import SchoolsModule from "@/store/schools";
+import StatusAlertsModule from "@/store/status-alerts";
+import TaskModule from "@/store/tasks";
+import ShareCourseModule from "@/store/share-course";
+
+// Each store is the singleton instance of its module class
+// Use these -- they have methods for state/getters/mutations/actions
+// (result from getModule(...))
+export let accountsModule: AccountsModule;
+export let authModule: AuthModule;
+export let autoLogoutModule: AutoLogoutModule;
+export let contentModule: ContentModule;
+export let copyModule: CopyModule;
+export let envConfigModule: EnvConfigModule;
+export let filePathsModule: FilePaths;
+export let filesPOCModule: FilesPOCModule;
+export let finishedTaskModule: FinishedTaskModule;
+export let importUsersModule: ImportUsersModule;
+export let loadingStateModule: LoadingStateModule;
+export let newsModule: NewsModule;
+export let notifierModule: NotifierModule;
+export let roomModule: RoomModule;
+export let roomsModule: RoomsModule;
+export let schoolsModule: SchoolsModule;
+export let statusAlertsModule: StatusAlertsModule;
+export let taskModule: TaskModule;
+export let shareCourseModule: ShareCourseModule;
+
+// initializer plugin: sets up state/getters/mutations/actions for each store
+export function initializeStores(store: Store<any>): void {
+	accountsModule = getModule(AccountsModule, store);
+	authModule = getModule(AuthModule, store);
+	autoLogoutModule = getModule(AutoLogoutModule, store);
+	contentModule = getModule(ContentModule, store);
+	envConfigModule = getModule(EnvConfigModule, store);
+	filePathsModule = getModule(FilePaths, store);
+	filesPOCModule = getModule(FilesPOCModule, store);
+	finishedTaskModule = getModule(FinishedTaskModule, store);
+	importUsersModule = getModule(ImportUsersModule, store);
+	newsModule = getModule(NewsModule, store);
+	roomModule = getModule(RoomModule, store);
+	roomsModule = getModule(RoomsModule, store);
+	schoolsModule = getModule(SchoolsModule, store);
+	statusAlertsModule = getModule(StatusAlertsModule, store);
+	taskModule = getModule(TaskModule, store);
+	notifierModule = getModule(NotifierModule, store);
+	copyModule = getModule(CopyModule, store);
+	loadingStateModule = getModule(LoadingStateModule, store);
+	shareCourseModule = getModule(ShareCourseModule, store);
+}
+
+// for use in 'modules' store init (see store/index.ts), so each module
+// appears as an element of the root store's state.
+// (This is required!)
+export const modules = {
+	accountsModule: AccountsModule,
+	authModule: AuthModule,
+	autoLogoutModule: AutoLogoutModule,
+	contentModule: ContentModule,
+	envConfigModule: EnvConfigModule,
+	filePathsModule: FilePaths,
+	filesPOCModule: FilesPOCModule,
+	finishedTaskModule: FinishedTaskModule,
+	importUsersModule: ImportUsersModule,
+	newsModule: NewsModule,
+	roomModule: RoomModule,
+	roomsModule: RoomsModule,
+	schoolsModule: SchoolsModule,
+	statusAlertsModule: StatusAlertsModule,
+	taskModule: TaskModule,
+	notifierModule: NotifierModule,
+	copyModule: CopyModule,
+	loadingStateModule: LoadingStateModule,
+	shareCourseModule: ShareCourseModule,
+};
