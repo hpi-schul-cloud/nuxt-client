aa4e9dda50ca7eda0c4fe490abbf60298a689ee2
Odalys Adam <odalys.adam@dataport.de>
2022-10-26 23:33:56 +0200

    fixed ContentSearchbar unit tests

--- src/components/molecules/ContentSearchbar.unit.js
+++ src/components/molecules/ContentSearchbar.unit.js
@@ -1,54 +1,39 @@
 import ContentSearchbar from "./ContentSearchbar";
 
-const contentSearchbarComponent = {
-	...createComponentMocks({
-		i18n: true,
-		vuetify: true,
-	}),
-	components: { ContentSearchbar },
-	data: () => ({
-		value: "",
-		placeholder: "Suche nach...",
-	}),
-	template: '<content-searchbar v-model="value" :placeholder="placeholder" />',
+const getWrapper = () => {
+	return mount(ContentSearchbar, {
+		...createComponentMocks({ i18n: true, vuetify: true }),
+		template:
+			'<content-searchbar v-model="value" :placeholder="placeholder" />',
+	});
 };
 
 describe("@components/molecules/ContentSearchbar", () => {
 	it(...isValidComponent(ContentSearchbar));
 
 	it("Defaults render", async () => {
-		const wrapper = mount(contentSearchbarComponent);
+		const wrapper = getWrapper();
 		expect(wrapper.find("input").exists()).toBe(true);
 		expect(wrapper.find(".search__container--icon").exists()).toBe(true);
 	});
 
-	it("Updates the v-model when typing", async () => {
-		const testString = "teststring";
-		const wrapper = mount(contentSearchbarComponent);
-		const inputField = wrapper.find("input");
-		expect(wrapper.vm.value).toBe("");
-		inputField.trigger("focus");
-		inputField.setValue(testString);
-		expect(wrapper.vm.value).toBe(testString);
-	});
-
 	it("Emits input event on any input value change", async () => {
 		const testInput = "some test input";
-		const wrapper = mount(ContentSearchbar);
+		const wrapper = getWrapper();
 		const input = wrapper.find('input[type="text"]');
 		input.setValue(testInput);
 		expect(wrapper.emitted("input")[0]).toStrictEqual([testInput]);
 	});
 
 	it("Emits keyup:enter event on ENTER keydown", async () => {
-		const wrapper = mount(ContentSearchbar);
+		const wrapper = getWrapper();
 		const input = wrapper.find('input[type="text"]');
 		input.trigger("keyup.enter");
 		expect(wrapper.emitted("keyup:enter")).toHaveLength(1);
 	});
 
 	it("Switches to active input and clears value when ESCAPE key is pressed", async () => {
-		const wrapper = mount(ContentSearchbar);
+		const wrapper = getWrapper();
 		const testValue = "testValue";
 		wrapper.setData({ isActive: false, inputValue: testValue });
 		expect(wrapper.vm.isActive).toBe(false);
@@ -61,7 +46,7 @@ describe("@components/molecules/ContentSearchbar", () => {
 	});
 
 	it("renders search icon when input is empty", async () => {
-		const wrapper = shallowMount(ContentSearchbar);
-		expect(wrapper.find(".search-icon").attributes("icon")).toBe("search");
+		const wrapper = getWrapper();
+		expect(wrapper.find(".v-icon").exists()).toBe(true);
 	});
 });
