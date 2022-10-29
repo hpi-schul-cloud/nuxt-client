5d455dcb57f45e203e229705f5d1b837cb233906
Christian Darsow <christian.darsow@dataport.de>
2022-10-29 14:26:14 +0200

    Add i18n test page

---
+++ vue3_poc/src/pages/I18nPage.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-container>
+		<v-row class="text-center">
+			<v-col class="mb-4">
+				<h1 class="display-2 font-weight-bold mb-3">
+					{{ $t("hello", { instance: "dBildungscloud" }) }}
+				</h1>
+				<p>{{ $t("only.exists.in.english") }}</p>
+				<br />
+				<p>
+					Current language: <i>{{ $i18n.locale }}</i
+					><br />
+					Fallback language: <i>{{ $i18n.fallbackLocale }}</i>
+				</p>
+			</v-col>
+			<language-selector></language-selector>
+		</v-row>
+	</v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LanguageSelector from "@/components/LanguageSelector.vue";
+
+export default defineComponent({
+	name: "I18nPage",
+	components: {
+		LanguageSelector,
+	},
+});
+</script>
