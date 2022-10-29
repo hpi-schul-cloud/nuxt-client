691a0486d6a6caa05c4398517b741e60148bf91b
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-10-29 13:31:36 +0200

    remove babel

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // process.env.TZ = "Europe/Berlin";
 
 module.exports = {
-	preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+	preset: "@vue/cli-plugin-unit-jest/presets/typescript",
 	testMatch: ["**/*.unit.{j,t}s?(x)"],
 	setupFiles: ["./tests/unit/setup.js", "./tests/unit/setup-new.js"],
 };
