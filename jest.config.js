9acd260b3dc67005bd33517b3b408bfb08fb6ce0
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-11-03 09:56:52 +0100

    fix test setup for vue components written in js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 // process.env.TZ = "Europe/Berlin";
 
 module.exports = {
-	preset: "@vue/cli-plugin-unit-jest/presets/typescript",
+	// NUXT_REMOVAL we have to remove babel later.
+	// we have to keep it for now as a migration path for compatibility with legacy js components / tests
+	// dependencies that can be removed later after fully moving to typescript:
+	// @vue/cli-plugin-babel, babel-jest
+	// (do not forget to remove babel.config.js as well)
+	preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
 	testMatch: ["**/*.unit.{j,t}s?(x)"],
 
-	transform: {
-		"^.+\\.jsx?$": require.resolve("ts-jest"),
-		"^.+\\.tsx?$": require.resolve("ts-jest"),
-	},
 	moduleNameMapper: {
 		"^axios$": require.resolve("axios"),
 		"^@@/(.*)$": "<rootDir>/$1",
