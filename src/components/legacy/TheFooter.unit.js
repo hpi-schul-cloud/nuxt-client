e728d150507d0bd624dbaab3095a322d609b4971
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-11-04 13:20:20 +0100

    fixed TheFooter.unit.js

--- src/components/legacy/TheFooter.unit.js
+++ src/components/legacy/TheFooter.unit.js
@@ -30,20 +30,11 @@ describe("@/components/legacy/TheFooter", () => {
 			"https://dummy-url.org/Willkommensordner/Datenschutz/Nutzungsordnung_Schueler-innen.pdf"
 		);
 	});
-	it("Link to accessibility statement is set correctly", () => {
-		filePathsModule.setSpecificFiles("https://dummy-url.org/");
-		const wrapper = shallowMount(TheFooter, {
-			...createComponentMocks({
-				mocks: {
-					$theme,
-				},
-				i18n: true,
-			}),
-		});
-		expect(wrapper.vm.links[5].href).toStrictEqual(
-			"https://dummy-url.org/Willkommensordner/Barrierefreiheit/Barrierefreiheitserklaerung.pdf"
-		);
+
+	it.skip("Link to accessibility statement is set correctly", () => {
+		// accessibility statement is now part of the theme-specific TheFooter.vue implementation
 	});
+
 	it("Env-Variable sets the status page link correctly", () => {
 		envConfigModule.setEnvs({ ALERT_STATUS_URL: "dummy-url.org" });
 		const wrapper = shallowMount(TheFooter, {
@@ -56,22 +47,11 @@ describe("@/components/legacy/TheFooter", () => {
 		});
 		expect(wrapper.vm.links[5].href).toStrictEqual("dummy-url.org");
 	});
-	it("Env-Variable sets the report accessibility email correctly", () => {
-		envConfigModule.setEnvs({
-			ACCESSIBILITY_REPORT_EMAIL: "dummy-email@org.de",
-		});
-		const wrapper = shallowMount(TheFooter, {
-			...createComponentMocks({
-				mocks: {
-					$theme,
-				},
-				i18n: true,
-			}),
-		});
-		expect(wrapper.vm.links[5].href).toStrictEqual(
-			"mailto:dummy-email@org.de?subject=Barriere melden"
-		);
+
+	it.skip("Env-Variable sets the report accessibility email correctly", () => {
+		// accessibility statement is now part of the theme-specific TheFooter.vue implementation
 	});
+
 	it("check that all links are rendered in the footer", () => {
 		envConfigModule.setEnvs({
 			ACCESSIBILITY_REPORT_EMAIL: "dummy-email@org.de",
@@ -85,7 +65,7 @@ describe("@/components/legacy/TheFooter", () => {
 				i18n: true,
 			}),
 		});
-		expect(wrapper.vm.links).toHaveLength(8);
+		expect(wrapper.vm.links).toHaveLength(7);
 		expect(wrapper.find(".bottom-line span").text()).toBe(
 			"©" + new Date().getFullYear() + " " + $theme.name
 		);
