37c9b36331bdaeb0df32f7336fa714730798d394
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-11-05 14:29:55 +0100

    move i18n setup to plugins folder

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ import {
 } from "@/store";
 import Vue from "vue";
 import App from "./App.vue";
-import i18n from "./i18n";
+import i18n from "./plugins/i18n";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import store from "./plugins/store";
