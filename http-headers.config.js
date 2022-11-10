1343ce688ffd8ae7580243b2a35a29138ed6f4d5
Andre Blome <andre-david.blome@capgemini.com>
2022-11-10 15:47:31 +0100

    h5p: cors headers changed

--- http-headers.config.js
+++ http-headers.config.js
@@ -44,12 +44,12 @@ module.exports = {
 		// Default Content-Security-Policy Header for every site
 		// Use 'strict-dynamic' 'nonce-<nonceValue>' (nonceValue auto generated) to create a whitelist
 		corsDefault: {
-			defaultSrc: `'self' data: blob: wss://dbildungscloud.de wss://scchat.dbildungscloud.de http://localhost:3030 https://api.dbildungscloud.de https://scchat.dbildungscloud.de https://s3.hidrive.strato.com https://libreoffice.dbildungscloud.de https://docs.dbildungscloud.de https://etherpad.dbildungscloud.de https://blog.dbildungscloud.de https://sc-content-resources.hpi-schul-cloud.de https://open.hpi.de https://upload.wikimedia.org https://rendering.services.dev-edusharing.staging.dbildungscloud.org ${matrixMessengerEmbed} ${matrixMessengerHomeserver} ${matrixMessengerDiscoverUri}`,
+			defaultSrc: `'self' data: blob: wss://dbildungscloud.de wss://scchat.dbildungscloud.de http://localhost:3030 https://api.dbildungscloud.de https://scchat.dbildungscloud.de https://s3.hidrive.strato.com https://libreoffice.dbildungscloud.de https://docs.dbildungscloud.de https://etherpad.dbildungscloud.de https://blog.dbildungscloud.de https://sc-content-resources.hpi-schul-cloud.de https://open.hpi.de https://upload.wikimedia.org ${matrixMessengerEmbed} ${matrixMessengerHomeserver} ${matrixMessengerDiscoverUri}`,
 			fontSrc: `'self' data: ${matrixMessengerEmbed}`,
 			styleSrc: `'self' 'unsafe-inline' ${matrixMessengerEmbed}`,
 			// scriptSrc: "'strict-dynamic' 'unsafe-eval' 'nonce-<nonceValue>'",
-			scriptSrc: `'self' 'unsafe-eval' ${matrixMessengerEmbed} https://rendering.services.dev-edusharing.staging.dbildungscloud.org`,
-			frameSrc: `'self' ${matrixMessengerEmbed} https://docs.dbildungscloud.de https://rendering.services.dev-edusharing.staging.dbildungscloud.org`,
+			scriptSrc: `'self' 'unsafe-eval' ${matrixMessengerEmbed}`,
+			frameSrc: `'self' ${matrixMessengerEmbed} https://docs.dbildungscloud.de`,
 			// Please activate for production
 			// upgradeInsecureRequestsSrc: 'upgrade-insecure-requests',
 			// blockAllMixedContentSrc: 'block-all-mixed-content',
