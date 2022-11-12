b19682d327e1d332ceb704f2ce9e75fc1347cc26
Odalys Adam <odalys.adam@dataport.de>
2022-11-12 20:17:34 +0100

    added duedate field

--- src/pages/TaskCreate.page.vue
+++ src/pages/TaskCreate.page.vue
@@ -10,6 +10,37 @@
 				v-model="description"
 				:label="$t('common.labels.description')"
 			/>
+			<v-row>
+				<v-col class="col-3">
+					<v-menu
+						v-model="showDatePicker"
+						:close-on-content-click="false"
+						:nudge-right="40"
+						transition="scale-transition"
+						offset-y
+						min-width="auto"
+					>
+						<template #activator="{ on, attrs }">
+							<v-text-field
+								:value="formattedDate"
+								label="Zu erledigen bis"
+								:append-icon="mdiCalendar"
+								readonly
+								v-bind="attrs"
+								v-on="on"
+							></v-text-field>
+						</template>
+						<v-date-picker
+							v-model="dueDate"
+							no-title
+							@input="showDatePicker = false"
+						></v-date-picker>
+					</v-menu>
+				</v-col>
+				<v-col class="col-2"
+					><v-text-field v-model="dueTime" type="time"
+				/></v-col>
+			</v-row>
 			<v-btn color="secondary" outlined @click="cancel">
 				{{ $t("common.actions.cancel") }}
 			</v-btn>
@@ -21,9 +52,11 @@
 </template>
 
 <script>
-import { inject, ref, onBeforeMount } from "@vue/composition-api";
+import { inject, ref, onBeforeMount, computed } from "@vue/composition-api";
+import moment from "moment";
 import { taskModule, authModule } from "@/store";
 import DefaultWireframe from "@components/templates/DefaultWireframe.vue";
+import { mdiCalendar } from "@mdi/js";
 
 // eslint-disable-next-line vue/require-direct-export
 export default {
@@ -43,6 +76,9 @@ export default {
 		const i18n = inject("i18n");
 		const name = ref("");
 		const description = ref("");
+		const dueDate = ref("");
+		const dueTime = ref("");
+		const showDatePicker = ref(false);
 
 		const breadcrumbs = [
 			{
@@ -51,20 +87,39 @@ export default {
 			},
 		];
 
+		const formattedDate = computed(() => {
+			return dueDate.value
+				? moment(dueDate.value).format(i18n.t("format.date"))
+				: "";
+		});
+
 		const save = async () => {
+			const dueDateTime = dueDate.value + " " + dueTime.value;
 			await taskModule.createTask({
 				name: name.value,
 				description: description.value,
+				dueDate: dueDateTime,
 			});
 
-			router.go(-1);
+			//router.go(-1);
 		};
 
 		const cancel = () => {
 			router.go(-1);
 		};
 
-		return { breadcrumbs, name, description, save, cancel };
+		return {
+			mdiCalendar,
+			formattedDate,
+			breadcrumbs,
+			name,
+			description,
+			dueDate,
+			dueTime,
+			showDatePicker,
+			save,
+			cancel,
+		};
 	},
 	head() {
 		return {
@@ -73,3 +128,10 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+::v-deep .v-input__icon--append .v-icon {
+	width: 20px;
+	height: 20px;
+}
+</style>
