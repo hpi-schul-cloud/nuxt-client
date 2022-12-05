a52289afd31044b9db07f77be9a53abe065ac732
Murat Merdoglu <64781656+murat-merdoglu-hpi@users.noreply.github.com>
2022-12-05 14:57:40 +0100

    BC-2596 - Error page refactoring (#2319)
    BC-2596 create error.ts store
    
    BC-2596 add new svg image in the component
    
    BC-2596 add composition api for Error page component
    
    BC-2596 add component to do error routing on appError
    
    BC-2596 add composable and error-listener pattern for errors thrown in components
    
    BC-2596 add global error handler for vue errors
    
    BC-2596 implement a sub component for ErrorPage
    
    
    Co-authored-by: Oliver Happe <oliver.happe@dataport.de>
    Co-authored-by: murat-merdoglu-hpi <murat-merdoglu-hpi@users.noreply.github.com>
    Co-authored-by: Ramona Weitzenberg <63390574+monaweitzenberg@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const emptyFn = () => {};
 Vue.use(Router);
 
 export function createRouter() {
-	const router = new Router({
+	const localRouter = new Router({
 		mode: "history",
 		base: "/",
 		linkActiveClass: "nuxt-link-active",
@@ -17,15 +17,16 @@ export function createRouter() {
 	});
 
 	// TODO: remove in Nuxt 3
-	const originalPush = router.push;
-	router.push = function push(location, onComplete = emptyFn, onAbort) {
+	const originalPush = localRouter.push;
+	localRouter.push = function push(location, onComplete = emptyFn, onAbort) {
 		return originalPush.call(this, location, onComplete, onAbort);
 	};
 
-	const resolve = router.resolve.bind(router);
-	router.resolve = (to, current, append) => {
+	const resolve = localRouter.resolve.bind(localRouter);
+	localRouter.resolve = (to, current, append) => {
 		return resolve(to, current, append);
 	};
 
-	return router;
+	return localRouter;
 }
+export const router = createRouter();
