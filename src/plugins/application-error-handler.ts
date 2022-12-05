52de5e67d0501772e36b46da58bb3d3faab0072b
Murat Merdoglu <murat.merdoglu@hpi.de>
2022-12-05 10:34:49 +0100

    chore: replace the function with a const

--- src/plugins/application-error-handler.ts
+++ src/plugins/application-error-handler.ts
@@ -7,10 +7,11 @@ export const handleApplicationError = (err: Error) => {
 	 * Note: The Global-ErrorHandler wraps the error
 	 * so we can't use instanceof ApplicationError here.
 	 */
+	const applicationError = err as ApplicationError;
 	if (err.name === "ApplicationError") {
 		applicationErrorModule.setError({
-			statusCode: asApplicationError(err).statusCode,
-			translationKey: asApplicationError(err).translationKey,
+			statusCode: applicationError.statusCode,
+			translationKey: applicationError.translationKey,
 		});
 		return;
 	}
@@ -19,8 +20,5 @@ export const handleApplicationError = (err: Error) => {
 		translationKey: "error.generic",
 	});
 };
-
-const asApplicationError: (err: Error) => ApplicationError = (err) =>
-	err as ApplicationError;
 
 Vue.config.errorHandler = handleApplicationError;
