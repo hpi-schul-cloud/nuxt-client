a6946103ffb3fdd9b8ccea6c12bd713673e48586
Odalys Adam <odalys.adam@dataport.de>
2022-12-07 11:57:27 +0100

    CardTitle unit tests

--- src/components/atoms/CardTitle.unit.ts
+++ src/components/atoms/CardTitle.unit.ts
@@ -13,55 +13,8 @@ const getWrapper = (props?: object, options?: object) => {
 };
 
 describe("CardTitle", () => {
-	beforeEach(() => {
-		window.location.pathname = "";
-	});
-
 	it("should render component with defaults", () => {
 		const wrapper = getWrapper();
 		expect(wrapper.findComponent(CardTitle).exists()).toBe(true);
-	});
-
-	it("should render with valid props", () => {
-		const wrapper = getWrapper({
-			label: "Title",
-			value: "Learn something today",
-		});
-
-		// TODO - can't find the label or input element for some reason
-		expect(wrapper.props().label).toStrictEqual("Title");
-		expect(wrapper.props().value).toStrictEqual("Learn something today");
-	});
-
-	it("should render with valid props", async () => {
-		const wrapper = getWrapper({
-			label: "Title",
-			value: "Learn something today",
-		});
-
-		// TODO - can't find the label or input element for some reason
-		console.log(wrapper.findAll("label").length); // = 0, should be 1
-		expect(wrapper.props().value).toStrictEqual("Learn something today");
-
-		wrapper.setProps({
-			value: "Learn something tomorrow",
-		});
-		await wrapper.vm.$nextTick;
-
-		// TODO - wanna test if name.value changes accordingly, but don't know how to access it
-		// TODO - don't know what to test in general tbh
-		expect(wrapper.props().value).toStrictEqual("Learn something tomorrow");
-	});
-
-	it("should not render with invalid props", () => {
-		try {
-			getWrapper({ value: 123 });
-		} catch (e) {
-			if (e instanceof Error) {
-				expect(e.message).toContain("Invalid prop");
-			}
-			return;
-		}
-		fail("No error on invalid prop");
 	});
 });
