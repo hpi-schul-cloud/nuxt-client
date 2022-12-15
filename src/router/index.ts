4e854fef415fcf3d3c4b232345e5ecb616ef08cd
Oliver Happe <oliver.happe@dataport.de>
2022-12-15 14:55:34 +0100

    adds router tracing to debug news-page redirect loop

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,15 @@ const router = new VueRouter({
 	fallback: false,
 });
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const tracing = (to, from, next) => {
+	console.log("Router Trace --- ", to, from);
+	next();
+};
+
 router.beforeEach(isAuthenticatedGuard);
 router.beforeEach(legacyCompatibilityGuard);
+router.beforeEach(tracing);
 
 export default router;
