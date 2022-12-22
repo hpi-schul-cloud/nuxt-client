3e7ea510b1d88b21c169416e23073b1ea3cd20d7
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-12-22 14:49:10 +0100

    add CSP article reference

--- webpack-config/nonce-placeholder-plugin.js
+++ webpack-config/nonce-placeholder-plugin.js
@@ -1,3 +1,6 @@
+// This is inspired by the excellent article on Content Security Policy by Quest Henkart
+// https://towardsdatascience.com/content-security-policy-how-to-create-an-iron-clad-nonce-based-csp3-policy-with-webpack-and-nginx-ce5a4605db90
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 class NoncePlaceholderPlugin {
 	apply(compiler) {
