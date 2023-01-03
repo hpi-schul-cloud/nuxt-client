53e726d9cd6697b48305264ff9352f8c22becb00
hoeppner-dataport <hoeppner-dataport@users.noreply.github.com>
2023-01-03 10:07:05 +0000

    npm run lint

--- src/plugins/authenticate.js
+++ src/plugins/authenticate.js
@@ -26,11 +26,18 @@ export default async ({ app, route }) => {
 function composeLoginUrlWithRedirect() {
 	const currentUrl = window.location.href;
 
-	const themeName = envConfigModule.getEnv.SC_THEME ?? 'default';
-	if (themeName === 'thr' && envConfigModule.getEnv.FEATURE_TSP_ENABLED === true) {
+	const themeName = envConfigModule.getEnv.SC_THEME ?? "default";
+	if (
+		themeName === "thr" &&
+		envConfigModule.getEnv.FEATURE_TSP_ENABLED === true
+	) {
 		const schulPortalUrl = envConfigModule.getEnv.TSP_API_BASE_URL;
-		const schulCloudLoginUrl = `https://${envConfigModule.getEnv.DOMAIN}/tsp-login?redirect=${encodeURIComponent(currentUrl)}`;
-		return `${schulPortalUrl}?service=${encodeURIComponent(schulCloudLoginUrl)}`;
+		const schulCloudLoginUrl = `https://${
+			envConfigModule.getEnv.DOMAIN
+		}/tsp-login?redirect=${encodeURIComponent(currentUrl)}`;
+		return `${schulPortalUrl}?service=${encodeURIComponent(
+			schulCloudLoginUrl
+		)}`;
 	}
 	return `/login?redirect=${currentUrl}`;
 }
