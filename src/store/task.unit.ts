aab1abb0b16ad13b9de4d64e75c66536859567e3
Christian Darsow <christian.darsow@dataport.de>
2023-01-11 17:36:16 +0100

    Adjust tests to cards API

--- src/store/task.unit.ts
+++ src/store/task.unit.ts
@@ -1,11 +1,27 @@
 import TaskModule from "@store/task";
 import * as serverApi from "../serverApi/v3/api";
-import { RichTextTypeEnum } from "../serverApi/v3/api";
 
 const mockTaskData = {
 	id: "123",
-	name: "abc",
-	description: { content: "Lorem ipsum", type: RichTextTypeEnum.RichtextCk5 },
+	cardElements: [],
+	draggable: true,
+	task: {
+		id: "456",
+		name: "",
+		courseName: "",
+		courseId: "",
+		createdAt: "",
+		updatedAt: "",
+		lessonHidden: false,
+		status: {
+			submitted: 0,
+			maxSubmissions: 0,
+			graded: 0,
+			isDraft: true,
+			isSubstitutionTeacher: true,
+			isFinished: false,
+		},
+	},
 };
 
 describe("task store", () => {
@@ -20,20 +36,20 @@ describe("task store", () => {
 				const setTaskDataMock = jest.spyOn(taskModule, "setTaskData");
 
 				const taskApiMock = {
-					taskControllerFindTask: jest.fn(async () => ({
+					taskCardControllerFindOne: jest.fn(async () => ({
 						data: mockTaskData,
 					})),
 				};
 				jest
-					.spyOn(serverApi, "TaskApiFactory")
+					.spyOn(serverApi, "CardsApiFactory")
 					.mockReturnValue(
-						taskApiMock as unknown as serverApi.TaskApiInterface
+						taskApiMock as unknown as serverApi.CardsApiInterface
 					);
 
 				await taskModule.findTask(mockTaskData.id);
 
-				expect(taskApiMock.taskControllerFindTask).toHaveBeenCalledTimes(1);
-				expect(taskApiMock.taskControllerFindTask).toHaveBeenCalledWith(
+				expect(taskApiMock.taskCardControllerFindOne).toHaveBeenCalledTimes(1);
+				expect(taskApiMock.taskCardControllerFindOne).toHaveBeenCalledWith(
 					mockTaskData.id
 				);
 				expect(setTaskDataMock).toHaveBeenCalledWith(mockTaskData);
@@ -44,17 +60,19 @@ describe("task store", () => {
 				const error = { statusCode: 418, message: "I'm a teapot", error: {} };
 
 				const taskApiMock = {
-					taskControllerFindTask: jest.fn(() => Promise.reject({ ...error })),
+					taskCardControllerFindOne: jest.fn(() =>
+						Promise.reject({ ...error })
+					),
 				};
 				jest
-					.spyOn(serverApi, "TaskApiFactory")
+					.spyOn(serverApi, "CardsApiFactory")
 					.mockReturnValue(
-						taskApiMock as unknown as serverApi.TaskApiInterface
+						taskApiMock as unknown as serverApi.CardsApiInterface
 					);
 
 				await taskModule.findTask("123");
 
-				expect(taskApiMock.taskControllerFindTask).toHaveBeenCalledTimes(1);
+				expect(taskApiMock.taskCardControllerFindOne).toHaveBeenCalledTimes(1);
 				expect(taskModule.businessError).toStrictEqual(error);
 			});
 		});
@@ -69,22 +87,22 @@ describe("task store", () => {
 				const setTaskDataMock = jest.spyOn(taskModule, "setTaskData");
 
 				const taskApiMock = {
-					taskControllerCreate: jest.fn(async () => ({
+					taskCardControllerCreate: jest.fn(async () => ({
 						data: mockTaskData,
 					})),
 				};
 				jest
-					.spyOn(serverApi, "TaskApiFactory")
+					.spyOn(serverApi, "CardsApiFactory")
 					.mockReturnValue(
-						taskApiMock as unknown as serverApi.TaskApiInterface
+						taskApiMock as unknown as serverApi.CardsApiInterface
 					);
 
 				await taskModule.createTask({
-					name: "abc",
-					description: "Lorem ipsum",
+					title: "abc",
+					text: ["Lorem ipsum"],
 				});
 
-				expect(taskApiMock.taskControllerCreate).toHaveBeenCalledTimes(1);
+				expect(taskApiMock.taskCardControllerCreate).toHaveBeenCalledTimes(1);
 				expect(setTaskDataMock).toHaveBeenCalledWith(mockTaskData);
 			});
 
@@ -93,20 +111,20 @@ describe("task store", () => {
 				const error = { statusCode: 418, message: "I'm a teapot", error: {} };
 
 				const taskApiMock = {
-					taskControllerCreate: jest.fn(() => Promise.reject({ ...error })),
+					taskCardControllerCreate: jest.fn(() => Promise.reject({ ...error })),
 				};
 				jest
-					.spyOn(serverApi, "TaskApiFactory")
+					.spyOn(serverApi, "CardsApiFactory")
 					.mockReturnValue(
-						taskApiMock as unknown as serverApi.TaskApiInterface
+						taskApiMock as unknown as serverApi.CardsApiInterface
 					);
 
 				await taskModule.createTask({
-					name: "abc",
-					description: "Lorem ipsum",
+					title: "abc",
+					text: ["Lorem ipsum"],
 				});
 
-				expect(taskApiMock.taskControllerCreate).toHaveBeenCalledTimes(1);
+				expect(taskApiMock.taskCardControllerCreate).toHaveBeenCalledTimes(1);
 				expect(taskModule.businessError).toStrictEqual(error);
 			});
 		});
@@ -117,35 +135,27 @@ describe("task store", () => {
 				const setTaskDataMock = jest.spyOn(taskModule, "setTaskData");
 
 				const taskApiMock = {
-					taskControllerUpdate: jest.fn(async () => ({
-						data: {
-							id: "123",
-							name: "def",
-							description: "Lorem ipsum",
-						},
+					taskCardControllerUpdate: jest.fn(async () => ({
+						data: mockTaskData,
 					})),
 				};
 				jest
-					.spyOn(serverApi, "TaskApiFactory")
+					.spyOn(serverApi, "CardsApiFactory")
 					.mockReturnValue(
-						taskApiMock as unknown as serverApi.TaskApiInterface
+						taskApiMock as unknown as serverApi.CardsApiInterface
 					);
 
 				taskModule.setTaskData(mockTaskData);
-				await taskModule.updateTask({ name: "def" });
+				await taskModule.updateTask({ cardElements: [] });
 
-				expect(taskApiMock.taskControllerUpdate).toHaveBeenCalledTimes(1);
-				expect(taskApiMock.taskControllerUpdate).toHaveBeenCalledWith(
+				expect(taskApiMock.taskCardControllerUpdate).toHaveBeenCalledTimes(1);
+				expect(taskApiMock.taskCardControllerUpdate).toHaveBeenCalledWith(
 					mockTaskData.id,
 					{
-						name: "def",
+						cardElements: [],
 					}
 				);
-				expect(setTaskDataMock).toHaveBeenCalledWith({
-					id: "123",
-					name: "def",
-					description: "Lorem ipsum",
-				});
+				expect(setTaskDataMock).toHaveBeenCalledWith(mockTaskData);
 			});
 
 			it("should handle an error", async () => {
@@ -153,51 +163,51 @@ describe("task store", () => {
 				const error = { statusCode: 418, message: "I'm a teapot", error: {} };
 
 				const taskApiMock = {
-					taskControllerUpdate: jest.fn(() => Promise.reject({ ...error })),
+					taskCardControllerUpdate: jest.fn(() => Promise.reject({ ...error })),
 				};
 				jest
-					.spyOn(serverApi, "TaskApiFactory")
+					.spyOn(serverApi, "CardsApiFactory")
 					.mockReturnValue(
-						taskApiMock as unknown as serverApi.TaskApiInterface
+						taskApiMock as unknown as serverApi.CardsApiInterface
 					);
 
 				taskModule.setTaskData(mockTaskData);
-				await taskModule.updateTask({ name: "def" });
+				await taskModule.updateTask({ cardElements: [] });
 
-				expect(taskApiMock.taskControllerUpdate).toHaveBeenCalledTimes(1);
+				expect(taskApiMock.taskCardControllerUpdate).toHaveBeenCalledTimes(1);
 				expect(taskModule.businessError).toStrictEqual(error);
 			});
 		});
-	});
 
-	describe("getters", () => {
-		describe("getLoading", () => {
-			it("should return loading state", () => {
-				const taskModule = new TaskModule({});
-
-				expect(taskModule.getLoading).toStrictEqual(false);
-				taskModule.setLoading(true);
-				expect(taskModule.getLoading).toStrictEqual(true);
-			});
-		});
-
-		describe("getBusinessError", () => {
-			it("should return business error", () => {
-				const taskModule = new TaskModule({});
-				taskModule.businessError = {
-					statusCode: "404",
-					message: "not found",
-				};
-
-				expect(taskModule.getBusinessError).toBe(taskModule.businessError);
-			});
-
-			describe("getTaskData", () => {
-				it("should return task data", () => {
+		describe("getters", () => {
+			describe("getLoading", () => {
+				it("should return loading state", () => {
 					const taskModule = new TaskModule({});
-					taskModule.taskData = mockTaskData;
 
-					expect(taskModule.getTaskData).toStrictEqual(mockTaskData);
+					expect(taskModule.getLoading).toStrictEqual(false);
+					taskModule.setLoading(true);
+					expect(taskModule.getLoading).toStrictEqual(true);
+				});
+			});
+
+			describe("getBusinessError", () => {
+				it("should return business error", () => {
+					const taskModule = new TaskModule({});
+					taskModule.businessError = {
+						statusCode: "404",
+						message: "not found",
+					};
+
+					expect(taskModule.getBusinessError).toBe(taskModule.businessError);
+				});
+
+				describe("getTaskData", () => {
+					it("should return task data", () => {
+						const taskModule = new TaskModule({});
+						taskModule.taskData = mockTaskData;
+
+						expect(taskModule.getTaskData).toStrictEqual(mockTaskData);
+					});
 				});
 			});
 		});
