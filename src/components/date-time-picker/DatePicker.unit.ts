6ac6334030e9fe3364c29525d3e7fe6c0a597619
Odalys Adam <odalys.adam@dataport.de>
2023-01-29 19:48:22 +0100

    DatePicker unit tests v1

---
+++ src/components/date-time-picker/DatePicker.unit.ts
@@ -0,0 +1,41 @@
+import { provide } from "@vue/composition-api";
+import { mount } from "@vue/test-utils";
+import createComponentMocks from "@@/tests/test-utils/componentMocks";
+import DatePicker from "@/components/date-time-picker/DatePicker.vue";
+
+const getWrapper = (props?: object, options?: object) => {
+	return mount(DatePicker, {
+		...createComponentMocks({
+			i18n: true,
+		}),
+		setup() {
+			provide("i18n", { t: (key: string) => key });
+		},
+		propsData: props,
+		...options,
+	});
+};
+
+describe("@components/date-time-picker/DatePicker", () => {
+	it("should render component", () => {
+		const wrapper = getWrapper({
+			date: new Date().toISOString(),
+			ariaLabel: "aria label",
+		});
+		expect(wrapper.findComponent(DatePicker).exists()).toBe(true);
+	});
+
+	// HELP
+	// it("should emit event on input", async () => {
+	// 	const wrapper = getWrapper({
+	// 		date: new Date().toISOString(),
+	// 		ariaLabel: "aria label",
+	// 	});
+
+	// 	wrapper.vm.showDateDialog = true;
+	// 	const v = wrapper.find("[data-testid='v']");
+
+	// 	v.vm.$emit("input");
+	// 	expect(wrapper.emitted("input")).toHaveLength(1);
+	// });
+});
