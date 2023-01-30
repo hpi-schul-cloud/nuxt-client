a61096e7f063cc650b86a400ad66d6591e073d3b
IgorCapCoder <IgorCapCoder@users.noreply.github.com>
2023-01-30 09:01:36 +0000

    npm run lint

--- src/pages/administration/StudentOverview.page.vue
+++ src/pages/administration/StudentOverview.page.vue
@@ -370,7 +370,9 @@ export default {
 			// filters out the lastLoginSystemChange and outdatedSince columns if FEATURE_SCHOOL_SANIS_USER_MIGRATION_ENABLED env is disabled
 			if (!this.getFeatureSchoolSanisUserMigrationEnabled) {
 				editedColumns = editedColumns.filter(
-					(col) => col.field !== "lastLoginSystemChange" && col.field !== "outdatedSince"
+					(col) =>
+						col.field !== "lastLoginSystemChange" &&
+						col.field !== "outdatedSince"
 				);
 			}
 
