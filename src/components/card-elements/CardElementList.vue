dc35cdf24b3c18cb42e2e1ebffbfcd1163188edd
Christian Darsow <christian.darsow@dataport.de>
2023-02-03 18:16:50 +0100

    Add add element at end of list

--- src/components/card-elements/CardElementList.vue
+++ src/components/card-elements/CardElementList.vue
@@ -1,36 +1,52 @@
 <template>
-	<draggable
-		ref="draggable"
-		v-model="elements"
-		:animation="400"
-		:delay="touchDelay"
-		handle=".handle"
-		ghost-class="ghost"
-		chosen-class="chosen"
-		drag-class="drag"
-		force-fallback="true"
-		@start="startDragging"
-		@end="endDragging"
-		@input="onSort"
-	>
-		<card-element-wrapper
-			v-for="(element, index) in elements"
-			ref="card-element"
-			:key="index"
-			v-model="element.model"
-			v-bind="element.props"
-			:disabled="dragInProgress"
-			@delete-element="deleteElement(index)"
-			@add-element="addElementAfter(index)"
-		/>
-	</draggable>
+	<div>
+		<draggable
+			ref="draggable"
+			v-model="elements"
+			:animation="400"
+			:delay="touchDelay"
+			handle=".handle"
+			ghost-class="ghost"
+			chosen-class="chosen"
+			drag-class="drag"
+			force-fallback="true"
+			@start="startDragging"
+			@end="endDragging"
+			@input="onSort"
+		>
+			<card-element-wrapper
+				v-for="(element, index) in elements"
+				ref="card-element"
+				:key="index"
+				v-model="element.model"
+				v-bind="element.props"
+				:disabled="dragInProgress"
+				@delete-element="deleteElement(index)"
+				@add-element="addElementAfter(index)"
+			/>
+		</draggable>
+		<v-card flat class="mb-6 ck-focused">
+			<v-col class="text-center">
+				<v-btn
+					icon
+					outlined
+					color="secondary"
+					class="add-element-btn"
+					data-testid="add-element-btn"
+					@click="addElement"
+				>
+					<v-icon>{{ mdiPlus }}</v-icon>
+				</v-btn>
+			</v-col>
+		</v-card>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, inject, ref } from "@vue/composition-api";
 import VueI18n from "vue-i18n";
 import { mdiPlus } from "@mdi/js";
-import CardElementWrapper from "@/components/card-elements/CardElement.vue";
+import CardElementWrapper from "@/components/card-elements/CardElementWrapper.vue";
 import { CardElementComponentEnum } from "@/store/types/card-element";
 import { CardElementResponseCardElementTypeEnum } from "@/serverApi/v3";
 import { useDrag } from "@/composables/drag";
@@ -64,10 +80,14 @@ export default defineComponent({
 				model: "",
 				props: {
 					component: CardElementComponentEnum.RichText,
-					placeholder: i18n.t("common.labels.description"),
+					placeholder: "common.labels.description",
 					editable: true,
 				},
 			});
+		};
+
+		const addElement = () => {
+			addElementAfter(elements.value.length);
 		};
 
 		const deleteElement = (index: number) => {
@@ -81,6 +101,7 @@ export default defineComponent({
 
 		return {
 			elements,
+			addElement,
 			addElementAfter,
 			deleteElement,
 			mdiPlus,
@@ -97,6 +118,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 .chosen,
 .drag {
+	border: solid thin var(--v-grey-lighten1);
 	box-shadow: 0 3px 3px -2px rgba(0, 0, 0, 0.2), 0 3px 4px 0 rgba(0, 0, 0, 0.14),
 		0 1px 8px 0 rgba(0, 0, 0, 0.12) !important;
 	opacity: 1 !important;
