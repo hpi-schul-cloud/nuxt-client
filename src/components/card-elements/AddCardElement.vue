d25787ea6361b134619931843ca2285beccf7f02
Christian Darsow <christian.darsow@dataport.de>
2023-02-06 16:34:36 +0100

    Styling smaller card

--- src/components/card-elements/AddCardElement.vue
+++ src/components/card-elements/AddCardElement.vue
@@ -41,7 +41,18 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+$btn-radius: 18;
+
 .dashed {
 	border: dashed thin var(--v-grey-base);
+}
+
+.add-element-btn {
+	position: absolute;
+	top: -#{$btn-radius / 3}px;
+	left: 50%;
+	/* stylelint-disable-next-line sh-waqar/declaration-use-variable */
+	margin-left: -#{$btn-radius}px;
+	background-color: var(--v-white-base);
 }
 </style>
