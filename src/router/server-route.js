811d5e851a8740f09b48a72606374e39ba5d4c62
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-02-06 10:08:09 +0100

    BC-3168 - add proxy for file storage

--- src/router/server-route.js
+++ src/router/server-route.js
@@ -1,7 +1,11 @@
 const isServer = (path) => {
 	return path.startsWith("/api/v");
 };
+const isFileStorage = (path) => {
+	return path.startsWith("/api/v3/file");
+};
 
 module.exports = {
 	isServer,
+	isFileStorage,
 };
