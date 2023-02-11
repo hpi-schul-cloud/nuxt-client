2fbf008ac0391c5e58ef2b651eeeeeefaf8ac269
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-02-11 14:14:48 +0100

    merge regexes into the validators consts as they are only used there, refactor ldapConstnts.js file unit tests a little bit, also add some more test cases

--- src/utils/ldapConstans.unit.ts
+++ src/utils/ldapConstans.unit.ts
@@ -1,62 +1,146 @@
 import {
 	ldapPathRegexValidator,
-	ldapSecuredURLRegex,
-	ldapURLRegex,
+	ldapURLRegexValidator,
+	ldapSecuredURLRegexValidator,
 } from "./ldapConstants";
 
+const ldapURLWithProtocolOnly = "ldap://";
+const ldapsURLWithProtocolOnly = "ldaps://";
+const ldapURLDomainWithPort = "ldap://asdf.de:123";
+const ldapURLDomainWithoutPort = "ldap://asdf.de";
+const ldapURLIPv4WithPort = "ldap://127.0.0.1:123";
+const ldapURLIPv4WithoutPort = "ldap://127.0.0.1";
+const ldapsURLDomainWithPort = "ldaps://asdf.de:123";
+const ldapsURLDomainWithoutPort = "ldaps://asdf.de";
+const ldapsURLIPv4WithPort = "ldaps://127.0.0.1:123";
+const ldapsURLIPv4WithoutPort = "ldaps://127.0.0.1";
+const randomDomain = "example.com";
+const randomText = "asdf";
+
 describe("ldapConstant", () => {
-	describe("ldapSecuredURLRegex", () => {
-		it("should validate domain without port", () => {
-			expect(ldapSecuredURLRegex.test("ldaps://asdf.de")).toBe(true);
+	describe("ldapPathRegexValidator", () => {
+		it("should accept simple LDAP path", () => {
+			expect(ldapPathRegexValidator("ou=groups")).toBe(true);
 		});
-		it("should validate domain with port", () => {
-			expect(ldapSecuredURLRegex.test("ldaps://asdf.de:123")).toBe(true);
+
+		it("should accept typical LDAP path", () => {
+			expect(ldapPathRegexValidator("o=school0,dc=de,dc=example,dc=org")).toBe(
+				true
+			);
 		});
-		it("should validate ipv4 with port", () => {
-			expect(ldapSecuredURLRegex.test("ldaps://127.0.0.1:123")).toBe(true);
+
+		it("should reject some random domain", () => {
+			expect(ldapPathRegexValidator(randomDomain)).toBe(false);
 		});
-		it("should validate ipv4 without port", () => {
-			expect(ldapSecuredURLRegex.test("ldaps://127.0.0.1")).toBe(true);
-		});
-		it("should fail domain with unsecure protocol", () => {
-			expect(ldapSecuredURLRegex.test("ldap://asdf.de")).toBe(false);
-		});
-		it("should fail ipv4 with unsecure protocol", () => {
-			expect(ldapSecuredURLRegex.test("ldap://127.0.0.1")).toBe(false);
-		});
-	});
-	describe("ldapURLRegex", () => {
-		it("should validate domain without port", () => {
-			expect(ldapURLRegex.test("ldaps://asdf.de")).toBe(true);
-		});
-		it("should validate domain with port", () => {
-			expect(ldapURLRegex.test("ldaps://asdf.de:123")).toBe(true);
-		});
-		it("should validate ipv4 with port", () => {
-			expect(ldapURLRegex.test("ldaps://127.0.0.1:123")).toBe(true);
-		});
-		it("should validate ipv4 without port", () => {
-			expect(ldapURLRegex.test("ldaps://127.0.0.1")).toBe(true);
-		});
-		it("should validate domain with unsecure protocol", () => {
-			expect(ldapURLRegex.test("ldap://asdf.de")).toBe(true);
-		});
-		it("should validate ipv4 with unsecure protocol", () => {
-			expect(ldapURLRegex.test("ldap://127.0.0.1")).toBe(true);
+
+		it("should reject some random text", () => {
+			expect(ldapPathRegexValidator(randomText)).toBe(false);
 		});
 	});
 
-	describe("ldapPathRegexValidator", () => {
-		it("should validate ldap path", () => {
-			const temp = ldapPathRegexValidator;
-			expect(temp("ou=groups")).toBe(true);
-			expect(temp("o=school0,dc=de,dc=example,dc=org")).toBe(true);
+	describe("ldapURLRegexValidator", () => {
+		it("should accept URL with ldaps:// protocol and domain without port", () => {
+			expect(ldapURLRegexValidator(ldapsURLDomainWithoutPort)).toBe(true);
 		});
-		it("should fail domain", () => {
-			expect(ldapPathRegexValidator("asdf.asdf")).toBe(false);
+
+		it("should accept URL with ldaps:// protocol and domain with port", () => {
+			expect(ldapURLRegexValidator(ldapsURLDomainWithPort)).toBe(true);
 		});
-		it("should fail text only", () => {
-			expect(ldapPathRegexValidator("asdf")).toBe(false);
+
+		it("should accept URL with ldaps:// protocol and IPv4 without port", () => {
+			expect(ldapURLRegexValidator(ldapsURLIPv4WithoutPort)).toBe(true);
+		});
+
+		it("should accept URL with ldaps:// protocol and IPv4 with port", () => {
+			expect(ldapURLRegexValidator(ldapsURLIPv4WithPort)).toBe(true);
+		});
+
+		it("should accept URL with ldap:// protocol and domain without port", () => {
+			expect(ldapURLRegexValidator(ldapURLDomainWithoutPort)).toBe(true);
+		});
+
+		it("should accept URL with ldap:// protocol and domain with port", () => {
+			expect(ldapURLRegexValidator(ldapURLDomainWithPort)).toBe(true);
+		});
+
+		it("should accept URL with ldap:// protocol and IPv4 without port", () => {
+			expect(ldapURLRegexValidator(ldapURLIPv4WithoutPort)).toBe(true);
+		});
+
+		it("should accept URL with ldap:// protocol and IPv4 with port", () => {
+			expect(ldapURLRegexValidator(ldapURLIPv4WithPort)).toBe(true);
+		});
+
+		it("should reject URL with just an ldap:// protocol", () => {
+			expect(ldapURLRegexValidator(ldapURLWithProtocolOnly)).toBe(false);
+		});
+
+		it("should reject URL with just an ldaps:// protocol", () => {
+			expect(ldapURLRegexValidator(ldapsURLWithProtocolOnly)).toBe(false);
+		});
+
+		it("should reject URL with just a domain", () => {
+			expect(ldapURLRegexValidator(randomDomain)).toBe(false);
+		});
+
+		it("should reject some random text", () => {
+			expect(ldapURLRegexValidator(randomText)).toBe(false);
+		});
+	});
+
+	describe("ldapSecuredURLRegexValidator", () => {
+		it("should accept URL with ldaps:// protocol and domain without port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapsURLDomainWithoutPort)).toBe(
+				true
+			);
+		});
+
+		it("should accept URL with ldaps:// protocol and domain with port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapsURLDomainWithPort)).toBe(true);
+		});
+
+		it("should accept URL with ldaps:// protocol and IPv4 without port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapsURLIPv4WithoutPort)).toBe(true);
+		});
+
+		it("should accept URL with ldaps:// protocol and IPv4 with port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapsURLIPv4WithPort)).toBe(true);
+		});
+
+		it("should reject URL with ldap:// protocol and domain without port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapURLDomainWithoutPort)).toBe(
+				false
+			);
+		});
+
+		it("should reject URL with ldap:// protocol and domain with port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapURLDomainWithPort)).toBe(false);
+		});
+
+		it("should reject URL with ldap:// protocol and IPv4 without port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapURLIPv4WithoutPort)).toBe(false);
+		});
+
+		it("should reject URL with ldap:// protocol and IPv4 with port", () => {
+			expect(ldapSecuredURLRegexValidator(ldapURLIPv4WithPort)).toBe(false);
+		});
+
+		it("should reject URL with just an ldaps:// protocol", () => {
+			expect(ldapSecuredURLRegexValidator(ldapsURLWithProtocolOnly)).toBe(
+				false
+			);
+		});
+
+		it("should reject URL with just an ldap:// protocol", () => {
+			expect(ldapSecuredURLRegexValidator(ldapURLWithProtocolOnly)).toBe(false);
+		});
+
+		it("should reject URL with just a domain", () => {
+			expect(ldapSecuredURLRegexValidator(randomDomain)).toBe(false);
+		});
+
+		it("should reject some random text", () => {
+			expect(ldapSecuredURLRegexValidator(randomText)).toBe(false);
 		});
 	});
 });
--- src/utils/ldapConstants.js
+++ src/utils/ldapConstants.js
@@ -1,38 +1,34 @@
 import { helpers } from "vuelidate/lib/validators";
 
-/**
- * Regex for the LDAP path.
- *
- * The composition of this regex is as follows:
- *  attributeType = [A-Za-z][\w-]*|\d+(?:\.\d+)*
- *  attributeValue = #(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*"
- *  nameComponent = (?#attributeType)=(?#attributeValue)(?:\+(?#attributeType)=(?#attributeValue))*
- *  and finally
- *  ldapPathRegex = (?#nameComponent)(?:,(?#nameComponent))*
- *
- *  It matches the following format: nameComponent[, or ;;]nameComponent
- */
-export const ldapPathRegex =
-	/^(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})+|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*")(?:\+(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*"))*(?:(,|;;)(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})+|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*")(?:\+(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*"))*)*$/;
-
-// Regex for the regular (possibly unsecured) LDAP URL. Allows both ldap:// and ldaps:// protocols.
-export const ldapURLRegex =
-	/^ldaps?:\/\/(([\w\.-]+\.[a-z]+)|((\d{0,3}\.){3}\d{0,3}))(:\d+)?$/;
-
-// Regex for the secured LDAP URL. Allows only the ldaps:// protocol.
-export const ldapSecuredURLRegex =
-	/^ldaps:\/\/(([\w\.-]+\.[a-z]+)|((\d{0,3}\.){3}\d{0,3}))(:\d+)?$/;
-
 // Regex-based validator for the LDAP path.
-export const ldapPathRegexValidator = helpers.regex("alpha", ldapPathRegex);
+//
+// The composition of this regex is as follows:
+//   attributeType = [A-Za-z][\w-]*|\d+(?:\.\d+)*
+//   attributeValue = #(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*"
+//   nameComponent = (?#attributeType)=(?#attributeValue)(?:\+(?#attributeType)=(?#attributeValue))*
+// and finally
+//   ldapPathRegex = (?#nameComponent)(?:,(?#nameComponent))*
+//
+// It matches the following format: nameComponent[, or ;;]nameComponent
+export const ldapPathRegexValidator = helpers.regex(
+	"alpha",
+	/^(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})+|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*")(?:\+(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*"))*(?:(,|;;)(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})+|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*")(?:\+(?:[A-Za-z][\w-]*|\d+(?:\.\d+)*)=(?:#(?:[\dA-Fa-f]{2})+|(?:[^,=\+<>#;\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*|"(?:[^\\"]|\\[,=\+<>#;\\"]|\\[\dA-Fa-f]{2})*"))*)*$/
+);
 
-// Regex-based validator for the LDAP URL (possibly unsecured). Not recommended for use in production environment.
-export const ldapURLRegexValidator = helpers.regex("alpha", ldapURLRegex);
+// Regex-based validator for the LDAP URL (possibly unsecured).
+// Allows both ldap:// and ldaps:// protocols.
+// Not recommended for use in production environment.
+export const ldapURLRegexValidator = helpers.regex(
+	"alpha",
+	/^ldaps?:\/\/(([\w\.-]+\.[a-z]+)|((\d{0,3}\.){3}\d{0,3}))(:\d+)?$/
+);
 
-// Regex-based validator for the secured LDAP URL. Should be used in every production environment.
+// Regex-based validator for the secured LDAP URL.
+// Allows only the ldaps:// protocol.
+// Should be used in every production environment.
 export const ldapSecuredURLRegexValidator = helpers.regex(
 	"alpha",
-	ldapSecuredURLRegex
+	/^ldaps:\/\/(([\w\.-]+\.[a-z]+)|((\d{0,3}\.){3}\d{0,3}))(:\d+)?$/
 );
 
 // Placeholder used for an unchanged password.
