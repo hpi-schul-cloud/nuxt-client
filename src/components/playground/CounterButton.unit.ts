10380013b5c993a4b072b8150283486b8f17c399
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2023-02-16 21:14:47 +0100

    easier mocking of store getters

--- src/components/playground/CounterButton.unit.ts
+++ src/components/playground/CounterButton.unit.ts
@@ -1,52 +1,22 @@
+import { useBoardStore } from "@/store/board.store";
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
 import { createTestingPinia } from "@pinia/testing";
 import { mount } from "@vue/test-utils";
-import { PiniaPlugin, PiniaPluginContext, PiniaVuePlugin } from "pinia";
 import CounterButton from "./CounterButton.vue";
-
-// idea from https://github.com/vuejs/pinia/pull/1201
-const mockPiniaGetters = (
-	mockGetters: Record<string, any> = {}
-): PiniaPlugin => {
-	const piniaPlugin: PiniaPlugin = (context: PiniaPluginContext) => {
-		const { store, options } = context;
-		const getters = options.getters || {};
-
-		if (mockGetters[store.$id]) {
-			Object.keys(getters).forEach((getter) => {
-				Object.defineProperty(store, getter, {
-					//@ts-ignore
-					get: () => mockGetters[store.$id][getter] || getters[getter],
-				});
-			});
-		}
-	};
-	return piniaPlugin;
-};
 
 describe("counter button", () => {
 	it("renders the board name", () => {
-		const componentOptions = createComponentMocks({});
-
-		const { localVue } = componentOptions;
-		localVue.use(PiniaVuePlugin);
 		const testingPinia = createTestingPinia();
+		const boardStore = useBoardStore();
 
-		testingPinia.use(
-			mockPiniaGetters({
-				boards: {
-					getName: "TestName",
-				},
-			})
-		);
+		jest.spyOn(boardStore, "getName", "get").mockReturnValue("TestName");
 
 		const wrapper = mount(CounterButton, {
-			...componentOptions,
+			...createComponentMocks({}),
 			pinia: testingPinia,
 		});
 
 		const button = wrapper.find("[data-testid='btn-update-board-name']");
-		console.log(button.text());
 		expect(button.text()).toBe("TestName");
 	});
 });
