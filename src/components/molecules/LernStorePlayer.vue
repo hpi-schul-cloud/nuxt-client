4f4363fb741e3c89a90fa0547b7dd914c6c7dbfc
stekrause <stephan.krause.ext@capgemini.com>
2023-02-17 14:42:37 +0100

    fix iframeSrc and scriptSrc of h5p elements

--- src/components/molecules/LernStorePlayer.vue
+++ src/components/molecules/LernStorePlayer.vue
@@ -10,14 +10,6 @@
 	<div v-else class="d-flex justify-center align-center min-height-screen">
 		<v-progress-circular indeterminate color="secondary" size="115" />
 	</div>
-	<!--
-						v-if="isIFrameLoading"
-						<v-progress-circular
-						v-else
-						class="loading"
-						:size="150"
-						indeterminate
-					></v-progress-circular> -->
 </template>
 
 <script :src="scriptSrc" charset="UTF-8"></script>
@@ -40,11 +32,14 @@ export default {
 	},
 	methods: {
 		async getPlayer() {
-			const response = await this.$axios.get(
-				`/v1/edu-sharing/player/${this.nodeId}`
-			);
-			this.iframeSrc = response.iframe_src;
-			this.scriptSrc = response.script_src;
+			await this.$axios
+				.get(`/v1/edu-sharing/player/${this.nodeId}`)
+				.then(
+					(response) => (
+						(this.iframeSrc = response.data.iframe_src),
+						(this.scriptSrc = response.data.script_src)
+					)
+				);
 			this.loading = false;
 		},
 	},
