67d984ad1e75fd58f09ddbb0a9a7bf9a521d986f
Christian Darsow <christian.darsow@dataport.de>
2023-03-03 08:00:45 +0100

    Add tests

--- src/components/date-time-picker/DateTimePicker.unit.ts
+++ src/components/date-time-picker/DateTimePicker.unit.ts
@@ -23,7 +23,7 @@ describe("@components/date-time-picker/DateTimePicker", () => {
 		expect(wrapper.findComponent(DateTimePicker).exists()).toBe(true);
 	});
 
-	it("should emit input event on input", async () => {
+	it("should emit input event on date input", async () => {
 		const wrapper = getWrapper({
 			dateTime: new Date().toISOString(),
 		});
@@ -37,5 +37,49 @@ describe("@components/date-time-picker/DateTimePicker", () => {
 		await wrapper.vm.$nextTick();
 
 		expect(wrapper.emitted("input")).toHaveLength(1);
+	});
+
+	it("should emit input event on time input", async () => {
+		const wrapper = getWrapper({
+			dateTime: new Date().toISOString(),
+		});
+
+		const timePicker = wrapper.findComponent({ name: "time-picker" });
+		expect(timePicker.exists()).toBe(true);
+		timePicker.vm.$emit("input", "00:00");
+
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted("input")).toHaveLength(1);
+	});
+
+	it("should emit error event on invalid date input", async () => {
+		const wrapper = getWrapper({
+			dateTime: new Date().toISOString(),
+		});
+
+		const datePicker = wrapper.findComponent({ name: "date-picker" });
+		expect(datePicker.exists()).toBe(true);
+		datePicker.vm.$emit("error");
+
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted("error")).toHaveLength(1);
+		expect(wrapper.emitted("input")).toBe(undefined);
+	});
+
+	it("should emit error event on invalid time input", async () => {
+		const wrapper = getWrapper({
+			dateTime: new Date().toISOString(),
+		});
+
+		const timePicker = wrapper.findComponent({ name: "time-picker" });
+		expect(timePicker.exists()).toBe(true);
+		timePicker.vm.$emit("error");
+
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted("error")).toHaveLength(1);
+		expect(wrapper.emitted("input")).toBe(undefined);
 	});
 });
