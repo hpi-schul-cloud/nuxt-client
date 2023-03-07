b9cadd5700465c47e2f33ae087e08869ae746305
Marvin Öhlerking <103562092+MarvinOehlerkingCap@users.noreply.github.com>
2023-03-07 15:52:28 +0100

    N21-509 Migration finish page (#2419)
    - adds user migration finish page

--- src/store/user-login-migration.unit.ts
+++ src/store/user-login-migration.unit.ts
@@ -3,37 +3,19 @@ import {
 	MigrationLinkRequest,
 	MigrationLinks,
 	MigrationPageOrigin,
-} from "./types/user-migration";
-import UserMigrationModule from "./user-migration";
+} from "./types/user-login-migration";
+import UserLoginMigrationModule from "./user-login-migration";
 
-// const createAxiosResponse = <T>(data: T): AxiosResponse<T> => ({
-// 	data,
-// 	status: 200,
-// 	statusText: "OK",
-// 	headers: {},
-// 	config: {},
-// });
-
-describe("UserMigrationModule", () => {
-	let module: UserMigrationModule;
+describe("UserLoginMigrationModule", () => {
+	let module: UserLoginMigrationModule;
 
 	beforeEach(() => {
-		module = new UserMigrationModule({});
+		module = new UserLoginMigrationModule({});
 	});
 
 	afterEach(() => {
 		jest.resetAllMocks();
 	});
-
-	// const mockApi = () => {
-	// 	const apiMock: jest.Mocked<UserMigrationApiInterface> = {
-	// 		userMigrationControllerGetMigrationPageDetails: jest.fn(),
-	// 	};
-
-	// 	jest.spyOn(serverApi, "UserMigrationApiFactory").mockReturnValue(apiMock);
-
-	// 	return { apiMock };
-	// };
 
 	describe("getter/setter", () => {
 		describe("Loading", () => {
