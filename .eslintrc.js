46ec55f7290bbcf5df783a339f6ae52fad46880f
Oliver Happe <oliver.happe@dataport.de>
2023-03-10 14:52:10 +0100

    fixes the last ban-types errors and activates the rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
 		"no-undef": NUXT_REMOVAL ? "warn" : "error",
 		"prefer-const": "error",
 		"@typescript-eslint/no-inferrable-types": NUXT_REMOVAL ? "warn" : "error",
-		"@typescript-eslint/ban-types": "warn",
+		"@typescript-eslint/ban-types": "error",
 		"prettier/prettier": "error",
 		"no-var": "error",
 		"vue/no-v-text-v-html-on-component": "warn",
