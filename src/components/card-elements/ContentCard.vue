8f44148fa7880328b4ab48e8aba060ed7d1a3300
Murat Merdoglu <murat.merdoglu@dataport.de>
2023-03-15 13:44:41 +0100

    create new component file

---
+++ src/components/card-elements/ContentCard.vue
@@ -0,0 +1,15 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "ContentCard",
+	props: {},
+	setup() {
+		return {};
+	},
+});
+</script>
