8f78daf35534536fc1002d26057ff955b527051b
MartinSchuhmacher <mschuhmacher@gmx.de>
2023-03-17 11:01:32 +0100

    add visibility option of task card as disabled

--- src/pages/TaskCard.page.vue
+++ src/pages/TaskCard.page.vue
@@ -16,6 +16,15 @@
 				disabled
 				:label="$t('common.labels.course')"
 			/>
+			<v-select
+				v-model="isVisible"
+				:items="visibilityOptions"
+				item-value="value"
+				item-text="text"
+				filled
+				disabled
+				:label="$t('common.labels.visibility')"
+			/>
 			<date-time-picker
 				class="mb-4"
 				required
@@ -65,7 +74,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, ref, onMounted, computed } from "vue";
+import { defineComponent, inject, ref, onMounted, computed, Ref } from "vue";
 import { useTitle } from "@vueuse/core";
 import { useRouter, useRoute } from "vue-router/composables";
 import VueI18n from "vue-i18n";
@@ -129,6 +138,17 @@ export default defineComponent({
 
 		const course = ref("");
 		const courses = ref<object[]>([]);
+		const isVisible: Ref<boolean> = ref(true);
+		const visibilityOptions = ref<object[]>([
+			{
+				text: t("common.labels.visible"),
+				value: true,
+			},
+			{
+				text: t("common.labels.notVisible"),
+				value: false,
+			},
+		]);
 		const title = ref<CardElement>({
 			id: "",
 			type: CardElementResponseCardElementTypeEnum.Title,
@@ -182,6 +202,7 @@ export default defineComponent({
 						title: taskCardData.courseName || "",
 					},
 				];
+				isVisible.value = !taskCardData.task.status.isDraft;
 				dueDate.value = taskCardData.dueDate;
 				initElements(taskCardData.cardElements);
 
@@ -338,6 +359,8 @@ export default defineComponent({
 			onError,
 			minDate,
 			maxDate,
+			isVisible,
+			visibilityOptions,
 		};
 	},
 });
