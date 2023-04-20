dc9d6398370022b7915351f70b97aa5544d3b155
Christian Darsow <christian.darsow@dataport.de>
2023-04-20 11:39:00 +0200

    Add tests for course assignment

--- src/components/beta-task/UserSelector.unit.ts
+++ src/components/beta-task/UserSelector.unit.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
 import { MountOptions, mount, Wrapper } from "@vue/test-utils";
 import UserSelector from "@/components/beta-task/UserSelector.vue";
-import { User } from "./types/User";
+import { User, COURSE_ASSIGNMENT } from "./types/User";
 
 const MOCK_USERS: User[] = [
 	{
@@ -27,6 +27,7 @@ const MOCK_SELECTED_USERS: User[] = [MOCK_USERS[1]];
 const defaultProps = {
 	users: MOCK_USERS,
 	selection: MOCK_SELECTED_USERS,
+	courseAssignment: false,
 	required: true,
 };
 
@@ -115,6 +116,56 @@ describe("@components/beta-task/UserSelector", () => {
 
 			jest.advanceTimersByTime(1000);
 			expect(wrapper.emitted("input")).toHaveLength(1);
+		});
+	});
+
+	describe("when mounted with courseAssignment", () => {
+		it("should render with all available users selected", async () => {
+			setup({ ...defaultProps, selection: [], courseAssignment: true });
+
+			const selectedUsers = wrapper.findAll(".v-chip__content");
+			expect(selectedUsers.length).toEqual(MOCK_USERS.length);
+		});
+	});
+
+	describe("when mounted without courseAssignment", () => {
+		it("should render with selected users", async () => {
+			setup();
+
+			const selectedUsers = wrapper.findAll(".v-chip__content");
+			expect(selectedUsers.length).toEqual(MOCK_SELECTED_USERS.length);
+		});
+	});
+
+	describe("when all available students are selected", () => {
+		it("should emit input with course assignment info", async () => {
+			setup({ ...defaultProps, selection: MOCK_USERS });
+			jest.useFakeTimers();
+			const selectBox = wrapper
+				.findComponent({ name: "v-autocomplete" })
+				.find("input");
+			await selectBox.trigger("blur");
+
+			jest.advanceTimersByTime(1000);
+			const emitted = wrapper.emitted("input") || [[]];
+			expect(emitted[0][0]).toBeDefined();
+			expect(emitted[0][0]).toStrictEqual(COURSE_ASSIGNMENT);
+		});
+	});
+
+	describe("when not all available students are selected", () => {
+		it("should emit input with user infos", async () => {
+			setup({ ...defaultProps, selection: MOCK_SELECTED_USERS });
+			jest.useFakeTimers();
+			const selectBox = wrapper
+				.findComponent({ name: "v-autocomplete" })
+				.find("input");
+			await selectBox.trigger("blur");
+
+			jest.advanceTimersByTime(1000);
+			const emitted = wrapper.emitted("input") || [[]];
+			expect(emitted[0][0]).toBeDefined();
+			expect(emitted[0][0]).toStrictEqual(MOCK_SELECTED_USERS);
 		});
 	});
 });
