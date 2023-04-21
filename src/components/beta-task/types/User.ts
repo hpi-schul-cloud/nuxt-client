40ceefece8a5e6ee45b449b1feb90b6c5b4bce7a
Christian Darsow <christian.darsow@dataport.de>
2023-04-21 16:17:07 +0200

    Add validation rule (temporaly)

--- src/components/beta-task/types/User.ts
+++ src/components/beta-task/types/User.ts
@@ -3,3 +3,5 @@ import { UsersList } from "@/serverApi/v3";
 export type User = UsersList;
 
 export const COURSE_ASSIGNMENT = "course_assignment";
+
+export type ValidationRule = (value: [] | null) => boolean | string;
