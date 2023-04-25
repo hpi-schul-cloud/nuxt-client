c1f1e10c123bd9d1f13422ecb97ab81d1fdf3487
Christian Darsow <christian.darsow@dataport.de>
2023-04-25 18:03:00 +0200

    Remove tempory type

--- src/components/beta-task/types/User.ts
+++ src/components/beta-task/types/User.ts
@@ -3,5 +3,3 @@ import { UsersList } from "@/serverApi/v3";
 export type User = UsersList;
 
 export const COURSE_ASSIGNMENT = "course_assignment";
-
-export type ValidationRule = (value: [] | null) => boolean | string;
