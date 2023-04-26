e9ac1adaff69a3f7e29ed271925a256167b9e2f2
Murat Merdoglu <murat.merdoglu@dataport.de>
2023-04-26 16:02:38 +0200

    add BoardApi.composable unit test

---
+++ src/components/feature-board/shared/BoardApi.composable.unit.ts
@@ -0,0 +1,216 @@
+import { BoardCardApiInterface } from "@/serverApi/v3";
+import { useBoardApi } from "./BoardApi.composable";
+import * as serverApi from "../../../serverApi/v3/api";
+import * as axios from "axios";
+import { initializeAxios } from "@/utils/api";
+
+jest.mock("axios");
+
+const createColumnResponseMock = {
+	id: "columnId",
+	title: "columnTitle",
+	cards: [],
+	timestamps: {
+		lastUpdatedAt: "",
+		createdAt: "",
+		deletedAt: "",
+	},
+};
+const mockApi = {
+	boardControllerCreateColumn: jest
+		.fn()
+		.mockImplementation(() => createColumnResponseMock),
+	cardControllerUpdateCardTitle: jest.fn(),
+	columnControllerUpdateColumnTitle: jest.fn(),
+	cardControllerCreateElement: jest.fn(),
+	cardControllerDeleteCard: jest.fn(),
+	columnControllerDeleteColumn: jest.fn(),
+	columnControllerCreateCard: jest
+		.fn()
+		.mockImplementation(() => ({ data: { ...createColumnResponseMock } })),
+	cardControllerMoveCard: jest.fn(),
+	columnControllerMoveColumn: jest.fn(),
+};
+
+initializeAxios({
+	request: async (path: string) => ({
+		data: "some data",
+	}),
+} as axios.AxiosInstance);
+
+describe("BoardApi.composable", () => {
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	describe("createColumnCall", () => {
+		it("should call boardControllerCreateColumn api", async () => {
+			jest
+				.spyOn(serverApi, "BoardApiFactory")
+				.mockReturnValue(mockApi as unknown as serverApi.BoardApiInterface);
+			const { createColumnCall } = useBoardApi();
+			const BOARD_ID = "test-board-id";
+
+			await createColumnCall(BOARD_ID);
+			expect(mockApi.boardControllerCreateColumn).toHaveBeenCalledWith(
+				BOARD_ID
+			);
+		});
+	});
+
+	describe("updateCardTitle", () => {
+		it("should call cardControllerUpdateCardTitle api", async () => {
+			jest
+				.spyOn(serverApi, "BoardCardApiFactory")
+				.mockReturnValue(mockApi as unknown as serverApi.BoardCardApiInterface);
+			const { updateCardTitle } = useBoardApi();
+			const PAYLOAD = {
+				id: "update-card-id",
+				title: "update-title",
+			};
+
+			await updateCardTitle(PAYLOAD.id, PAYLOAD.title);
+			expect(mockApi.cardControllerUpdateCardTitle).toHaveBeenCalledWith(
+				PAYLOAD.id,
+				{ title: PAYLOAD.title }
+			);
+		});
+	});
+
+	describe("updateColumnTitleCall", () => {
+		it("should call columnControllerUpdateColumnTitle api", async () => {
+			jest
+				.spyOn(serverApi, "BoardColumnApiFactory")
+				.mockReturnValue(
+					mockApi as unknown as serverApi.BoardColumnApiInterface
+				);
+			const { updateColumnTitleCall } = useBoardApi();
+			const PAYLOAD = {
+				id: "update-column-id",
+				title: "update-title",
+			};
+
+			await updateColumnTitleCall(PAYLOAD.id, PAYLOAD.title);
+			expect(mockApi.columnControllerUpdateColumnTitle).toHaveBeenCalledWith(
+				PAYLOAD.id,
+				{ title: PAYLOAD.title }
+			);
+		});
+	});
+
+	describe("createElement", () => {
+		it("should call cardControllerCreateElement api", async () => {
+			jest
+				.spyOn(serverApi, "BoardColumnApiFactory")
+				.mockReturnValue(
+					mockApi as unknown as serverApi.BoardColumnApiInterface
+				);
+			const { createElement } = useBoardApi();
+			const PAYLOAD = "card-id";
+
+			await createElement(PAYLOAD);
+			expect(mockApi.cardControllerCreateElement).toHaveBeenCalledWith(PAYLOAD);
+		});
+	});
+
+	describe("deleteCardCall", () => {
+		it("should call cardControllerDeleteCard api", async () => {
+			jest
+				.spyOn(serverApi, "BoardCardApiFactory")
+				.mockReturnValue(mockApi as unknown as serverApi.BoardCardApiInterface);
+			const { deleteCardCall } = useBoardApi();
+			const PAYLOAD = "card-id";
+
+			await deleteCardCall(PAYLOAD);
+			expect(mockApi.cardControllerDeleteCard).toHaveBeenCalledWith(PAYLOAD);
+		});
+	});
+
+	describe("deleteColumnCall", () => {
+		it("should call columnControllerDeleteColumn api", async () => {
+			jest
+				.spyOn(serverApi, "BoardColumnApiFactory")
+				.mockReturnValue(
+					mockApi as unknown as serverApi.BoardColumnApiInterface
+				);
+			const { deleteColumnCall } = useBoardApi();
+			const PAYLOAD = "column-id";
+
+			await deleteColumnCall(PAYLOAD);
+			expect(mockApi.columnControllerDeleteColumn).toHaveBeenCalledWith(
+				PAYLOAD
+			);
+		});
+	});
+
+	describe("createCardCall", () => {
+		it("should call columnControllerDeleteColumn api", async () => {
+			jest
+				.spyOn(serverApi, "BoardCardApiFactory")
+				.mockReturnValue(mockApi as unknown as serverApi.BoardCardApiInterface);
+			const { createCardCall } = useBoardApi();
+			const PAYLOAD = "column-id";
+
+			await createCardCall(PAYLOAD);
+			expect(mockApi.columnControllerCreateCard).toHaveBeenCalledWith(PAYLOAD);
+		});
+	});
+
+	describe("moveCardCall", () => {
+		it("should call cardControllerMoveCard api", async () => {
+			jest
+				.spyOn(serverApi, "BoardCardApiFactory")
+				.mockReturnValue(mockApi as unknown as serverApi.BoardCardApiInterface);
+			const { moveCardCall } = useBoardApi();
+			const PAYLOAD = {
+				cardId: "card-id",
+				position: {
+					toColumnId: "col-id",
+					toPosition: 3,
+				},
+			};
+
+			await moveCardCall(
+				PAYLOAD.cardId,
+				PAYLOAD.position.toColumnId,
+				PAYLOAD.position.toPosition
+			);
+			expect(mockApi.cardControllerMoveCard).toHaveBeenCalledWith(
+				PAYLOAD.cardId,
+				{
+					...PAYLOAD.position,
+				}
+			);
+		});
+	});
+
+	describe("moveColumnCall", () => {
+		it("should call columnControllerMoveColumn api", async () => {
+			jest
+				.spyOn(serverApi, "BoardColumnApiFactory")
+				.mockReturnValue(
+					mockApi as unknown as serverApi.BoardColumnApiInterface
+				);
+			const { moveColumnCall } = useBoardApi();
+			const PAYLOAD = {
+				columnId: "column-id",
+				position: {
+					toBoardId: "board-id",
+					toPosition: 3,
+				},
+			};
+
+			await moveColumnCall(
+				PAYLOAD.columnId,
+				PAYLOAD.position.toBoardId,
+				PAYLOAD.position.toPosition
+			);
+			expect(mockApi.columnControllerMoveColumn).toHaveBeenCalledWith(
+				PAYLOAD.columnId,
+				{
+					...PAYLOAD.position,
+				}
+			);
+		});
+	});
+});
