650b3246cb0d6a35037efa73835224b2d2cadda3
Odalys Adam <odalys.adam@dataport.de>
2023-04-27 20:16:26 +0200

    add more unit test to TaskStudentView

--- src/components/beta-task/TaskStudentView.unit.ts
+++ src/components/beta-task/TaskStudentView.unit.ts
@@ -7,17 +7,23 @@ import TaskStudentView from "@/components/beta-task/TaskStudentView.vue";
 import { TaskCard } from "@/store/types/beta-task/beta-task";
 
 const mockBetaTaskData = betaTaskFactory();
+const mockCompletedBetaTaskData = betaTaskFactory({
+	completedBy: ["0000d231816abba584714c9e"],
+});
+
+let taskCardModuleMock = createModuleMocks(TaskCardModule);
 
 const getWrapper = (task: TaskCard, completed = false) => {
+	taskCardModuleMock = createModuleMocks(TaskCardModule, {
+		getCompletedForStudent: completed,
+	});
 	return mount(TaskStudentView, {
 		...createComponentMocks({
 			i18n: true,
 		}),
 		provide: {
 			i18n: { t: (key: string) => key },
-			taskCardModule: createModuleMocks(TaskCardModule, {
-				getCompletedForStudent: completed,
-			}),
+			taskCardModule: taskCardModuleMock,
 		},
 		propsData: task,
 	});
@@ -27,5 +33,47 @@ describe("TaskStudentView", () => {
 	it("should render component", () => {
 		const wrapper = getWrapper(mockBetaTaskData as TaskCard);
 		expect(wrapper.findComponent(TaskStudentView).exists()).toBe(true);
+	});
+
+	describe("when task is not completed", () => {
+		it("should set checkbox to false", () => {
+			const wrapper = getWrapper(mockBetaTaskData as TaskCard);
+			expect(wrapper.findComponent(TaskStudentView).exists()).toBe(true);
+
+			const checkBox = wrapper.findComponent({ name: "v-checkbox" });
+			expect(checkBox.exists()).toBe(true);
+			expect(checkBox.classes()).not.toContain("v-input--is-label-active");
+		});
+
+		it("should undo completion on click", async () => {
+			const wrapper = getWrapper(mockCompletedBetaTaskData as TaskCard);
+			const checkBox = wrapper.findComponent({ name: "v-checkbox" });
+			checkBox.vm.$emit("change", true);
+			await wrapper.vm.$nextTick();
+
+			expect(taskCardModuleMock.completeTaskCard).toHaveBeenCalledTimes(1);
+		});
+	});
+
+	describe("when task is completed", () => {
+		it("should set checkbox to true", () => {
+			const wrapper = getWrapper(mockCompletedBetaTaskData as TaskCard, true);
+			expect(wrapper.findComponent(TaskStudentView).exists()).toBe(true);
+
+			const checkBox = wrapper.findComponent({ name: "v-checkbox" });
+			expect(checkBox.exists()).toBe(true);
+			expect(checkBox.classes()).toContain("v-input--is-label-active");
+		});
+
+		it("should undo completion on click", async () => {
+			const wrapper = getWrapper(mockCompletedBetaTaskData as TaskCard, true);
+			const checkBox = wrapper.findComponent({ name: "v-checkbox" });
+			checkBox.vm.$emit("change", false);
+			await wrapper.vm.$nextTick();
+
+			expect(
+				taskCardModuleMock.undoCompletionForTaskCard
+			).toHaveBeenCalledTimes(1);
+		});
 	});
 });
