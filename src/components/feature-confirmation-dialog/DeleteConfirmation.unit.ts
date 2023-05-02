490ffdc57ed2fde633d32c52433974ea0c774192
Oliver Happe <oliver.happe@dataport.de>
2023-05-02 15:39:48 +0200

    fix delete-confirmation test

--- src/components/feature-confirmation-dialog/DeleteConfirmation.unit.ts
+++ src/components/feature-confirmation-dialog/DeleteConfirmation.unit.ts
@@ -1,18 +1,33 @@
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
 import { MountOptions, shallowMount, Wrapper } from "@vue/test-utils";
-import Vue from "vue";
+import Vue, { ref } from "vue";
+import { useInternalDeleteConfirmation } from "./delete-confirmation.composable";
 import DeleteConfirmation from "./DeleteConfirmation.vue";
 
-describe(DeleteConfirmation.name, () => {
-	let wrapper: Wrapper<Vue>;
+jest.mock("./delete-confirmation.composable");
+const mockedUseInternalDeleteConfirmation = jest.mocked(
+	useInternalDeleteConfirmation
+);
 
+describe(DeleteConfirmation.name, () => {
+	const confirmSpy = jest.fn();
+	const cancelSpy = jest.fn();
 	const setup = (options: {
 		isDeleteModalOpen?: boolean;
 		title?: string;
 		typeName?: string;
 	}) => {
 		document.body.setAttribute("data-app", "true");
-		wrapper = shallowMount(DeleteConfirmation as MountOptions<Vue>, {
+
+		mockedUseInternalDeleteConfirmation.mockReturnValue({
+			confirm: confirmSpy,
+			cancel: cancelSpy,
+			askInternal: jest.fn(),
+			dialogOptions: ref({ message: "TestMessage" }),
+			isDialogOpen: ref(true),
+		});
+
+		const wrapper = shallowMount(DeleteConfirmation as MountOptions<Vue>, {
 			...createComponentMocks({ i18n: true }),
 			provide: {
 				i18n: { t: (key: string) => key },
@@ -23,32 +38,29 @@ describe(DeleteConfirmation.name, () => {
 				typeName: options.typeName || "card",
 			},
 		});
+		return wrapper;
 	};
 
 	describe("when component is mounted", () => {
 		it("should be found in dom", () => {
-			setup({});
+			const wrapper = setup({});
 			expect(wrapper.findComponent(DeleteConfirmation).exists()).toBe(true);
 		});
 	});
 
 	describe("when a dialog button clicked", () => {
-		it("should emit 'delete-confirm' if 'remove' button clicked", () => {
-			setup({});
+		it("should broadcast 'confirm' if 'remove' button clicked", () => {
+			const wrapper = setup({});
 			const dialog = wrapper.findComponent({ name: "vCustomDialog" });
 			dialog.vm.$emit("dialog-confirmed");
-			const emitted = wrapper.emitted();
-
-			expect(emitted["delete-confirm"]).toBeDefined();
+			expect(confirmSpy).toHaveBeenCalled();
 		});
 
 		it("should emit 'dialog-cancel' if 'cancel' button clicked", () => {
-			setup({});
+			const wrapper = setup({});
 			const dialog = wrapper.findComponent({ name: "vCustomDialog" });
 			dialog.vm.$emit("dialog-closed");
-			const emitted = wrapper.emitted();
-
-			expect(emitted["dialog-cancel"]).toBeDefined();
+			expect(cancelSpy).toHaveBeenCalled();
 		});
 	});
 });
