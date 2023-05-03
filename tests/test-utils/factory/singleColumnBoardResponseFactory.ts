ce9f267092b0f59f09e84a7d9b5df98d50047d4d
Christian Darsow <christian.darsow@dataport.de>
2023-05-03 13:39:31 +0200

    Remove comments

--- tests/test-utils/factory/singleColumnBoardResponseFactory.ts
+++ tests/test-utils/factory/singleColumnBoardResponseFactory.ts
@@ -1,21 +1,3 @@
-// import { SingleColumnBoardResponse } from "@/serverApi/v3";
-
-// const defaultRoom: SingleColumnBoardResponse = {
-// 	roomId: "123",
-// 	title: "room",
-// 	displayColor: "#ff00ff",
-// 	elements: [],
-// };
-
-// export const roomFactory = (
-// 	overwrites: Partial<SingleColumnBoardResponse> = {}
-// ): SingleColumnBoardResponse => {
-// 	return {
-// 		...defaultRoom,
-// 		...overwrites,
-// 	};
-// };
-
 import { Factory } from "fishery";
 import { SingleColumnBoardResponse } from "@/serverApi/v3";
 
