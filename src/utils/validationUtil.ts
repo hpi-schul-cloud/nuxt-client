3dded49b29119841ee5ce0a29daf44d5431aeac3
Arne Gnisa <arne.gnisa@capgemini.com>
2023-05-10 10:04:33 +0200

    N21-887 adapts officialSchoolNumber regex for migration

--- src/utils/validationUtil.ts
+++ src/utils/validationUtil.ts
@@ -7,5 +7,8 @@ export const REGEX_ACTIVATION_CODE = "[a-z0-9]+";
 export const isMongoId = (val: unknown): boolean =>
 	isString(val) && new RegExp(REGEX_ID).test(val);
 
+const REGEX_OFFICIAL_SCHOOL_NUMBER =
+	"^(Dev-\\d{1,2}|EXPERTEN|MZ-[A-Z]{1,3}|ORG-[A-Z0-9]{2,7}|T\\d{4}|[0-9]{5,6}-[A-Za-z]{1,10}|[A-Za-z]{2}-\\d{1,10}|[0-9]{6})$";
+
 export const isOfficialSchoolNumber = (value: unknown): boolean =>
-	isString(value) && /[0-9]{5}/.test(value);
+	isString(value) && new RegExp(REGEX_OFFICIAL_SCHOOL_NUMBER).test(value);
---
+++ src/utils/validationUtil.unit.ts
@@ -0,0 +1,27 @@
+import { isOfficialSchoolNumber } from "./validationUtil";
+
+describe("validationUtil", () => {
+	describe("isOfficialSchoolNumber", () => {
+		it("should return true for valid official school numbers", () => {
+			expect(isOfficialSchoolNumber("Dev-12")).toBe(true);
+			expect(isOfficialSchoolNumber("EXPERTEN")).toBe(true);
+			expect(isOfficialSchoolNumber("MZ-ABC")).toBe(true);
+			expect(isOfficialSchoolNumber("MZ-A")).toBe(true);
+			expect(isOfficialSchoolNumber("ORG-A134V55")).toBe(true);
+			expect(isOfficialSchoolNumber("ORG-A1")).toBe(true);
+			expect(isOfficialSchoolNumber("T1234")).toBe(true);
+			expect(isOfficialSchoolNumber("12345-Y")).toBe(true);
+			expect(isOfficialSchoolNumber("AB-123456")).toBe(true);
+			expect(isOfficialSchoolNumber("123456")).toBe(true);
+		});
+
+		it("should return false for invalid official school numbers", () => {
+			expect(isOfficialSchoolNumber("T12345")).toBe(false);
+			expect(isOfficialSchoolNumber("X1234")).toBe(false);
+			expect(isOfficialSchoolNumber("1234-Y")).toBe(false);
+			expect(isOfficialSchoolNumber("Dev")).toBe(false);
+			expect(isOfficialSchoolNumber("Dev-123")).toBe(false);
+			expect(isOfficialSchoolNumber("ORG-A134V550")).toBe(false);
+		});
+	});
+});
