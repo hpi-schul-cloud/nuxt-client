e4575e3658513e7a9556daa062fda7b5a8f05c27
Christian Darsow <christian.darsow@dataport.de>
2023-05-11 13:33:57 +0200

    Rename setup function

--- src/components/editor/CKEditor.unit.ts
+++ src/components/editor/CKEditor.unit.ts
@@ -1,6 +1,14 @@
+import Vue from "vue";
+import { MountOptions, mount } from "@vue/test-utils";
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
 import CkEditor from "@/components/editor/CKEditor.vue";
-import { mount } from "@vue/test-utils";
+
+type CkEditorProps = {
+	value?: string;
+	placeholder?: string;
+	mode?: string;
+	disabled?: boolean;
+};
 
 class ResizeObserver {
 	observe() {
@@ -15,18 +23,20 @@ class ResizeObserver {
 }
 
 describe("@/components/editor/CKEditor", () => {
-	const getWrapper: any = (attrs = {}) => {
-		const wrapper = mount(CkEditor, {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	let wrapper: any;
+
+	const setup = (props: CkEditorProps = {}) => {
+		document.body.setAttribute("data-app", "true");
+		wrapper = mount(CkEditor as MountOptions<Vue>, {
 			...createComponentMocks({
 				i18n: true,
 			}),
+			propsData: props,
 			provide: {
 				i18n: { t: (key: string) => key },
 			},
-			...attrs,
 		});
-
-		return wrapper;
 	};
 
 	beforeEach(() => {
@@ -36,75 +46,77 @@ describe("@/components/editor/CKEditor", () => {
 	});
 
 	it("should render component", () => {
-		const wrapper = getWrapper();
+		setup();
 		expect(wrapper.findComponent(CkEditor).exists()).toBe(true);
 	});
 
 	it("should not render with invalid mode property", () => {
 		try {
-			getWrapper({ propsData: { mode: "wrong_mode" } });
+			setup({ mode: "wrong_mode" });
 		} catch (e) {
 			expect((e as Error).message.includes("Invalid prop")).toBeTruthy();
 		}
 	});
 
 	it("should have reduced toolbar items in simple mode", () => {
-		const wrapper = getWrapper({ propsData: { mode: "simple" } });
+		setup({ mode: "simple" });
 		expect(wrapper.vm.config.toolbar.items).toHaveLength(8);
 	});
 
 	it("should have all plugins available in simple mode", () => {
-		const wrapper = getWrapper({ propsData: { mode: "simple" } });
+		setup({ mode: "simple" });
 		expect(wrapper.vm.config.plugins).toHaveLength(21);
 	});
 
 	it("should have all toolbar items in regular mode", () => {
-		const wrapper = getWrapper({ propsData: { mode: "regular" } });
+		setup({ mode: "regular" });
 		expect(wrapper.vm.config.toolbar.items).toHaveLength(24);
 	});
 
 	it("should have all plugins available in regular mode", () => {
-		const wrapper = getWrapper({ propsData: { mode: "regular" } });
+		setup({ mode: "regular" });
 		expect(wrapper.vm.config.plugins).toHaveLength(21);
 	});
 
-	it("should emit input on content changes", async () => {
-		const wrapper = getWrapper();
+	describe("events", () => {
+		it("should emit input on content changes", async () => {
+			setup();
 
-		const ck = wrapper.findComponent({
-			ref: "ck",
+			const ck = wrapper.findComponent({
+				ref: "ck",
+			});
+			ck.vm.$emit("input");
+			await wrapper.vm.$nextTick();
+
+			const emitted = wrapper.emitted();
+			expect(emitted["input"]).toHaveLength(1);
 		});
-		ck.vm.$emit("input");
-		await wrapper.vm.$nextTick();
 
-		const emitted = wrapper.emitted();
-		expect(emitted["input"]).toHaveLength(1);
-	});
+		it("should emit focus on editor focus", async () => {
+			setup();
 
-	it("should emit focus on editor focus", async () => {
-		const wrapper = getWrapper();
+			const ck = wrapper.findComponent({
+				ref: "ck",
+			});
+			ck.vm.$emit("focus");
+			await wrapper.vm.$nextTick();
 
-		const ck = wrapper.findComponent({
-			ref: "ck",
+			const emitted = wrapper.emitted();
+			expect(emitted["focus"]).toHaveLength(1);
 		});
-		ck.vm.$emit("focus");
-		await wrapper.vm.$nextTick();
 
-		const emitted = wrapper.emitted();
-		expect(emitted["focus"]).toHaveLength(1);
-	});
+		it("should emit delayed blur on editor blur", async () => {
+			jest.useFakeTimers();
+			setup();
 
-	it("should emit delayed blur on editor blur", async () => {
-		jest.useFakeTimers();
-		const wrapper = getWrapper();
+			const ck = wrapper.findComponent({
+				ref: "ck",
+			});
+			ck.vm.$emit("blur");
+			jest.advanceTimersByTime(200);
 
-		const ck = wrapper.findComponent({
-			ref: "ck",
+			const emitted = wrapper.emitted();
+			expect(emitted["blur"]).toHaveLength(1);
 		});
-		ck.vm.$emit("blur");
-		jest.advanceTimersByTime(200);
-
-		const emitted = wrapper.emitted();
-		expect(emitted["blur"]).toHaveLength(1);
 	});
 });
