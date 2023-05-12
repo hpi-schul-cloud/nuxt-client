d1781b49b4c8a2e0f90fe92be76eec7243688beb
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-05-12 11:07:37 +0200

    BC-3339 upload component wip

--- src/components/feature-board/card/CardHost.vue
+++ src/components/feature-board/card/CardHost.vue
@@ -1,57 +1,60 @@
 <template>
-	<CardHostInteractionHandler
-		:isEditMode="isEditMode"
-		@start-edit-mode="onStartEditMode"
-		@end-edit-mode="onEndEditMode"
-		@move:card-keyboard="onMoveCardKeyboard"
-	>
-		<VCard
-			ref="cardHost"
-			:height="isLoading ? height : 'auto'"
-			class="w-100 transition-swing"
-			:class="{ 'drag-disabled': isEditMode }"
-			outlined
-			tabindex="0"
-			min-height="120px"
-			:elevation="isEditMode ? 6 : 0"
-			:id="cardId"
-			:ripple="false"
-			:hover="isHovered"
+	<div>
+		<CardHostInteractionHandler
+			:isEditMode="isEditMode"
+			@start-edit-mode="onStartEditMode"
+			@end-edit-mode="onEndEditMode"
+			@move:card-keyboard="onMoveCardKeyboard"
 		>
-			<template v-if="isLoading">
-				<CardSkeleton :height="height" />
-			</template>
-			<template v-if="!isLoading && card">
-				<CardTitle
-					:isEditMode="isEditMode"
-					:value="card.title"
-					scope="card"
-					@update:value="onUpdateCardTitle"
-				>
-				</CardTitle>
+			<VCard
+				ref="cardHost"
+				:height="isLoading ? height : 'auto'"
+				class="w-100 transition-swing"
+				:class="{ 'drag-disabled': isEditMode }"
+				outlined
+				tabindex="0"
+				min-height="120px"
+				:elevation="isEditMode ? 6 : 0"
+				:id="cardId"
+				:ripple="false"
+				:hover="isHovered"
+			>
+				<template v-if="isLoading">
+					<CardSkeleton :height="height" />
+				</template>
+				<template v-if="!isLoading && card">
+					<CardTitle
+						:isEditMode="isEditMode"
+						:value="card.title"
+						scope="card"
+						@update:value="onUpdateCardTitle"
+					>
+					</CardTitle>
 
-				<div class="board-menu" :class="boardMenuClasses">
-					<BoardMenu scope="card">
-						<BoardMenuAction @click="onTryDelete">
-							<VIcon>
-								{{ mdiTrashCanOutline }}
-							</VIcon>
-							{{ $t("components.board.action.delete") }}
-						</BoardMenuAction>
-					</BoardMenu>
-				</div>
+					<div class="board-menu" :class="boardMenuClasses">
+						<BoardMenu scope="card">
+							<BoardMenuAction @click="onTryDelete">
+								<VIcon>
+									{{ mdiTrashCanOutline }}
+								</VIcon>
+								{{ $t("components.board.action.delete") }}
+							</BoardMenuAction>
+						</BoardMenu>
+					</div>
 
-				<ContentElementList
-					:elements="card.elements"
-					:isEditMode="isEditMode"
-				></ContentElementList>
-				<CardAddElementMenu
-					@add-element="onAddElement"
-					v-if="isEditMode"
-				></CardAddElementMenu>
-			</template>
-		</VCard>
-	</CardHostInteractionHandler>
+					<ContentElementList
+						:elements="card.elements"
+						:isEditMode="isEditMode"
+					></ContentElementList>
+					<CardAddElementMenu
+						@add-element="onAddElement"
+						v-if="isEditMode"
+					></CardAddElementMenu>
+				</template>
+			</VCard>
+		</CardHostInteractionHandler>
+		<input ref="openFilePicker" type="file" @selected:file="uploadFile" />
+	</div>
 </template>
 
 <script lang="ts">
@@ -70,12 +73,12 @@ import { useBoardFocusHandler } from "../shared/BoardFocusHandler.composable";
 import BoardMenu from "../shared/BoardMenu.vue";
 import BoardMenuAction from "../shared/BoardMenuAction.vue";
 import { useEditMode } from "../shared/EditMode.composable";
+import { useElementTypeSelection } from "../shared/element-type-selection.composable";
 import { useCardState } from "../state/CardState.composable";
 import CardAddElementMenu from "./CardAddElementMenu.vue";
 import CardHostInteractionHandler from "./CardHostInteractionHandler.vue";
 import CardSkeleton from "./CardSkeleton.vue";
 import CardTitle from "./CardTitle.vue";
-import { useElementTypeSelection } from "../shared/element-type-selection.composable";
 
 export default defineComponent({
 	name: "CardHost",
@@ -96,6 +99,7 @@ export default defineComponent({
 	setup(props, { emit }) {
 		const i18n: VueI18n | undefined = inject<VueI18n>("i18n");
 		const cardHost = ref(undefined);
+		const openFilePicker: any = ref(null);
 		const { isFocusContained } = useBoardFocusHandler(props.cardId, cardHost);
 		const isHovered = useElementHover(cardHost);
 		const { isLoading, card, updateTitle, updateCardHeight, addElement } =
@@ -132,9 +136,20 @@ export default defineComponent({
 
 			const type = await askType();
 			if (type) {
+				if (type === "file") {
+					openFilePicker.value.click();
+				}
 				await addElement(type);
 			}
 		};
+
+		/* 		const uploadFile = (file: Buffer, type: string) => {
+			const res = await addElement(type);
+
+			if(res.id) {
+				await uploadFileToFileStorage(parent)
+			}
+		} */
 
 		const onStartEditMode = () => {
 			startEditMode();
@@ -170,6 +185,7 @@ export default defineComponent({
 			cardHost,
 			isEditMode,
 			mdiTrashCanOutline,
+			openFilePicker,
 		};
 	},
 });
