6dfba8ef174ce385ecdcde68d265487949868bff
Murat Merdoglu <murat.merdoglu@dataport.de>
2023-05-15 15:49:30 +0200

    add user(/me) permissions to state

--- src/components/feature-board/shared/BoardPermissions.composable.ts
+++ src/components/feature-board/shared/BoardPermissions.composable.ts
@@ -1,33 +1,51 @@
+import { createSharedComposable } from "@vueuse/core";
 import { ref, Ref } from "vue";
+import { authModule } from "@/store";
 
 /**
  * Shares board permissions
  *
  */
-export const useBoardPermissions = () => {
-	const permissions: Ref<Array<string>> = ref([]);
-	// permissions = authModule.getUserPermissions;
-	// can be mapped board spesific permissions
-	permissions.value = [
-		"move",
-		"delete",
-		"card_create",
-		"column_create",
-		"board_edit",
-	];
-
-	const hasMovePermission = permissions.value.includes("move");
-	const hasCardCreatePermission = permissions.value.includes("card_create");
-	const hasColumnCreatePermission = permissions.value.includes("column_create");
-	const hasEditPermission = permissions.value.includes("board_edit");
-	const hasDeletePermission = permissions.value.includes("delete");
+const boardPermissions = () => {
+	const permissions = authModule.getUserPermissions;
 
 	return {
 		permissions,
-		hasMovePermission,
-		hasCardCreatePermission,
-		hasColumnCreatePermission,
-		hasEditPermission,
-		hasDeletePermission,
+		hasBoardMovePermission: permissions.includes("course_create"),
+		hasBoardCardCreatePermission: permissions.includes("course_create"),
+		hasBoardColumnCreatePermission: permissions.includes("course_create"),
+		hasBoardEditPermission: permissions.includes("course_edit"),
+		hasBoardDeletePermission: permissions.includes("course_remove"),
 	};
+};
+export const useBoardPermissions = createSharedComposable(boardPermissions);
+
+declare type boardObjectPermissionType = {
+	id: string;
+	permissions: Array<string>;
+};
+
+export const useBoardElementPermissions = (id: string) => {
+	const permissions: Ref<Array<boardObjectPermissionType>> = ref([]);
+	// TODO: find a way to push each board object's permissions here
+	permissions.value = [
+		{
+			id: "643ea6ebe8ba9e586e041e8c", // card
+			permissions: ["edit", "change", "delete", "create"],
+		},
+		{
+			id: "643ea6ebe8ba9e586e041e89", // card
+			permissions: ["edit", "delete"],
+		},
+		{
+			id: "643ea6ebe8ba9e586e041ed3", // text content element
+			permissions: ["edit", "delete"],
+		},
+	];
+
+	const result = permissions.value.find(
+		(boardObject) => boardObject.id === id
+	)?.permissions;
+
+	return result;
 };
