cc4c638442426b088340c3b108d7166dc2acedf6
Odalys Adam <odalys.adam@dataport.de>
2023-05-15 11:32:43 +0200

    revert v-html for editor display

--- src/components/feature-board/content-elements/RichTextContentElementDisplay.vue
+++ src/components/feature-board/content-elements/RichTextContentElementDisplay.vue
@@ -1,18 +1,30 @@
-<!-- eslint-disable vue/no-v-html -->
 <template>
-	<div v-html="value" />
+	<ck-editor v-model="content" disabled />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
+import CkEditor from "@/components/editor/CKEditor.vue";
 
 export default defineComponent({
 	name: "RichTextContentElementDisplay",
+	components: { CkEditor },
 	props: {
 		value: {
 			type: String,
 			required: true,
 		},
+	},
+	setup(props) {
+		const content = ref(props.value);
+
+		watch(
+			() => props.value,
+			(newValue) => {
+				content.value = newValue;
+			}
+		);
+		return { content };
 	},
 });
 </script>
