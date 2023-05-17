26de7ab5164dcc3273ebe96fc258a5f7345b76b4
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-05-17 10:55:19 +0200

    BC-3339 first test for component

--- src/components/feature-board/shared/FilePicker.unit.ts
+++ src/components/feature-board/shared/FilePicker.unit.ts
@@ -1,24 +1,45 @@
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
-import { MountOptions, shallowMount, Wrapper } from "@vue/test-utils";
-import Vue from "vue";
+import { mount, MountOptions, Wrapper } from "@vue/test-utils";
+import Vue, { nextTick, ref } from "vue";
+import { useFilePicker } from "./FilePicker.composable";
 import FilePicker from "./FilePicker.vue";
-jest.mock("../shared/EditMode.composable");
+jest.mock("./FilePicker.composable");
 
 describe("FilePicker", () => {
 	let wrapper: Wrapper<Vue>;
+	const mockedUseFilePicker = jest.mocked(useFilePicker);
 
 	const setup = () => {
 		document.body.setAttribute("data-app", "true");
-		wrapper = shallowMount(FilePicker as MountOptions<Vue>, {
+
+		const isFilePickerOpen = ref(false);
+		const triggerFilePicker = jest.fn();
+
+		mockedUseFilePicker.mockReturnValue({
+			isFilePickerOpen,
+			triggerFilePicker,
+		});
+
+		wrapper = mount(FilePicker as MountOptions<Vue>, {
 			...createComponentMocks({}),
 		});
 		const filePicker = wrapper.findComponent({ name: "FilePicker" });
 
-		return { filePicker };
+		return { filePicker, isFilePickerOpen, triggerFilePicker };
 	};
 
 	it("should be found in dom", () => {
 		setup();
 		expect(wrapper.findComponent(FilePicker).exists()).toBe(true);
+	});
+
+	describe("when isFilePickerOpen is changed from false to true", () => {
+		it("should call triggerFilePicker", async () => {
+			const { isFilePickerOpen, triggerFilePicker } = setup();
+			isFilePickerOpen.value = true;
+			await nextTick();
+
+			expect(triggerFilePicker).toBeCalled();
+		});
 	});
 });
