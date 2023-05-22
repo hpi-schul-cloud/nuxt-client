dc2244b6864ea36a68119449a634bed9ae5ea3b5
Odalys Adam <odalys.adam@dataport.de>
2023-05-22 11:33:24 +0200

    remove comments

--- src/components/feature-board/content-elements/RichTextContentElementEdit.vue
+++ src/components/feature-board/content-elements/RichTextContentElementEdit.vue
@@ -12,8 +12,8 @@
 </template>
 <script lang="ts">
 import CkEditor from "@/components/editor/CKEditor.vue";
-import { Fn, useEventListener, useVModel } from "@vueuse/core";
-import { computed, defineComponent, onUnmounted, onMounted } from "vue";
+import { useEventListener, useVModel } from "@vueuse/core";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
 	name: "RichTextContentElementEdit",
@@ -32,8 +32,6 @@ export default defineComponent({
 	setup(props, { emit }) {
 		const modelValue = useVModel(props, "value", emit);
 
-		// let bubbleClickListenerCleanup: Fn | undefined = undefined;
-
 		const onFocus = () => {
 			const elements = computed(() =>
 				document.getElementsByClassName("ck-balloon-panel")
@@ -44,32 +42,9 @@ export default defineComponent({
 				useEventListener(element, "click", (event: PointerEvent) => {
 					event.stopPropagation();
 					event.stopImmediatePropagation();
-					// event.preventDefault();
 				});
 			}
-			// 	const element = computed(() =>
-			// 		document.querySelector<HTMLElement>(
-			// 			"div.ck-balloon-panel.ck-balloon-panel_visible"
-			// 		)
-			// 	);
-			// 	if (bubbleClickListenerCleanup !== undefined) return;
-			// 	bubbleClickListenerCleanup = useEventListener(
-			// 		element,
-			// 		"click",
-			// 		(event: PointerEvent) => {
-			// 			event.stopPropagation();
-			// 			event.stopImmediatePropagation();
-			// 			event.preventDefault();
-			// 		}
-			// 	);
 		};
-
-		// onUnmounted(() => {
-		// 	if (bubbleClickListenerCleanup) {
-		// 		bubbleClickListenerCleanup();
-		// 		bubbleClickListenerCleanup = undefined;
-		// 	}
-		// });
 
 		return { modelValue, onFocus };
 	},
