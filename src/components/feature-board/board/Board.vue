ce156bcb3f9c1bdedc7c769fcf6e7583d57cc645
Nicole Friedo <nicole.friedo@dataport.de>
2023-05-24 16:41:19 +0200

    add breadcrumbs to columnboard

--- src/components/feature-board/board/Board.vue
+++ src/components/feature-board/board/Board.vue
@@ -1,47 +1,53 @@
 <template>
-	<div class="ml-8 mr-8">
-		<div>
-			<h1>Board</h1>
+	<DefaultWireframe ref="main" :full-width="true" :breadcrumbs="breadcrumbs">
+		<div class="ml-8 mr-8">
+			<div>
+				<h1>Board</h1>
+			</div>
+			<div class="d-flex flex-row flex-shrink-1 ml-n4">
+				<template v-if="board">
+					<Container
+						orientation="horizontal"
+						group-name="columns"
+						lock-axis="x"
+						:get-child-payload="getColumnId"
+						:drop-placeholder="columnDropPlaceholderOptions"
+						@drop="onDropColumn"
+					>
+						<Draggable
+							v-for="(column, index) in board.columns"
+							:key="column.id"
+						>
+							<BoardColumn
+								:column="column"
+								:index="index"
+								@create:card="onCreateCard"
+								@delete:card="onDeleteCard"
+								@delete:column="onDeleteColumn"
+								@move:column-keyboard="
+									onMoveColumnKeyboard(index, column.id, $event)
+								"
+								@update:card-position="onUpdateCardPosition(index, $event)"
+								@update:column-title="onUpdateColumnTitle(column.id, $event)"
+							/>
+						</Draggable>
+					</Container>
+					<BoardColumnGhost
+						@create:column="onCreateColumn"
+						@create:column-with-card="onCreateColumnWithCard"
+					></BoardColumnGhost>
+					<DeleteConfirmation></DeleteConfirmation>
+					<ElementTypeSelection></ElementTypeSelection>
+				</template>
+			</div>
 		</div>
-		<div class="d-flex flex-row flex-shrink-1 ml-n4">
-			<template v-if="board">
-				<Container
-					orientation="horizontal"
-					group-name="columns"
-					lock-axis="x"
-					:get-child-payload="getColumnId"
-					:drop-placeholder="columnDropPlaceholderOptions"
-					@drop="onDropColumn"
-				>
-					<Draggable v-for="(column, index) in board.columns" :key="column.id">
-						<BoardColumn
-							:column="column"
-							:index="index"
-							@create:card="onCreateCard"
-							@delete:card="onDeleteCard"
-							@delete:column="onDeleteColumn"
-							@move:column-keyboard="
-								onMoveColumnKeyboard(index, column.id, $event)
-							"
-							@update:card-position="onUpdateCardPosition(index, $event)"
-							@update:column-title="onUpdateColumnTitle(column.id, $event)"
-						/>
-					</Draggable>
-				</Container>
-				<BoardColumnGhost
-					@create:column="onCreateColumn"
-					@create:column-with-card="onCreateColumnWithCard"
-				></BoardColumnGhost>
-				<DeleteConfirmation></DeleteConfirmation>
-				<ElementTypeSelection></ElementTypeSelection>
-			</template>
-		</div>
-	</div>
+	</DefaultWireframe>
 </template>
 
 <script lang="ts">
+import DefaultWireframe from "@/components/templates/DefaultWireframe.vue";
 import DeleteConfirmation from "@/components/feature-confirmation-dialog/DeleteConfirmation.vue";
-import { defineComponent } from "vue";
+import { defineComponent, inject } from "vue";
 import { useRoute } from "vue-router/composables";
 import { Container, Draggable } from "vue-smooth-dnd";
 import { useBodyScrolling } from "../shared/BodyScrolling.composable";
@@ -56,18 +62,21 @@ import {
 } from "../types/DragAndDrop";
 import BoardColumn from "./BoardColumn.vue";
 import BoardColumnGhost from "./BoardColumnGhost.vue";
+import VueI18n from "vue-i18n";
 
 export default defineComponent({
 	name: "Board",
 	components: {
 		BoardColumn,
 		Container,
+		DefaultWireframe,
 		Draggable,
 		BoardColumnGhost,
 		DeleteConfirmation,
 		ElementTypeSelection,
 	},
 	setup() {
+		const i18n: VueI18n | undefined = inject<VueI18n>("i18n");
 		const route = useRoute();
 		const {
 			board,
@@ -81,6 +90,17 @@ export default defineComponent({
 			moveColumn,
 			updateColumnTitle,
 		} = useBoardState(route.params?.id);
+
+		const breadcrumbs = [
+			{
+				text: i18n?.t("pages.courses.index.title"),
+				to: "/rooms-overview",
+			},
+			{
+				text: "Kurs-Titel",
+				to: "/rooms-overview",
+			},
+		];
 
 		useBodyScrolling();
 
@@ -136,6 +156,7 @@ export default defineComponent({
 
 		return {
 			board,
+			breadcrumbs,
 			columnDropPlaceholderOptions,
 			getColumnId,
 			onCreateCard,
