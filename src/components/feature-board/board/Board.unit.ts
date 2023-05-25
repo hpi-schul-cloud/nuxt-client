db277e08649eb1ffb896bb3408e0b819cf4a57d4
Murat Merdoglu <64781656+muratmerdoglu-dp@users.noreply.github.com>
2023-05-25 19:01:36 +0200

    BC-3602 - Adding user permission check on column-board (#2538)
    Implement user-based permission checks on the column-board as a basic implementation.
    ---------
    
    Co-authored-by: Nicole Friedo <nfriedo@dataport.de>

--- src/components/feature-board/board/Board.unit.ts
+++ src/components/feature-board/board/Board.unit.ts
@@ -5,7 +5,8 @@ import { Route } from "vue-router";
 import BoardVue from "./Board.vue";
 import BoardColumnVue from "./BoardColumn.vue";
 import { useBoardState } from "../state/BoardState.composable";
-import { Board } from "../types/Board";
+import { Board, BoardPermissionsTypes } from "../types/Board";
+import { useBoardPermissions } from "../shared/BoardPermissions.composable";
 
 const MOCK_BOARD_ONE_COLUMN: Board = {
 	columns: [
@@ -98,6 +99,9 @@ const MOCK_BOARD_TWO_COLUMNS: Board = {
 jest.mock("../state/BoardState.composable");
 const mockedUseBoardState = jest.mocked(useBoardState);
 
+jest.mock("../shared/BoardPermissions.composable");
+const mockedUserPermissions = jest.mocked(useBoardPermissions);
+
 const $route: Route = {
 	params: {
 		id: "a1b2c3",
@@ -113,26 +117,52 @@ const $route: Route = {
 
 const $router = { replace: jest.fn(), push: jest.fn(), afterEach: jest.fn() };
 
+const defaultPermissions = {
+	hasMovePermission: true,
+	hasCreateCardPermission: true,
+	hasCreateColumnPermission: true,
+	hasDeletePermission: true,
+	hasEditPermission: true,
+};
+
+const createCardMock = jest.fn();
+const createColumnMock = jest.fn();
+const createColumnWithCardMock = jest.fn();
+const deleteCardMock = jest.fn();
+const deleteColumnMock = jest.fn();
+const moveColumnMock = jest.fn();
+const moveCardMock = jest.fn();
+const updateColumnTitleMock = jest.fn();
+const fetchBoardMock = jest.fn();
+
 describe("Board", () => {
 	let wrapper: Wrapper<Vue>;
 
-	const setup = (options?: { board?: Board; isLoading?: boolean }) => {
+	const setup = (options?: {
+		board?: Board;
+		isLoading?: boolean;
+		permissions?: BoardPermissionsTypes;
+	}) => {
 		const { board, isLoading } = options ?? {};
 		document.body.setAttribute("data-app", "true");
 		mockedUseBoardState.mockReturnValue({
 			board: ref<Board | undefined>(board ?? MOCK_BOARD_ONE_COLUMN),
 			isLoading: ref(isLoading ?? false),
-			createCard: jest.fn(),
-			createColumn: jest.fn(),
-			createColumnWithCard: jest.fn(),
-			deleteColumn: jest.fn(),
-			deleteCard: jest.fn(),
+			createCard: createCardMock,
+			createColumn: createColumnMock,
+			createColumnWithCard: createColumnWithCardMock,
+			deleteColumn: deleteColumnMock,
+			deleteCard: deleteCardMock,
 			extractCard: jest.fn(),
-			fetchBoard: jest.fn(),
+			fetchBoard: fetchBoardMock,
 			getColumnId: jest.fn(),
-			moveCard: jest.fn(),
-			moveColumn: jest.fn(),
-			updateColumnTitle: jest.fn(),
+			moveCard: moveCardMock,
+			moveColumn: moveColumnMock,
+			updateColumnTitle: updateColumnTitleMock,
+		});
+		mockedUserPermissions.mockReturnValue({
+			...defaultPermissions,
+			...options?.permissions,
 		});
 		wrapper = shallowMount(BoardVue, {
 			...createComponentMocks({}),
@@ -170,6 +200,229 @@ describe("Board", () => {
 		it("should fetch board from store and render two columns", () => {
 			setup({ board: MOCK_BOARD_TWO_COLUMNS });
 			expect(wrapper.findAllComponents(BoardColumnVue)).toHaveLength(2);
+		});
+
+		describe("BoardColumnGhost component", () => {
+			describe("when user has create column permission", () => {
+				it("should not be rendered on DOM", () => {
+					setup();
+					const ghostColumnComponent = wrapper.findComponent({
+						name: "BoardColumnGhost",
+					});
+
+					expect(ghostColumnComponent.vm).toBeDefined();
+				});
+			});
+
+			describe("when user doesn't have create column permission", () => {
+				it("should not be rendered on DOM", () => {
+					setup({ permissions: { hasCreateColumnPermission: false } });
+					const ghostColumnComponent = wrapper.findComponent({
+						name: "BoardColumnGhost",
+					});
+
+					expect(ghostColumnComponent.vm).not.toBeDefined();
+				});
+			});
+		});
+	});
+
+	describe("user permissions", () => {
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		describe("when user is not permitted to move", () => {
+			it("should set lock-axis to 'x,y'", () => {
+				setup({ permissions: { hasMovePermission: false } });
+
+				const dndContainer = wrapper.findComponent({ name: "Container" });
+				expect(dndContainer.element.outerHTML).toContain('lockaxis="x,y"');
+			});
+		});
+
+		describe("@onCreateCard", () => {
+			describe("when user is permitted to create card", () => {
+				it("should call the createCard method", () => {
+					setup();
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("create:card");
+
+					expect(createCardMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("when user is not permitted to create card", () => {
+				it("should not call the createCard method", () => {
+					setup({ permissions: { hasCreateCardPermission: false } });
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("create:card");
+
+					expect(createCardMock).not.toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onCreateColumn", () => {
+			describe("when user is permitted to create a column", () => {
+				it("should call createColumn method", () => {
+					setup();
+					const ghostColumnComponent = wrapper.findComponent({
+						name: "BoardColumnGhost",
+					});
+					ghostColumnComponent.vm.$emit("create:column");
+
+					expect(createColumnMock).toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onCreateColumnWithCard", () => {
+			describe("when user is permitted to create a column with card", () => {
+				it("should call createColumn method", () => {
+					setup();
+					const ghostColumnComponent = wrapper.findComponent({
+						name: "BoardColumnGhost",
+					});
+					ghostColumnComponent.vm.$emit("create:column-with-card");
+
+					expect(createColumnWithCardMock).toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onDeleteCard", () => {
+			describe("when user is permitted to delete a card", () => {
+				it("should call deleteCard method", () => {
+					setup();
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("delete:card");
+
+					expect(deleteCardMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("when user is not permitted to delete a card", () => {
+				it("should not call deleteCard method", () => {
+					setup({ permissions: { hasCreateCardPermission: false } });
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("delete:card");
+
+					expect(deleteCardMock).not.toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onDeleteColumn", () => {
+			describe("when user is permitted to delete a column", () => {
+				it("should call deleteColumn method", () => {
+					setup();
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("delete:column");
+
+					expect(deleteColumnMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("when user is not permitted to delete a column", () => {
+				it("should not call deleteColumn method", () => {
+					setup({ permissions: { hasDeletePermission: false } });
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("delete:column");
+
+					expect(deleteColumnMock).not.toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onDropColumn", () => {
+			describe("when user is permitted to move a column", () => {
+				it("should call moveColumn method", () => {
+					setup();
+					const containerComponent = wrapper.findComponent({
+						name: "Container",
+					});
+					containerComponent.vm.$emit("drop");
+
+					expect(moveColumnMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("when user is not permitted to move a column", () => {
+				it("should not call moveColumn method", () => {
+					setup({ permissions: { hasMovePermission: false } });
+					const containerComponent = wrapper.findComponent({
+						name: "Container",
+					});
+					containerComponent.vm.$emit("drop");
+
+					expect(moveColumnMock).not.toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onUpdateCardPosition", () => {
+			describe("when user is permitted to move a card", () => {
+				it("should call moveCardMock method", () => {
+					setup();
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("update:card-position");
+
+					expect(moveCardMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("when user is not permitted to move a card", () => {
+				it("should not call moveCardMock method", () => {
+					setup({ permissions: { hasMovePermission: false } });
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("update:card-position");
+
+					expect(moveCardMock).not.toHaveBeenCalled();
+				});
+			});
+		});
+
+		describe("@onUpdateColumnTitle", () => {
+			describe("when user is permitted to edit", () => {
+				it("should call updateColumnTitle method", () => {
+					setup();
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("update:column-title");
+
+					expect(updateColumnTitleMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("when user is not permitted to edit", () => {
+				it("should not call updateColumnTitle method", () => {
+					setup({ permissions: { hasEditPermission: false } });
+					const columnComponent = wrapper.findComponent({
+						name: "BoardColumn",
+					});
+					columnComponent.vm.$emit("update:column-title");
+
+					expect(updateColumnTitleMock).not.toHaveBeenCalled();
+				});
+			});
 		});
 	});
 });
