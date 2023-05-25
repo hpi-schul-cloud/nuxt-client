e8c833ece81d47cc4d5b3ac7bead4b3ca800dbdb
Nicole Friedo <nicole.friedo@dataport.de>
2023-05-25 12:14:18 +0200

    WIP: add tests for RoomBoardCard

--- src/components/molecules/RoomBoardCard.unit.ts
+++ src/components/molecules/RoomBoardCard.unit.ts
@@ -3,6 +3,10 @@ import { MountOptions, shallowMount, Wrapper } from "@vue/test-utils";
 import Vue from "vue";
 import RoomBoardCard from "./RoomBoardCard.vue";
 
+const $router = {
+	push: jest.fn(),
+};
+
 describe("RoomBoardCard", () => {
 	let wrapper: Wrapper<Vue>;
 
@@ -13,6 +17,8 @@ describe("RoomBoardCard", () => {
 			provide: {
 				i18n: { t: (key: string) => key },
 			},
+			propsData: { keyDrag: false, dragInProgress: false },
+			mocks: { $router },
 		});
 	};
 	describe("when component is mounted", () => {
@@ -20,5 +26,32 @@ describe("RoomBoardCard", () => {
 			setup();
 			expect(wrapper.findComponent(RoomBoardCard).exists()).toBe(true);
 		});
+	});
+
+	describe("when a board card is rendered", () => {
+		it.todo("should have correct board title", () => {
+			setup();
+			const expectedBoardTitle = "Kurs-Board";
+			const boardTitle = wrapper.find(".board-title");
+		});
+
+		it("should redirect to column board when clicking on the card", () => {
+			setup();
+			const boardCard = wrapper.findComponent({ name: "VCard" });
+
+			boardCard.vm.$emit("click");
+
+			expect($router.push).toHaveBeenCalledTimes(1);
+			expect($router.push).toHaveBeenCalledWith(
+				"646e1722d921ff87bc02f7df/board"
+			);
+		});
+
+		it.todo(
+			"should NOT redirect to column board if dragging is in progress",
+			() => {
+				setup();
+			}
+		);
 	});
 });
