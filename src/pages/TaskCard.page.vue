97bd22e7e3b5e5f87c11b3b9ea5f071ec61c7057
Christian Darsow <christian.darsow@dataport.de>
2023-05-26 10:49:06 +0200

    Test sanitize config

--- src/pages/TaskCard.page.vue
+++ src/pages/TaskCard.page.vue
@@ -4,6 +4,7 @@
 		:breadcrumbs="breadcrumbs"
 		:headline="t('pages.rooms.fab.add.betatask')"
 	>
+		<div v-dompurify-html:[sanitizeProfile]="html"></div>
 		<v-form
 			v-if="isEditMode && !isLoading"
 			class="d-flex flex-column"
@@ -510,6 +511,10 @@ export default defineComponent({
 			return !!course.value;
 		});
 
+		const html = "<h2>Headline</h2><b class='hello'>bold</b> <u>underline</u>";
+		// const sanitizeProfile = "ck5Profile";
+		const sanitizeProfile = "otherProfile";
+
 		return {
 			breadcrumbs,
 			title,
@@ -537,6 +542,8 @@ export default defineComponent({
 			isLoading,
 			form,
 			isDeletable,
+			html,
+			sanitizeProfile,
 		};
 	},
 });
