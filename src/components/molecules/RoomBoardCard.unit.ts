399c8d7b59c97d6b9d499939136f4f908b033000
Nicole Friedo <nicole.friedo@dataport.de>
2023-05-26 10:45:25 +0200

    fix test

--- src/components/molecules/RoomBoardCard.unit.ts
+++ src/components/molecules/RoomBoardCard.unit.ts
@@ -10,27 +10,36 @@ const $router = {
 describe("RoomBoardCard", () => {
 	let wrapper: Wrapper<Vue>;
 
-	const setup = (props: { dragInProgress: boolean; keyDrag: boolean }) => {
+	const setup = () => {
 		document.body.setAttribute("data-app", "true");
 		wrapper = shallowMount(RoomBoardCard as MountOptions<Vue>, {
 			...createComponentMocks({ i18n: true }),
 			provide: {
 				i18n: { t: (key: string) => key },
 			},
-			propsData: props,
+			propsData: {
+				dragInProgress: false,
+				keyDrag: false,
+				id: "646e1722d921ff87bc02f7df",
+			},
 			mocks: { $router },
 		});
 	};
+
+	beforeEach(() => {
+		$router.push.mockReset();
+	});
+
 	describe("when component is mounted", () => {
 		it("should be found in dom", () => {
-			setup({ dragInProgress: false, keyDrag: false });
+			setup();
 			expect(wrapper.findComponent(RoomBoardCard).exists()).toBe(true);
 		});
 	});
 
 	describe("when a board card is rendered", () => {
 		it("should have correct board title", () => {
-			setup({ dragInProgress: false, keyDrag: false });
+			setup();
 			const expectedBoardTitle = "Kurs-Board";
 			const boardTitle = wrapper.find(".board-title").element.textContent;
 
@@ -38,19 +47,20 @@ describe("RoomBoardCard", () => {
 		});
 
 		it("should redirect to column board when clicking on the card", () => {
-			setup({ dragInProgress: false, keyDrag: false });
+			setup();
+			const boardId = wrapper.props().id;
 			const boardCard = wrapper.findComponent({ name: "VCard" });
 
 			boardCard.vm.$emit("click");
 
 			expect($router.push).toHaveBeenCalledTimes(1);
-			expect($router.push).toHaveBeenCalledWith(
-				"646e1722d921ff87bc02f7df/board"
-			);
+			expect($router.push).toHaveBeenCalledWith(`${boardId}/board`);
 		});
 
 		it("should not redirect to column board if a card is dragged", async () => {
-			setup({ dragInProgress: true, keyDrag: false });
+			setup();
+			await wrapper.setProps({ dragInProgress: true });
+
 			const boardCard = wrapper.findComponent({ name: "VCard" });
 
 			boardCard.vm.$emit("click");
