a4e360d84dfdf7626d63e6511b70fa501ee22f0b
Murat Merdoglu <murat.merdoglu@dataport.de>
2023-05-30 12:42:32 +0200

    chore: add page title

--- src/pages/ColumnBoard.page.vue
+++ src/pages/ColumnBoard.page.vue
@@ -4,19 +4,25 @@
 	</DefaultWireframe>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, inject } from "vue";
 import { useRoute } from "vue-router/composables";
 import { useBoardBreadcrums } from "@/components/feature-board/shared/BoardBreadcrums.composable";
 import DefaultWireframe from "@/components/templates/DefaultWireframe.vue";
 import Board from "@/components/feature-board/board/Board.vue";
+import Theme from "@/theme.config";
+import { useTitle } from "@vueuse/core";
+import VueI18n from "vue-i18n";
 
 export default defineComponent({
 	name: "ColumnBoardPage",
 	components: { Board, DefaultWireframe },
 	setup() {
+		const i18n = inject<VueI18n | undefined>("i18n");
 		const route = useRoute();
 		const boardId = route.params?.id;
 		const breadcrumbs = useBoardBreadcrums(boardId);
+
+		useTitle(`${i18n?.t("components.board")} -  ${Theme.name}`);
 
 		return {
 			breadcrumbs,
