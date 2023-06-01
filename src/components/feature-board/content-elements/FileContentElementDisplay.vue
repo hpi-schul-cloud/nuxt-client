16466d2e07dfe1ac053942105fd84c5f23ec419a
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-06-01 14:11:08 +0200

    add first draft of file content element

--- src/components/feature-board/content-elements/FileContentElementDisplay.vue
+++ src/components/feature-board/content-elements/FileContentElementDisplay.vue
@@ -1,12 +1,18 @@
 <template>
-	<div>
-		<div>
+	<div class="file-content-element-display">
+		<v-icon x-large>{{ mdiFileDocumentOutline }}</v-icon>
+		<a
+			class="file-content-element-display-label truncate"
+			href="https://niedersachsen.cloud/"
+		>
 			{{ caption }}
-		</div>
+		</a>
 	</div>
 </template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mdiFileDocumentOutline } from "@mdi/js";
 
 export default defineComponent({
 	name: "FileContentElementDisplay",
@@ -16,5 +22,38 @@ export default defineComponent({
 			required: true,
 		},
 	},
+
+	setup(props, { emit }) {
+		return {
+			mdiFileDocumentOutline,
+		};
+	},
 });
 </script>
+
+<style lang="scss" scoped>
+@import "@/styles/mixins";
+@import "~vuetify/src/styles/styles.sass";
+.file-content-element-display {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 8px;
+	flex: none;
+	order: 0;
+	align-self: stretch;
+	flex-grow: 0;
+}
+
+.file-content-element-display-label {
+	font-family: "PT Sans";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 20px;
+	display: flex;
+	align-items: center;
+	letter-spacing: 0.02px;
+	color: #9e292b;
+}
+</style>
--- src/components/feature-board/content-elements/FileContentElementEdit.vue
+++ src/components/feature-board/content-elements/FileContentElementEdit.vue
@@ -1,9 +1,21 @@
 <template>
-	<vTextField v-model="modelCaption" label="Caption"></vTextField>
+	<div class="d-flex flex-column">
+		<div class="d-flex flex-sm-row align-items-center">
+			<v-icon x-large>{{ mdiFileDocumentOutline }}</v-icon>
+			<a
+				class="file-content-element-display-label truncate"
+				href="https://niedersachsen.cloud/"
+			>
+				{{ caption }}
+			</a>
+		</div>
+		<vTextField v-model="modelCaption" label="Caption"></vTextField>
+	</div>
 </template>
 <script lang="ts">
 import { useVModel } from "@vueuse/core";
 import { defineComponent } from "vue";
+import { mdiFileDocumentOutline } from "@mdi/js";
 
 export default defineComponent({
 	name: "FileContentElementEdit",
@@ -20,7 +32,37 @@ export default defineComponent({
 	emits: ["update:caption"],
 	setup(props, { emit }) {
 		const modelCaption = useVModel(props, "caption", emit);
-		return { modelCaption };
+		return {
+			mdiFileDocumentOutline,
+			modelCaption,
+		};
 	},
 });
 </script>
+
+<style lang="scss" scoped>
+@import "@/styles/mixins";
+@import "~vuetify/src/styles/styles.sass";
+.file-content-element-edit {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 8px;
+	flex: none;
+	order: 0;
+	align-self: stretch;
+	flex-grow: 0;
+}
+
+.file-content-element-edit-label {
+	font-family: "PT Sans";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 20px;
+	display: flex;
+	align-items: center;
+	letter-spacing: 0.02px;
+	color: #9e292b;
+}
+</style>
