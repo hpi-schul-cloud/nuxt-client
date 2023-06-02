397b8016306830b78211b0ac89d9201aba9a3c6f
Franz Schuenzel <f.schuenzel@googlemail.com>
2023-06-02 16:18:15 +0200

    test styles for text

--- src/components/feature-board/content-elements/RichTextContentElement.vue
+++ src/components/feature-board/content-elements/RichTextContentElement.vue
@@ -51,4 +51,32 @@ export default defineComponent({
 	color: rgba(0, 0, 0, 0.87);
 	font-size: 1rem;
 }
+
+::v-deep {
+	.ck-content {
+		h4 {
+			font-style: normal;
+			font-weight: 500;
+			font-size: 20px;
+			line-height: 24px;
+			letter-spacing: 0.15px;
+			color: #1b1b1b;
+			margin: 0 0 0.5rem 0;
+		}
+
+		h5 {
+			font-style: normal;
+			font-weight: 500;
+			font-size: 18px;
+			line-height: 20px;
+			letter-spacing: 0.1px;
+			color: #1b1b1b;
+			margin: 0 0 0.5rem 0;
+		}
+
+		ul {
+			list-style-type: square;
+		}
+	}
+}
 </style>
