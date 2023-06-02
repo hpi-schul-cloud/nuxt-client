e36fe0ed0a92e7ab1740fa4a7b4de3987a676e4c
Max Bischof <maximilian.bischof@dataport.de>
2023-06-02 12:11:43 +0200

    Fix typing in file content elements onmount and add more tests

--- src/components/feature-board/content-elements/FileContentElement.unit.ts
+++ src/components/feature-board/content-elements/FileContentElement.unit.ts
@@ -24,9 +24,9 @@ describe("FileContentElement", () => {
 		return { wrapper };
 	};
 
-	describe("when file is available", () => {
-		describe("when component is not in edit mode", () => {
-			const setup = () => {
+	describe("when file is already uploaded", () => {
+		describe("when file is already loaded in state", () => {
+			const setup = (isEditMode: boolean) => {
 				const element = fileElementResponse.build();
 				document.body.setAttribute("data-app", "true");
 
@@ -34,63 +34,122 @@ describe("FileContentElement", () => {
 				const getFileMock = jest.fn().mockReturnValueOnce(fileRecordResponse);
 				setupFileStorageApiMock({ getFileMock });
 
-				const { wrapper } = getWrapper({ element, isEditMode: false });
+				const { wrapper } = getWrapper({ element, isEditMode });
 
 				return { wrapper };
 			};
 
-			it("should be found in dom", () => {
-				const { wrapper } = setup();
+			describe("when component is not in edit mode", () => {
+				it("should be found in dom", () => {
+					const { wrapper } = setup(false);
 
-				const fileContentElement = wrapper.findComponent(FileContentElement);
-				expect(fileContentElement.exists()).toBe(true);
+					const fileContentElement = wrapper.findComponent(FileContentElement);
+					expect(fileContentElement.exists()).toBe(true);
+				});
+
+				it("should render FileContentElementDisplay component", async () => {
+					const { wrapper } = setup(false);
+
+					await wrapper.vm.$nextTick();
+
+					const fileContentElementDisplay = wrapper.findComponent(
+						FileContentElementDisplay
+					);
+					expect(fileContentElementDisplay.exists()).toBe(true);
+				});
 			});
 
-			it("should render FileContentElementDisplay component", async () => {
-				const { wrapper } = setup();
+			describe("when component is in edit mode", () => {
+				it("should be found in dom", () => {
+					const { wrapper } = setup(true);
+					expect(wrapper.findComponent(FileContentElement).exists()).toBe(true);
+				});
 
-				await wrapper.vm.$nextTick();
+				it("should render FileContentElementEdit component", async () => {
+					const { wrapper } = setup(true);
 
-				const fileContentElementDisplay = wrapper.findComponent(
-					FileContentElementDisplay
-				);
-				expect(fileContentElementDisplay.exists()).toBe(true);
+					await wrapper.vm.$nextTick();
+
+					const fileContentElementEdit = wrapper.findComponent(
+						FileContentElementEdit
+					);
+					expect(fileContentElementEdit.exists()).toBe(true);
+				});
 			});
 		});
 
-		describe("when component is in edit mode", () => {
-			const setup = () => {
+		describe("when file needs to be loaded", () => {
+			const setup = (isEditMode: boolean) => {
 				const element = fileElementResponse.build();
 				document.body.setAttribute("data-app", "true");
 
 				const fileRecordResponse = fileRecordResponseFactory.build();
-				const getFileMock = jest.fn().mockReturnValueOnce(fileRecordResponse);
-				setupFileStorageApiMock({ getFileMock });
+				const getFileMock = jest
+					.fn()
+					.mockReturnValueOnce(undefined)
+					.mockReturnValueOnce(fileRecordResponse);
+				const { fetchFiles } = setupFileStorageApiMock({
+					getFileMock,
+				});
 
-				const { wrapper } = getWrapper({ element, isEditMode: true });
+				const { wrapper } = getWrapper({ element, isEditMode });
 
-				return { wrapper };
+				return { wrapper, fetchFiles, getFileMock };
 			};
 
-			it("should be found in dom", () => {
-				const { wrapper } = setup();
-				expect(wrapper.findComponent(FileContentElement).exists()).toBe(true);
+			describe("when component is not in edit mode", () => {
+				it("should be found in dom", () => {
+					const { wrapper } = setup(false);
+
+					const fileContentElement = wrapper.findComponent(FileContentElement);
+					expect(fileContentElement.exists()).toBe(true);
+				});
+
+				it("should render FileContentElementDisplay component", async () => {
+					const { wrapper, getFileMock } = setup(false);
+
+					await wrapper.vm.$nextTick();
+					await wrapper.vm.$nextTick();
+
+					const fileContentElementDisplay = wrapper.findComponent(
+						FileContentElementDisplay
+					);
+					expect(getFileMock).toHaveBeenCalledTimes(2);
+
+					expect(fileContentElementDisplay.exists()).toBe(true);
+				});
+
+				it("should render FileContentElementDisplay component", async () => {
+					const { wrapper, fetchFiles } = setup(false);
+
+					await wrapper.vm.$nextTick();
+
+					expect(fetchFiles).toHaveBeenCalledTimes(1);
+				});
 			});
 
-			it("should render FileContentElementEdit component", async () => {
-				const { wrapper } = setup();
+			describe("when component is in edit mode", () => {
+				it("should be found in dom", () => {
+					const { wrapper } = setup(true);
+					expect(wrapper.findComponent(FileContentElement).exists()).toBe(true);
+				});
 
-				await wrapper.vm.$nextTick();
+				it("should render FileContentElementEdit component", async () => {
+					const { wrapper } = setup(true);
 
-				const fileContentElementEdit = wrapper.findComponent(
-					FileContentElementEdit
-				);
-				expect(fileContentElementEdit.exists()).toBe(true);
+					await wrapper.vm.$nextTick();
+					await wrapper.vm.$nextTick();
+
+					const fileContentElementEdit = wrapper.findComponent(
+						FileContentElementEdit
+					);
+					expect(fileContentElementEdit.exists()).toBe(true);
+				});
 			});
 		});
 	});
 
-	describe("when file is not available", () => {
+	describe("when file is not upload onMount", () => {
 		const setup = () => {
 			const element = fileElementResponse.build();
 			document.body.setAttribute("data-app", "true");
@@ -111,7 +170,7 @@ describe("FileContentElement", () => {
 		});
 	});
 
-	describe("when newFileForParent becomes defined", () => {
+	describe("when file finished uploading and newFileForParent becomes defined", () => {
 		describe("when newFileForParent equals element id", () => {
 			const setup = () => {
 				const element = fileElementResponse.build();
