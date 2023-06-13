625c6efb1e25c065de13176c659f93a1a17c256d
Max Bischof <maximilian.bischof@dataport.de>
2023-06-13 11:40:05 +0200

    Modify mocks

--- tests/test-utils/composable-mocks/fileStorageApiMock.ts
+++ tests/test-utils/composable-mocks/fileStorageApiMock.ts
@@ -5,17 +5,15 @@ import { ref } from "vue";
 
 interface Props {
 	uploadMock?: jest.Mock;
-	downloadMock?: jest.Mock;
 	fetchFilesMock?: jest.Mock;
 	renameMock?: jest.Mock;
 	getFileMock?: jest.Mock;
 }
 
 export const setupFileStorageApiMock = (props: Props) => {
-	const { downloadMock, fetchFilesMock, renameMock, uploadMock, getFileMock } =
-		props;
+	const { fetchFilesMock, renameMock, uploadMock, getFileMock } = props;
 	const mockedFileStorageApi = jest.mocked(useFileStorageApi);
-	const download = downloadMock ?? jest.fn();
+
 	const fetchFiles = fetchFilesMock ?? jest.fn();
 	const rename = renameMock ?? jest.fn();
 	const upload = uploadMock ?? jest.fn();
@@ -25,18 +23,13 @@ export const setupFileStorageApiMock = (props: Props) => {
 		statusCode: "",
 		message: "",
 	});
-	const fileRecords = ref({});
-	const newFileForParent = ref("");
 
 	const mocks = {
-		download,
 		fetchFiles,
 		rename,
 		upload,
 		getFile,
 		businessError,
-		fileRecords,
-		newFileForParent,
 	};
 
 	mockedFileStorageApi.mockReturnValue(mocks);
--- tests/test-utils/composable-mocks/selectedFileMock.ts
+++ tests/test-utils/composable-mocks/selectedFileMock.ts
@@ -1,11 +1,17 @@
 import { jest } from "@jest/globals";
 import { useSelectedFile } from "@/components/feature-board/shared/SelectedFile.composable";
 
-export const setupSelectedFileMock = () => {
+interface Props {
+	getSelectedFileMock?: () => File | undefined;
+	setSelectedFileMock?: (file?: File) => void;
+}
+
+export const setupSelectedFileMock = (props: Props) => {
+	const { getSelectedFileMock, setSelectedFileMock } = props;
 	const mockedSelectedFile = jest.mocked(useSelectedFile);
 
-	const getSelectedFile: () => File | undefined = jest.fn();
-	const setSelectedFile = jest.fn();
+	const getSelectedFile = getSelectedFileMock ?? jest.fn();
+	const setSelectedFile = setSelectedFileMock ?? jest.fn();
 
 	const mocks = {
 		getSelectedFile,
