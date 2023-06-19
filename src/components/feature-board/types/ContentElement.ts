a206e5a31eabb6393060c9164910ea3165ffe2b6
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-06-19 15:05:31 +0200

    BC-4054 - remove `DeleteElementEventPayload` type

--- src/components/feature-board/types/ContentElement.ts
+++ src/components/feature-board/types/ContentElement.ts
@@ -1,8 +1,3 @@
 import { FileElementResponse, RichTextElementResponse } from "@/serverApi/v3";
 
 export type AnyContentElement = RichTextElementResponse | FileElementResponse;
-
-export type DeleteElementEventPayload = {
-	elementId: string;
-	name?: string;
-};
--- tests/test-utils/factory/deleteElementEventPayloadFactory.ts
+++
@@ -1,8 +0,0 @@
-import { Factory } from "fishery";
-import { DeleteElementEventPayload } from "@/components/feature-board/types/ContentElement";
-
-export const deleteElementEventPayloadFactory =
-	Factory.define<DeleteElementEventPayload>(({ sequence }) => ({
-		elementId: `id ${sequence}`,
-		name: `name ${sequence}`,
-	}));
