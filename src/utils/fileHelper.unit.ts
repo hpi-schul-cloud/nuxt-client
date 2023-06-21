af09e3701596295559b237564ffff9223a32e7f2
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-06-21 13:17:20 +0200

    BC-4120 - minor change to `fileHelper` unit tests

--- src/utils/fileHelper.unit.ts
+++ src/utils/fileHelper.unit.ts
@@ -1,27 +1,29 @@
 import { downloadFile } from "./fileHelper";
 
-describe("downloadFile", () => {
-	it("should download the file", () => {
-		const url = "test-url";
-		const fileName = "test-file.ext";
-		const link = {
-			href: "",
-			download: "",
-			click: jest.fn(),
-		} as any;
-		const createElementSpy = jest
-			.spyOn(document, "createElement")
-			.mockImplementation(() => link);
-		document.body.appendChild = jest.fn();
-		document.body.removeChild = jest.fn();
+describe("@/utils/fileHelper", () => {
+	describe("downloadFile", () => {
+		it("should download the file", () => {
+			const url = "test-url";
+			const fileName = "test-file.ext";
+			const link = {
+				href: "",
+				download: "",
+				click: jest.fn(),
+			} as any;
+			const createElementSpy = jest
+				.spyOn(document, "createElement")
+				.mockImplementation(() => link);
+			document.body.appendChild = jest.fn();
+			document.body.removeChild = jest.fn();
 
-		downloadFile(url, fileName);
+			downloadFile(url, fileName);
 
-		expect(createElementSpy).toBeCalledWith("a");
-		expect(link.href).toEqual(url);
-		expect(link.download).toEqual(fileName);
-		expect(document.body.appendChild).toBeCalledWith(link);
-		expect(link.click).toHaveBeenCalledTimes(1);
-		expect(document.body.removeChild).toBeCalledWith(link);
+			expect(createElementSpy).toBeCalledWith("a");
+			expect(link.href).toEqual(url);
+			expect(link.download).toEqual(fileName);
+			expect(document.body.appendChild).toBeCalledWith(link);
+			expect(link.click).toHaveBeenCalledTimes(1);
+			expect(document.body.removeChild).toBeCalledWith(link);
+		});
 	});
 });
