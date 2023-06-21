f32d48b71078ca72c2c089a97c42472d82d7b9e3
dyedwiper <maximilian.becker@dataport.de>
2023-06-21 14:13:01 +0200

    Update tests for chips

--- src/components/feature-board/content-elements/FileContentElementChips.unit.ts
+++ src/components/feature-board/content-elements/FileContentElementChips.unit.ts
@@ -1,57 +1,49 @@
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
-import { MountOptions, shallowMount, Wrapper } from "@vue/test-utils";
-import Vue from "vue";
+import { mount } from "@vue/test-utils";
 import FileContentElementChips from "./FileContentElementChips.vue";
-import {
-	convertFileSizeToHumanReadable,
-	getFileExtension,
-} from "@/utils/fileHelper";
 
 describe("FileContentElementChips", () => {
-	let wrapper: Wrapper<Vue>;
-
-	const setup = (props: { fileRecordSize: number; fileRecordName: string }) => {
-		wrapper = shallowMount(FileContentElementChips as MountOptions<Vue>, {
+	const setup = () => {
+		const wrapper = mount(FileContentElementChips, {
 			...createComponentMocks({}),
-			propsData: props,
+			propsData: {
+				fileSize: 3800,
+				fileName: "test.jpeg",
+			},
 		});
+
+		return { wrapper };
 	};
 
-	describe("when component is mounted", () => {
-		it("should be found in dom when a file is added", () => {
-			setup({ fileRecordSize: 3800, fileRecordName: "image.jpeg" });
-			expect(wrapper.findComponent(FileContentElementChips).exists()).toBe(
-				true
-			);
-		});
+	it("should be found in dom", () => {
+		const { wrapper } = setup();
 
-		it("should return file extension in upper case", () => {
-			setup({ fileRecordSize: 3800, fileRecordName: "image.jpeg" });
-			expect(getFileExtension("image.jpeg")).toBe("JPEG");
-		});
+		const chipsComponent = wrapper.findComponent(FileContentElementChips);
 
-		it("should return file extension in upper case, even if a dot exists in filename", () => {
-			setup({ fileRecordSize: 3800, fileRecordName: "text.1.pdf" });
-			expect(getFileExtension("text.1.pdf")).toBe("PDF");
-		});
+		expect(chipsComponent.exists()).toBe(true);
+	});
 
-		it("should return file size converted in KB", () => {
-			setup({ fileRecordSize: 3800, fileRecordName: "image.jpeg" });
-			expect(convertFileSizeToHumanReadable(3800)).toStrictEqual("3.71 KB");
-		});
+	it("should contain two chips", () => {
+		const { wrapper } = setup();
 
-		it("should return file size converted in MB", () => {
-			setup({ fileRecordSize: 104857600, fileRecordName: "image.jpeg" });
-			expect(convertFileSizeToHumanReadable(104857600)).toStrictEqual(
-				"100.00 MB"
-			);
-		});
+		const chips = wrapper.findAll(".v-chip");
 
-		it("should return file size converted in GB", () => {
-			setup({ fileRecordSize: 1073741824, fileRecordName: "image.jpeg" });
-			expect(convertFileSizeToHumanReadable(1073741824)).toStrictEqual(
-				"1.00 GB"
-			);
-		});
+		expect(chips.length).toBe(2);
+	});
+
+	it("should contain a chip with the file extension", () => {
+		const { wrapper } = setup();
+
+		const chip = wrapper.findAll(".v-chip").at(0);
+
+		expect(chip.text()).toBe("JPEG");
+	});
+
+	it("should contain a chip with the file size", () => {
+		const { wrapper } = setup();
+
+		const chip = wrapper.findAll(".v-chip").at(1);
+
+		expect(chip.text()).toBe("3,71 KB");
 	});
 });
