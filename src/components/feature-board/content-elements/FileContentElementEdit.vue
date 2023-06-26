6c075941052cdfe1340334004a7a5335acadef4e
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-06-26 15:45:44 +0200

    BC-4210 - move `BoardMenu` out of `v-list`
    - also make `<a>` used for `downloadFile` "hidden"

--- src/components/feature-board/content-elements/FileContentElementEdit.vue
+++ src/components/feature-board/content-elements/FileContentElementEdit.vue
@@ -1,58 +1,66 @@
 <template>
-	<v-list flat class="py-0 rounded-b-0" data-testid="board-file-element-edit">
-		<v-list-item class="px-2 grey lighten-3">
-			<v-list-item-icon class="my-2 mr-2">
-				<v-icon
-					class="deco-icon--text"
-					data-testid="board-file-element-edit-file-icon"
-					large
-					>{{ mdiFileDocumentOutline }}</v-icon
+	<div
+		class="d-flex align-stretch rounded-t-sm"
+		data-testid="board-file-element-edit"
+	>
+		<v-list
+			flat
+			class="grey lighten-3 px-0 py-0 rounded-tl-sm"
+			max-width="calc(100% - 44px)"
+		>
+			<v-list-item class="pr-2">
+				<v-list-item-icon class="my-2 mr-2">
+					<v-icon
+						class="deco-icon--text"
+						data-testid="board-file-element-edit-file-icon"
+						large
+						>{{ mdiFileDocumentOutline }}</v-icon
+					>
+				</v-list-item-icon>
+
+				<v-list-item-content>
+					<v-list-item-title data-testid="board-file-element-edit-file-name">{{
+						fileName
+					}}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
+		<div class="d-flex align-center grey lighten-3 pr-2 rounded-tr-sm">
+			<BoardMenu scope="element">
+				<BoardMenuAction data-testid="board-file-element-edit-menu-move-up">
+					<VIcon>
+						{{ mdiArrowCollapseUp }}
+					</VIcon>
+					{{ $t("components.board.action.moveUp") }}
+				</BoardMenuAction>
+				<BoardMenuAction data-testid="board-file-element-edit-menu-move-down">
+					<VIcon>
+						{{ mdiArrowCollapseDown }}
+					</VIcon>
+					{{ $t("components.board.action.moveDown") }}
+				</BoardMenuAction>
+				<BoardMenuAction
+					v-if="!isBlocked"
+					data-testid="board-file-element-edit-menu-download"
+					@click="onDownload"
 				>
-			</v-list-item-icon>
-
-			<v-list-item-content>
-				<v-list-item-title data-testid="board-file-element-edit-file-name">{{
-					fileName
-				}}</v-list-item-title>
-			</v-list-item-content>
-
-			<v-list-item-icon class="my-2 ml-2">
-				<BoardMenu scope="element">
-					<BoardMenuAction data-testid="board-file-element-edit-menu-move-up">
-						<VIcon>
-							{{ mdiArrowCollapseUp }}
-						</VIcon>
-						{{ $t("components.board.action.moveUp") }}
-					</BoardMenuAction>
-					<BoardMenuAction data-testid="board-file-element-edit-menu-move-down">
-						<VIcon>
-							{{ mdiArrowCollapseDown }}
-						</VIcon>
-						{{ $t("components.board.action.moveDown") }}
-					</BoardMenuAction>
-					<BoardMenuAction
-						v-if="!isBlocked"
-						data-testid="board-file-element-edit-menu-download"
-						@click="onDownload"
-					>
-						<VIcon>
-							{{ mdiTrayArrowDown }}
-						</VIcon>
-						{{ $t("components.board.action.download") }}
-					</BoardMenuAction>
-					<BoardMenuAction
-						data-testid="board-file-element-edit-menu-delete"
-						@click="onDelete"
-					>
-						<VIcon>
-							{{ mdiTrashCanOutline }}
-						</VIcon>
-						{{ $t("components.board.action.delete") }}
-					</BoardMenuAction>
-				</BoardMenu>
-			</v-list-item-icon>
-		</v-list-item>
-	</v-list>
+					<VIcon>
+						{{ mdiTrayArrowDown }}
+					</VIcon>
+					{{ $t("components.board.action.download") }}
+				</BoardMenuAction>
+				<BoardMenuAction
+					data-testid="board-file-element-edit-menu-delete"
+					@click="onDelete"
+				>
+					<VIcon>
+						{{ mdiTrashCanOutline }}
+					</VIcon>
+					{{ $t("components.board.action.delete") }}
+				</BoardMenuAction>
+			</BoardMenu>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -108,3 +116,12 @@ export default defineComponent({
 	},
 });
 </script>
+
+<style scoped>
+.board-menu {
+	position: absolute;
+	top: 0.25rem;
+	right: 0.25rem;
+	z-index: 1;
+}
+</style>
