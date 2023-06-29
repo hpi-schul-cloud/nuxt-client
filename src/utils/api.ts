bb8ecf4431e813423ee729db3f3ba71e1452804b
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-06-29 13:12:14 +0200

    BC-4053 - add response error mapping

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
-import { AxiosInstance } from "axios";
+import { ApiResponseError } from "@/store/types/commons";
+import { AxiosInstance, isAxiosError } from "axios";
 import Vue from "vue";
 
 let $axios: AxiosInstance;
@@ -7,5 +8,24 @@ export function initializeAxios(axios: AxiosInstance) {
 	$axios = axios;
 	Vue.prototype.$axios = axios;
 }
+
+export const mapAxiosErrorToApiResponseError = (
+	error: unknown
+): ApiResponseError => {
+	const apiError = { message: "", code: 0, title: "", type: "" };
+
+	if (isAxiosError(error)) {
+		const { message, code, title, type } = error.response?.data;
+
+		apiError.message = message;
+		apiError.code = code;
+		apiError.title = title;
+		apiError.type = type;
+	} else if (error instanceof Error) {
+		apiError.message = error.message;
+	}
+
+	return apiError;
+};
 
 export { $axios };
