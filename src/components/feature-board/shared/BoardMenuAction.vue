3cd8a380520a4b5a13566e395e97e29f41a2dadb
Oliver Happe <108657965+OliverHappe@users.noreply.github.com>
2023-06-29 14:57:06 +0200

    Bc 3981 UI polishing for mvp (#2605)
    * fix column title placeholder
    
    * adjust background color of 3-dot on cards
    
    * added cards receive focus when entering the board
    
    * adjust font alignment of menu items
    
    * fix box-shadow overlap of columns
    
    * add break-word to delete modal
    
    * prevent column drag in edit mode

--- src/components/feature-board/shared/BoardMenuAction.vue
+++ src/components/feature-board/shared/BoardMenuAction.vue
@@ -1,5 +1,6 @@
 <template>
-	<VListItem @click="onClick($event)" @keydown.enter="onClick($event)">
+	<VListItem @click.prevent="onClick">
+		<VListItemIcon class="mr-2"><slot name="icon"></slot></VListItemIcon>
 		<VListItemTitle><slot></slot></VListItemTitle>
 	</VListItem>
 </template>
