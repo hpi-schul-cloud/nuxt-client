f267202f2762853f5375e93e9a26ebdf7d7157cb
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2023-07-03 14:45:24 +0200

    BC-3981 - display breadcrumbs using board context api (#2600)
    * implement breadCrumbs composable
    
    * implement breadcrumbs using board context
    
    ---------
    
    Co-authored-by: hoeppner-dataport <marcus.hoeppner@dataport.de>

---
+++ tests/test-utils/mockApiResponse.ts
@@ -0,0 +1,16 @@
+import { createMock } from "@golevelup/ts-jest";
+import { AxiosHeaders, AxiosResponse, AxiosResponseHeaders } from "axios";
+
+export const mockApiResponse = <T>(
+	values: Partial<AxiosResponse<T>>
+): AxiosResponse<T> => {
+	const response: AxiosResponse<T> = {
+		data: {} as unknown as T,
+		status: 200,
+		statusText: "",
+		headers: createMock<AxiosResponseHeaders>(),
+		config: { headers: createMock<AxiosHeaders>() },
+		...values,
+	};
+	return response;
+};
