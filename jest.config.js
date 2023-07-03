8d4197d912fb2981d30011faf3ad96234b3d4ac5
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-07-03 11:58:16 +0200

    extend `jest.config.js` to also check for `*.ts` files in `/src/utils`

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,7 @@ module.exports = {
 		"<rootDir>/src/mixins/**/*.js",
 		"<rootDir>/src/plugins/**/*.(js|ts)",
 		"<rootDir>/src/store/**/*.(js|ts)",
-		"<rootDir>/src/utils/**/*.js",
+		"<rootDir>/src/utils/**/*.(js|ts)",
 		// Exclude
 		"!<rootDir>/src/components/base/_globals.js",
 		"!<rootDir>/src/components/icons/**/*",
