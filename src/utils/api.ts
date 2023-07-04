8398a860c8584f0cff1a552b77e3ea1987394c2f
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-07-04 14:16:27 +0200

    BC-4053 - api.ts review and add unit tests

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,6 @@
 import { ApiResponseError, ApiValidationError } from "@/store/types/commons";
-import { AxiosError, AxiosInstance, isAxiosError } from "axios";
+import { isObject, isString } from "@vueuse/core";
+import { AxiosInstance, isAxiosError } from "axios";
 import Vue from "vue";
 
 let $axios: AxiosInstance;
@@ -9,38 +10,31 @@ export function initializeAxios(axios: AxiosInstance) {
 	Vue.prototype.$axios = axios;
 }
 
-const createApiResponseError = (error: AxiosError): ApiResponseError => {
-	const { message, code, title, type } = error.response
-		?.data as ApiResponseError;
-
-	return { message, code, title, type };
-};
-
-const createApiValidationError = (error: AxiosError): ApiValidationError => {
-	const { message, code, title, type, validationErrors } = error.response
-		?.data as ApiValidationError;
-
-	return { message, code, title, type, validationErrors };
-};
-
-export const mapAxiosErrorToApiResponseError = (
+export const mapAxiosErrorToResponseError = (
 	error: unknown
 ): ApiResponseError | ApiValidationError => {
 	let apiError: ApiResponseError | ApiValidationError = {
-		message: "",
-		code: 0,
+		message: "UNKNOWN_ERROR",
+		code: 1,
 		title: "",
-		type: "",
+		type: "Unknown error",
 	};
 
-	if (isAxiosError(error)) {
+	if (
+		isAxiosError<
+			ApiValidationError | ApiResponseError,
+			Record<string, unknown>
+		>(error)
+	) {
 		const errorPayload = error.response?.data;
-		apiError =
-			"validationErrors" in errorPayload
-				? createApiValidationError(errorPayload)
-				: createApiResponseError(errorPayload);
-	} else if (error instanceof Error) {
-		apiError.message = error.message;
+		if (errorPayload && isObject(errorPayload)) {
+			apiError = errorPayload;
+		} else if (isString(errorPayload)) {
+			apiError.message = errorPayload;
+			apiError.code = error.response?.status || apiError.code;
+			apiError.type = error.code || apiError.type;
+			apiError.title = error.response?.statusText || apiError.title;
+		}
 	}
 
 	return apiError;
