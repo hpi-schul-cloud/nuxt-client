8f89c3f8b6d128343f8f79580c811b133ac96e4f
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-07-10 14:02:02 +0200

    BC-4170 - extend unit tests for `FileRecord.composable`

--- src/components/feature-board/content-elements/FileRecord.composable.unit.ts
+++ src/components/feature-board/content-elements/FileRecord.composable.unit.ts
@@ -45,6 +45,38 @@ describe("FileRecord Composable", () => {
 		expect(url.value).toBe(fileRecordResponse.value.url);
 	});
 
+	describe("when file record is undefined", () => {
+		const setup = () => {
+			const fileRecordResponse = ref(undefined);
+
+			return { fileRecordResponse };
+		};
+
+		it("should set isImage to be false", async () => {
+			const { fileRecordResponse } = setup();
+
+			const { isImage } = useFileRecord(fileRecordResponse);
+
+			expect(isImage.value).toBe(false);
+		});
+
+		it("should set isBlockedByVirusScan to be false", async () => {
+			const { fileRecordResponse } = setup();
+
+			const { isBlockedByVirusScan } = useFileRecord(fileRecordResponse);
+
+			expect(isBlockedByVirusScan.value).toBe(false);
+		});
+
+		it("should set url to be empty", async () => {
+			const { fileRecordResponse } = setup();
+
+			const { url } = useFileRecord(fileRecordResponse);
+
+			expect(url.value).toBe("");
+		});
+	});
+
 	describe("when file is an image", () => {
 		const setup = () => {
 			const parentId = ObjectIdMock();
