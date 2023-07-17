434934ea6c7e65b11a2e049b9b2e6e5be73dc16b
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-07-17 08:06:07 +0200

    BC-4170 - fix finally `delayWithAbort` unit tests

--- src/utils/helpers.unit.js
+++ src/utils/helpers.unit.js
@@ -28,7 +28,13 @@ describe("helpers", () => {
 	describe("delayWithAbort", () => {
 		test("should not resolve until timeout has passed", async () => {
 			const abortController = new AbortController();
-			const signal = abortController.signal;
+			let signal = abortController.signal;
+			signal = {
+				...signal,
+				throwIfAborted: jest.fn(),
+				addEventListener: jest.fn(),
+				removeEventListener: jest.fn(),
+			};
 
 			jest.useFakeTimers();
 			const thenSpy = jest.fn();
@@ -50,14 +56,43 @@ describe("helpers", () => {
 
 		it("should return a promise", () => {
 			const abortController = new AbortController();
-			const signal = abortController.signal;
+			let signal = abortController.signal;
+			signal = {
+				...signal,
+				throwIfAborted: jest.fn(),
+				addEventListener: jest.fn(),
+				removeEventListener: jest.fn(),
+			};
 
 			expect(delayWithAbort(1, { signal: signal })).toBeInstanceOf(Promise);
 		});
 
 		test("should throw an error when aborted", async () => {
-			const abortController = new AbortController();
-			const signal = abortController.signal;
+			let abortController = new AbortController();
+			abortController = { ...abortController, abort: () => signal.abort() };
+
+			let signal = abortController.signal;
+			let _listener = undefined;
+			const addEventListenerMock = jest
+				.fn()
+				.mockImplementation((type, listener) => {
+					_listener = listener;
+				});
+			const removeEventListenerMock = jest
+				.fn()
+				.mockImplementation((type, listener) => {
+					_listener = undefined;
+				});
+			const abortMock = () => {
+				_listener();
+			};
+			signal = {
+				...signal,
+				throwIfAborted: jest.fn(),
+				addEventListener: addEventListenerMock,
+				removeEventListener: removeEventListenerMock,
+				abort: abortMock,
+			};
 
 			jest.useFakeTimers();
 			const thenSpy = jest.fn();
@@ -67,12 +102,14 @@ describe("helpers", () => {
 			// not resolved after half time
 			jest.advanceTimersByTime(50);
 			await Promise.resolve();
+			await jest.runAllTicks();
 			expect(thenSpy).not.toHaveBeenCalled();
 			expect(catchSpy).not.toHaveBeenCalled();
 
 			// abort the delay function
 			abortController.abort();
 			await Promise.resolve();
+			await jest.runAllTicks();
 
 			expect(thenSpy).not.toHaveBeenCalled();
 			expect(catchSpy).toHaveBeenCalled();
