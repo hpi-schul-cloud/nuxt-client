f36484c7416d62708f39a8c091b2be0cfd3ce527
Malte Berg <malte.berg@capgemini.com>
2023-07-19 14:11:41 +0200

    change custom-parameter

--- src/store/external-tool/tool-parameter.enum.ts
+++ src/store/external-tool/tool-parameter.enum.ts
@@ -2,7 +2,7 @@ export enum ToolParameterType {
 	String = "string",
 	Number = "number",
 	Boolean = "boolean",
-	AutoCourseid = "auto_courseid",
-	AutoCoursename = "auto_coursename",
+	AutoContextid = "auto_contextid",
+	AutoContextname = "auto_contextname",
 	AutoSchoolid = "auto_schoolid",
 }
