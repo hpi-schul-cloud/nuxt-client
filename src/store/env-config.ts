d0c5b2226c228519b4eaef2657aa28ae876d923c
Malte Berg <malte.berg@capgemini.com>
2023-07-27 08:32:58 +0200

    adjust getter for FEATURE_FLAG

--- src/store/env-config.ts
+++ src/store/env-config.ts
@@ -167,7 +167,7 @@ export default class EnvConfigModule extends VuexModule {
 	}
 
 	get getShowOutdatedUsers(): boolean {
-		return this.env.FEATURE_CTL_TOOLS_TAB_ENABLED ?? false;
+		return this.env.FEATURE_SHOW_OUTDATED_USERS ?? false;
 	}
 
 	get getEnv(): Envs {
