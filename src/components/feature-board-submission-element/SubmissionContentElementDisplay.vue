5d4672374e0f071c7333b3afcb57a285d195d576
Odalys Adam <odalys.adam@dataport.de>
2023-08-25 14:25:20 +0200

    create title component to ensure consistency

--- src/components/feature-board-submission-element/SubmissionContentElementDisplay.vue
+++ src/components/feature-board-submission-element/SubmissionContentElementDisplay.vue
@@ -1,25 +1,6 @@
 <template>
 	<div class="grey lighten-4 pa-4 rounded">
-		<v-card-title
-			class="pa-0"
-			data-testid="board-submission-element-display"
-			inactive
-		>
-			<v-icon
-				color="black"
-				size="18"
-				class="mr-2"
-				data-testid="board-submission-element-display-icon"
-			>
-				$mdiLightbulbOnOutline
-			</v-icon>
-			<span
-				class="subtitle-1 d-inline-block text-truncate black--text font-weight-bold"
-				data-testid="board-submission-element-display-content"
-			>
-				{{ $t("components.cardElement.submissionElement") }}
-			</span>
-		</v-card-title>
+		<SubmissionContentElementTitle />
 		<v-card-subtitle
 			class="pa-0 mt-1 subtitle-1"
 			data-testid="board-submission-element-due-date"
@@ -32,9 +13,11 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import dayjs from "dayjs";
+import SubmissionContentElementTitle from "./SubmissionContentElementTitle.vue";
 
 export default defineComponent({
 	name: "SubmissionContentElementDisplay",
+	components: { SubmissionContentElementTitle },
 	props: {
 		dueDate: {
 			type: String,
--- src/components/feature-board-submission-element/SubmissionContentElementEdit.vue
+++ src/components/feature-board-submission-element/SubmissionContentElementEdit.vue
@@ -1,36 +1,19 @@
 <template>
 	<div class="grey lighten-4 pa-4 rounded">
-		<v-card-title
-			class="pa-0"
-			data-testid="board-submission-element-edit"
-			inactive
-		>
-			<v-icon
-				color="black"
-				size="18"
-				class="mr-2"
-				data-testid="board-submission-element-edit-icon"
-			>
-				$mdiLightbulbOnOutline
-			</v-icon>
-			<span
-				class="subtitle-1 d-inline-block text-truncate black--text font-weight-bold"
-				data-testid="board-submission-element-edit-content"
-			>
-				{{ $t("components.cardElement.submissionElement") }}
-			</span>
-			<div class="menu">
-				<SubmissionContentElementMenu
-					:isFirstElement="isFirstElement"
-					:isLastElement="isLastElement"
-					:hasMultipleElements="hasMultipleElements"
-					@move-down:element="onMoveElementDown"
-					@move-up:element="onMoveElementUp"
-					@delete:element="onDeleteElement"
-				/>
-			</div>
-		</v-card-title>
+		<SubmissionContentElementTitle />
+		<div class="menu">
+			<SubmissionContentElementMenu
+				:isFirstElement="isFirstElement"
+				:isLastElement="isLastElement"
+				:hasMultipleElements="hasMultipleElements"
+				@move-down:element="onMoveElementDown"
+				@move-up:element="onMoveElementUp"
+				@delete:element="onDeleteElement"
+			/>
+		</div>
 		<date-time-picker
+			class="mt-1"
+			:dateTime="dueDate"
 			:date-input-label="t('common.labels.date')"
 			:time-input-label="t('common.labels.time')"
 		/>
@@ -40,12 +23,17 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import SubmissionContentElementMenu from "./SubmissionContentElementMenu.vue";
+import SubmissionContentElementTitle from "./SubmissionContentElementTitle.vue";
 import { DateTimePicker } from "@feature-date-time-picker";
 import { useI18n } from "@/composables/i18n.composable";
 
 export default defineComponent({
 	name: "SubmissionContentElementEdit",
-	components: { SubmissionContentElementMenu, DateTimePicker },
+	components: {
+		SubmissionContentElementMenu,
+		SubmissionContentElementTitle,
+		DateTimePicker,
+	},
 	props: {
 		dueDate: {
 			type: String,
