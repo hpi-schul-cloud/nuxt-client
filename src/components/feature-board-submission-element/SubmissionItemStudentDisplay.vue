28e60e3dd6a48ed97035263a6a55610b6d4e6a80
Christian Darsow <christian.darsow@dataport.de>
2023-09-01 14:59:50 +0200

    Add checkbox

--- src/components/feature-board-submission-element/SubmissionItemStudentDisplay.vue
+++ src/components/feature-board-submission-element/SubmissionItemStudentDisplay.vue
@@ -1,9 +1,11 @@
 <template>
-	<div>{{ submissionItems[0]?.completed }}</div>
+	<div>
+		<v-checkbox v-model="completed" label="erledigt"></v-checkbox>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, computed, PropType } from "vue";
 import { SubmissionItemResponse } from "@/serverApi/v3";
 
 export default defineComponent({
@@ -13,6 +15,15 @@ export default defineComponent({
 			type: Array as PropType<Array<SubmissionItemResponse>>,
 			required: true,
 		},
+	},
+	setup(props) {
+		const completed = computed(() => {
+			return props.submissionItems[0]?.completed;
+		});
+
+		return {
+			completed,
+		};
 	},
 });
 </script>
