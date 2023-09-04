1b94b95f4786d9db5906a04bf99794da04e72252
Christian Darsow <christian.darsow@dataport.de>
2023-09-04 11:37:30 +0200

    Add user role tests

--- src/components/feature-board-submission-element/SubmissionContentElementDisplay.unit.ts
+++ src/components/feature-board-submission-element/SubmissionContentElementDisplay.unit.ts
@@ -1,29 +1,34 @@
+import Vue from "vue";
 import { AUTH_MODULE_KEY, I18N_KEY } from "@/utils/inject";
 import createComponentMocks from "@@/tests/test-utils/componentMocks";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, MountOptions } from "@vue/test-utils";
 import AuthModule from "@/store/auth";
 import { createModuleMocks } from "@/utils/mock-store-module";
 import SubmissionContentElementDisplay from "./SubmissionContentElementDisplay.vue";
+import SubmissionItemStudentDisplay from "./SubmissionItemStudentDisplay.vue";
 
 describe("SubmissionContentElementDisplay", () => {
-	const setup = () => {
+	const setup = (role: "teacher" | "student" = "teacher") => {
 		document.body.setAttribute("data-app", "true");
 
 		const propsData = {
 			dueDate: "01.01.2023 01:23",
 		};
 		const authModule = createModuleMocks(AuthModule, {
-			getUserRoles: ["student"],
+			getUserRoles: [role],
 		});
 
-		const wrapper = shallowMount(SubmissionContentElementDisplay, {
-			...createComponentMocks({ i18n: true }),
-			propsData,
-			provide: {
-				[I18N_KEY.valueOf()]: { t: (key: string) => key },
-				[AUTH_MODULE_KEY.valueOf()]: authModule,
-			},
-		});
+		const wrapper = shallowMount(
+			SubmissionContentElementDisplay as MountOptions<Vue>,
+			{
+				...createComponentMocks({ i18n: true }),
+				propsData,
+				provide: {
+					[I18N_KEY.valueOf()]: { t: (key: string) => key },
+					[AUTH_MODULE_KEY.valueOf()]: authModule,
+				},
+			}
+		);
 
 		return {
 			wrapper,
@@ -64,5 +69,19 @@ describe("SubmissionContentElementDisplay", () => {
 			.text();
 
 		expect(submissionDueDate).toBe(dueDate);
+	});
+
+	it("should render SubmissionItemStudentDisplay as a student", () => {
+		const { wrapper } = setup("student");
+
+		const component = wrapper.findComponent(SubmissionItemStudentDisplay);
+		expect(component.exists()).toBe(true);
+	});
+
+	it("should not render SubmissionItemStudentDisplay as a teacher", () => {
+		const { wrapper } = setup("teacher");
+
+		const component = wrapper.findComponent(SubmissionItemStudentDisplay);
+		expect(component.exists()).toBe(false);
 	});
 });
