ec7493b2e89b925bce2341d2e834158f04ab17a2
Christian Darsow <christian.darsow@dataport.de>
2023-09-04 15:15:16 +0200

    Add more error handling

--- src/components/feature-board-submission-element/SubmissionContentElementState.composable.ts
+++ src/components/feature-board-submission-element/SubmissionContentElementState.composable.ts
@@ -24,15 +24,26 @@ export const useSubmissionContentElementState = (id: string) => {
 	};
 
 	const createSubmissionItem = async (completed: boolean) => {
-		createSubmissionItemCall(id, completed);
+		try {
+			const response = await createSubmissionItemCall(id, completed);
+			submissionItems.value.push(response);
+		} catch (error) {
+			notifyWithTemplate("notCreated", "boardElement")();
+		}
 	};
 
 	const updateSubmissionItem = async (completed: boolean) => {
 		if (submissionItems.value.length === 0) {
-			createSubmissionItem(completed);
+			await createSubmissionItem(completed);
 			return;
 		}
-		updateSubmissionItemCall(submissionItems.value[0].id, completed);
+
+		try {
+			await updateSubmissionItemCall(submissionItems.value[0].id, completed);
+			submissionItems.value[0].completed = completed;
+		} catch (error) {
+			notifyWithTemplate("notUpdated", "boardElement")();
+		}
 	};
 
 	const completed = computed(() => {
@@ -49,6 +60,7 @@ export const useSubmissionContentElementState = (id: string) => {
 
 	return {
 		submissionItems,
+		fetchSubmissionItems,
 		updateSubmissionItem,
 		completed,
 		loading,
