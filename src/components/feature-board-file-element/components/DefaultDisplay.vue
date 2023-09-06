387841e15d07c6f54309626ece0a803df195c7e5
Max Bischof <maximilian.bischof@dataport.de>
2023-09-06 15:00:06 +0200

    Use v-card components in stead of list components in default display

--- src/components/feature-board-file-element/components/DefaultDisplay.vue
+++ src/components/feature-board-file-element/components/DefaultDisplay.vue
@@ -1,27 +1,15 @@
 <template>
-	<v-list-item
-		class="grey lighten-3 px-2 rounded-t-sm"
-		data-testid="board-file-element-display"
-		inactive
-		:ripple="false"
-	>
-		<v-list-item-icon class="my-2 mr-2">
-			<v-icon
-				class="grey--text"
-				data-testid="board-file-element-display-file-icon"
-				large
-				>{{ mdiFileDocumentOutline }}</v-icon
-			>
-		</v-list-item-icon>
-
-		<v-list-item-content class="mr-9">
-			<span
-				class="subtitle-1 d-inline-block text-truncate"
-				data-testid="board-file-element-display-file-name"
-				>{{ name }}</span
-			>
-		</v-list-item-content>
-	</v-list-item>
+	<v-card-item>
+		<v-icon
+			class="grey--text"
+			data-testid="board-file-element-display-file-icon"
+			large
+			>{{ mdiFileDocumentOutline }}</v-icon
+		>
+		<v-card-subtitle data-testid="board-file-element-display-file-name">{{
+			name
+		}}</v-card-subtitle>
+	</v-card-item>
 </template>
 
 <script lang="ts">
