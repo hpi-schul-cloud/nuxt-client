c1296b470997037d009362b1b81f98567479c1cf
Max Bischof <maximilian.bischof@dataport.de>
2023-09-06 14:47:02 +0200

    Fix Image Display Tests

--- src/components/feature-board-file-element/components/ImageDisplay.unit.ts
+++ src/components/feature-board-file-element/components/ImageDisplay.unit.ts
@@ -3,19 +3,17 @@ import { shallowMount } from "@vue/test-utils";
 import ImageDisplay from "./ImageDisplay.vue";
 
 describe("ImageDisplay", () => {
-	const setupProps = () => ({
-		name: "file-record #1.txt",
-		previewUrl: "preview/1/file-record #1.txt",
-		isEditMode: false,
-	});
-
-	const setup = (isEditMode = false) => {
+	const setup = (isEditMode: boolean) => {
 		document.body.setAttribute("data-app", "true");
 
-		const propsData = setupProps();
+		const propsData = {
+			name: "file-record #1.txt",
+			previewUrl: "preview/1/file-record #1.txt",
+			isEditMode,
+		};
 
 		const wrapper = shallowMount(ImageDisplay, {
-			propsData: { ...propsData, isEditMode },
+			propsData,
 			...createComponentMocks({}),
 		});
 
@@ -25,36 +23,90 @@ describe("ImageDisplay", () => {
 			previewUrl: propsData.previewUrl,
 		};
 	};
+	const vImageSelektor = "v-img-stub";
 
-	it("should be found in dom", () => {
-		const { wrapper } = setup();
+	describe("when isEditMode is false", () => {
+		it("should be found in dom", () => {
+			const { wrapper } = setup(false);
 
-		const fileContentElement = wrapper.findComponent(ImageDisplay);
+			const fileContentElement = wrapper.findComponent(ImageDisplay);
 
-		expect(fileContentElement.exists()).toBe(true);
+			expect(fileContentElement.exists()).toBe(true);
+		});
+
+		it("should display image", () => {
+			const { wrapper } = setup(false);
+
+			const image = wrapper.find(vImageSelektor);
+
+			expect(image.exists()).toBe(true);
+		});
+
+		it("should have set src correctly", () => {
+			const { wrapper, previewUrl } = setup(false);
+
+			const src = wrapper.find(vImageSelektor).attributes("src");
+
+			expect(src).toBe(previewUrl);
+		});
+
+		it("should have set alt correctly", () => {
+			const { wrapper, fileNameProp } = setup(false);
+
+			const alt = wrapper.find(vImageSelektor).attributes("alt");
+
+			expect(alt).toBe(fileNameProp);
+		});
+
+		it("should not display div with class 'menu-background'", () => {
+			const { wrapper } = setup(false);
+
+			const div = wrapper.find(".menu-background");
+			console.log(wrapper.props());
+
+			expect(div.exists()).toBe(false);
+		});
 	});
 
-	it("should display image", () => {
-		const { wrapper } = setup();
+	describe("when isEditMode is true", () => {
+		it("should be found in dom", () => {
+			const { wrapper } = setup(true);
 
-		const image = wrapper.find("v-img");
+			const fileContentElement = wrapper.findComponent(ImageDisplay);
 
-		expect(image.exists()).toBe(true);
-	});
+			expect(fileContentElement.exists()).toBe(true);
+		});
 
-	it("should have set src correctly", () => {
-		const { wrapper, previewUrl } = setup();
+		it("should display image", () => {
+			const { wrapper } = setup(true);
 
-		const src = wrapper.find("v-img").attributes("src");
+			const image = wrapper.find(vImageSelektor);
 
-		expect(src).toBe(previewUrl);
-	});
+			expect(image.exists()).toBe(true);
+		});
 
-	it("should have set alt correctly", () => {
-		const { wrapper, fileNameProp } = setup();
+		it("should have set src correctly", () => {
+			const { wrapper, previewUrl } = setup(true);
 
-		const alt = wrapper.find("v-img").attributes("alt");
+			const src = wrapper.find(vImageSelektor).attributes("src");
 
-		expect(alt).toBe(fileNameProp);
+			expect(src).toBe(previewUrl);
+		});
+
+		it("should have set alt correctly", () => {
+			const { wrapper, fileNameProp } = setup(true);
+
+			const alt = wrapper.find(vImageSelektor).attributes("alt");
+
+			expect(alt).toBe(fileNameProp);
+		});
+
+		it("should display div with class 'menu-background'", () => {
+			const { wrapper } = setup(true);
+
+			const div = wrapper.find(".menu-background");
+
+			expect(div.exists()).toBe(true);
+		});
 	});
 });
