a641f8204b712cc597fa8e6f02b7f4661bd050b9
Mrika Llabani <mrika.llabani@capgemini.com>
2023-09-07 11:03:08 +0200

    N21-841 active status fix

--- src/components/administration/AdminMigrationSection.vue
+++ src/components/administration/AdminMigrationSection.vue
@@ -26,7 +26,7 @@
 					/>
 				</v-alert>
 			</div>
-			<div v-else-if="oauthMigration.startedAt && oauthMigration.closedAt">
+			<div v-else-if="oauthMigration.startedAt && !oauthMigration.closedAt">
 				<v-alert light prominent text type="info">
 					<RenderHTML
 						data-testid="migration-active-status"
