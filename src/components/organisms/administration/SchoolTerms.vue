d24612ccc99379e6c5914a8866bcd40a4cd175bb
davwas <daw.wasik@gmail.com>
2023-09-07 16:40:55 +0200

    add terms of use vue components

---
+++ src/components/organisms/administration/SchoolTerms.vue
@@ -0,0 +1,193 @@
+<template>
+	<section>
+		<h4 class="text-h4 mb-6">
+			{{ t("common.words.termsOfUse") }}
+		</h4>
+		<v-alert
+			v-if="status === 'error'"
+			light
+			prominent
+			text
+			type="error"
+			class="mb-6"
+			data-testid="error-alert"
+		>
+			{{ t("pages.administration.school.index.termsOfUse.error") }}
+		</v-alert>
+		<template v-else>
+			<v-progress-linear
+				v-if="status === 'pending'"
+				indeterminate
+				class="mb-6"
+				data-testid="progress-bar"
+			/>
+			<v-list-item v-else two-line dense class="mb-6" data-testid="terms-item">
+				<v-list-item-icon>
+					<v-icon>{{ pdfIcon }}</v-icon>
+				</v-list-item-icon>
+				<v-list-item-content>
+					<v-list-item-title class="text-body-1 black--text mb-2">
+						{{ t("pages.administration.school.index.termsOfUse.fileName") }}
+					</v-list-item-title>
+					<v-list-item-subtitle class="text-body-2">
+						<template v-if="termsOfUse">
+							{{
+								t("pages.administration.school.index.termsOfUse.uploadedOn", {
+									date: dayjs(termsOfUse.publishedAt).format(
+										"DD.MM.YYYY HH:mm"
+									),
+								})
+							}}
+						</template>
+						<template v-else>
+							{{
+								t("pages.administration.school.index.termsOfUse.notUploadedYet")
+							}}
+						</template>
+					</v-list-item-subtitle>
+				</v-list-item-content>
+				<v-list-item-action
+					v-if="hasSchoolEditPermission"
+					class="edit-icon"
+					data-testid="edit-button"
+					@click="isSchoolTermsFormDialogOpen = true"
+				>
+					<v-btn
+						icon
+						:aria-label="t('pages.administration.school.index.termsOfUse.edit')"
+					>
+						<v-icon>
+							{{ mdiPencilOutline }}
+						</v-icon>
+					</v-btn>
+				</v-list-item-action>
+				<v-list-item-action
+					v-if="termsOfUse"
+					class="download-icon"
+					data-testid="download-button"
+					@click="downloadFile"
+				>
+					<v-btn
+						icon
+						:aria-label="
+							t('pages.administration.school.index.termsOfUse.download')
+						"
+					>
+						<v-icon>
+							{{ mdiTrayArrowDown }}
+						</v-icon>
+					</v-btn>
+				</v-list-item-action>
+			</v-list-item>
+			<school-terms-form-dialog
+				v-if="hasSchoolEditPermission"
+				:is-open="isSchoolTermsFormDialogOpen"
+				@close="closeDialog"
+				data-testid="form-dialog"
+			/>
+		</template>
+	</section>
+</template>
+
+<script lang="ts">
+import SchoolTermsFormDialog from "@/components/organisms/administration/SchoolTermsFormDialog.vue";
+import dayjs from "dayjs";
+import { mdiPencilOutline, mdiTrayArrowDown } from "@mdi/js";
+import {
+	computed,
+	ComputedRef,
+	defineComponent,
+	inject,
+	ref,
+	Ref,
+	watch,
+} from "vue";
+import AuthModule from "@/store/auth";
+import SchoolsModule from "@/store/schools";
+import TermsOfUseModule from "@/store/terms-of-use";
+import { I18N_KEY, injectStrict } from "@/utils/inject";
+import VueI18n from "vue-i18n";
+import { School } from "@/store/types/schools";
+import { ConsentVersion } from "@/store/types/consent-version";
+import { BusinessError } from "@/store/types/commons";
+
+export default defineComponent({
+	name: "SchoolTerms",
+	components: {
+		SchoolTermsFormDialog,
+	},
+	setup() {
+		const authModule: AuthModule | undefined = inject<AuthModule>("authModule");
+		const schoolsModule: SchoolsModule | undefined =
+			inject<SchoolsModule>("schoolsModule");
+		const termsOfUseModule: TermsOfUseModule | undefined =
+			inject<TermsOfUseModule>("termsOfUseModule");
+		const i18n = injectStrict(I18N_KEY);
+
+		if (!authModule || !schoolsModule || !termsOfUseModule || !i18n) {
+			throw new Error("Injection of dependencies failed");
+		}
+
+		const t = (key: string, values?: VueI18n.Values | undefined): string => {
+			const translateResult = i18n.t(key, values);
+			if (typeof translateResult === "string") {
+				return translateResult;
+			}
+			return "unknown translation-key:" + key;
+		};
+
+		const isSchoolTermsFormDialogOpen: Ref<boolean> = ref(false);
+
+		const school: ComputedRef<School> = computed(() => schoolsModule.getSchool);
+		watch(
+			school,
+			async (newValue) => {
+				await termsOfUseModule.fetchTermsOfUse(newValue.id);
+			},
+			{ immediate: true }
+		);
+
+		const hasSchoolEditPermission: ComputedRef<boolean> = computed(() =>
+			authModule.getUserPermissions.includes("school_edit")
+		);
+		const termsOfUse: ComputedRef<ConsentVersion | null> = computed(
+			() => termsOfUseModule.getTermsOfUse
+		);
+		const status: ComputedRef<string> = computed(
+			() => termsOfUseModule.getStatus
+		);
+		const error: ComputedRef<BusinessError> = computed(
+			() => termsOfUseModule.getBusinessError
+		);
+		const pdfIcon = "$file_pdf_outline";
+
+		const downloadFile = () => {
+			const link = document.createElement("a");
+			link.href = termsOfUse.value?.consentData.data as string;
+			link.download = t(
+				"pages.administration.school.index.termsOfUse.fileName"
+			);
+			link.click();
+		};
+
+		const closeDialog = () => {
+			isSchoolTermsFormDialogOpen.value = false;
+		};
+
+		return {
+			t,
+			isSchoolTermsFormDialogOpen,
+			hasSchoolEditPermission,
+			termsOfUse,
+			status,
+			error,
+			mdiPencilOutline,
+			mdiTrayArrowDown,
+			pdfIcon,
+			downloadFile,
+			dayjs,
+			closeDialog,
+		};
+	},
+});
+</script>
---
+++ src/components/organisms/administration/SchoolTermsFormDialog.vue
@@ -0,0 +1,220 @@
+<template>
+	<v-custom-dialog :is-open="isOpen" :size="450" @dialog-closed="cancel">
+		<h4 class="text-h4 mt-0" slot="title">
+			{{ t("common.words.termsOfUse") }}
+		</h4>
+		<template slot="content">
+			<v-form ref="termsForm" v-model="isValid">
+				<v-alert light text type="warning" class="mb-10" :icon="mdiAlert">
+					<div class="replace-alert-text">
+						{{
+							t(
+								"pages.administration.school.index.termsOfUse.longText.willReplaceAndSendConsent"
+							)
+						}}
+					</div>
+				</v-alert>
+				<v-file-input
+					ref="input-file"
+					class="input-file mb-2"
+					data-testid="input-file"
+					v-model="file"
+					dense
+					accept="application/pdf"
+					truncate-length="30"
+					:label="
+						t('pages.administration.school.index.termsOfUse.labels.uploadFile')
+					"
+					:hint="
+						t('pages.administration.school.index.termsOfUse.hints.uploadFile')
+					"
+					:persistent-hint="true"
+					:rules="[rules.required, rules.mustBePdf, rules.maxSize(4194304)]"
+					@blur="onBlur"
+				>
+					<template v-slot:append>
+						<v-icon
+							v-if="!isValid && isTouched"
+							color="var(--v-error-base)"
+							data-testid="warning-icon"
+						>
+							{{ mdiAlert }}
+						</v-icon>
+					</template>
+				</v-file-input>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<div class="button-section button-right">
+						<v-btn
+							class="dialog-closed"
+							depressed
+							text
+							@click="cancel"
+							data-testid="cancel-button"
+						>
+							{{ t("pages.administration.school.index.termsOfUse.cancel") }}
+						</v-btn>
+						<v-btn
+							class="icon-button dialog-confirmed px-6"
+							type="submit"
+							color="primary"
+							depressed
+							:disabled="!isValid"
+							@click.prevent="submit"
+							data-testid="submit-button"
+						>
+							<v-icon dense class="mr-1">{{ mdiFileReplaceOutline }}</v-icon>
+							{{ t("pages.administration.school.index.termsOfUse.replace") }}
+						</v-btn>
+					</div>
+				</v-card-actions>
+			</v-form>
+		</template>
+	</v-custom-dialog>
+</template>
+
+<script lang="ts">
+import vCustomDialog from "@/components/organisms/vCustomDialog.vue";
+import { computed, ComputedRef, defineComponent, inject, ref, Ref } from "vue";
+import SchoolsModule from "@/store/schools";
+import TermsOfUseModule from "@/store/terms-of-use";
+import { I18N_KEY, injectStrict, NOTIFIER_MODULE_KEY } from "@/utils/inject";
+import VueI18n from "vue-i18n";
+import { mdiAlert, mdiFileReplaceOutline } from "@mdi/js";
+import { School } from "@/store/types/schools";
+import { currentDate } from "@/plugins/datetime";
+import { toBase64 } from "@/utils/fileHelper";
+import { CreateConsentVersionPayload } from "@/store/types/consent-version";
+
+export default defineComponent({
+	name: "SchoolTermsFormDialog",
+	components: {
+		vCustomDialog,
+	},
+	emits: ["close"],
+	props: {
+		isOpen: {
+			type: Boolean,
+			required: true,
+		},
+	},
+	setup(props, { emit }) {
+		const schoolsModule: SchoolsModule | undefined =
+			inject<SchoolsModule>("schoolsModule");
+		const termsOfUseModule: TermsOfUseModule | undefined =
+			inject<TermsOfUseModule>("termsOfUseModule");
+		const notifierModule = injectStrict(NOTIFIER_MODULE_KEY);
+		const i18n = injectStrict(I18N_KEY);
+
+		if (!notifierModule || !schoolsModule || !termsOfUseModule || !i18n) {
+			throw new Error("Injection of dependencies failed");
+		}
+
+		const t = (key: string, values?: VueI18n.Values | undefined): string => {
+			const translateResult = i18n.t(key, values);
+			if (typeof translateResult === "string") {
+				return translateResult;
+			}
+			return "unknown translation-key:" + key;
+		};
+
+		const termsForm: Ref = ref(null);
+		const isValid: Ref<boolean> = ref(false);
+		const isTouched: Ref<boolean> = ref(false);
+		const file: Ref<File | null> = ref(null);
+
+		const school: ComputedRef<School> = computed(() => schoolsModule.getSchool);
+
+		const rules = {
+			required: (value: string) => !!value || t("common.validation.required"),
+			mustBePdf: (value: File) =>
+				!value ||
+				value.type === "application/pdf" ||
+				t("pages.administration.school.index.termsOfUse.validation.notPdf"),
+			maxSize: (bytes: number) => (value: File) =>
+				!value ||
+				value.size <= bytes ||
+				t("pages.administration.school.index.termsOfUse.validation.fileTooBig"),
+		};
+
+		const onBlur = () => {
+			isTouched.value = true;
+		};
+
+		const resetForm = () => {
+			termsForm.value.reset();
+			isValid.value = false;
+			isTouched.value = false;
+		};
+
+		const cancel = () => {
+			resetForm();
+			emit("close");
+		};
+
+		const submit = async () => {
+			if (isValid.value) {
+				const newConsentVersion: CreateConsentVersionPayload = {
+					schoolId: school.value.id,
+					title: t("pages.administration.school.index.termsOfUse.fileName"),
+					consentText: "",
+					consentTypes: ["termsOfUse"],
+					publishedAt: currentDate().toString(),
+					consentData: (await toBase64(file.value as File)) as string,
+				};
+
+				emit("close");
+				await termsOfUseModule.createTermsOfUse(newConsentVersion);
+
+				notifierModule.show({
+					text: t("pages.administration.school.index.termsOfUse.success"),
+					status: "success",
+					timeout: 10000,
+				});
+
+				resetForm();
+			}
+		};
+
+		return {
+			t,
+			file,
+			rules,
+			mdiFileReplaceOutline,
+			mdiAlert,
+			cancel,
+			submit,
+			onBlur,
+			isValid,
+			isTouched,
+			termsForm,
+			school,
+		};
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+.replace-alert-text {
+	color: var(--v-black-base) !important;
+}
+
+.button-left {
+	width: 25%;
+	text-align: left;
+}
+
+.button-right {
+	display: inline-block;
+	width: 75%;
+	text-align: right;
+}
+
+.button-section {
+	margin-bottom: calc(var(--space-base-vuetify) * 2);
+}
+
+.button-section > button {
+	margin-left: calc(var(--space-base-vuetify) * 2);
+}
+</style>
