b89ef634f1612e1c88fcacb2e0f9d27a27e07206
Odalys Adam <odalys.adam@dataport.de>
2023-09-08 14:26:23 +0200

    adjust design

--- src/components/feature-board-submission-element/components/SubmissionContentElementDisplay.vue
+++ src/components/feature-board-submission-element/components/SubmissionContentElementDisplay.vue
@@ -1,12 +1,14 @@
 <template>
-	<div class="grey lighten-4 pa-4 rounded">
-		<SubmissionContentElementTitle />
-		<v-card-subtitle
-			class="pa-0 mt-1 subtitle-1"
-			data-testid="board-submission-element-due-date"
-		>
-			{{ formattedDueDate }}
-		</v-card-subtitle>
+	<div>
+		<div class="grey lighten-4 pa-4 rounded-t">
+			<SubmissionContentElementTitle />
+			<v-card-subtitle
+				class="pa-0 mt-1 subtitle-1"
+				data-testid="board-submission-element-due-date"
+			>
+				{{ formattedDueDate }}
+			</v-card-subtitle>
+		</div>
 		<SubmissionItemStudentDisplay
 			v-if="isStudent"
 			:completed="completed"
@@ -18,13 +20,13 @@
 </template>
 
 <script lang="ts">
-import SubmissionItemStudentDisplay from "./SubmissionItemStudentDisplay.vue";
 import AuthModule from "@/store/auth";
 import { AUTH_MODULE_KEY, injectStrict } from "@/utils/inject";
 import { defineComponent, ref, computed, toRef } from "vue";
 import dayjs from "dayjs";
 import { useI18n } from "@/composables/i18n.composable";
 import SubmissionContentElementTitle from "./SubmissionContentElementTitle.vue";
+import SubmissionItemStudentDisplay from "./SubmissionItemStudentDisplay.vue";
 
 export default defineComponent({
 	name: "SubmissionContentElementDisplay",
