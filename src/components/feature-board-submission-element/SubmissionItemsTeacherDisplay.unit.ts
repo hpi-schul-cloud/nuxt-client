a186660959a3a95877009bbca7c7b087db39a41a
Christian Darsow <christian.darsow@dataport.de>
2023-09-13 08:12:29 +0200

    Add loading tests

--- src/components/feature-board-submission-element/SubmissionItemsTeacherDisplay.unit.ts
+++ src/components/feature-board-submission-element/SubmissionItemsTeacherDisplay.unit.ts
@@ -43,4 +43,44 @@ describe("SubmissionItemsTeacherDisplay", () => {
 		const component = wrapper.findComponent(SubmissionItemsTeacherDisplay);
 		expect(component.exists()).toBe(true);
 	});
+
+	describe("while loading", () => {
+		it("should show loading skeleton", () => {
+			const loading = true;
+			const { wrapper } = setup(loading);
+
+			const skeleton = wrapper.findComponent({ name: "v-skeleton-loader" });
+			expect(skeleton.exists()).toBe(true);
+		});
+
+		it("should not show submissionItems", () => {
+			const loading = true;
+			const { wrapper } = setup(loading);
+
+			const submissionItems = wrapper.findComponent({
+				name: "v-expansion-panels",
+			});
+			expect(submissionItems.exists()).toBe(false);
+		});
+	});
+
+	describe("after loading", () => {
+		it("should not show loading skeleton", () => {
+			const loading = false;
+			const { wrapper } = setup(loading);
+
+			const skeleton = wrapper.findComponent({ name: "v-skeleton-loader" });
+			expect(skeleton.exists()).toBe(false);
+		});
+
+		it("should show submissionItems", () => {
+			const loading = false;
+			const { wrapper } = setup(loading);
+
+			const submissionItems = wrapper.findComponent({
+				name: "v-expansion-panels",
+			});
+			expect(submissionItems.exists()).toBe(true);
+		});
+	});
 });
