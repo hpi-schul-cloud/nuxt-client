0cedcbf8523f34f62b8802ee9a0d0bff6b481a99
KalliSfak <kalliopi.sfakianaki@dataport.de>
2023-09-15 11:58:48 +0200

    fix persistent hint error

--- src/components/feature-board-file-element/content/alternative-text/AlternativeText.vue
+++ src/components/feature-board-file-element/content/alternative-text/AlternativeText.vue
@@ -5,7 +5,7 @@
 			auto-grow
 			v-model="modelValue"
 			class="v-secondary-lighten1"
-			persistent-hint="true"
+			:persistent-hint="true"
 			:hint="$t('components.cardElement.fileElement.altDescription')"
 			:label="$t('components.cardElement.fileElement.alternativeText')"
 		/>
