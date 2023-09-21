8f1fd85954a37cba1ff0904768a1a51f6be2e2d5
Max Bischof <maximilian.bischof@dataport.de>
2023-09-21 15:37:20 +0200

    Adjust file description test

--- src/components/feature-board-file-element/content/display/file-description/FileDescription.unit.ts
+++ src/components/feature-board-file-element/content/display/file-description/FileDescription.unit.ts
@@ -4,10 +4,20 @@ import { shallowMount } from "@vue/test-utils";
 import FileDescription from "./FileDescription.vue";
 
 describe("FileDescription", () => {
-	const setup = () => {
+	const setup = (props: {
+		isEditMode: boolean;
+		showTitle: boolean;
+		name?: string;
+		caption?: string;
+	}) => {
 		document.body.setAttribute("data-app", "true");
 
-		const propsData = { name: "test-name" };
+		const propsData = {
+			name: props.name ?? "testName",
+			caption: props.caption ?? "testCaption",
+			isEditMode: props.isEditMode,
+			showTitle: props.showTitle,
+		};
 		const wrapper = shallowMount(FileDescription, {
 			propsData,
 			...createComponentMocks({}),
@@ -16,30 +26,129 @@ describe("FileDescription", () => {
 		return {
 			wrapper,
 			name: propsData.name,
+			caption: propsData.caption,
 		};
 	};
 
-	it("should be found in dom", () => {
-		const { wrapper } = setup();
+	describe("when isEditMode is true", () => {
+		describe("when showTitle is true", () => {
+			it("should render title", () => {
+				const { wrapper, name } = setup({
+					isEditMode: true,
+					showTitle: true,
+				});
 
-		const defaultDisplay = wrapper.findComponent(FileDescription);
+				const text = wrapper.text();
 
-		expect(defaultDisplay.exists()).toBe(true);
+				expect(text).toContain(name);
+			});
+
+			it("should render icon", () => {
+				const { wrapper } = setup({
+					isEditMode: true,
+					showTitle: true,
+				});
+
+				const icon = wrapper.find("v-icon-stub").text();
+
+				expect(icon).toBe(mdiFileDocumentOutline);
+			});
+
+			it("should not render caption", () => {
+				const { wrapper, caption } = setup({
+					isEditMode: true,
+					showTitle: true,
+				});
+
+				const text = wrapper.text();
+
+				expect(text).not.toContain(caption);
+			});
+		});
+
+		describe("when showTitle is false", () => {
+			it("should not render title", () => {
+				const { wrapper, name } = setup({
+					isEditMode: true,
+					showTitle: false,
+				});
+
+				const text = wrapper.text();
+
+				expect(text).not.toContain(name);
+			});
+
+			it("should not render caption", () => {
+				const { wrapper, caption } = setup({
+					isEditMode: true,
+					showTitle: false,
+				});
+
+				const text = wrapper.text();
+
+				expect(text).not.toContain(caption);
+			});
+		});
 	});
 
-	it("should render name prop", () => {
-		const { wrapper, name } = setup();
+	describe("when isEditMode is false", () => {
+		describe("when showTitle is true", () => {
+			it("should render title", () => {
+				const { wrapper, name } = setup({
+					isEditMode: false,
+					showTitle: true,
+				});
 
-		const text = wrapper.text();
+				const text = wrapper.text();
 
-		expect(text).toContain(name);
-	});
+				expect(text).toContain(name);
+			});
 
-	it("should render icon", () => {
-		const { wrapper } = setup();
+			it("should render icon", () => {
+				const { wrapper } = setup({
+					isEditMode: false,
+					showTitle: true,
+				});
 
-		const icon = wrapper.find("v-icon-stub").text();
+				const icon = wrapper.find("v-icon-stub").text();
 
-		expect(icon).toBe(mdiFileDocumentOutline);
+				expect(icon).toBe(mdiFileDocumentOutline);
+			});
+
+			it("should render caption", () => {
+				const { wrapper, caption } = setup({
+					isEditMode: false,
+					showTitle: true,
+				});
+
+				const text = wrapper.text();
+
+				expect(text).toContain(caption);
+			});
+		});
+
+		describe("when showTitle is false", () => {
+			it("should not render title", () => {
+				const { wrapper, name } = setup({
+					isEditMode: false,
+					showTitle: false,
+				});
+
+				const text = wrapper.text();
+
+				expect(text).not.toContain(name);
+			});
+
+			it("should render caption", () => {
+				const { wrapper, caption } = setup({
+					isEditMode: false,
+					showTitle: false,
+				});
+
+				const text = wrapper.text();
+
+				expect(text).toContain(caption);
+			});
+		});
 	});
 });
