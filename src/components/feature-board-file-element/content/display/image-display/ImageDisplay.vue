18e372c2acba13f9e67b563a7ff9a7efd4d95dd3
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-09-21 15:32:48 +0200

    BC-4391 - add hover effects to ImageDisplay

--- src/components/feature-board-file-element/content/display/image-display/ImageDisplay.vue
+++ src/components/feature-board-file-element/content/display/image-display/ImageDisplay.vue
@@ -1,7 +1,8 @@
 <template>
-	<div>
+	<div class="image-display-container">
+		<v-icon class="image-display-icon" size="44">{{ mdiMagnifyExpand }}</v-icon>
 		<img
-			class="rounded-t-sm image"
+			class="image-display-image rounded-t-sm"
 			loading="lazy"
 			:src="previewUrl"
 			:alt="name"
@@ -12,6 +13,7 @@
 
 <script lang="ts">
 import { PreviewOutputMimeTypes } from "@/fileStorageApi/v3";
+import { mdiMagnifyExpand } from "@mdi/js";
 import { useLightBox } from "@ui-light-box";
 import { defineComponent } from "vue";
 
@@ -40,6 +42,7 @@ export default defineComponent({
 		};
 
 		return {
+			mdiMagnifyExpand,
 			onClick,
 		};
 	},
@@ -47,9 +50,32 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.image {
+.image-display-container {
+	position: relative;
+}
+
+.image-display-container:hover .image-display-icon {
+	display: block;
+}
+.image-display-image {
 	display: block;
 	margin-right: auto;
 	margin-left: auto;
+}
+
+.image-display-image:hover {
+	border-radius: 3px 3px 0px 0px;
+	opacity: 0.54;
+	background: var(--shades-v-black-base, #1b1b1b);
+}
+
+.image-display-icon {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	z-index: var(--layer-page);
+	transform: translate(-50%, -50%);
+	display: none;
+	color: var(--shades-v-white-base, #fff);
 }
 </style>
