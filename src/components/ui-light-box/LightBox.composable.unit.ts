ddcd36abeb3bf4ed20ae50c064985da2ccbf8477
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-09-27 08:51:19 +0200

    BC-4391 - add more unit tests for `LightBox.composable`
    - add more unit tests for `LightBox.composable` to satisfy sonarcloud's code coverage

--- src/components/ui-light-box/LightBox.composable.unit.ts
+++ src/components/ui-light-box/LightBox.composable.unit.ts
@@ -1,9 +1,9 @@
 import { I18N_KEY } from "@/utils/inject";
 import { mountComposable } from "@@/tests/test-utils/mountComposable";
-import { useLightBox } from "./LightBox.composable";
+import { useInternalLightBox, useLightBox } from "./LightBox.composable";
 
 describe("LightBox composable", () => {
-	describe("open", () => {
+	describe("useLightBox", () => {
 		const setup = () => {
 			const { open, isLightBoxOpen } = mountComposable(() => useLightBox(), {
 				[I18N_KEY.valueOf()]: { t: (k: string) => k },
@@ -19,11 +19,52 @@ describe("LightBox composable", () => {
 			jest.clearAllMocks();
 		});
 
-		describe("when open is called", () => {
-			it("should open the LightBox", async () => {
-				const { open, isLightBoxOpen } = setup();
+		describe("open", () => {
+			describe("when open is called", () => {
+				it("should open the LightBox", async () => {
+					const { open, isLightBoxOpen } = setup();
 
-				expect(isLightBoxOpen.value).toBe(false);
+					expect(isLightBoxOpen.value).toBe(false);
+
+					const data = {
+						url: "url-string",
+						previewUrl: "previewUrl-string",
+						alt: "alt-string",
+						name: "name-string",
+					};
+					open(data);
+
+					expect(isLightBoxOpen.value).toBe(true);
+				});
+			});
+		});
+	});
+
+	describe("useInternalLightBox", () => {
+		const setup = () => {
+			const { close, isLightBoxOpen, lightBoxOptions, openInternal } =
+				mountComposable(() => useInternalLightBox(), {
+					[I18N_KEY.valueOf()]: { t: (k: string) => k },
+				});
+
+			return {
+				close,
+				isLightBoxOpen,
+				lightBoxOptions,
+				openInternal,
+			};
+		};
+
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		describe("when openInternal is called", () => {
+			it("should open the LightBox", async () => {
+				const { openInternal, isLightBoxOpen, lightBoxOptions } = setup();
+
+				isLightBoxOpen.value = false;
+				lightBoxOptions.value = undefined;
 
 				const data = {
 					url: "url-string",
@@ -31,9 +72,28 @@ describe("LightBox composable", () => {
 					alt: "alt-string",
 					name: "name-string",
 				};
-				open(data);
+				openInternal(data);
 
 				expect(isLightBoxOpen.value).toBe(true);
+				expect(lightBoxOptions.value).toBe(data);
+			});
+		});
+
+		describe("when close is called", () => {
+			it("should close the LightBox and set lightBoxOptions to undefined", async () => {
+				const { close, isLightBoxOpen, lightBoxOptions } = setup();
+
+				isLightBoxOpen.value = true;
+				lightBoxOptions.value = {
+					url: "url-string",
+					previewUrl: "previewUrl-string",
+					alt: "alt-string",
+					name: "name-string",
+				};
+				close();
+
+				expect(isLightBoxOpen.value).toBe(false);
+				expect(lightBoxOptions.value).toBeUndefined();
 			});
 		});
 	});
