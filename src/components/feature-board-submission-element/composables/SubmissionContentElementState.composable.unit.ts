ffedad7533d7f5d2fc549ad9dadc55999478fc7a
Christian Darsow <christian.darsow@dataport.de>
2023-09-29 08:58:32 +0200

    Adjust tests

--- src/components/feature-board-submission-element/composables/SubmissionContentElementState.composable.unit.ts
+++ src/components/feature-board-submission-element/composables/SubmissionContentElementState.composable.unit.ts
@@ -4,45 +4,44 @@ import { useSubmissionItemApi } from "./SubmissionItemApi.composable";
 import { I18N_KEY, NOTIFIER_MODULE_KEY } from "@/utils/inject";
 import NotifierModule from "@/store/notifier";
 import { createModuleMocks } from "@/utils/mock-store-module";
-import { createMock, DeepMocked } from "@golevelup/ts-jest";
-import { SubmissionsResponse } from "@/serverApi/v3";
+import { createMock } from "@golevelup/ts-jest";
 import { submissionsResponseFactory } from "@@/tests/test-utils";
+import { SubmissionItemResponse, UserDataResponse } from "@/serverApi/v3";
 
 const notifierModule = createModuleMocks(NotifierModule);
-
-const mockedSubmissionsResponse = submissionsResponseFactory.build();
 
 jest.mock("./SubmissionItemApi.composable");
 const mockedUseSubmissionItemApi = jest.mocked(useSubmissionItemApi);
 
+const mockedUseSubmissionItemApiCalls =
+	createMock<ReturnType<typeof useSubmissionItemApi>>();
+mockedUseSubmissionItemApi.mockReturnValue(mockedUseSubmissionItemApiCalls);
+
+const mockedsubmissions = submissionsResponseFactory.build(
+	{},
+	{
+		transient: {
+			numberOfSubmissionItems: 1,
+			numberOfUsers: 1,
+			completed: false,
+		},
+	}
+);
+const mockedContentElementId = "123124";
+const mockedDueDate = "2100-12-31T00:00:00.000Z";
+
 describe("SubmissionContentElementState.composable", () => {
-	let mockedUseSubmissionItemApiCalls: DeepMocked<
-		ReturnType<typeof useSubmissionItemApi>
-	>;
-
-	beforeEach(() => {
-		mockedUseSubmissionItemApiCalls =
-			createMock<ReturnType<typeof useSubmissionItemApi>>();
-		mockedUseSubmissionItemApi.mockReturnValue(mockedUseSubmissionItemApiCalls);
-	});
-
-	afterEach(() => {
-		jest.resetAllMocks();
-	});
-
 	const setup = (
-		contentElementId = "123123",
-		submissionItems = [],
-		users: [],
-		dueDate = "2100-12-31T00:00:00.000Z"
+		submissionItems: Array<SubmissionItemResponse> = [],
+		users: Array<UserDataResponse> = []
 	) => {
 		return mountComposable(
 			() =>
 				useSubmissionContentElementState(
-					contentElementId,
+					mockedContentElementId,
 					submissionItems,
 					users,
-					dueDate
+					mockedDueDate
 				),
 			{
 				[I18N_KEY.valueOf()]: { t: (key: string) => key },
@@ -52,41 +51,34 @@ describe("SubmissionContentElementState.composable", () => {
 	};
 
 	describe("if the student created a submission item before", () => {
-		it.skip("should return update function that updates the completed state", async () => {
-			// const contentElementId = "123124";
-			// mockedUseSubmissionItemApiCalls.fetchSubmissionItemsCall.mockReturnValue(
-			// 	mockedSubmissionsResponse as unknown as Promise<SubmissionsResponse>
-			// );
-			// const { fetchSubmissionItems, updateSubmissionItem } =
-			// 	setup(contentElementId);
-			// await fetchSubmissionItems(contentElementId);
-			// const completed = true;
-			// await updateSubmissionItem(completed);
-			// expect(
-			// 	mockedUseSubmissionItemApiCalls.updateSubmissionItemCall
-			// ).toHaveBeenLastCalledWith(
-			// 	mockedSubmissionsResponse.submissionItemsResponse[0].id,
-			// 	completed
-			// );
+		it("should return update function that updates the completed state", async () => {
+			const { updateSubmissionItem } = setup(
+				mockedsubmissions.submissionItemsResponse,
+				mockedsubmissions.users
+			);
+
+			const completed = true;
+			await updateSubmissionItem(completed);
+
+			expect(
+				mockedUseSubmissionItemApiCalls.updateSubmissionItemCall
+			).toHaveBeenLastCalledWith(
+				mockedsubmissions.submissionItemsResponse[0].id,
+				completed
+			);
 		});
 	});
 
 	describe("if the student did not create a submission item so far", () => {
-		it.skip("should return update function that creates initial completed state", async () => {
-			// const contentElementId = "123124";
-			// const submissions = submissionsResponseFactory.build();
-			// submissions.submissionItemsResponse = [];
-			// mockedUseSubmissionItemApiCalls.fetchSubmissionItemsCall.mockReturnValue(
-			// 	submissions as unknown as Promise<SubmissionsResponse>
-			// );
-			// const { fetchSubmissionItems, updateSubmissionItem } =
-			// 	setup(contentElementId);
-			// await fetchSubmissionItems(contentElementId);
-			// const completed = true;
-			// await updateSubmissionItem(completed);
-			// expect(
-			// 	mockedUseSubmissionItemApiCalls.createSubmissionItemCall
-			// ).toHaveBeenLastCalledWith(contentElementId, completed);
+		it("should return update function that creates initial completed state", async () => {
+			const { updateSubmissionItem } = setup([], mockedsubmissions.users);
+
+			const completed = true;
+			await updateSubmissionItem(completed);
+
+			expect(
+				mockedUseSubmissionItemApiCalls.createSubmissionItemCall
+			).toHaveBeenLastCalledWith(mockedContentElementId, completed);
 		});
 	});
 });
