ca6e692f0ee03b40335e85adafcf0982536f4242
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-10-05 20:44:52 +0200

    BC-4391 - fix unit tests

--- src/components/feature-board-file-element/content/display/image-display/ImageDisplay.unit.ts
+++ src/components/feature-board-file-element/content/display/image-display/ImageDisplay.unit.ts
@@ -112,6 +112,32 @@ describe("ImageDisplay", () => {
 			);
 		});
 
+		describe("When alternative text is defined", () => {
+			it("should have set alt correctly", () => {
+				const alternativeText = "alternative text";
+				const { wrapper } = setup({
+					isEditMode: false,
+					alternativeText,
+				});
+
+				const alt = wrapper.find(imageSelektor).attributes("alt");
+
+				expect(alt).toBe(alternativeText);
+			});
+		});
+
+		describe("When alternative text is undefined", () => {
+			it("should have set alt correctly", () => {
+				const { wrapper, nameProp } = setup({ isEditMode: false });
+
+				const alt = wrapper.find(imageSelektor).attributes("alt");
+
+				expect(alt).toBe(
+					"components.cardElement.fileElement.emptyAlt " + nameProp
+				);
+			});
+		});
+
 		it("should call open function, when the container is clicked", () => {
 			const alternativeText = "alternative text";
 			const { wrapper, urlProp, nameProp, open } = setup({
@@ -226,7 +252,7 @@ describe("ImageDisplay", () => {
 			expect(overlayBlur.exists()).toBe(false);
 		});
 
-		it("should call open function, when Enter key is pressed, while container is focused", async () => {
+		it("should call open function, when Enter key is pressed", async () => {
 			const alternativeText = "alternative text";
 			const { wrapper, urlProp, nameProp, open } = setup({
 				isEditMode: false,
@@ -247,7 +273,7 @@ describe("ImageDisplay", () => {
 			expect(open).toHaveBeenCalledWith(options);
 		});
 
-		it("should call open function, when Space key is pressed, while container is focused", async () => {
+		it("should call open function, when Space key is pressed", async () => {
 			const alternativeText = "alternative text";
 			const { wrapper, urlProp, nameProp, open } = setup({
 				isEditMode: false,
@@ -285,32 +311,6 @@ describe("ImageDisplay", () => {
 			const overlayLeave = wrapper.find(".image-display-overlay");
 
 			expect(overlayLeave.exists()).toBe(false);
-		});
-
-		describe("When alternative text is defined", () => {
-			it("should have set alt correctly", () => {
-				const alternativeText = "alternative text";
-				const { wrapper } = setup({
-					isEditMode: false,
-					alternativeText,
-				});
-
-				const alt = wrapper.find(imageSelektor).attributes("alt");
-
-				expect(alt).toBe(alternativeText);
-			});
-		});
-
-		describe("When alternative text is undefined", () => {
-			it("should have set alt correctly", () => {
-				const { wrapper, nameProp } = setup({ isEditMode: false });
-
-				const alt = wrapper.find(imageSelektor).attributes("alt");
-
-				expect(alt).toBe(
-					"components.cardElement.fileElement.emptyAlt " + nameProp
-				);
-			});
 		});
 	});
 
@@ -363,6 +363,123 @@ describe("ImageDisplay", () => {
 					"components.cardElement.fileElement.emptyAlt " + nameProp
 				);
 			});
+		});
+
+		it("should not call open function, when the container is clicked", () => {
+			const alternativeText = "alternative text";
+			const { wrapper, open } = setup({
+				isEditMode: true,
+				alternativeText,
+			});
+
+			const container = wrapper.find(".image-display-container");
+			container.trigger("click");
+
+			expect(open).toHaveBeenCalledTimes(0);
+		});
+
+		it("should not call open function, when the image is clicked", () => {
+			const alternativeText = "alternative text";
+			const { wrapper, open } = setup({
+				isEditMode: true,
+				alternativeText,
+			});
+
+			const image = wrapper.find(".image-display-image");
+			image.trigger("click");
+
+			expect(open).toHaveBeenCalledTimes(0);
+		});
+
+		it("should not show overlay, when the image is focused (using focusin and focusout)", async () => {
+			const { wrapper } = setup({ isEditMode: true });
+
+			wrapper.trigger("focusin");
+			await nextTick();
+
+			const overlayFocusIn = wrapper.find(".image-display-overlay");
+
+			expect(overlayFocusIn.exists()).toBe(false);
+
+			wrapper.trigger("focusout");
+			await nextTick();
+
+			const overlayFocusOut = wrapper.find(".image-display-overlay");
+
+			expect(overlayFocusOut.exists()).toBe(false);
+		});
+
+		it("should not show overlay, when the image is focused (using focus and blur)", async () => {
+			const { wrapper } = setup({ isEditMode: true });
+
+			const container = wrapper.find(".image-display-container")
+				.element as HTMLDivElement;
+
+			expect(document.activeElement).toBe(document.body);
+
+			container.focus();
+			await nextTick();
+
+			expect(document.activeElement).toBe(container);
+
+			const overlayFocus = wrapper.find(".image-display-overlay");
+
+			expect(overlayFocus.exists()).toBe(false);
+
+			container.blur();
+			await nextTick();
+
+			expect(document.activeElement).toBe(document.body);
+
+			const overlayBlur = wrapper.find(".image-display-overlay");
+
+			expect(overlayBlur.exists()).toBe(false);
+		});
+
+		it("should not call open function, when Enter key is pressed", async () => {
+			const alternativeText = "alternative text";
+			const { wrapper, open } = setup({
+				isEditMode: true,
+				alternativeText,
+			});
+
+			const container = wrapper.find(".image-display-container");
+			container.trigger("keydown.enter");
+			await nextTick();
+
+			expect(open).toHaveBeenCalledTimes(0);
+		});
+
+		it("should not call open function, when Space key is pressed", async () => {
+			const alternativeText = "alternative text";
+			const { wrapper, open } = setup({
+				isEditMode: true,
+				alternativeText,
+			});
+
+			const container = wrapper.find(".image-display-container");
+			container.trigger("keydown.space");
+			await nextTick();
+
+			expect(open).toHaveBeenCalledTimes(0);
+		});
+
+		it("should not show overlay, when the user hovers over the image", async () => {
+			const { wrapper } = setup({ isEditMode: true });
+
+			wrapper.trigger("mouseenter");
+			await nextTick();
+
+			const overlayEnter = wrapper.find(".image-display-overlay");
+
+			expect(overlayEnter.exists()).toBe(false);
+
+			wrapper.trigger("mouseleave");
+			await nextTick();
+
+			const overlayLeave = wrapper.find(".image-display-overlay");
+
+			expect(overlayLeave.exists()).toBe(false);
 		});
 	});
 });
