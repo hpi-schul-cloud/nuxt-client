d7fd1e0876083ec1fd9207cf77a505aaf6564123
Max Bischof <maximilian.bischof@dataport.de>
2023-10-06 11:54:31 +0200

    Use content element bar for lightbox

--- src/components/ui-light-box/LightBox.vue
+++ src/components/ui-light-box/LightBox.vue
@@ -1,25 +1,27 @@
 <template>
 	<v-dialog fullscreen hide-overlay :value="isLightBoxOpen">
-		<v-toolbar flat>
+		<v-app-bar>
 			<v-btn icon @click="close">
 				<v-icon>{{ mdiClose }}</v-icon>
 			</v-btn>
-			<v-toolbar-title
-				v-if="lightBoxOptions.name !== ''"
-				class="d-flex align-items-center"
-			>
-				<v-icon color="black" class="mr-2" size="18">{{
-					mdiFileDocumentOutline
-				}}</v-icon>
-				<span class="subtitle-1 font-weight-bold text-truncate mr-8">{{
-					lightBoxOptions.name
-				}}</span>
-			</v-toolbar-title>
-			<v-spacer />
-			<v-btn v-if="lightBoxOptions.downloadUrl !== ''" icon @click="download">
-				<v-icon>{{ mdiTrayArrowDown }}</v-icon>
-			</v-btn>
-		</v-toolbar>
+
+			<ContentElementBar :icon="mdiFileDocumentOutline" class="flex-grow-1">
+				<template #title>
+					<ContentElementTitle>{{ lightBoxOptions.name }}</ContentElementTitle>
+				</template>
+
+				<template #menu>
+					<v-btn
+						v-if="lightBoxOptions.downloadUrl !== ''"
+						icon
+						@click="download"
+					>
+						<v-icon>{{ mdiTrayArrowDown }}</v-icon>
+					</v-btn>
+				</template>
+			</ContentElementBar>
+		</v-app-bar>
+
 		<v-overlay absolute class="mt-16 pa-4" @click="close">
 			<div
 				class="d-flex align-items-center justify-content-center"
@@ -42,9 +44,11 @@ import { mdiClose, mdiFileDocumentOutline, mdiTrayArrowDown } from "@mdi/js";
 import { onKeyStroke } from "@vueuse/core";
 import { defineComponent } from "vue";
 import { useInternalLightBox } from "./LightBox.composable";
+import { ContentElementBar, ContentElementTitle } from "@ui-board";
 
 export default defineComponent({
 	name: "LightBox",
+	components: { ContentElementBar, ContentElementTitle },
 	setup() {
 		const { close, isLightBoxOpen, lightBoxOptions } = useInternalLightBox();
 
