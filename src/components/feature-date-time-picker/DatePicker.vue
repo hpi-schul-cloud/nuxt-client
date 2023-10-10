81cd33e64b994afdffd0cf12f7c35c93513dde6f
Christian Darsow <104998795+christian-darsow@users.noreply.github.com>
2023-10-10 08:20:28 +0200

    BC-4956 - due date validation (#2835)
    * Remove past handling
    
    * Adjust validation
    
    * Fix broken reactivity
    
    * Set default time on empty time
    
    * Add hint when date in past
    
    * Add shared element menu
    
    * Add expose locale

--- src/components/feature-date-time-picker/DatePicker.vue
+++ src/components/feature-date-time-picker/DatePicker.vue
@@ -20,6 +20,7 @@
 					:class="{ 'menu-open': showDateDialog }"
 					append-icon="$mdiCalendar"
 					readonly
+					:messages="messages"
 					:rules="rules"
 					data-testid="date-input"
 					@keydown.space="showDateDialog = true"
@@ -46,7 +47,6 @@
 </template>
 
 <script lang="ts">
-import { I18N_KEY, injectStrict } from "@/utils/inject";
 import { mdiCalendarClock } from "@mdi/js";
 import { useDebounceFn } from "@vueuse/core";
 import dayjs from "dayjs";
@@ -64,12 +64,11 @@ export default defineComponent({
 		required: { type: Boolean },
 		minDate: { type: String },
 		maxDate: { type: String },
+		dateTimeInPast: { type: Boolean, default: false },
 	},
 	emits: ["update:date"],
 	setup(props, { emit }) {
-		const { t } = useI18n();
-		const i18n = injectStrict(I18N_KEY);
-		const locale = i18n.locale;
+		const { t, locale } = useI18n();
 
 		const modelValue = computed<string>({
 			get() {
@@ -142,6 +141,14 @@ export default defineComponent({
 			showDateDialog.value = false;
 		}, 50);
 
+		const messages = computed(() => {
+			if (props.dateTimeInPast) {
+				return t("components.datePicker.messages.future");
+			}
+
+			return [];
+		});
+
 		return {
 			mdiCalendarClock,
 			locale,
@@ -154,6 +161,7 @@ export default defineComponent({
 			onInput,
 			onError,
 			onMenuToggle,
+			messages,
 		};
 	},
 });
@@ -170,6 +178,10 @@ export default defineComponent({
 	.v-input__icon--append .v-icon {
 		width: 20px;
 		height: 20px;
+	}
+
+	.v-messages__message {
+		line-height: 14px;
 	}
 }
 </style>
