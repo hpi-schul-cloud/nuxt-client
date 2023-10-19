ed4337ccd51904a9411193a0955431ff79c007c9
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2023-10-19 09:30:27 +0200

    BC-5586: remove legacy directives

--- src/plugins/directives.js
+++
@@ -1,9 +0,0 @@
-// VUE3_UPGRADE - can we remove this?
-
-export const mountDirectives = (app) => {
-	app.directive("focus-on-mount", {
-		mounted(el, binding) {
-			if (binding.value) el.focus();
-		},
-	});
-};
