c07f1f7af36da83a950a02a63a35e31bd209510a
Max Bischof <106820326+bischofmax@users.noreply.github.com>
2023-10-20 14:47:53 +0200

    BC-5595 - Show progress bar (#2875)

--- src/components/feature-board-file-element/FileContentElement.unit.ts
+++ src/components/feature-board-file-element/FileContentElement.unit.ts
@@ -109,7 +109,7 @@ describe("FileContentElement", () => {
 				await wrapper.vm.$nextTick();
 
 				const fileUpload = wrapper.findComponent(FileUpload);
-				expect(fileUpload.exists()).toBe(false);
+				expect(fileUpload.exists()).toBe(true);
 			});
 
 			it("should not render slot menu component", async () => {
@@ -388,6 +388,7 @@ describe("FileContentElement", () => {
 					const props = wrapper.findComponent(FileUpload).props();
 
 					expect(props.elementId).toEqual(element.id);
+					expect(props.isEditMode).toBe(true);
 				});
 
 				describe("when FileUpload emits upload:file event", () => {
