27f1de2f376fb80f5e1ccaf3a7ebafdbafc66d55
Oliver Happe <oliver.happe@dataport.de>
2023-10-25 14:02:42 +0200

    adds explanatory comments

--- src/components/util-input-masks/InputMask.factory.ts
+++ src/components/util-input-masks/InputMask.factory.ts
@@ -12,6 +12,8 @@ import { DirectiveFunction } from "vue";
  *  @see https://beholdr.github.io/maska/#/?id=maskinput-options
  */
 export const createInputMask = (options: MaskInputOptions) => {
+	// DirectiveFunction type is necessary to ensure compatibility with defineComponent()-Syntax in vue 2.7
+	// going forward with Vue3 we should be able to use Directive<HTMLElement> type
 	const bindMaska: DirectiveFunction = (el, binding, vNode, oldVnode) => {
 		const newBinding = { ...binding, arg: options as string };
 		(vMaska as DirectiveFunction)(el, newBinding, vNode, oldVnode);
