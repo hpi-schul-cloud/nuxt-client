36004ec4798d915b302adf39f23d058480c51168
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-10-26 09:21:01 +0200

    BC-5598 - rework structure of unit tests for `ColorOverlay`

--- src/components/ui-color-overlay/ColorOverlay.unit.ts
+++ src/components/ui-color-overlay/ColorOverlay.unit.ts
@@ -45,115 +45,129 @@ describe("ColorOverlay", () => {
 			expect(wrapper.text()).toContain("test-slot-content");
 		});
 
-		it("should emit on:action event, when the container is clicked", () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when the container is clicked", () => {
+			it("should emit on:action event", () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			const container = wrapper.find(".display-container");
-			container.trigger("click");
+				const container = wrapper.find(".display-container");
+				container.trigger("click");
 
-			expect(wrapper.emitted("on:action")).toHaveLength(1);
+				expect(wrapper.emitted("on:action")).toHaveLength(1);
+			});
 		});
 
-		it("should emit on:action event, when the overlay is clicked", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when the overlay is clicked", () => {
+			it("should emit on:action event", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			wrapper.trigger("focusin");
-			await nextTick();
+				wrapper.trigger("focusin");
+				await nextTick();
 
-			const overlay = wrapper.find(".display-overlay");
+				const overlay = wrapper.find(".display-overlay");
 
-			expect(overlay.exists()).toBe(true);
-			expect(overlay.isVisible()).toBe(true);
+				expect(overlay.exists()).toBe(true);
+				expect(overlay.isVisible()).toBe(true);
 
-			overlay.trigger("click");
+				overlay.trigger("click");
 
-			expect(wrapper.emitted("on:action")).toHaveLength(1);
+				expect(wrapper.emitted("on:action")).toHaveLength(1);
+			});
 		});
 
-		it("should show overlay, when the image is focused, and hide it, when not (using focusin and focusout)", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when the image is focused (using focusin and focusout)", () => {
+			it("should show overlay and hide it, when not ", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			wrapper.trigger("focusin");
-			await nextTick();
+				wrapper.trigger("focusin");
+				await nextTick();
 
-			const overlayFocusIn = wrapper.find(".display-overlay");
+				const overlayFocusIn = wrapper.find(".display-overlay");
 
-			expect(overlayFocusIn.exists()).toBe(true);
-			expect(overlayFocusIn.isVisible()).toBe(true);
+				expect(overlayFocusIn.exists()).toBe(true);
+				expect(overlayFocusIn.isVisible()).toBe(true);
 
-			wrapper.trigger("focusout");
-			await nextTick();
+				wrapper.trigger("focusout");
+				await nextTick();
 
-			const overlayFocusOut = wrapper.find(".display-overlay");
+				const overlayFocusOut = wrapper.find(".display-overlay");
 
-			expect(overlayFocusOut.exists()).toBe(false);
+				expect(overlayFocusOut.exists()).toBe(false);
+			});
 		});
 
-		it("should show overlay, when the container is focused, and hide it, when not (using focus and blur)", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when the container is focused (using focus and blur)", () => {
+			it("should show overlay and hide it, when not ", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			const container = wrapper.find(".display-container")
-				.element as HTMLDivElement;
+				const container = wrapper.find(".display-container")
+					.element as HTMLDivElement;
 
-			expect(document.activeElement).toBe(document.body);
+				expect(document.activeElement).toBe(document.body);
 
-			container.focus();
-			await nextTick();
+				container.focus();
+				await nextTick();
 
-			expect(document.activeElement).toBe(container);
+				expect(document.activeElement).toBe(container);
 
-			const overlay = wrapper.find(".display-overlay");
+				const overlay = wrapper.find(".display-overlay");
 
-			expect(overlay.exists()).toBe(true);
-			expect(overlay.isVisible()).toBe(true);
+				expect(overlay.exists()).toBe(true);
+				expect(overlay.isVisible()).toBe(true);
 
-			container.blur();
-			await nextTick();
+				container.blur();
+				await nextTick();
 
-			expect(document.activeElement).toBe(document.body);
+				expect(document.activeElement).toBe(document.body);
 
-			const overlayBlur = wrapper.find(".display-overlay");
+				const overlayBlur = wrapper.find(".display-overlay");
 
-			expect(overlayBlur.exists()).toBe(false);
+				expect(overlayBlur.exists()).toBe(false);
+			});
 		});
 
-		it("should emit on:action event, when Enter key is pressed", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when Enter key is pressed", () => {
+			it("should emit on:action event", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			const container = wrapper.find(".display-container");
-			container.trigger("keydown.enter");
-			await nextTick();
+				const container = wrapper.find(".display-container");
+				container.trigger("keydown.enter");
+				await nextTick();
 
-			expect(wrapper.emitted("on:action")).toHaveLength(1);
+				expect(wrapper.emitted("on:action")).toHaveLength(1);
+			});
 		});
 
-		it("should emit on:action event, when Space key is pressed", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when Space key is pressed", () => {
+			it("should emit on:action event", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			const container = wrapper.find(".display-container");
-			container.trigger("keydown.space");
-			await nextTick();
+				const container = wrapper.find(".display-container");
+				container.trigger("keydown.space");
+				await nextTick();
 
-			expect(wrapper.emitted("on:action")).toHaveLength(1);
+				expect(wrapper.emitted("on:action")).toHaveLength(1);
+			});
 		});
 
-		it("should show overlay, when the user hovers over the image, and hides when not", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: false });
+		describe("when the user hovers over the image", () => {
+			it("should show overlay and hide it, when not", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: false });
 
-			wrapper.trigger("mouseenter");
-			await nextTick();
+				wrapper.trigger("mouseenter");
+				await nextTick();
 
-			const overlayEnter = wrapper.find(".display-overlay");
+				const overlayEnter = wrapper.find(".display-overlay");
 
-			expect(overlayEnter.exists()).toBe(true);
-			expect(overlayEnter.isVisible()).toBe(true);
+				expect(overlayEnter.exists()).toBe(true);
+				expect(overlayEnter.isVisible()).toBe(true);
 
-			wrapper.trigger("mouseleave");
-			await nextTick();
+				wrapper.trigger("mouseleave");
+				await nextTick();
 
-			const overlayLeave = wrapper.find(".display-overlay");
+				const overlayLeave = wrapper.find(".display-overlay");
 
-			expect(overlayLeave.exists()).toBe(false);
+				expect(overlayLeave.exists()).toBe(false);
+			});
 		});
 
 		describe("when color property is undefined", () => {
@@ -215,8 +229,8 @@ describe("ColorOverlay", () => {
 			});
 		});
 
-		describe("when color property is defined", () => {
-			it("should use defined color value", async () => {
+		describe("when opacity property is defined", () => {
+			it("should use defined opacity value", async () => {
 				const opacity = 0.8;
 				const { wrapper } = setup({ isOverlayDisabled: false, opacity });
 
@@ -250,105 +264,119 @@ describe("ColorOverlay", () => {
 			expect(wrapper.text()).toContain("test-slot-content");
 		});
 
-		it("should not emit on:action event, when the container is clicked", () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when the container is clicked", () => {
+			it("should not emit on:action event", () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			const container = wrapper.find(".display-container");
-			container.trigger("click");
+				const container = wrapper.find(".display-container");
+				container.trigger("click");
 
-			expect(wrapper.emitted("on:action")).toBeUndefined();
+				expect(wrapper.emitted("on:action")).toBeUndefined();
+			});
 		});
 
-		it("should not emit on:action event, when the image is clicked", () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when the image is clicked", () => {
+			it("should not emit on:action event, ", () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			const slotContent = wrapper.find(".slot-content");
-			slotContent.trigger("click");
+				const slotContent = wrapper.find(".slot-content");
+				slotContent.trigger("click");
 
-			expect(wrapper.emitted("on:action")).toBeUndefined();
+				expect(wrapper.emitted("on:action")).toBeUndefined();
+			});
 		});
 
-		it("should not show overlay, when the image is focused (using focusin and focusout)", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when the image is focused (using focusin and focusout)", () => {
+			it("should not show overlay", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			wrapper.trigger("focusin");
-			await nextTick();
+				wrapper.trigger("focusin");
+				await nextTick();
 
-			const overlayFocusIn = wrapper.find(".display-overlay");
+				const overlayFocusIn = wrapper.find(".display-overlay");
 
-			expect(overlayFocusIn.exists()).toBe(false);
+				expect(overlayFocusIn.exists()).toBe(false);
 
-			wrapper.trigger("focusout");
-			await nextTick();
+				wrapper.trigger("focusout");
+				await nextTick();
 
-			const overlayFocusOut = wrapper.find(".display-overlay");
+				const overlayFocusOut = wrapper.find(".display-overlay");
 
-			expect(overlayFocusOut.exists()).toBe(false);
+				expect(overlayFocusOut.exists()).toBe(false);
+			});
 		});
 
-		it("should not show overlay, when the image is focused (using focus and blur)", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when the image is focused (using focus and blur)", () => {
+			it("should not show overlay", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			const container = wrapper.find(".display-container")
-				.element as HTMLDivElement;
+				const container = wrapper.find(".display-container")
+					.element as HTMLDivElement;
 
-			expect(document.activeElement).toBe(document.body);
+				expect(document.activeElement).toBe(document.body);
 
-			container.focus();
-			await nextTick();
+				container.focus();
+				await nextTick();
 
-			expect(document.activeElement).toBe(container);
+				expect(document.activeElement).toBe(container);
 
-			const overlayFocus = wrapper.find(".display-overlay");
+				const overlayFocus = wrapper.find(".display-overlay");
 
-			expect(overlayFocus.exists()).toBe(false);
+				expect(overlayFocus.exists()).toBe(false);
 
-			container.blur();
-			await nextTick();
+				container.blur();
+				await nextTick();
 
-			expect(document.activeElement).toBe(document.body);
+				expect(document.activeElement).toBe(document.body);
 
-			const overlayBlur = wrapper.find(".display-overlay");
+				const overlayBlur = wrapper.find(".display-overlay");
 
-			expect(overlayBlur.exists()).toBe(false);
+				expect(overlayBlur.exists()).toBe(false);
+			});
 		});
 
-		it("should not emit on:action event, when Enter key is pressed", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when Enter key is pressed", () => {
+			it("should not emit on:action event", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			const container = wrapper.find(".display-container");
-			container.trigger("keydown.enter");
-			await nextTick();
+				const container = wrapper.find(".display-container");
+				container.trigger("keydown.enter");
+				await nextTick();
 
-			expect(wrapper.emitted("on:action")).toBeUndefined();
+				expect(wrapper.emitted("on:action")).toBeUndefined();
+			});
 		});
 
-		it("should not emit on:action event, when Space key is pressed", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when Space key is pressed", () => {
+			it("should not emit on:action event", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			const container = wrapper.find(".display-container");
-			container.trigger("keydown.space");
-			await nextTick();
+				const container = wrapper.find(".display-container");
+				container.trigger("keydown.space");
+				await nextTick();
 
-			expect(wrapper.emitted("on:action")).toBeUndefined();
+				expect(wrapper.emitted("on:action")).toBeUndefined();
+			});
 		});
 
-		it("should not show overlay, when the user hovers over the image", async () => {
-			const { wrapper } = setup({ isOverlayDisabled: true });
+		describe("when the user hovers over the image", () => {
+			it("should not show overlay", async () => {
+				const { wrapper } = setup({ isOverlayDisabled: true });
 
-			wrapper.trigger("mouseenter");
-			await nextTick();
+				wrapper.trigger("mouseenter");
+				await nextTick();
 
-			const overlayEnter = wrapper.find(".display-overlay");
+				const overlayEnter = wrapper.find(".display-overlay");
 
-			expect(overlayEnter.exists()).toBe(false);
+				expect(overlayEnter.exists()).toBe(false);
 
-			wrapper.trigger("mouseleave");
-			await nextTick();
+				wrapper.trigger("mouseleave");
+				await nextTick();
 
-			const overlayLeave = wrapper.find(".display-overlay");
+				const overlayLeave = wrapper.find(".display-overlay");
 
-			expect(overlayLeave.exists()).toBe(false);
+				expect(overlayLeave.exists()).toBe(false);
+			});
 		});
 	});
 });
