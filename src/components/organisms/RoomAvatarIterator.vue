4ebc5a2e24d0c9aa6d2bae53c7049645c2e84e0a
Murat Merdoglu <murat.merdoglu@dataport.de>
2023-10-26 14:16:24 +0200

    BC-5508: fix broken avatar iterator component

--- src/components/organisms/RoomAvatarIterator.vue
+++ src/components/organisms/RoomAvatarIterator.vue
@@ -5,11 +5,11 @@
 		hide-default-footer
 		no-data-text=""
 	>
-		<template #default="props">
+		<template #default>
 			<v-container>
 				<v-row align="center">
 					<v-col
-						v-for="(item, key) in props.items"
+						v-for="(item, key) in items"
 						:key="key"
 						class="d-flex justify-center ma-0 mt-1 mb-0.5 pa-0"
 						:cols="colCount"
