29ebb7524403299e26ded38fb3ba5ade3d32f56f
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-11-03 11:31:01 +0100

    chore: add test for metaTagExtractorApi.composable

---
+++ src/components/feature-board-link-element/composables/MetaTagExtractorApi.composable.unit.ts
@@ -0,0 +1,109 @@
+import * as serverApi from "@/serverApi/v3/api";
+import { MetaTagExtractorResponse } from "@/serverApi/v3/api";
+import { createMock, DeepMocked } from "@golevelup/ts-jest";
+import { mount } from "@vue/test-utils";
+import { defineComponent } from "vue";
+import { useMetaTagExtractorApi } from "./MetaTagExtractorApi.composable";
+import { mockApiResponse } from "@@/tests/test-utils";
+
+describe("useMetaTagExtractorApi", () => {
+	let api: DeepMocked<serverApi.MetaTagExtractorApi>;
+
+	beforeEach(() => {
+		api = createMock<serverApi.MetaTagExtractorApi>();
+
+		jest.spyOn(serverApi, "MetaTagExtractorApiFactory").mockReturnValue(api);
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	const getWrapper = () => {
+		let composable: ReturnType<typeof useMetaTagExtractorApi> | undefined;
+
+		const TestComponent = defineComponent({
+			template: "<div/>",
+			setup() {
+				composable = useMetaTagExtractorApi();
+			},
+		});
+
+		const wrapper = mount(TestComponent, {});
+		return { wrapper, composable };
+	};
+
+	describe("getData", () => {
+		describe("when meta tags could be extracted", () => {
+			const setup = () => {
+				const mockedResponse: MetaTagExtractorResponse = {
+					url: "",
+					title: "",
+					description: "",
+					imageUrl: "",
+				};
+
+				api.metaTagExtractorControllerGetData.mockResolvedValue(
+					mockApiResponse({ data: mockedResponse })
+				);
+
+				const { wrapper, composable } = getWrapper();
+
+				return {
+					wrapper,
+					mockedResponse,
+					composable,
+				};
+			};
+
+			it("should be defined", () => {
+				const { composable } = setup();
+
+				expect(composable?.getData).toBeDefined();
+			});
+
+			it("should return the data", async () => {
+				const { composable, mockedResponse } = setup();
+
+				const url = "https://test.de/my-article";
+				const data = await composable?.getData(url);
+
+				expect(data).toEqual(mockedResponse);
+			});
+		});
+
+		describe("when meta tags extraction failed", () => {
+			const setup = () => {
+				const mockedResponse: MetaTagExtractorResponse = {
+					url: "",
+					title: "",
+					description: "",
+					imageUrl: "",
+				};
+
+				api.metaTagExtractorControllerGetData.mockRejectedValue(false);
+
+				const { wrapper, composable } = getWrapper();
+
+				return {
+					wrapper,
+					mockedResponse,
+					composable,
+				};
+			};
+
+			it("should return default values", async () => {
+				const { composable } = setup();
+
+				const url = "https://test.de/my-article";
+				const data = await composable?.getData(url);
+
+				expect(data).toEqual({ url, title: "", description: "" });
+			});
+		});
+	});
+});
