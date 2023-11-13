6aebd5554bbd6b75108f7d3ab9200289fcec44b4
KalliSfak <kalliopi.sfakianaki@dataport.de>
2023-11-13 11:27:32 +0100

    play-pause functionality

--- src/components/feature-board-file-element/content/display/audio-display/AudioDisplay.vue
+++ src/components/feature-board-file-element/content/display/audio-display/AudioDisplay.vue
@@ -1,22 +1,29 @@
 <template>
-	<ContentElementBar class="my-4">
+	<ContentElementBar class="audioplayer">
 		<template #element>
-			<audio
-				controls
-				controlsList="nodownload noplaybackrate"
-				loading="lazy"
-				:src="src"
-				v-on:error="onError"
-				class="audio"
-			/>
+			<audio ref="audio" class="audio" loading="lazy" v-on:error="onError" />
+			<v-btn icon @click="playing = !playing">
+				<v-icon
+					role="img"
+					aria-hidden="false"
+					style="color: white"
+					v-if="!playing"
+					>{{ mdiPlay }}</v-icon
+				>
+				<v-icon role="img" aria-hidden="false" style="color: white" v-else>
+					{{ mdiPause }}</v-icon
+				>
+			</v-btn>
 		</template>
 		<template #menu><slot /></template>
 	</ContentElementBar>
 </template>
 
 <script lang="ts">
+import { mdiPause, mdiPlay } from "@mdi/js";
 import { ContentElementBar } from "@ui-board";
-import { defineComponent } from "vue";
+import { useMediaControls } from "@vueuse/core";
+import { computed, defineComponent, ref } from "vue";
 import { FileAlert } from "../../../shared/types/FileAlert.enum";
 
 export default defineComponent({
@@ -27,14 +34,36 @@ export default defineComponent({
 	},
 	emits: ["error"],
 	setup(props, { emit }) {
+		const audio = ref();
+		const source = computed(() => {
+			return props.src;
+		});
+
+		const { playing } = useMediaControls(audio, {
+			src: source,
+		});
+
 		const onError = () => {
 			emit("error", FileAlert.AUDIO_FORMAT_ERROR);
 		};
-		return { onError };
+
+		return {
+			audio,
+			playing,
+			mdiPlay,
+			mdiPause,
+			onError,
+		};
 	},
 });
 </script>
 <style scoped>
+.audioplayer {
+	background-color: #424242;
+	margin-top: 0px;
+	border-top-right-radius: 4px;
+	border-top-left-radius: 4px;
+}
 .audio {
 	width: 100%;
 }
