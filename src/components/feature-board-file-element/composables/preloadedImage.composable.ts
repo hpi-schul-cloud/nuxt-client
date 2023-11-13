527372728f9ac7a569f4157987d03fb2c618e36b
Max Bischof <maximilian.bischof@dataport.de>
2023-11-13 17:15:25 +0100

    Add comment

--- src/components/feature-board-file-element/composables/preloadedImage.composable.ts
+++ src/components/feature-board-file-element/composables/preloadedImage.composable.ts
@@ -1,5 +1,7 @@
 import { onMounted, ref } from "vue";
 
+// Loads image without the need to display an image element.
+// When image element is rendered after image is loaded image for element is taken from cache.
 export const usePreloadedImage = (imageUrl: string) => {
 	const isImageLoading = ref(true);
 
