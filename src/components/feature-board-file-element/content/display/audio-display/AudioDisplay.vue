0197d7b425a5da7c9eaa809631e8ad16cc499389
KalliSfak <kalliopi.sfakianaki@dataport.de>
2023-11-13 14:41:16 +0100

    speed up menu

--- src/components/feature-board-file-element/content/display/audio-display/AudioDisplay.vue
+++ src/components/feature-board-file-element/content/display/audio-display/AudioDisplay.vue
@@ -8,76 +8,79 @@
 				v-on:error="onError"
 				controlsList="nodownload noplaybackrate"
 			/>
-			<v-icon
-				@click="playing = !playing"
-				role="img"
-				aria-hidden="false"
-				style="color: white"
-				v-if="!playing"
-				>{{ mdiPlay }}</v-icon
-			>
-			<v-icon
-				@click="playing = !playing"
-				role="img"
-				aria-hidden="false"
-				style="color: white"
-				v-else
-			>
-				{{ mdiPause }}</v-icon
-			>
+			<button @click="playing = !playing">
+				<v-icon
+					role="img"
+					aria-hidden="false"
+					style="color: white"
+					v-if="!playing"
+					>{{ mdiPlay }}</v-icon
+				>
+				<v-icon role="img" aria-hidden="false" style="color: white" v-else>
+					{{ mdiPause }}</v-icon
+				>
+			</button>
 			<span
 				style="color: white; font-size: 10px; padding: 10px"
 				class="duration text-body-2"
 			>
 				{{ formatDuration(currentTime) }} / {{ formatDuration(duration) }}</span
 			>
-			<v-icon
-				@click="muted = !muted"
-				role="img"
-				aria-hidden="false"
-				style="color: white"
-				v-if="muted"
-				>{{ mdiVolumeOff }}</v-icon
-			>
-			<v-icon
-				@click="muted = !muted"
-				role="img"
-				aria-hidden="false"
-				style="color: white; padding: 10px"
-				v-else
-				>{{ mdiVolumeHigh }}</v-icon
-			>
+			<SpeedMenu />
+			<button @click="muted = !muted">
+				<v-icon
+					role="img"
+					aria-hidden="false"
+					style="color: white"
+					v-if="muted"
+					>{{ mdiVolumeOff }}</v-icon
+				>
+				<v-icon
+					role="img"
+					aria-hidden="false"
+					style="color: white; padding: 10px"
+					v-else
+					>{{ mdiVolumeHigh }}</v-icon
+				>
+			</button>
 		</template>
 		<template #menu><slot /></template>
 	</ContentElementBar>
 </template>
 
 <script lang="ts">
-import { mdiPause, mdiPlay, mdiVolumeHigh, mdiVolumeOff } from "@mdi/js";
+import {
+	mdiPause,
+	mdiPlay,
+	mdiPlaySpeed,
+	mdiVolumeHigh,
+	mdiVolumeOff,
+} from "@mdi/js";
 import { ContentElementBar } from "@ui-board";
 import { useMediaControls } from "@vueuse/core";
 import { computed, defineComponent, onMounted, ref } from "vue";
 import { FileAlert } from "../../../shared/types/FileAlert.enum";
+import SpeedMenu from "./SpeedMenu.vue";
 
 export default defineComponent({
 	name: "AudioDisplay",
-	components: { ContentElementBar },
+	components: { ContentElementBar, SpeedMenu },
 	props: {
 		src: { type: String, required: true },
 	},
 	emits: ["error"],
 	setup(props, { emit }) {
 		const audio = ref();
+		const isOpen = ref(true);
 		const source = computed(() => {
 			return props.src;
 		});
 
-		const { playing, currentTime, duration, volume, muted } = useMediaControls(
-			audio,
-			{
-				src: source,
-			}
-		);
+		const controls = useMediaControls(audio, {
+			src: source,
+		});
+
+		const { playing, currentTime, duration, volume, muted, rate } = controls;
 
 		const onError = () => {
 			emit("error", FileAlert.AUDIO_FORMAT_ERROR);
@@ -85,6 +88,16 @@ export default defineComponent({
 
 		const formatDuration = (seconds: number) => {
 			return new Date(1000 * seconds).toISOString().slice(14, 19);
+		};
+
+		const open = () => {
+			console.log("open", isOpen.value);
+			return (isOpen.value = true);
+		};
+
+		const close = () => {
+			console.log("close");
+			return (isOpen.value = false);
 		};
 
 		onMounted(() => {
@@ -99,17 +112,25 @@ export default defineComponent({
 			duration,
 			volume,
 			muted,
+			rate,
 			mdiPlay,
 			mdiPause,
 			mdiVolumeOff,
 			mdiVolumeHigh,
+			mdiPlaySpeed,
 			onError,
 			formatDuration,
+			open,
+			close,
 		};
 	},
 });
 </script>
 <style scoped>
+button:focus {
+	outline: none;
+}
+
 .audioplayer {
 	background-color: #424242;
 	margin-top: 0px;
---
+++ src/components/feature-board-file-element/content/display/audio-display/SpeedMenu.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="text-center">
+		<v-menu offset-y max-height="120px">
+			<template v-slot:activator="{ on, attrs }">
+				<button v-bind="attrs" v-on="on">
+					<v-icon style="color: white">{{ mdiPlaySpeed }}</v-icon>
+				</button>
+			</template>
+			<v-list>
+				<v-list-item v-for="(item, index) in items" :key="index">
+					<v-list-item-title>{{ item.title }}</v-list-item-title>
+				</v-list-item>
+			</v-list>
+		</v-menu>
+	</div>
+</template>
+
+<script lang="ts">
+import { mdiPlaySpeed } from "@mdi/js";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "SpeedMenu",
+	setup() {
+		const items = [
+			{ title: "0.25" },
+			{ title: "0.5" },
+			{ title: "0.75" },
+			{ title: "Normal" },
+			{ title: "1.25" },
+			{ title: "1.5" },
+			{ title: "1.75" },
+			{ title: "2" },
+		];
+
+		return {
+			items,
+			mdiPlaySpeed,
+		};
+	},
+});
+</script>
+<style scoped>
+button:focus {
+	outline: none;
+}
+.v-list-item {
+	min-height: 38px;
+}
+</style>
