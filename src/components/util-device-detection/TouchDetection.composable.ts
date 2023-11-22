37b8d35666ec90f4233ade3acf864f4839cad62f
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-11-22 10:49:36 +0100

    consolidate touch and click events for the same interaction

--- src/components/util-device-detection/TouchDetection.composable.ts
+++ src/components/util-device-detection/TouchDetection.composable.ts
@@ -7,20 +7,25 @@ export const useTouchDetection = () => {
 	const hasTouchCapability3 = navigator.maxTouchPoints >= 1;
 
 	const isTouchDetected = ref(hasTouchCapability);
-
-	console.log("onTouchStart", hasTouchCapability);
-	console.log("touchstart", hasTouchCapability2);
-	console.log("maxTouchPoints", hasTouchCapability3);
+	let lastTouchDetection = new Date().getTime();
+	console.log(".", lastTouchDetection);
 
 	useEventListener(
 		"touchstart",
 		(evt) => {
+			const now = new Date();
+			console.log("age", now.getTime() - lastTouchDetection);
 			if (evt.touches[0].force >= 0.999) {
+				if (now.getTime() - lastTouchDetection > 200) {
+					isTouchDetected.value = false;
+				}
 				// stylus or mouse on touch-device
-				isTouchDetected.value = false;
 			} else {
 				isTouchDetected.value = true;
+				lastTouchDetection = now.getTime();
+				console.log("lastTouchDetection", lastTouchDetection);
 			}
+			console.log("lastTouchDetection", lastTouchDetection);
 			console.log("touch event: ", evt.touches[0]);
 			console.log("isTouchDetected:", isTouchDetected.value);
 			console.log("onTouchStart", hasTouchCapability);
@@ -35,9 +40,12 @@ export const useTouchDetection = () => {
 	useEventListener(
 		"mousedown",
 		() => {
-			isTouchDetected.value = false;
-			console.log("isTouchDetected: false");
-
+			const now = new Date();
+			console.log("age", now.getTime() - lastTouchDetection);
+			if (now.getTime() - lastTouchDetection > 200) {
+				isTouchDetected.value = false;
+			}
+			console.log("isTouchDetected:", isTouchDetected.value);
 			console.log("onTouchStart", hasTouchCapability);
 			console.log("touchstart", hasTouchCapability2);
 			console.log("maxTouchPoints", hasTouchCapability3);
