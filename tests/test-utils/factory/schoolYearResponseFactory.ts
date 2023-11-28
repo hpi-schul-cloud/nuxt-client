fcf24c844573956d9aa9eb647340632261f96032
dyedwiper <maximilian.becker@dataport.de>
2023-11-28 14:31:27 +0100

    Update schoolYearResponseFactory

--- tests/test-utils/factory/schoolYearResponseFactory.ts
+++ tests/test-utils/factory/schoolYearResponseFactory.ts
@@ -1,14 +1,31 @@
 import { SchoolYearResponse } from "@/serverApi/v3";
 import { Factory } from "fishery";
 
-export const schoolYearResponseFactory = Factory.define<SchoolYearResponse>(
-	({ sequence }) => {
+type SchoolYearResponseTransientParams = {
+	startYear: number;
+};
+
+class SchoolYearResponseFactory extends Factory<
+	SchoolYearResponse,
+	SchoolYearResponseTransientParams
+> {
+	public withStartYear(startYear: number): this {
+		this.rewindSequence();
+		return this.transient({ startYear });
+	}
+}
+
+export const schoolYearResponseFactory = SchoolYearResponseFactory.define(
+	({ sequence, transientParams }) => {
 		const id = `school-year-${sequence}`;
-		const year = new Date().getFullYear();
-		const nextYear = (year + 1).toString().substr(-2);
-		const name = `${year}/${nextYear}`;
-		const startDate = new Date(`${year}-08-01`).toISOString();
-		const endDate = new Date(`${year + 1}-07-31`).toISOString();
+
+		const startYearWithoutSequence =
+			transientParams?.startYear ?? new Date().getFullYear();
+		const startYear = startYearWithoutSequence + sequence - 1;
+
+		const name = `${startYear}/${(startYear + 1).toString().slice(-2)}`;
+		const startDate = new Date(`${startYear}-08-01`).toISOString();
+		const endDate = new Date(`${startYear + 1}-07-31`).toISOString();
 
 		return { id, name, startDate, endDate };
 	}
