7685ca5fe295d1a6449ef62a778498f9e0c710b6
Odalys Adam <odalys.adam@dataport.de>
2023-11-30 01:00:37 +0100

    datetimepicker in script setup

--- src/components/ui-date-time-picker/DateTimePicker.vue
+++ src/components/ui-date-time-picker/DateTimePicker.vue
@@ -29,132 +29,108 @@
 	</div>
 </template>
 
-<script lang="ts">
-import DatePicker from "./DatePicker.vue";
-import TimePicker from "./TimePicker.vue";
+<script setup lang="ts">
+import { defineProps, computed, ref } from "vue";
 import { useVModel } from "@vueuse/core";
-import { computed, defineComponent, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import dayjs from "dayjs";
 import { isDateTimeInPast, DATETIME_FORMAT } from "@/plugins/datetime";
+import DatePicker from "./DatePicker.vue";
+import TimePicker from "./TimePicker.vue";
 
-export default defineComponent({
-	name: "DateTimePicker",
-	components: {
-		DatePicker,
-		TimePicker,
+const props = defineProps({
+	dateTime: {
+		type: String,
+		default: "",
 	},
-	props: {
-		dateTime: {
-			type: String,
-			default: "",
-		},
-		dateInputLabel: { type: String, default: "" },
-		dateInputAriaLabel: { type: String, default: "" },
-		timeInputLabel: { type: String, default: "" },
-		timeInputAriaLabel: { type: String, default: "" },
-		minDate: { type: String },
-		maxDate: { type: String },
-	},
-	emits: ["input"],
-	setup(props, { emit }) {
-		const { locale, t } = useI18n();
-
-		const getDate = (dateIsoString: string) => {
-			if (!dateIsoString) {
-				return "";
-			}
-
-			return dateIsoString;
-		};
-
-		const getTime = (dateIsoString: string) => {
-			if (!dateIsoString) {
-				return "";
-			}
-			return new Date(dateIsoString).toLocaleTimeString(locale.value, {
-				timeStyle: "short",
-				hourCycle: "h23",
-			});
-		};
-
-		const dateTime = useVModel(props, "dateTime");
-		const date = ref(getDate(dateTime.value));
-		const time = ref(getTime(dateTime.value));
-		const dateRequired = computed(() => time.value !== "");
-		const dateTimeInPast = ref(
-			dateTime.value && isDateTimeInPast(dateTime.value)
-		);
-
-		const emitDateTime = () => {
-			if (date.value === "" && time.value === "") {
-				dateTimeInPast.value = false;
-				emit("input", null);
-				return;
-			}
-
-			if (date.value === "" && dateRequired.value) {
-				return;
-			}
-
-			const timeValue = time.value || "23:59";
-			const dateTime = new Date(date.value);
-			const hoursAndMinutes = timeValue.split(":");
-
-			dateTime.setHours(
-				parseInt(hoursAndMinutes[0]),
-				parseInt(hoursAndMinutes[1])
-			);
-			dateTimeInPast.value = isDateTimeInPast(dateTime);
-			emit("input", dateTime.toISOString());
-		};
-
-		const errors = ref<Array<string>>([]);
-		const message = computed(() => {
-			if (errors.value.length > 0) return "";
-
-			if (dateRequired.value && !date.value) {
-				return t("components.datePicker.validation.required");
-			}
-
-			if (dateTimeInPast.value) {
-				return t("components.datePicker.messages.future");
-			}
-
-			return "";
-		});
-
-		const onError = (errorOrigin: string) => {
-			if (errors.value.indexOf(errorOrigin) === -1) {
-				errors.value.push(errorOrigin);
-			}
-		};
-
-		const onDateUpdate = (newDate: string) => {
-			date.value = newDate;
-			errors.value = errors.value.filter((item) => item !== "date");
-
-			emitDateTime();
-		};
-
-		const onTimeUpdate = (newTime: string) => {
-			time.value = newTime;
-			errors.value = errors.value.filter((item) => item !== "time");
-			emitDateTime();
-		};
-
-		return {
-			date,
-			time,
-			onDateUpdate,
-			onTimeUpdate,
-			dateRequired,
-			message,
-			errors,
-			onError,
-		};
-	},
+	dateInputLabel: { type: String, default: "" },
+	dateInputAriaLabel: { type: String, default: "" },
+	timeInputLabel: { type: String, default: "" },
+	timeInputAriaLabel: { type: String, default: "" },
+	minDate: { type: String },
+	maxDate: { type: String },
 });
+const emit = defineEmits(["input"]);
+
+const { locale, t } = useI18n();
+
+const getDate = (dateIsoString: string) => {
+	if (!dateIsoString) {
+		return "";
+	}
+
+	return dateIsoString;
+};
+
+const getTime = (dateIsoString: string) => {
+	if (!dateIsoString) {
+		return "";
+	}
+	return new Date(dateIsoString).toLocaleTimeString(locale.value, {
+		timeStyle: "short",
+		hourCycle: "h23",
+	});
+};
+
+const dateTime = useVModel(props, "dateTime");
+const date = ref(getDate(dateTime.value));
+const time = ref(getTime(dateTime.value));
+const dateRequired = computed(() => time.value !== "");
+const dateTimeInPast = ref(dateTime.value && isDateTimeInPast(dateTime.value));
+
+const emitDateTime = () => {
+	if (date.value === "" && time.value === "") {
+		dateTimeInPast.value = false;
+		emit("input", null);
+		return;
+	}
+
+	if (date.value === "" && dateRequired.value) {
+		return;
+	}
+
+	const timeValue = time.value || "23:59";
+	const dateTime = new Date(date.value);
+	const hoursAndMinutes = timeValue.split(":");
+
+	dateTime.setHours(parseInt(hoursAndMinutes[0]), parseInt(hoursAndMinutes[1]));
+	dateTimeInPast.value = isDateTimeInPast(dateTime);
+	emit("input", dateTime.toISOString());
+};
+
+const errors = ref<Array<string>>([]);
+const message = computed(() => {
+	if (errors.value.length > 0) return "";
+
+	if (dateRequired.value && !date.value) {
+		return t("components.datePicker.validation.required");
+	}
+
+	if (dateTimeInPast.value) {
+		return t("components.datePicker.messages.future");
+	}
+
+	return "";
+});
+
+const onError = (errorOrigin: string) => {
+	if (errors.value.indexOf(errorOrigin) === -1) {
+		errors.value.push(errorOrigin);
+	}
+};
+
+const onDateUpdate = (newDate: string) => {
+	date.value = newDate;
+	errors.value = errors.value.filter((item) => item !== "date");
+
+	emitDateTime();
+};
+
+const onTimeUpdate = (newTime: string) => {
+	time.value = newTime;
+	errors.value = errors.value.filter((item) => item !== "time");
+	emitDateTime();
+};
 </script>
 
 <style lang="scss" scoped>
