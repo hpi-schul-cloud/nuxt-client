3fc916d19afe21a59c335f845afc82b1ea6f45b3
Max Bischof <maximilian.bischof@dataport.de>
2023-12-11 15:42:41 +0100

    Refactor speedmenu test

--- src/components/feature-board-file-element/content/display/audio-display/SpeedMenu.unit.ts
+++ src/components/feature-board-file-element/content/display/audio-display/SpeedMenu.unit.ts
@@ -1,126 +1,84 @@
+import createComponentMocks from "@@/tests/test-utils/componentMocks";
 import { mdiCheck } from "@mdi/js";
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import SpeedMenu from "./SpeedMenu.vue";
 
 describe("SpeedMenu", () => {
-	const setup = () => {
-		document.body.setAttribute("data-app", "true");
+	describe("when rate is 0.5", () => {
+		const setup = () => {
+			document.body.setAttribute("data-app", "true");
 
-		const wrapper = mount(SpeedMenu, {
-			attachTo: document.body,
-		});
-		return {
-			wrapper,
+			const rate = 0.5;
+			const wrapper = shallowMount(SpeedMenu, {
+				attachTo: document.body,
+				propsData: {
+					rate,
+				},
+				...createComponentMocks({}),
+			});
+			return {
+				wrapper,
+				rate,
+			};
 		};
-	};
-	it("should be found in dom", () => {
-		const { wrapper } = setup();
 
-		const speedMenuElement = wrapper.findComponent(SpeedMenu);
-		expect(speedMenuElement.exists()).toBe(true);
-	});
+		it("should display check icon on listitem with value 0.5", () => {
+			const { wrapper, rate } = setup();
 
-	it("should have a menu", () => {
-		const { wrapper } = setup();
-		const menu = wrapper.find("v-menu");
+			const listItem = wrapper
+				.findAllComponents({ name: "v-list-item" })
+				.filter((listItem) => listItem.text().includes(`${rate}`))
+				.at(0);
+			const icon = listItem.findComponent({ name: "v-list-item-icon" });
 
-		expect(menu.exists()).toBe(true);
-	});
+			expect(icon.text()).toBe(mdiCheck);
+		});
 
-	it("should have a button and an icon", () => {
-		const { wrapper } = setup();
-		const button = wrapper.find("v-btn");
-		const icon = wrapper.find("v-icon");
+		describe("when selecting 1.25 value", () => {
+			it("should emit updateRate", () => {
+				const { wrapper } = setup();
+				const value = 1.25;
+				const listItem = wrapper
+					.findAllComponents({ name: "v-list-item" })
+					.filter((listItem) => listItem.text().includes(`${value}`))
+					.at(0);
+				listItem.vm.$emit("click");
 
-		wrapper.vm.$nextTick(() => {
-			expect(button.exists()).toBe(true);
-			expect(icon.exists()).toBe(true);
+				const emits = wrapper.emitted("updateRate");
+				expect(emits).toHaveLength(1);
+				expect(emits?.[0]).toEqual([value]);
+			});
 		});
 	});
 
-	it("should display the icon correctly", () => {
-		const { wrapper } = setup();
-		const icon = wrapper.find("v-icon");
+	describe("when rate is 0.75", () => {
+		const setup = () => {
+			document.body.setAttribute("data-app", "true");
 
-		wrapper.vm.$nextTick(() => {
-			expect(icon.text()).toBe("mdiPlaySpeed");
+			const rate = 0.75;
+			const wrapper = shallowMount(SpeedMenu, {
+				attachTo: document.body,
+				propsData: {
+					rate,
+				},
+				...createComponentMocks({}),
+			});
+			return {
+				wrapper,
+				rate,
+			};
+		};
+
+		it("should display check icon on listitem with value 0.75", () => {
+			const { wrapper, rate } = setup();
+
+			const listItem = wrapper
+				.findAllComponents({ name: "v-list-item" })
+				.filter((listItem) => listItem.text().includes(`${rate}`))
+				.at(0);
+			const icon = listItem.findComponent({ name: "v-list-item-icon" });
+
+			expect(icon.text()).toBe(mdiCheck);
 		});
-	});
-
-	it("should display a list of speeds", () => {
-		const { wrapper } = setup();
-		const list = wrapper.find("v-list");
-
-		expect(list.exists()).toBe(true);
-	});
-
-	it("should display correctly the default speed", () => {
-		const { wrapper } = setup();
-		const speed = wrapper.find("v-list-item");
-
-		wrapper.vm.$nextTick(() => {
-			expect(speed.text()).toBe("Normal");
-		});
-	});
-
-	it("should display check icon on normal value", () => {
-		const { wrapper } = setup();
-
-		const icon = wrapper.findAll("v-list-item").at(3).find("v-list-item-icon");
-
-		expect(icon.text()).toBe(mdiCheck);
-	});
-
-	describe("when selection 0.25 value", () => {
-		it("should display check icon on normal value", async () => {
-			const { wrapper } = setup();
-
-			const listItem = wrapper.findAll("v-list-item").at(0);
-
-			listItem.trigger("click");
-			await wrapper.vm.$nextTick();
-
-			const listItemIcon = wrapper
-				.findAll("v-list-item")
-				.at(0)
-				.find("v-list-item-icon");
-
-			expect(listItemIcon.text()).toBe(mdiCheck);
-		});
-	});
-
-	it("should display correctly the check icon when selected", () => {
-		const { wrapper } = setup();
-		const icon = wrapper.find("v-list-item-icon");
-
-		expect(icon.text()).toBe(mdiCheck);
-	});
-
-	it("should display correctly the speed value", () => {
-		const { wrapper } = setup();
-		const title = wrapper.find("v-list-item-title");
-		const speed = wrapper.find("v-list-item");
-
-		wrapper.vm.$nextTick(() => {
-			expect(title.text()).toBe("0.25");
-			expect(speed).toBe(0.25);
-		});
-	});
-});
-
-describe("when picking a speed through select", () => {
-	it("should emit rate on click", async () => {
-		const wrapper = mount(SpeedMenu, {
-			attachTo: document.body,
-		});
-		const listItem = wrapper.find("v-list-item");
-		await listItem.trigger("click");
-
-		await wrapper.vm.$nextTick;
-
-		expect(wrapper.emitted("rate")).toBeTruthy();
-		expect(wrapper.emitted("speedSelected")).toBeTruthy();
-		expect(wrapper.emitted("speedSelected")).toHaveLength(1);
-		expect(wrapper.vm.$data.speed).toBe(0.25);
 	});
 });
