e68244d1d451328ca6bbcad854c42c18c6109191
NFriedo <69233063+NFriedo@users.noreply.github.com>
2023-12-12 16:52:58 +0100

    BC-3844: adjust admin alerts accordin to changes in main, remove prominent prop

--- src/components/organisms/administration/SchoolPolicy.vue
+++ src/components/organisms/administration/SchoolPolicy.vue
@@ -2,7 +2,6 @@
 	<div>
 		<v-alert
 			v-if="status === 'error'"
-			prominent
 			variant="tonal"
 			type="error"
 			class="mb-6"
@@ -104,7 +103,7 @@
 					</h4>
 				</template>
 				<template #content>
-					<v-alert prominent variant="tonal" type="info" class="mb-0">
+					<v-alert variant="tonal" type="info" class="mb-0">
 						<div class="alert-text">
 							{{
 								t("pages.administration.school.index.schoolPolicy.delete.text")
