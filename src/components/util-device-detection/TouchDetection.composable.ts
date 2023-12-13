37ccac522eb204d6545dc15efe8163b165102ab8
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-12-13 14:35:52 +0100

    chore: improve readability

--- src/components/util-device-detection/TouchDetection.composable.ts
+++ src/components/util-device-detection/TouchDetection.composable.ts
@@ -2,35 +2,36 @@ import { useEventListener } from "@vueuse/core";
 import { ref } from "vue";
 
 export const useTouchDetection = () => {
-	const hasTouchCapability = "ontouchstart" in window; // what is the best condition?
-	const hasTouchCapability2 = "touchstart" in window;
-	const hasTouchCapability3 = navigator.maxTouchPoints >= 1;
-
-	console.log("hasTouchCapability", hasTouchCapability);
-	console.log("hasTouchCapability2", hasTouchCapability2);
-	console.log("hasTouchCapability3", hasTouchCapability3);
+	const hasTouchCapability = "ontouchstart" in window || "touchstart" in window;
 
 	const isTouchDetected = ref(hasTouchCapability);
 	let lastTouchDetection = new Date().getTime();
 
 	const handler = <T extends MouseEvent | TouchEvent>(evt: T) => {
+		isTouchDetected.value = isTouchEvent(evt);
+	};
+
+	const isTouchEvent = <T extends MouseEvent | TouchEvent>(evt: T): boolean => {
 		const now = new Date();
-		console.log("age", now.getTime() - lastTouchDetection);
-		if ("touches" in evt) {
-			console.log("evt.touches", evt.touches[0]);
-			if (evt.touches[0].force >= 0.999) {
-				if (now.getTime() - lastTouchDetection > 200) {
-					isTouchDetected.value = false;
-				}
-				// stylus or mouse on touch-device
-			} else {
-				isTouchDetected.value = true;
-				lastTouchDetection = now.getTime();
-			}
-		} else {
-			isTouchDetected.value = false;
+		if (!("touches" in evt)) {
+			// not a touch event
+			return false;
 		}
-		console.log("event: ", evt.type, isTouchDetected.value);
+
+		if (evt.touches[0].force < 1) {
+			// a touch event with distinct information on the used force
+			lastTouchDetection = now.getTime();
+			return true;
+		}
+
+		// a non-touch-event on touch-capable device
+
+		if (now.getTime() - lastTouchDetection < 200) {
+			// interprete as touch-event if last touch-event happen less than 200 seconds ago
+			return true;
+		}
+
+		return false;
 	};
 
 	useEventListener("touchstart", (evt) => handler<TouchEvent>(evt), {
