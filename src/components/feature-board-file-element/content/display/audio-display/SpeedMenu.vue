bfb900ffcd630659e7451d5d17c8e710817f0a3d
KalliSfak <kalliopi.sfakianaki@dataport.de>
2023-12-13 15:18:02 +0100

    deleted wrapper div

--- src/components/feature-board-file-element/content/display/audio-display/SpeedMenu.vue
+++ src/components/feature-board-file-element/content/display/audio-display/SpeedMenu.vue
@@ -1,96 +1,92 @@
 <template>
-	<div class="text-center">
-		<v-menu offset-y max-height="120px" min-width="150px" nudge-left="75px">
-			<template v-slot:activator="{ on, attrs }">
-				<v-btn
-					data-testid="speed-menu-play-speed"
-					v-bind="attrs"
-					v-on="on"
-					icon
-					small
-					color="white"
-					:aria-label="
-						$t('component.cardElement.fileElement.audioPlayer.speed')
-					"
-				>
-					<v-icon>
-						{{ mdiPlaySpeed }}
-					</v-icon>
-				</v-btn>
-			</template>
+	<v-menu offset-y max-height="120px" min-width="150px" nudge-left="75px">
+		<template v-slot:activator="{ on, attrs }">
+			<v-btn
+				data-testid="speed-menu-play-speed"
+				v-bind="attrs"
+				v-on="on"
+				icon
+				small
+				color="white"
+				:aria-label="$t('component.cardElement.fileElement.audioPlayer.speed')"
+			>
+				<v-icon>
+					{{ mdiPlaySpeed }}
+				</v-icon>
+			</v-btn>
+		</template>
 
-			<v-list dense>
-				<v-list-item-group>
-					<v-list-item @click="onSelect(0.25)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>0.25 </v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="0.25 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(0.5)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>0.5 </v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="0.5 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(0.75)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>0.75 </v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="0.75 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(1)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>{{
-								$t("component.cardElement.fileElement.audioPlayer.speed.normal")
-							}}</v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="1 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(1.25)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>1.25</v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="1.25 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(1.5)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>1.5</v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="1.5 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(1.75)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>1.75</v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="1.75 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-					<v-list-item @click="onSelect(2)">
-						<v-list-item-content class="ml-3">
-							<v-list-item-title>2</v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-icon v-if="2 === rate">
-							<v-icon>{{ mdiCheck }}</v-icon>
-						</v-list-item-icon>
-					</v-list-item>
-				</v-list-item-group>
-			</v-list>
-		</v-menu>
-	</div>
+		<v-list dense>
+			<v-list-item-group>
+				<v-list-item @click="onSelect(0.25)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>0.25 </v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="0.25 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(0.5)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>0.5 </v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="0.5 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(0.75)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>0.75 </v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="0.75 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(1)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>{{
+							$t("component.cardElement.fileElement.audioPlayer.speed.normal")
+						}}</v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="1 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(1.25)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>1.25</v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="1.25 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(1.5)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>1.5</v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="1.5 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(1.75)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>1.75</v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="1.75 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+				<v-list-item @click="onSelect(2)">
+					<v-list-item-content class="ml-3">
+						<v-list-item-title>2</v-list-item-title>
+					</v-list-item-content>
+					<v-list-item-icon v-if="2 === rate">
+						<v-icon>{{ mdiCheck }}</v-icon>
+					</v-list-item-icon>
+				</v-list-item>
+			</v-list-item-group>
+		</v-list>
+	</v-menu>
 </template>
 
 <script lang="ts">
