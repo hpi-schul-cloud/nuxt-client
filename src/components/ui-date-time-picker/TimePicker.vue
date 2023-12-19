212a423f1e671fd86e72b170e137f3d606d68229
odalys-dataport <82401838+odalys-dataport@users.noreply.github.com>
2023-12-19 15:41:42 +0100

    BC-5940 - Vue 3 Time Picker (#2962)
    * remove code for time select menu
    * remove getDate
    * debounced messages on DateTimePicker
    * refactor dateRequired to dateMissing
    * move getTime to datetime plugin

--- src/components/ui-date-time-picker/TimePicker.vue
+++ src/components/ui-date-time-picker/TimePicker.vue
@@ -1,25 +1,25 @@
 <template>
-	<v-text-field
-		ref="inputField"
-		v-model="timeValue"
-		variant="underlined"
-		color="primary"
-		:label="label"
-		:aria-label="ariaLabel"
-		placeholder="HH:MM"
-		append-icon="$mdiClockOutline"
-		data-testid="time-input"
-		:error-messages="errorMessages"
-		v-time-input-mask
-		@keydown.up.down.stop
-		@update:model-value="validate"
-	/>
+	<div>
+		<v-text-field
+			v-model="timeValue"
+			data-testid="time-input"
+			variant="underlined"
+			color="primary"
+			append-inner-icon="$mdiClockOutline"
+			:label="label"
+			:aria-label="ariaLabel"
+			placeholder="HH:MM"
+			:error-messages="errorMessages"
+			v-time-input-mask
+			@update:model-value="validate"
+			@keydown.up.down.stop
+		/>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { computedAsync, useDebounceFn } from "@vueuse/core";
 import { computed, ref, watchEffect } from "vue";
-// import { useTimePickerState } from "./TimePickerState.composable";
 import { useI18n } from "vue-i18n";
 import { useVuelidate } from "@vuelidate/core";
 import { helpers, requiredIf } from "@vuelidate/validators";
@@ -33,11 +33,9 @@ const props = defineProps({
 	required: { type: Boolean },
 });
 const emit = defineEmits(["update:time", "error"]);
-const { t } = useI18n();
 
-const inputField = ref<HTMLInputElement | null>(null);
+const { t } = useI18n();
 const timeValue = ref<undefined | string>();
-// const { timesOfDayList } = useTimePickerState();
 
 watchEffect(() => {
 	timeValue.value = props.time;
@@ -67,7 +65,7 @@ const getErrorMessages = useDebounceFn((validationModel: any) => {
 		return e.$message;
 	});
 	return messages;
-}, 1000);
+}, 700);
 
 const validate = () => {
 	v$.value.timeValue.$touch();
@@ -83,34 +81,9 @@ const validate = () => {
 const emitTime = () => {
 	emit("update:time", timeValue.value);
 };
-
-// const closeAndEmit = () => {
-// 	showDateDialog.value = false;
-// 	inputField.value?.focus();
-
-// 	emit(
-// 		"update:date",
-// 		dayjs(timeValue.value, DATETIME_FORMAT.date).toISOString()
-// 	);
-// };
-
-// const onSelect = async (selected: string) => {
-// 	inputField.value?.focus();
-// 	modelValue.value = selected;
-// 	valid.value = true;
-// 	await closeMenu();
-// };
 </script>
 
 <style lang="scss" scoped>
-@import "@/styles/variables";
-
-.time-list-item {
-	min-height: 42px;
-	text-align: center;
-	letter-spacing: $btn-letter-spacing;
-}
-
 :deep {
 	.v-field__append-inner .v-icon {
 		width: 20px;
