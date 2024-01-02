9caed0d3cece906f3b40e4224040a7e6fd794b0c
hoeppner-dataport <106819770+hoeppner-dataport@users.noreply.github.com>
2024-01-02 13:10:13 +0100

    BC-5130 - improve device detection (#2915)
    * provide boolean reference on user currently using a touch-input-device
    * use this information to set (in touch context) or remove(in mouse pointer context) drag-begin-delay
    * try to initially set the value by deriving from touch capabilites being available
    
    ---------
    
    Co-authored-by: Oliver Happe <oliver.happe@dataport.de>

---
+++ src/components/util-device-detection/util/isTouchEvent.ts
@@ -0,0 +1,26 @@
+let lastTouchDetection = new Date().getTime();
+
+export const isTouchEvent = <T extends MouseEvent | TouchEvent>(
+	evt: T
+): boolean => {
+	const now = new Date();
+	if (!("touches" in evt)) {
+		// not a touch event
+		return false;
+	}
+
+	if (evt.touches[0]?.force < 1) {
+		// a touch event with distinct information on the used force
+		lastTouchDetection = now.getTime();
+		return true;
+	}
+
+	// a non-touch-event on touch-capable device
+
+	if (now.getTime() - lastTouchDetection < 200) {
+		// interprete as touch-event if last touch-event happend less than 200 milliseconds ago
+		return true;
+	}
+
+	return false;
+};
