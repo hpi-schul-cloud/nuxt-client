f67bbf7bf6e7ca2cfd76bd127b4b02fb29bd72da
Arne Gnisa <arne.gnisa@capgemini.com>
2024-01-08 11:28:03 +0100

    N21-1539 Adds error handling to new class page

--- src/components/data-group/GroupState.composable.ts
+++ src/components/data-group/GroupState.composable.ts
@@ -1,13 +1,18 @@
-import { useErrorHandler } from "@/components/error-handling/ErrorHandler.composable";
 import { ref, Ref } from "vue";
 import { Group, useGroupApi } from "@data-group";
+import { injectStrict, NOTIFIER_MODULE_KEY } from "@/utils/inject";
+import { mapAxiosErrorToResponseError } from "@/utils/api";
+import { BusinessError } from "@/store/types/commons";
+import { useI18n } from "@/composables/i18n.composable";
 
 export const useGroupState = () => {
-	const { handleError } = useErrorHandler();
 	const { getGroup } = useGroupApi();
+	const { t } = useI18n();
+	const notifierModule = injectStrict(NOTIFIER_MODULE_KEY);
 
 	const isLoading: Ref<boolean> = ref(false);
 	const group: Ref<Group | undefined> = ref();
+	const error: Ref<BusinessError | undefined> = ref();
 
 	const fetchGroup = async (groupId: string): Promise<void> => {
 		isLoading.value = true;
@@ -15,11 +20,18 @@ export const useGroupState = () => {
 		try {
 			const fetchedGroup: Group = await getGroup(groupId);
 			group.value = fetchedGroup;
-		} catch (error) {
-			// TODO: fix this
-			handleError(error, {
-				404: undefined,
-				500: undefined,
+		} catch (errorResponse) {
+			const apiError = mapAxiosErrorToResponseError(errorResponse);
+
+			error.value = {
+				error: apiError,
+				statusCode: apiError.code,
+				message: apiError.message,
+			};
+
+			notifierModule.show({
+				text: t("error.load"),
+				status: "error",
 			});
 		}
 
