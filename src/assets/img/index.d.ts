501017bda2a4bff413488c58ddcd709c117e8842
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-01-10 08:47:14 +0100

    BC-5733 - add locals and image

--- src/assets/img/index.d.ts
+++ src/assets/img/index.d.ts
@@ -2,3 +2,7 @@ declare module "@/assets/img/tldraw.png" {
 	const value: string;
 	export default value;
 }
+declare module "@/assets/img/preview-not-possible.svg" {
+	const value: string;
+	export default value;
+}
