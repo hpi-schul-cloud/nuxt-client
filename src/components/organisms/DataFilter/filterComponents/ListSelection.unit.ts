4105edbcf05e246dc8c8794446a16afddc5c80ac
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-01-12 15:52:00 +0100

    chore: add more test for Listselection component

--- src/components/organisms/DataFilter/filterComponents/ListSelection.unit.ts
+++ src/components/organisms/DataFilter/filterComponents/ListSelection.unit.ts
@@ -28,10 +28,12 @@ const mountComponent = (
 describe("@components/DataFilter/filterComponents/ListSelection.vue", () => {
 	describe("should render the component", () => {
 		it("should render the checkboxes", async () => {
-			const wrapper = mountComponent();
-
-			await wrapper.setProps({ selectionList: mockProps.selectionList });
-			await wrapper.setProps({ selectedList: mockProps.selectedList });
+			const wrapper = mountComponent({
+				props: {
+					selectionList: mockProps.selectionList,
+					selectedList: mockProps.selectedList,
+				},
+			});
 
 			const checkboxComponents = wrapper.findAllComponents(
 				'[data-testid="list-selection"]'
@@ -46,37 +48,67 @@ describe("@components/DataFilter/filterComponents/ListSelection.vue", () => {
 			expect(checkboxComponents[2].text()).toBe(
 				mockProps.selectionList[2].label
 			);
+
+			expect(wrapper.vm.selectedList).toStrictEqual(mockProps.selectedList);
 		});
 	});
 
 	describe("should emit the events", () => {
-		it("should emit update:filter when add button is clicked", async () => {
-			const wrapper = mountComponent();
-			await wrapper.setProps({ selectionList: mockProps.selectionList });
-			await wrapper.setProps({ selectedList: [] });
+		describe("when add button is clicked", () => {
+			it("should emit update:filter", async () => {
+				const wrapper = mountComponent({
+					props: {
+						selectionList: mockProps.selectionList,
+						selectedList: mockProps.selectedList,
+					},
+				});
 
-			const checkboxComponent = wrapper.getComponent({ name: "v-checkbox" });
-			const actionButtonComponent = wrapper.getComponent({
-				name: "FilterActionButtons",
+				const checkboxComponent = wrapper.getComponent({ name: "v-checkbox" });
+				const actionButtonComponent = wrapper.getComponent({
+					name: "FilterActionButtons",
+				});
+
+				await checkboxComponent.vm.$emit(
+					"update:modelValue",
+					mockProps.selectedList
+				);
+				await actionButtonComponent.vm.$emit("update:filter");
+
+				expect(wrapper.emitted()).toHaveProperty("update:filter");
+				expect(wrapper.emitted()["update:filter"][0]).toStrictEqual([
+					["1A", "1C"],
+				]);
+				expect(wrapper.emitted()).toHaveProperty("dialog-closed");
 			});
 
-			await checkboxComponent.vm.$emit(
-				"update:modelValue",
-				mockProps.selectedList
-			);
-			await actionButtonComponent.vm.$emit("update:filter");
+			it("should emit remove:filter if selectedList prop is empty", async () => {
+				const wrapper = mountComponent({
+					props: {
+						selectionList: mockProps.selectionList,
+						selectedList: [],
+					},
+				});
 
-			expect(wrapper.emitted()).toHaveProperty("update:filter");
-			expect(wrapper.emitted()["update:filter"][0]).toStrictEqual([
-				["1A", "1C"],
-			]);
-			expect(wrapper.emitted()).toHaveProperty("dialog-closed");
+				const checkboxComponent = wrapper.getComponent({ name: "v-checkbox" });
+				const actionButtonComponent = wrapper.getComponent({
+					name: "FilterActionButtons",
+				});
+
+				await checkboxComponent.vm.$emit("update:modelValue", []);
+				await actionButtonComponent.vm.$emit("update:filter");
+
+				expect(wrapper.emitted()).toHaveProperty("remove:filter");
+				expect(wrapper.emitted()).toHaveProperty("dialog-closed");
+			});
 		});
 
 		it("should emit dialog-closed when cancel button is clicked", async () => {
-			const wrapper = mountComponent();
-			await wrapper.setProps({ selectionList: mockProps.selectionList });
-			await wrapper.setProps({ selectedList: [] });
+			const wrapper = mountComponent({
+				props: {
+					selectionList: mockProps.selectionList,
+					selectedList: [],
+				},
+			});
 
 			const actionButtonComponent = wrapper.getComponent({
 				name: "FilterActionButtons",
@@ -88,9 +120,12 @@ describe("@components/DataFilter/filterComponents/ListSelection.vue", () => {
 		});
 
 		it("should emit remove:filter when remove button is clicked", async () => {
-			const wrapper = mountComponent();
-			await wrapper.setProps({ selectionList: mockProps.selectionList });
-			await wrapper.setProps({ selectedList: [] });
+			const wrapper = mountComponent({
+				props: {
+					selectionList: mockProps.selectionList,
+					selectedList: [],
+				},
+			});
 
 			const actionButtonComponent = wrapper.getComponent({
 				name: "FilterActionButtons",
