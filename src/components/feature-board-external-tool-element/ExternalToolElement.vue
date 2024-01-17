08985b6a0edc844f99772cd9d534ccc6c0460735
Igor Richter <igor.richter@capgemini.com>
2024-01-17 12:22:46 +0100

    use content element bar - todo: icon

--- src/components/feature-board-external-tool-element/ExternalToolElement.vue
+++ src/components/feature-board-external-tool-element/ExternalToolElement.vue
@@ -13,35 +13,27 @@
 		@keydown.up.down="onKeydownArrow"
 		@click="onClickElement"
 	>
-		<div class="card-container d-flex gap-8 grey lighten-4">
-			<div
-				v-if="displayData && displayData.logoUrl"
-				class="logo-container my-auto mr-1"
-			>
-				<v-img
-					height="100%"
-					class="mx-auto"
-					:src="displayData.logoUrl"
-					contain
-				/>
-			</div>
-			<v-icon v-else>{{ mdiPuzzleOutline }}</v-icon>
-			<span class="align-self-center title flex-1 break-word">
+		<ContentElementBar :has-grey-background="true" :icon="getIcon">
+			<template #title>
 				{{
 					hasLinkedTool
 						? toolDisplayName
 						: t("feature-board-external-tool-element.placeholder.selectTool")
 				}}
-			</span>
-			<ExternalToolElementMenu
-				v-if="isEditMode"
-				ref="externalToolElementMenu"
-				@move-down:element="onMoveElementDown"
-				@move-up:element="onMoveElementUp"
-				@delete:element="onDeleteElement"
-				@edit:element="onEditElement"
-			/>
-		</div>
+			</template>
+			<template #menu>
+				<slot>
+					<ExternalToolElementMenu
+						v-if="isEditMode"
+						ref="externalToolElementMenu"
+						@move-down:element="onMoveElementDown"
+						@move-up:element="onMoveElementUp"
+						@delete:element="onDeleteElement"
+						@edit:element="onEditElement"
+					/>
+				</slot>
+			</template>
+		</ContentElementBar>
 		<ExternalToolElementAlert
 			:toolDisplayName="toolDisplayName"
 			:error="error"
@@ -85,9 +77,11 @@ import ExternalToolElementAlert from "./ExternalToolElementAlert.vue";
 import ExternalToolElementConfigurationDialog from "./ExternalToolElementConfigurationDialog.vue";
 import ExternalToolElementMenu from "./ExternalToolElementMenu.vue";
 import { ContextExternalToolConfigurationStatus } from "@/store/external-tool";
+import ContentElementBar from "@ui-board/content-element/ContentElementBar.vue";
 
 export default defineComponent({
 	components: {
+		ContentElementBar,
 		ExternalToolElementAlert,
 		ExternalToolElementConfigurationDialog,
 		ExternalToolElementMenu,
@@ -123,6 +117,14 @@ export default defineComponent({
 		const element: Ref<ExternalToolElementResponse> = toRef(props, "element");
 		useBoardFocusHandler(element.value.id, ref(null), () => {
 			autofocus.value = true;
+		});
+
+		const getIcon: ComputedRef<string> = computed(() => {
+			if (displayData.value && displayData.value?.logoUrl) {
+				console.log(displayData.value?.logoUrl);
+				return displayData.value?.logoUrl;
+			}
+			return mdiPuzzleOutline;
 		});
 
 		const { lastCreatedElementId, resetLastCreatedElementId } =
@@ -227,6 +229,7 @@ export default defineComponent({
 
 		return {
 			t,
+			getIcon,
 			hasLinkedTool,
 			toolDisplayName,
 			displayData,
