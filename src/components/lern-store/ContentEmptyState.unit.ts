e848ede4dae1462cfc76e000f8a0d1de901aaf5c
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-01-30 09:51:47 +0100

    BC-6339 - fix ContentEmptyState

--- src/components/lern-store/ContentEmptyState.unit.ts
+++ src/components/lern-store/ContentEmptyState.unit.ts
@@ -1,21 +1,56 @@
-import ContentEmptyState from "./ContentEmptyState";
+import vCustomEmptyState from "@/components/molecules/vCustomEmptyState.vue";
+import {
+	createTestingI18n,
+	createTestingVuetify,
+} from "@@/tests/test-utils/setup";
+import { shallowMount } from "@vue/test-utils";
+import ContentEmptyState from "./ContentEmptyState.vue";
 
 describe("@/components/molecules/ContentEmptyState", () => {
-	const wrapper = mount(ContentEmptyState, {
-		...createComponentMocks({ i18n: true }),
+	const wrapper = shallowMount(ContentEmptyState, {
+		global: {
+			plugins: [createTestingVuetify(), createTestingI18n()],
+		},
 		data: () => ({}),
 	});
 
-	it("Renders an image", () => {
-		expect(wrapper.find(".img-container").exists()).toBe(true);
+	it("Renders an vCustomEmptyState", () => {
+		const vCustomEmptyStateElement = wrapper.findComponent(vCustomEmptyState);
+
+		expect(vCustomEmptyStateElement.exists()).toBe(true);
 	});
-	it("Provides proper title", () => {
-		expect(wrapper.find(".title").exists()).toBe(true);
-		expect(wrapper.find(".title").text()).toBe(
-			wrapper.vm.$i18n.t("pages.content.empty_state.error.title")
+
+	it("Provides prop title", () => {
+		const vCustomEmptyStateElement = wrapper.findComponent(vCustomEmptyState);
+
+		expect(vCustomEmptyStateElement.props().title).toBe(
+			"pages.content.empty_state.error.title"
 		);
 	});
-	it("Provides proper message", () => {
-		expect(wrapper.find(".description").exists()).toBe(true);
+
+	it("Provides prop image", () => {
+		const vCustomEmptyStateElement = wrapper.findComponent(vCustomEmptyState);
+
+		expect(vCustomEmptyStateElement.props().image).toBe("content-empty");
+	});
+
+	it("Provides prop message", () => {
+		const vCustomEmptyStateElement = wrapper.findComponent(vCustomEmptyState);
+
+		const renderHTMLElement =
+			vCustomEmptyStateElement.vm.$slots.description()[0];
+
+		expect(renderHTMLElement.props.html).toBe(
+			"pages.content.empty_state.error.message"
+		);
+	});
+
+	it("Provides prop component", () => {
+		const vCustomEmptyStateElement = wrapper.findComponent(vCustomEmptyState);
+
+		const renderHTMLElement =
+			vCustomEmptyStateElement.vm.$slots.description()[0];
+
+		expect(renderHTMLElement.props.component).toBe("span");
 	});
 });
