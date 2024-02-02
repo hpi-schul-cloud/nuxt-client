6eb9d2f7adcf3059594763997f5853382915a9fa
Oliver Happe <oliver.happe@dataport.de>
2024-02-02 15:33:56 +0100

    remove debug code

--- src/components/util-device-detection/VuetifyBreakpoints.composable.ts
+++ src/components/util-device-detection/VuetifyBreakpoints.composable.ts
@@ -1,5 +1,3 @@
 import { breakpointsVuetify, useBreakpoints } from "@vueuse/core";
 
 export const useVuetifyBreakpoints = () => useBreakpoints(breakpointsVuetify);
-
-useVuetifyBreakpoints().smallerOrEqual("sm");
