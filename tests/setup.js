2ef23fb7d7d64055dbabaffd85f52b344e963bd4
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2024-02-02 16:02:34 +0100

    add window.matchMedia for JSDOM

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,4 @@
+import { jest } from "@jest/globals";
 import { config, mount, shallowMount } from "@vue/test-utils";
 
 // VUE3_UPGRADE enable rendering of default slot on stubbed components
@@ -6,3 +7,22 @@ config.global.renderStubDefaultSlot = true;
 
 global.mount = mount;
 global.shallowMount = shallowMount;
+
+/**
+ * matchMedia is used by useBreakpoints from VueUse to distinguish breakpoints.
+ * We need to mock it to run tests that rely on useBreakpoints
+ * @see https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+ */
+Object.defineProperty(window, "matchMedia", {
+	writable: true,
+	value: jest.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	})),
+});
