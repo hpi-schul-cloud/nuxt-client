4fab9eab470b039bfd8d720640f7429d10cfb285
Oliver Happe <oliver.happe@dataport.de>
2024-02-14 13:23:19 +0100

    deleted unused test setups related to i18n

--- tests/test-utils/i18nMock.ts
+++ tests/test-utils/i18nMock.ts
@@ -1,8 +1,11 @@
-import { I18nKeys } from "@/plugins/i18n";
+import { I18nLanguage } from "@/plugins/i18n";
 import { createMock } from "@golevelup/ts-jest";
 
 export const i18nMock = createMock<{
-	t: (key: keyof I18nKeys, placeholders?: Record<string, unknown>) => string;
+	t: (
+		key: keyof I18nLanguage,
+		placeholders?: Record<string, unknown>
+	) => string;
 }>({
 	t: (key, placeholders) => {
 		const str = `${key} ${
