97440a98468451a70834ac5003779e25b0812bc1
Artemida <Artemida>
2024-02-29 10:19:52 +0100

    Fix lint

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -1539,8 +1539,7 @@ export default {
 	"tldraw.error.ws.4400":
 		"Falta el nombre de la sala en los parámetros de la URL",
 	"tldraw.error.ws.4401": "Usted no tiene permiso para esta pizarra",
-	"tldraw.error.ws.4404":
-		"No se ha encontrado la pizarra con este nombre",
+	"tldraw.error.ws.4404": "Nose ha encontrado la pizarra con este nombre",
 	"tldraw.error.ws.4500":
 		"Se ha producido un error en la conexión tldraw websocket",
 	"ui-confirmation-dialog.ask-delete": "¿Eliminar {type} {title}?",
