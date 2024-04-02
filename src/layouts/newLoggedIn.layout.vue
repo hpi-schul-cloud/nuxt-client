bcaadbbbc23e6bbb71f9712f7fe2f5d5cfb89eb1
Odalys Adam <odalys.adam@dataport.de>
2024-04-02 14:43:02 +0200

    testing layout with all vue pages

--- src/layouts/newLoggedIn.layout.vue
+++ src/layouts/newLoggedIn.layout.vue
@@ -1,26 +1,34 @@
 <template>
-	<Sidebar v-model="sidebar" />
-	<v-app-bar title="Application bar">
-		<template v-slot:prepend>
-			<v-app-bar-nav-icon @click="onClick" />
-		</template>
-	</v-app-bar>
+	<div>
+		<SkipLinks />
+		<Sidebar v-model="sidebar" />
+		<v-app-bar title="Application bar">
+			<template v-slot:prepend>
+				<v-app-bar-nav-icon @click="onClick" />
+			</template>
+		</v-app-bar>
 
-	<v-main id="main-content" class="content">
-		<application-error-wrapper>
-			<snackbar />
-			<router-view />
-		</application-error-wrapper>
-	</v-main>
-	<loading-state-dialog />
+		<v-main id="main-content" class="content">
+			<application-error-wrapper>
+				<snackbar />
+				<router-view />
+			</application-error-wrapper>
+		</v-main>
+		<loading-state-dialog />
+		<keep-alive>
+			<autoLogoutWarning />
+		</keep-alive>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import SkipLinks from "@/components/molecules/SkipLinks.vue";
 import Sidebar from "@/components/feature-layout/Sidebar.vue";
 import Snackbar from "@/components/molecules/Alert.vue";
 import LoadingStateDialog from "@/components/molecules/LoadingStateDialog.vue";
 import ApplicationErrorWrapper from "@/components/molecules/ApplicationErrorWrapper.vue";
+import autoLogoutWarning from "@/components/organisms/AutoLogoutWarning.vue";
 
 const sidebar = ref(true);
 
