9c35d86ebdeac5e85710aba5f99e9b0cdbf46852
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-04-15 15:07:46 +0200

    connect to BoardCollaboration -app

--- src/modules/data/board/socket/socketApi.ts
+++ src/modules/data/board/socket/socketApi.ts
@@ -1,7 +1,28 @@
+import { io } from "socket.io-client";
 import { Action } from "@/types/board/ActionFactory";
 
 export const useBoardSocketApi = (dispatch: (action: Action) => void) => {
 	// implement socket.io here
+	const socket = io(
+		// "https://bc-6683-poc-board-collaboration-server.dbc.dbildungscloud.dev",
+		"ws://localhost:4450",
+		{
+			path: "/collaboration",
+			withCredentials: true,
+		}
+
+		// { path: "/collaboration", transports: ["polling"] }
+	);
+	socket.on("connect", function () {
+		console.log("connected");
+	});
+	socket.onAny((event, ...args) => {
+		dispatch({ type: event, payload: args[0] });
+	});
+
+	socket.on("disconnect", () => {
+		// TODO reconnect?
+	});
 
 	const emitOnSocket = (action: string, data: any) => {
 		console.log("dispatching", dispatch);
