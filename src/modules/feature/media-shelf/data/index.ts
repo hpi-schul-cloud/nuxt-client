42ba61a5df4de27ca2295399ff03324629a551bb
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-04-17 14:56:16 +0200

    fix index file

--- src/modules/feature/media-shelf/data/index.ts
+++ src/modules/feature/media-shelf/data/index.ts
@@ -2,5 +2,3 @@ export * from "./types";
 export * from "./static-data";
 export * from "./mediaBoardApi.composable";
 export * from "./mediaBoardState.composable";
-export * from "./mediaBoardApi.composable.unit";
-export * from "./mediaBoardState.composable.unit";
