980e35f4cca6595a7ec5e603221957928c76de69
Malte Berg <malte.berg@capgemini.com>
2024-04-19 08:44:22 +0200

    fix text

--- src/composables/user-login-migration-mappings.composable.unit.ts
+++ src/composables/user-login-migration-mappings.composable.unit.ts
@@ -2,91 +2,136 @@ import { useUserLoginMigrationMappings } from "./user-login-migration-mappings.c
 import { BusinessError } from "@/store/types/commons";
 
 describe("useUserLoginMigrationMapping", () => {
-	const setup = () => {
-		const { getBusinessErrorTranslationKey } = useUserLoginMigrationMappings();
-
-		return {
-			getBusinessErrorTranslationKey,
-		};
-	};
-
 	describe("getBusinessErrorTranslationKey", () => {
-		it("should return undefined when error is undefined", () => {
-			const { getBusinessErrorTranslationKey } = setup();
+		describe("when error type was found", () => {
+			const setup = () => {
+				const { getBusinessErrorTranslationKey } =
+					useUserLoginMigrationMappings();
 
-			const translationKey: string | undefined =
-				getBusinessErrorTranslationKey(undefined);
-			expect(translationKey).toBeUndefined();
+				const key =
+					"pages.administration.migration.moin_schule_system_not_found";
+				const apiError = {
+					message: "Cannot find moin.schule system",
+					code: 500,
+					title: "moin.schule system not found",
+					type: "MOIN_SCHULE_SYSTEM_NOT_FOUND",
+				};
+				const businessError: BusinessError = {
+					statusCode: apiError.code,
+					message: apiError.message,
+					error: apiError,
+				};
+
+				return { getBusinessErrorTranslationKey, businessError, key };
+			};
+
+			it("should return translation key", () => {
+				const { getBusinessErrorTranslationKey, businessError, key } = setup();
+
+				const translationKey: string | undefined =
+					getBusinessErrorTranslationKey(businessError);
+				expect(translationKey).toEqual(key);
+			});
 		});
 
-		it("should return translation key when message was found", () => {
-			const { getBusinessErrorTranslationKey } = setup();
-			const apiError = {
-				message: "Cannot find moin.schule system",
-				code: 500,
-				title: "moin.schule system not found",
-				type: "MOIN_SCHULE_SYSTEM_NOT_FOUND",
-			};
-			const error: BusinessError = {
-				statusCode: apiError.code,
-				message: apiError.message,
-				error: apiError,
+		describe("when translation key was not found", () => {
+			const setup = () => {
+				const { getBusinessErrorTranslationKey } =
+					useUserLoginMigrationMappings();
+
+				const apiError = {
+					message: "message",
+					code: 500,
+					title: "title",
+					type: "some_key",
+				};
+				const businessError: BusinessError = {
+					statusCode: apiError.code,
+					message: apiError.message,
+					error: apiError,
+				};
+
+				return { getBusinessErrorTranslationKey, businessError };
 			};
 
-			const translationKey: string | undefined =
-				getBusinessErrorTranslationKey(error);
-			expect(translationKey).toEqual(
-				"pages.administration.migration.moin_schule_system_not_found"
-			);
+			it("should return original error message", () => {
+				const { getBusinessErrorTranslationKey, businessError } = setup();
+
+				const translationKey: string | undefined =
+					getBusinessErrorTranslationKey(businessError);
+				expect(translationKey).toEqual(businessError.message);
+			});
 		});
 
-		it("should return original message when key was not found", () => {
-			const { getBusinessErrorTranslationKey } = setup();
-			const apiError = {
-				message: "message",
-				code: 500,
-				title: "title",
-				type: "some_key",
-			};
-			const error: BusinessError = {
-				statusCode: apiError.code,
-				message: apiError.message,
-				error: apiError,
+		describe("when businessError.error is undefined", () => {
+			const setup = () => {
+				const { getBusinessErrorTranslationKey } =
+					useUserLoginMigrationMappings();
+
+				const businessError: BusinessError = {
+					statusCode: 500,
+					message: "original message",
+					error: undefined,
+				};
+
+				return { getBusinessErrorTranslationKey, businessError };
 			};
 
-			const translationKey: string | undefined =
-				getBusinessErrorTranslationKey(error);
-			expect(translationKey).toEqual(error.message);
+			it("should return original error message", () => {
+				const { getBusinessErrorTranslationKey, businessError } = setup();
+
+				const translationKey: string | undefined =
+					getBusinessErrorTranslationKey(businessError);
+
+				expect(translationKey).toEqual(businessError.message);
+			});
 		});
 
-		it("should return original message when error.type doesn't exists", () => {
-			const { getBusinessErrorTranslationKey } = setup();
-			const apiError = {
-				message: "message",
-				code: 500,
-				title: "title",
-			};
-			const error: BusinessError = {
-				statusCode: apiError.code,
-				message: apiError.message,
-				error: apiError,
+		describe("when error.type doesn't exist", () => {
+			const setup = () => {
+				const { getBusinessErrorTranslationKey } =
+					useUserLoginMigrationMappings();
+
+				const apiError = {
+					message: "message",
+					code: 500,
+					title: "title",
+				};
+				const businessError: BusinessError = {
+					statusCode: apiError.code,
+					message: apiError.message,
+					error: apiError,
+				};
+
+				return { getBusinessErrorTranslationKey, businessError };
 			};
 
-			const translationKey: string | undefined =
-				getBusinessErrorTranslationKey(error);
-			expect(translationKey).toEqual(error.message);
+			it("should return original error message", () => {
+				const { getBusinessErrorTranslationKey, businessError } = setup();
+
+				const translationKey: string | undefined =
+					getBusinessErrorTranslationKey(businessError);
+				expect(translationKey).toEqual(businessError.message);
+			});
 		});
 
-		it("should return original message when parameter error is undefined", () => {
-			const { getBusinessErrorTranslationKey } = setup();
-			const error: BusinessError = {
-				statusCode: 500,
-				message: "original message",
+		describe("when businessError is undefined", () => {
+			const setup = () => {
+				const { getBusinessErrorTranslationKey } =
+					useUserLoginMigrationMappings();
+
+				return {
+					getBusinessErrorTranslationKey,
+				};
 			};
 
-			const translationKey: string | undefined =
-				getBusinessErrorTranslationKey(error);
-			expect(translationKey).toEqual(error.message);
+			it("should return undefined", () => {
+				const { getBusinessErrorTranslationKey } = setup();
+
+				const translationKey: string | undefined =
+					getBusinessErrorTranslationKey(undefined);
+				expect(translationKey).toBeUndefined();
+			});
 		});
 	});
 });
