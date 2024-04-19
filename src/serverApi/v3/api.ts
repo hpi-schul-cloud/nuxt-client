2f335e31290e0f6c624368dc92a8b374fd072193
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-04-19 09:51:59 +0200

    generate api

--- src/serverApi/v3/api.ts
+++ src/serverApi/v3/api.ts
@@ -3556,10 +3556,10 @@ export interface LoginResponse {
 export interface Lti11ToolConfigCreateParams {
     /**
      * 
-     * @type {string}
+     * @type {ToolConfigType}
      * @memberof Lti11ToolConfigCreateParams
      */
-    type: string;
+    type: ToolConfigType;
     /**
      * 
      * @type {string}
@@ -3580,16 +3580,16 @@ export interface Lti11ToolConfigCreateParams {
     secret: string;
     /**
      * 
-     * @type {string}
+     * @type {LtiMessageType}
      * @memberof Lti11ToolConfigCreateParams
      */
-    lti_message_type: string;
+    lti_message_type: LtiMessageType;
     /**
      * 
-     * @type {string}
+     * @type {LtiPrivacyPermission}
      * @memberof Lti11ToolConfigCreateParams
      */
-    privacy_permission: string;
+    privacy_permission: LtiPrivacyPermission;
     /**
      * 
      * @type {string}
@@ -3605,10 +3605,10 @@ export interface Lti11ToolConfigCreateParams {
 export interface Lti11ToolConfigUpdateParams {
     /**
      * 
-     * @type {string}
+     * @type {ToolConfigType}
      * @memberof Lti11ToolConfigUpdateParams
      */
-    type: string;
+    type: ToolConfigType;
     /**
      * 
      * @type {string}
@@ -3629,16 +3629,16 @@ export interface Lti11ToolConfigUpdateParams {
     secret?: string;
     /**
      * 
-     * @type {string}
+     * @type {LtiMessageType}
      * @memberof Lti11ToolConfigUpdateParams
      */
-    lti_message_type: string;
+    lti_message_type: LtiMessageType;
     /**
      * 
-     * @type {string}
+     * @type {LtiPrivacyPermission}
      * @memberof Lti11ToolConfigUpdateParams
      */
-    privacy_permission: string;
+    privacy_permission: LtiPrivacyPermission;
     /**
      * 
      * @type {string}
@@ -3646,6 +3646,28 @@ export interface Lti11ToolConfigUpdateParams {
      */
     launch_presentation_locale: string;
 }
+/**
+ * 
+ * @export
+ * @enum {string}
+ */
+export enum LtiMessageType {
+    BasicLtiLaunchRequest = 'basic-lti-launch-request'
+}
+
+/**
+ * 
+ * @export
+ * @enum {string}
+ */
+export enum LtiPrivacyPermission {
+    Anonymous = 'anonymous',
+    EMail = 'e-mail',
+    Name = 'name',
+    Public = 'public',
+    Pseudonymous = 'pseudonymous'
+}
+
 /**
  * 
  * @export
@@ -6826,6 +6848,17 @@ export interface TimestampsResponse {
  */
 export enum Timezone {
     EuropeBerlin = 'Europe/Berlin'
+}
+
+/**
+ * 
+ * @export
+ * @enum {string}
+ */
+export enum ToolConfigType {
+    Basic = 'basic',
+    Oauth2 = 'oauth2',
+    Lti11 = 'lti11'
 }
 
 /**
