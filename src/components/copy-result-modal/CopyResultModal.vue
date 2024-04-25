db3e059dfc55c2b793bdf9967880c6fb994fe8f2
Artemida <Artemida>
2024-04-25 13:19:50 +0200

    Add whiteboard

--- src/components/copy-result-modal/CopyResultModal.vue
+++ src/components/copy-result-modal/CopyResultModal.vue
@@ -156,6 +156,12 @@ export default {
 				CopyApiResponseTypeEnum.LessonContentNexboard
 			);
 		},
+		hasWhiteboardElement() {
+			return this.hasElementOfType(
+				this.items,
+				CopyApiResponseTypeEnum.DrawingElement
+			);
+		},
 		hasDrawingElement() {
 			return this.isCourse && copyModule.getHasDrawingChild;
 		},
