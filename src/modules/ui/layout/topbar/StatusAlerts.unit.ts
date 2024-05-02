4820915ccd8c5c29427c1539eacc5f33f82b21f4
Odalys Adam <odalys.adam@dataport.de>
2024-05-02 18:47:22 +0200

    adjust statusalerts tests

---
+++ src/modules/ui/layout/topbar/StatusAlerts.unit.ts
@@ -0,0 +1,69 @@
+import { mount } from "@vue/test-utils";
+import StatusAlerts from "./StatusAlerts.vue";
+import { mockStatusAlerts } from "@@/tests/test-utils/mockStatusAlerts";
+import {
+	createTestingI18n,
+	createTestingVuetify,
+} from "@@/tests/test-utils/setup";
+
+const testProps = {
+	statusAlerts: mockStatusAlerts,
+};
+
+describe("@ui-layout/StatusAlerts", () => {
+	const setup = () => {
+		const wrapper = mount(StatusAlerts, {
+			global: {
+				plugins: [createTestingVuetify(), createTestingI18n()],
+			},
+			props: testProps,
+		});
+
+		return { wrapper };
+	};
+
+	it("should not render status alert items, when none are given", () => {
+		const { wrapper } = setup();
+
+		expect(wrapper.findAll("v-list-item")).toHaveLength(0);
+	});
+
+	it("should show alert title", () => {
+		const { wrapper } = setup();
+		const title = wrapper.find("[data-testid='alert-title-0']");
+		expect(title.element.textContent).toContain(mockStatusAlerts[0].title);
+	});
+
+	it("should show alert text", () => {
+		const { wrapper } = setup();
+
+		const title = wrapper.find("[data-testid='alert-text-0']");
+
+		expect(title.element.textContent).toContain(mockStatusAlerts[0].text);
+	});
+
+	it("should show multiple alerts", () => {
+		const { wrapper } = setup();
+
+		const title0 = wrapper.find("[data-testid='alert-title-0']");
+		expect(title0.element.textContent).toContain(mockStatusAlerts[0].title);
+
+		const title1 = wrapper.find("[data-testid='alert-title-1']");
+		expect(title1.element.textContent).toContain(mockStatusAlerts[1].title);
+
+		const title2 = wrapper.find("[data-testid='alert-title-2']");
+		expect(title2.element.textContent).toContain(mockStatusAlerts[2].title);
+	});
+
+	it("should show formatted timestamp", () => {
+		const { wrapper } = setup();
+
+		const updatedText = wrapper
+			.findAllComponents({
+				name: "VListItemSubtitle",
+			})[1]
+			.html();
+
+		expect(updatedText).toMatch("05.05.2023");
+	});
+});
