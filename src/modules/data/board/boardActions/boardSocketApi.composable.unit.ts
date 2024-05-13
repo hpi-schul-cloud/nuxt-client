64dccc81794df0b1c79cffa900ad88a4e670108a
NFriedo <69233063+NFriedo@users.noreply.github.com>
2024-05-13 16:46:20 +0200

    add tests for dispatch function board socket api

--- src/modules/data/board/boardActions/boardSocketApi.composable.unit.ts
+++ src/modules/data/board/boardActions/boardSocketApi.composable.unit.ts
@@ -5,6 +5,32 @@ import { CardMove } from "@/types/board/DragAndDrop";
 import { useBoardRestApi } from "./boardRestApi.composable";
 import { useBoardNotifier } from "@util-board";
 import { useI18n } from "vue-i18n";
+import {
+	CreateCardFailurePayload,
+	CreateColumnFailurePayload,
+	DeleteColumnFailurePayload,
+	DisconnectSocketRequestPayload,
+	MoveCardFailurePayload,
+	MoveColumnFailurePayload,
+	UpdateBoardTitleFailurePayload,
+	UpdateBoardVisibilityFailurePayload,
+	UpdateColumnTitleFailurePayload,
+} from "./boardActionPayload";
+import { setActivePinia } from "pinia";
+import { createTestingPinia } from "@pinia/testing";
+import * as BoardActions from "./boardActions";
+import {
+	boardResponseFactory,
+	cardResponseFactory,
+	columnResponseFactory,
+	mockedPiniaStoreTyping,
+} from "@@/tests/test-utils";
+import { useErrorHandler } from "@/components/error-handling/ErrorHandler.composable";
+
+import {
+	DeleteCardFailurePayload,
+	DeleteCardRequestPayload,
+} from "../cardActions/cardActionPayload";
 
 jest.mock("../Board.store");
 const mockedUseBoardStore = jest.mocked(useBoardStore);
@@ -21,6 +47,9 @@ jest.mock("vue-i18n");
 jest.mock("@util-board");
 const mockedUseBoardNotifier = jest.mocked(useBoardNotifier);
 
+jest.mock("@/components/error-handling/ErrorHandler.composable");
+const mockedUseErrorHandler = jest.mocked(useErrorHandler);
+
 describe("useBoardSocketApi", () => {
 	let mockedSocketConnectionHandler: DeepMocked<
 		ReturnType<typeof useSocketConnection>
@@ -28,6 +57,7 @@ describe("useBoardSocketApi", () => {
 	let mockedBoardRestApiHandler: DeepMocked<ReturnType<typeof useBoardRestApi>>;
 	let mockedBoardStore: DeepMocked<ReturnType<typeof useBoardStore>>;
 	let mockedBoardNotifierCalls: DeepMocked<ReturnType<typeof useBoardNotifier>>;
+	let mockedErrorHandler: DeepMocked<ReturnType<typeof useErrorHandler>>;
 
 	beforeEach(() => {
 		mockedSocketConnectionHandler =
@@ -41,6 +71,9 @@ describe("useBoardSocketApi", () => {
 		mockedBoardStore = createMock<ReturnType<typeof useBoardStore>>();
 		mockedUseBoardStore.mockReturnValue(mockedBoardStore);
 
+		mockedErrorHandler = createMock<ReturnType<typeof useErrorHandler>>();
+		mockedUseErrorHandler.mockReturnValue(mockedErrorHandler);
+
 		mockedBoardNotifierCalls =
 			createMock<ReturnType<typeof useBoardNotifier>>();
 		mockedUseBoardNotifier.mockReturnValue(mockedBoardNotifierCalls);
@@ -48,6 +81,305 @@ describe("useBoardSocketApi", () => {
 
 	it("should be defined", () => {
 		expect(useBoardSocketApi).toBeDefined();
+	});
+
+	describe("dispatch", () => {
+		beforeEach(() => {
+			setActivePinia(createTestingPinia({}));
+		});
+
+		it("should call disconnectSocket for corresponding action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			dispatch(BoardActions.disconnectSocket({}));
+
+			expect(mockedSocketConnectionHandler.disconnectSocket).toHaveBeenCalled();
+		});
+
+		it("should call createCardSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				newCard: cardResponseFactory.build(),
+				columnId: "columnId",
+			};
+			dispatch(BoardActions.createCardSuccess(payload));
+
+			expect(boardStore.createCardSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call createColumnSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				newColumn: columnResponseFactory.build(),
+			};
+			dispatch(BoardActions.createColumnSuccess(payload));
+
+			expect(boardStore.createColumnSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it.skip("should call deleteCardSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: DeleteCardRequestPayload = {
+				cardId: "cardId",
+			};
+
+			// dispatch(CardActions.deleteCardSuccess(payload));
+
+			expect(boardStore.deleteCardSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call deleteColumnSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				columnId: "columnId",
+			};
+			dispatch(BoardActions.deleteColumnSuccess(payload));
+
+			expect(boardStore.deleteColumnSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call moveCardSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				cardId: "cardId",
+				oldIndex: 0,
+				newIndex: 0,
+				fromColumnId: "fromColumnId",
+			};
+
+			dispatch(BoardActions.moveCardSuccess(payload));
+
+			expect(boardStore.moveCardSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call moveColumnSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				columnMove: { addedIndex: 1, columnId: "testColumnId" },
+				byKeyboard: false,
+			};
+
+			dispatch(BoardActions.moveColumnSuccess(payload));
+
+			expect(boardStore.moveColumnSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call fetchBoardSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const board = boardResponseFactory.build();
+
+			dispatch(BoardActions.fetchBoardSuccess(board));
+
+			expect(boardStore.fetchBoardSuccess).toHaveBeenCalledWith(board);
+		});
+
+		it("should call updateColumnTitleSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				columnId: "cardId",
+				newTitle: "newTitle",
+			};
+			dispatch(BoardActions.updateColumnTitleSuccess(payload));
+
+			expect(boardStore.updateColumnTitleSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call updateBoardTitleSuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				boardId: "cardId",
+				newTitle: "newTitle",
+			};
+			dispatch(BoardActions.updateBoardTitleSuccess(payload));
+
+			expect(boardStore.updateBoardTitleSuccess).toHaveBeenCalledWith(payload);
+		});
+
+		it("should call updateBoardVisibilitySuccess for corresponding action", () => {
+			const boardStore = mockedPiniaStoreTyping(useBoardStore);
+			const { dispatch } = useBoardSocketApi();
+
+			const payload = {
+				boardId: "cardId",
+				isVisible: true,
+			};
+			dispatch(BoardActions.updateBoardVisibilitySuccess(payload));
+
+			expect(boardStore.updateBoardVisibilitySuccess).toHaveBeenCalledWith(
+				payload
+			);
+		});
+
+		it("should call notifySocketError for createCard action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: CreateCardFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardCard",
+			};
+			dispatch(BoardActions.createCardFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for createColumnFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: CreateColumnFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardColumn",
+			};
+			dispatch(BoardActions.createColumnFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it.skip("should call notifySocketError for deleteCardFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: DeleteCardFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardCard",
+			};
+			// dispatch(CardActions.deleteCardFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for deleteColumnFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: DeleteColumnFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardColumn",
+			};
+
+			dispatch(BoardActions.deleteColumnFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for moveCardFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: MoveCardFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardCard",
+			};
+
+			dispatch(BoardActions.moveCardFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for moveColumnFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: MoveColumnFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardColumn",
+			};
+
+			dispatch(BoardActions.moveColumnFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for updateColumnTitleFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: UpdateColumnTitleFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardColumn",
+			};
+
+			dispatch(BoardActions.updateColumnTitleFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for updateBoardTitleFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: UpdateBoardTitleFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "boardColumn",
+			};
+
+			dispatch(BoardActions.updateBoardTitleFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+
+		it("should call notifySocketError for updateBoardVisibilityFailure action", () => {
+			const { dispatch } = useBoardSocketApi();
+
+			const payload: UpdateBoardVisibilityFailurePayload = {
+				errorType: "notUpdated",
+				boardObjectType: "board",
+			};
+
+			dispatch(BoardActions.updateBoardVisibilityFailure(payload));
+
+			expect(mockedErrorHandler.notifySocketError).toHaveBeenCalledWith(
+				payload.errorType,
+				payload.boardObjectType
+			);
+		});
+	});
+
+	describe("disconnectSocketRequest", () => {
+		const payload: DisconnectSocketRequestPayload = {};
+
+		it("should call disconnectSocket", () => {
+			const { disconnectSocketRequest } = useBoardSocketApi();
+
+			disconnectSocketRequest(payload);
+
+			expect(mockedSocketConnectionHandler.disconnectSocket).toHaveBeenCalled();
+		});
 	});
 
 	describe("createCardRequest", () => {
