a54603e1fa1f121c8d8249e21552e52c186debc2
MartinSchuhmacher <mschuhmacher@gmx.de>
2024-05-16 14:53:46 +0200

    add bottom padding for long sidebar

--- src/modules/ui/layout/sidebar/Sidebar.vue
+++ src/modules/ui/layout/sidebar/Sidebar.vue
@@ -28,7 +28,7 @@
 				/>
 			</div>
 		</VList>
-		<div data-testid="instanz-sidebar" class="ml-4 text-disabled">
+		<div data-testid="instanz-sidebar" class="ml-4 pb-3 text-disabled">
 			&#169; {{ currentYear }} {{ theme.name }}
 		</div>
 	</VNavigationDrawer>
