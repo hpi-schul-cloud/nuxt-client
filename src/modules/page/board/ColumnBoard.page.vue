4a67292e60a31e1a470d1569a5e0c8426c21c74e
Odalys Adam <odalys.adam@dataport.de>
2024-05-22 15:57:48 +0200

    ColumnBoard page in script setup

--- src/modules/page/board/ColumnBoard.page.vue
+++ src/modules/page/board/ColumnBoard.page.vue
@@ -2,29 +2,19 @@
 	<Board :board-id="boardId" :breadcrumbs="breadcrumbs" />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { useSharedBoardPageInformation } from "@data-board";
 import { Board } from "@feature-board";
 import { useTitle } from "@vueuse/core";
-import { defineComponent } from "vue";
 
-export default defineComponent({
-	name: "ColumnBoardPage",
-	components: { Board },
-	props: {
-		boardId: {
-			type: String,
-			required: true,
-		},
-	},
-	setup() {
-		const { breadcrumbs, pageTitle } = useSharedBoardPageInformation();
-
-		useTitle(pageTitle);
-
-		return {
-			breadcrumbs,
-		};
+defineProps({
+	boardId: {
+		type: String,
+		required: true,
 	},
 });
+
+const { breadcrumbs, pageTitle } = useSharedBoardPageInformation();
+
+useTitle(pageTitle);
 </script>
