f8c390e567c8d95487818699e774de257b16abb0
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-05-23 14:17:41 +0200

    chore: fix boardRestApi unit test

--- src/modules/data/board/boardActions/boardRestApi.composable.unit.ts
+++ src/modules/data/board/boardActions/boardRestApi.composable.unit.ts
@@ -408,7 +408,7 @@ describe("boardRestApi", () => {
 				expect(boardStore.moveCardSuccess).toHaveBeenCalledWith({
 					...cardPayload,
 					toColumnId: newColumn.id,
-					toColumnIndex: 2,
+					toColumnIndex: 1,
 				});
 			});
 
