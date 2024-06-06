4e5d9afa51f908c87c5b417b300d4c6feaad60ae
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-06-06 17:01:23 +0200

    add debouncer into ariaLiveNotifier file

--- src/composables/ariaLiveNotifier.ts
+++ src/composables/ariaLiveNotifier.ts
@@ -1,24 +1,21 @@
-export const useAriaLive = () => {
-	const notifyOnScreenReader = (
-		message: string,
-		importance: "off" | "polite" | "assertive"
-	) => {
-		// should be a div with aria-live="polite | assertive" attribute
-		// and should be appended to the upper level of the DOM tree
-		// the aria-live attribute should be set polite or assertive based on the importance of the message
-		const element = document.getElementById("notify-on-screen-reader");
-		if (!element) return;
+import { useDebounceFn } from "@vueuse/core";
 
-		element.setAttribute("aria-live", importance);
-		element.innerHTML = message;
+export const useAriaLiveNotifier = () => {
+	const notifyOnScreenReader = useDebounceFn(
+		(message: string, importance: "off" | "polite" | "assertive") => {
+			// should be a div with aria-live="polite | assertive" attribute
+			// and should be appended to the upper level of the DOM tree
+			// the aria-live attribute should be set polite or assertive based on the importance of the message
+			const element = document.getElementById("notify-on-screen-reader");
+			if (!element) return;
 
-		// should have a setTimeout to remove the message after a few seconds
-		// that is because if the same message is repeated, the screen reader will not reread it.
-		setTimeout(() => {
 			element.innerHTML = "";
-			element.setAttribute("aria-live", "off");
-		}, 10000);
-	};
+			element.setAttribute("aria-live", importance);
+			element.innerHTML = message;
+		},
+		1000,
+		{ maxWait: 2000 }
+	);
 
 	return {
 		notifyOnScreenReader,
