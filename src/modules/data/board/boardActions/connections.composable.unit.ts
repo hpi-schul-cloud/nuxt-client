a15674e0f6514377b613edfd680a747112a1cc21
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-07-08 08:08:38 +0200

    chore: improve coverage

--- src/modules/data/board/boardActions/connections.composable.unit.ts
+++ src/modules/data/board/boardActions/connections.composable.unit.ts
@@ -16,6 +16,11 @@ const mocks = {
 	boardStore: {
 		fetchBoardRequest: jest.fn(),
 	},
+	useTimeoutFn: {
+		start: jest.fn(),
+		stop: jest.fn(),
+		isPending: false,
+	},
 };
 
 jest.mock("vue-i18n", () => ({
@@ -30,6 +35,17 @@ jest.mock("../Board.store", () => ({
 		fetchBoardRequest: mocks.boardStore.fetchBoardRequest,
 	}),
 }));
+
+jest.mock("@vueuse/core", () => {
+	return {
+		...jest.requireActual("@vueuse/core"),
+		useTimeoutFn: jest.fn().mockReturnValue({
+			start: () => mocks.useTimeoutFn.start,
+			stop: () => mocks.useTimeoutFn.stop,
+			isPending: () => mocks.useTimeoutFn.isPending,
+		}),
+	};
+});
 
 const setup = () => {
 	return mountComposable(() => useConnectionStatus(), {
@@ -61,21 +77,90 @@ describe("connections.composable", () => {
 	});
 
 	describe("reloadBoardAndNotify", () => {
-		it("should call the notifier module with 'info' parameter", () => {
-			const { reloadBoardAndNotify } = setup();
-			connectionOptions.lossSocketConnection = true;
-			reloadBoardAndNotify();
+		describe("when lossSocketConnection is true", () => {
+			it("should call the notifier module with 'info' parameter", () => {
+				const { reloadBoardAndNotify } = setup();
+				connectionOptions.lossSocketConnection = true;
+				reloadBoardAndNotify();
 
-			expect(mocks.notifier.show).toHaveBeenCalled();
-			expect(mocks.notifier.show.mock.calls[0][0].status).toBe("info");
+				expect(mocks.notifier.show).toHaveBeenCalled();
+				expect(mocks.notifier.show.mock.calls[0][0].status).toBe("info");
+			});
+
+			it("should call the boardStore.fetchBoardRequest method", () => {
+				const { reloadBoardAndNotify } = setup();
+				connectionOptions.lossSocketConnection = true;
+				reloadBoardAndNotify();
+
+				expect(mocks.boardStore.fetchBoardRequest).toHaveBeenCalled();
+			});
 		});
 
-		it("should call the boardStore.fetchBoardRequest method", () => {
-			const { reloadBoardAndNotify } = setup();
-			connectionOptions.lossSocketConnection = true;
-			reloadBoardAndNotify();
+		describe("when lossSocketConnection is false", () => {
+			it("should not call notifier module", () => {
+				const { reloadBoardAndNotify } = setup();
+				connectionOptions.lossSocketConnection = false;
+				reloadBoardAndNotify();
 
-			expect(mocks.boardStore.fetchBoardRequest).toHaveBeenCalled();
+				expect(mocks.notifier.show).not.toHaveBeenCalled();
+			});
+
+			it("should not call boardStore.fetchBoardRequest method", () => {
+				const { reloadBoardAndNotify } = setup();
+				connectionOptions.lossSocketConnection = false;
+				reloadBoardAndNotify();
+
+				expect(mocks.boardStore.fetchBoardRequest).not.toHaveBeenCalled();
+			});
+		});
+	});
+
+	describe("window events", () => {
+		describe("offline event", () => {
+			it("should call the notifier module with 'error' parameter", () => {
+				setup();
+				connectionOptions.lossInternetConnection = false;
+				window.dispatchEvent(new Event("offline"));
+
+				expect(connectionOptions.lossInternetConnection).toBe(true);
+				expect(mocks.notifier.show).toHaveBeenCalled();
+				expect(mocks.notifier.show.mock.calls[0][0].status).toBe("error");
+			});
+		});
+
+		describe("online event", () => {
+			it("should call the notifier module with 'info' parameter", () => {
+				setup();
+				connectionOptions.lossInternetConnection = true;
+				window.dispatchEvent(new Event("online"));
+
+				expect(connectionOptions.lossInternetConnection).toBe(false);
+				expect(mocks.notifier.show).toHaveBeenCalled();
+				expect(mocks.notifier.show.mock.calls[0][0].status).toBe("info");
+			});
+		});
+	});
+
+	describe("visibilitychange event", () => {
+		describe("when timeout value is true", () => {
+			it("should call the notifier module with 'info' parameter", () => {
+				setup();
+				connectionOptions.timeout = true;
+				window.dispatchEvent(new Event("visibilitychange"));
+
+				expect(mocks.notifier.show).toHaveBeenCalled();
+				expect(mocks.notifier.show.mock.calls[0][0].status).toBe("info");
+			});
+		});
+
+		describe("when timeout value is false", () => {
+			it("should not call notifier module", () => {
+				setup();
+				connectionOptions.timeout = false;
+				window.dispatchEvent(new Event("visibilitychange"));
+
+				expect(mocks.notifier.show).not.toHaveBeenCalled();
+			});
 		});
 	});
 });
