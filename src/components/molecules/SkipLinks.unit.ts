fd97d1406487845768ef69f4846b00dca4ba637d
wolfganggreschus <wolfgang@greschus.de>
2024-07-10 13:39:41 +0200

    Refactor SkipLinks component

--- src/components/molecules/SkipLinks.unit.ts
+++ src/components/molecules/SkipLinks.unit.ts
@@ -7,39 +7,54 @@ jest.mock("vue-router", () => ({
 }));
 
 describe("@/components/molecules/SkipLinks", () => {
-	const setup = () => {
-		const wrapper = mount(SkipLinks, {
-			global: {
-				plugins: [createTestingI18n()],
-			},
+	describe(" element skip-link", () => {
+		const setup = () => {
+			const wrapper = mount(SkipLinks, {
+				global: {
+					plugins: [createTestingI18n()],
+				},
+			});
+
+			return { wrapper };
+		};
+
+		it("Should render its skip link", async () => {
+			const { wrapper } = setup();
+
+			expect(wrapper.find("#skip-link").exists()).toBe(true);
 		});
-		const mainContent = window.document.createElement("div");
-		mainContent.id = "main-content";
-		const linkElement = window.document.createElement("a");
-		mainContent.appendChild(linkElement);
-		window.document.body.appendChild(mainContent);
 
-		return { wrapper, linkElement };
-	};
+		it("Should have tabindex 0", async () => {
+			const { wrapper } = setup();
 
-	it("Should render its skip link", async () => {
-		const { wrapper } = setup();
-
-		expect(wrapper.find("#skip-link").exists()).toBe(true);
+			expect(wrapper.attributes("tabindex")).toBe("0");
+		});
 	});
+	describe("skipToContent", () => {
+		const setup = () => {
+			const wrapper = mount(SkipLinks, {
+				global: {
+					plugins: [createTestingI18n()],
+				},
+			});
 
-	it("Should have tabindex 0", async () => {
-		const { wrapper } = setup();
+			const mainContent = window.document.createElement("div");
+			mainContent.id = "main-content";
+			const linkElement = window.document.createElement("a");
+			linkElement.setAttribute("tabindex", "0");
+			mainContent.appendChild(linkElement);
+			window.document.body.appendChild(mainContent);
 
-		expect(wrapper.attributes("tabindex")).toBe("0");
-	});
-	it("should skip to the link element in main content", async () => {
-		const { wrapper, linkElement } = setup();
+			return { wrapper, linkElement };
+		};
+		it("should skip to the link element in main content", async () => {
+			const { wrapper, linkElement } = setup();
 
-		await wrapper.find("#skip-link").trigger("click");
+			await wrapper.find("#skip-link").trigger("click");
 
-		await wrapper.vm.$nextTick();
+			await wrapper.vm.$nextTick();
 
-		expect(window.document.activeElement).toBe(linkElement);
+			expect(window.document.activeElement).toBe(linkElement);
+		});
 	});
 });
