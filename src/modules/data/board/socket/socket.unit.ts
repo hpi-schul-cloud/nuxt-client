30d69ee2250b1d130e0859f7a9dd1f965623f005
NFriedo <69233063+NFriedo@users.noreply.github.com>
2024-07-10 11:44:36 +0200

    add tests for connect and disconnect events

--- src/modules/data/board/socket/socket.unit.ts
+++ src/modules/data/board/socket/socket.unit.ts
@@ -1,6 +1,10 @@
 import { envConfigModule } from "@/store";
 import EnvConfigModule from "@/store/env-config";
-import { envsFactory, mockedPiniaStoreTyping } from "@@/tests/test-utils";
+import {
+	boardResponseFactory,
+	envsFactory,
+	mockedPiniaStoreTyping,
+} from "@@/tests/test-utils";
 import setupStores from "@@/tests/test-utils/setupStores";
 import { useBoardStore, useSocketConnection } from "@data-board";
 import { createMock, DeepMocked } from "@golevelup/ts-jest";
@@ -26,7 +30,8 @@ describe("socket.ts", () => {
 	let dispatchMock: jest.Mock;
 	let mockSocket: Partial<socketModule.Socket>;
 	let timeoutResponseMock: { emitWithAck: jest.Mock };
-	let mockedBoardNotifierCalls: DeepMocked<ReturnType<typeof useBoardNotifier>>;
+	let mockBoardNotifierCalls: DeepMocked<ReturnType<typeof useBoardNotifier>>;
+	let socketHandlers: Record<string, () => void> | undefined = undefined;
 
 	beforeAll(() => {
 		timeoutResponseMock = { emitWithAck: jest.fn() };
@@ -40,6 +45,9 @@ describe("socket.ts", () => {
 			timeout: jest.fn().mockReturnValue(timeoutResponseMock),
 		};
 		mockSocketIOClient.io.mockReturnValue(mockSocket as socketModule.Socket);
+
+		mockBoardNotifierCalls = createMock<ReturnType<typeof useBoardNotifier>>();
+		mockUseBoardNotifier.mockReturnValue(mockBoardNotifierCalls);
 	});
 
 	beforeEach(() => {
@@ -51,18 +59,99 @@ describe("socket.ts", () => {
 		});
 		envConfigModule.setEnvs(envs);
 
-		mockedPiniaStoreTyping(useBoardStore);
-
-		mockedBoardNotifierCalls =
-			createMock<ReturnType<typeof useBoardNotifier>>();
-		mockUseBoardNotifier.mockReturnValue(mockedBoardNotifierCalls);
-
 		dispatchMock = jest.fn();
 		mockSocket.connected = true;
 	});
 
 	afterEach(() => {
 		jest.clearAllMocks();
+	});
+
+	const setupBoardStore = (options: { createBoard: boolean }) => {
+		const boardStore = mockedPiniaStoreTyping(useBoardStore);
+		if (options.createBoard) {
+			const board = boardResponseFactory.build();
+			boardStore.board = board;
+		}
+
+		return { boardStore };
+	};
+
+	const getEventCallback = (eventName: string) => {
+		const listener = (mockSocket.on as jest.Mock).mock.calls.find(
+			([event]) => event === eventName
+		);
+		return listener?.[1];
+	};
+
+	function getEventCallbacks() {
+		socketHandlers = socketHandlers ?? {
+			connect: getEventCallback("connect"),
+			disconnect: getEventCallback("disconnect"),
+		};
+		return socketHandlers;
+	}
+
+	describe("connect event", () => {
+		it("should showInfo when socket is connected", () => {
+			useSocketConnection(dispatchMock);
+
+			const eventCallbacks = getEventCallbacks();
+
+			// calling disconnect to set connectionOptions.isSocketConnectionLost to true
+			eventCallbacks.disconnect();
+			jest.clearAllMocks();
+
+			eventCallbacks.connect();
+
+			expect(mockBoardNotifierCalls.showInfo).toHaveBeenCalledWith(
+				"common.notification.connection.restored"
+			);
+		});
+
+		it("should reloadBoard when socket is connected and board exists", () => {
+			const { boardStore } = setupBoardStore({ createBoard: true });
+			useSocketConnection(dispatchMock);
+
+			const eventCallbacks = getEventCallbacks();
+
+			// calling disconnect to set connectionOptions.isSocketConnectionLost to true
+			eventCallbacks.disconnect();
+			jest.clearAllMocks();
+
+			eventCallbacks.connect();
+
+			expect(boardStore.reloadBoard).toHaveBeenCalled();
+		});
+
+		it("should not reloadBoard when socket is connected and board not exists", () => {
+			const { boardStore } = setupBoardStore({ createBoard: false });
+			useSocketConnection(dispatchMock);
+
+			const eventCallbacks = getEventCallbacks();
+
+			// calling disconnect to set connectionOptions.isSocketConnectionLost to true
+			eventCallbacks.disconnect();
+			jest.clearAllMocks();
+
+			eventCallbacks.connect();
+
+			expect(boardStore.reloadBoard).not.toHaveBeenCalled();
+		});
+	});
+
+	describe("disconnect event", () => {
+		it("should showFailure when socket is disconnected", () => {
+			useSocketConnection(dispatchMock);
+
+			const eventCallbacks = getEventCallbacks();
+
+			eventCallbacks.disconnect();
+
+			expect(mockBoardNotifierCalls.showFailure).toHaveBeenCalledWith(
+				"error.4500"
+			);
+		});
 	});
 
 	describe("emitOnSocket", () => {
