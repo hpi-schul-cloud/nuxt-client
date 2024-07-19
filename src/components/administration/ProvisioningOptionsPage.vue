0dc0a6f9cfb21cb60eaf68d0354c9acb85656b07
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-07-19 14:39:04 +0200

    add skeleton loader

--- src/components/administration/ProvisioningOptionsPage.vue
+++ src/components/administration/ProvisioningOptionsPage.vue
@@ -4,99 +4,97 @@
 		:breadcrumbs="breadcrumbs"
 		max-width="short"
 	>
-		<div class="d-flex flex-column ga-9">
-			<div>
-				<VCheckboxBtn
-					color="primary"
-					:label="
-						$t('components.administration.provisioningOptions.class.label')
-					"
-					:loading="isLoading"
-					v-model="provisioningOptions.class"
-					data-testid="checkbox-option-class"
-				/>
-				<div class="ml-10 text-caption text-medium-emphasis">
-					{{
-						$t(
-							"components.administration.provisioningOptions.class.description",
-							{
-								instance: theme.name,
-							}
-						)
-					}}
+		<VSkeletonLoader :loading="isLoading" type="list-item-two-line@4">
+			<div class="d-flex flex-column ga-9">
+				<div>
+					<VCheckboxBtn
+						color="primary"
+						:label="
+							$t('components.administration.provisioningOptions.class.label')
+						"
+						v-model="provisioningOptions.class"
+						data-testid="checkbox-option-class"
+					/>
+					<div class="ml-10 text-caption text-medium-emphasis">
+						{{
+							$t(
+								"components.administration.provisioningOptions.class.description",
+								{
+									instance: theme.name,
+								}
+							)
+						}}
+					</div>
 				</div>
-			</div>
 
-			<div>
-				<VCheckboxBtn
-					color="primary"
-					:label="
-						$t('components.administration.provisioningOptions.course.label')
-					"
-					:loading="isLoading"
-					v-model="provisioningOptions.course"
-					data-testid="checkbox-option-course"
-				/>
-				<div class="ml-10 text-caption text-medium-emphasis">
-					{{
-						$t(
-							"components.administration.provisioningOptions.course.description",
-							{
-								instance: theme.name,
-							}
-						)
-					}}
+				<div>
+					<VCheckboxBtn
+						color="primary"
+						:label="
+							$t('components.administration.provisioningOptions.course.label')
+						"
+						v-model="provisioningOptions.course"
+						data-testid="checkbox-option-course"
+					/>
+					<div class="ml-10 text-caption text-medium-emphasis">
+						{{
+							$t(
+								"components.administration.provisioningOptions.course.description",
+								{
+									instance: theme.name,
+								}
+							)
+						}}
+					</div>
 				</div>
-			</div>
 
-			<div>
-				<VCheckboxBtn
-					color="primary"
-					:label="
-						$t(
-							'components.administration.provisioningOptions.otherGroups.label'
-						)
-					"
-					:loading="isLoading"
-					v-model="provisioningOptions.others"
-					data-testid="checkbox-option-others"
-				/>
-				<div class="ml-10 text-caption text-medium-emphasis">
-					{{
-						$t(
-							"components.administration.provisioningOptions.otherGroups.description",
-							{
-								instance: theme.name,
-							}
-						)
-					}}
+				<div>
+					<VCheckboxBtn
+						color="primary"
+						:label="
+							$t(
+								'components.administration.provisioningOptions.otherGroups.label'
+							)
+						"
+						v-model="provisioningOptions.others"
+						data-testid="checkbox-option-others"
+					/>
+					<div class="ml-10 text-caption text-medium-emphasis">
+						{{
+							$t(
+								"components.administration.provisioningOptions.otherGroups.description",
+								{
+									instance: theme.name,
+								}
+							)
+						}}
+					</div>
 				</div>
-			</div>
 
-			<div v-if="isMediaLicensingEnabled">
-				<VCheckboxBtn
-					color="primary"
-					:label="
-						$t(
-							'components.administration.provisioningOptions.schoolExternalTools.label'
-						)
-					"
-					:loading="isLoading"
-					v-model="provisioningOptions.schoolExternalTools"
-					data-testid="checkbox-option-school-external-tools"
-				/>
-				<div class="ml-10 text-caption text-medium-emphasis">
-					{{
-						$t(
-							"components.administration.provisioningOptions.schoolExternalTools.description",
-							{
-								instance: theme.name,
-							}
-						)
-					}}
+				<div v-if="isMediaLicensingEnabled">
+					<VCheckboxBtn
+						color="primary"
+						:label="
+							$t(
+								'components.administration.provisioningOptions.schoolExternalTools.label'
+							)
+						"
+						v-model="provisioningOptions.schoolExternalTools"
+						data-testid="checkbox-option-school-external-tools"
+					/>
+					<div class="ml-10 text-caption text-medium-emphasis">
+						{{
+							$t(
+								"components.administration.provisioningOptions.schoolExternalTools.description",
+								{
+									instance: theme.name,
+								}
+							)
+						}}
+					</div>
 				</div>
 			</div>
-		</div>
+		</VSkeletonLoader>
 
 		<v-row class="justify-end mt-10">
 			<v-btn
