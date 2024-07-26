1f77673ff0d07324770ac13f186c782e6bca3c80
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2024-07-26 09:53:28 +0200

    fix icon button grid

--- src/modules/feature/board/shared/AddElementDialog.vue
+++ src/modules/feature/board/shared/AddElementDialog.vue
@@ -8,22 +8,17 @@
 			<VCardTitle class="text-h4 text-break px-6 pt-4">
 				{{ $t("components.elementTypeSelection.dialog.title") }}
 			</VCardTitle>
-			<VCardText class="d-flex justify-center">
-				<div
-					class="d-flex flex-row flex-wrap align-items-center"
-					:class="{
-						'justify-space-between': submissionsEnabled,
-					}"
-				>
-					<ExtendedIconBtn
-						v-for="(item, key) in elementTypeOptions"
-						:key="key"
-						:data-testid="item.testId"
-						:icon="item.icon"
-						:label="item.label"
-						@click.stop="item.action"
-					/>
-				</div>
+			<VCardText
+				class="d-flex flex-row flex-wrap align-items-center justify-space-between"
+			>
+				<ExtendedIconBtn
+					v-for="(item, key) in elementTypeOptions"
+					:key="key"
+					:data-testid="item.testId"
+					:icon="item.icon"
+					:label="item.label"
+					@click.stop="item.action"
+				/>
 			</VCardText>
 			<VCardActions class="mb-2 px-6">
 				<VBtn
@@ -39,19 +34,12 @@
 </template>
 
 <script setup lang="ts">
-import { ENV_CONFIG_MODULE_KEY, injectStrict } from "@/utils/inject";
 import { computed, ComputedRef } from "vue";
 import { useSharedElementTypeSelection } from "./SharedElementTypeSelection.composable";
 import { ExtendedIconBtn } from "@ui-extended-icon-btn";
 
-const envConfigModule = injectStrict(ENV_CONFIG_MODULE_KEY);
-
 const { isDialogOpen, closeDialog, elementTypeOptions } =
 	useSharedElementTypeSelection();
-
-const submissionsEnabled = computed(
-	() => envConfigModule.getEnv.FEATURE_COLUMN_BOARD_SUBMISSIONS_ENABLED
-);
 
 const dialogWidth: ComputedRef<number> = computed(() =>
 	elementTypeOptions.value.length >= 3 ? 426 : 320
