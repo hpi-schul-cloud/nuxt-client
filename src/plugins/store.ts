15a16a237860b291090d306363ed0780b0a6a99c
wolfganggreschus <wolfgang@greschus.de>
2024-08-01 15:01:25 +0200

    Remove courses.js store and related unit test files

--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -8,7 +8,6 @@ import activation from "@/store/activation";
 import { bulkConsent } from "@/store/bulkConsent";
 import calendar from "@/store/calendar";
 import classes from "@/store/classes";
-import courses from "@/store/courses";
 import { ldapConfig } from "@/store/ldap-config";
 import lessons from "@/store/lessons";
 import uiState from "@/store/uiState";
@@ -31,7 +30,6 @@ export default createStore({
 		bulkConsent,
 		calendar,
 		classes,
-		courses,
 		"ldap-config": ldapConfig,
 		lessons,
 		uiState,
