974432e6dd41bef5031a717671eb86b8ffdfae68
NFriedo <69233063+NFriedo@users.noreply.github.com>
2024-08-07 17:38:08 +0200

    add keyboard and aria-label tests for collaborative text editor

--- src/modules/feature/board-collaborative-text-editor-element/CollaborativeTextEditorElement.unit.ts
+++ src/modules/feature/board-collaborative-text-editor-element/CollaborativeTextEditorElement.unit.ts
@@ -12,6 +12,7 @@ import { createMock } from "@golevelup/ts-jest";
 import { ContentElementBar } from "@ui-board";
 import { mount } from "@vue/test-utils";
 import { nextTick } from "vue";
+import { VCard } from "vuetify/lib/components/index.mjs";
 import CollaborativeTextEditorElementMenu from "./components/CollaborativeTextEditorElementMenu.vue";
 import { setupCollaborativeTextEditorApiMock } from "./test-utils/collaborativeTextEditorApiMock";
 
@@ -96,6 +97,78 @@ describe("CollaborativeTextEditorElement", () => {
 			expect(boardMenu.exists()).toBe(false);
 		});
 
+		it("should have the correct aria-label", () => {
+			const { wrapper } = setup({
+				isEditMode: false,
+			});
+
+			const elementCard = wrapper.findComponent(VCard);
+
+			expect(elementCard.attributes("aria-label")).toEqual(
+				"components.cardElement.collaborativeTextEditorElement, common.ariaLabel.newTab"
+			);
+		});
+
+		describe("when element is focused", () => {
+			it.each(["enter", "space"])(
+				"should open element in new tab when %s is pressed",
+				async (key) => {
+					const { wrapper, windowMock, element } = setup({
+						isEditMode: false,
+					});
+
+					const elementCard = wrapper.findComponent({
+						ref: "collaborativetextEditorElement",
+					});
+
+					await elementCard.trigger(`keydown.${key}`);
+
+					expect(window.open).toHaveBeenCalledTimes(1);
+					expect(windowMock.location).toBe(
+						`${CollaborativeTextEditorParentType.ContentElement}/${element.id}`
+					);
+				}
+			);
+
+			it.each(["enter", "space"])(
+				"should not open element in new tab when %s is pressed getUrl is undefined",
+				async (key) => {
+					const { wrapper, windowMock, element } = setup({
+						isEditMode: false,
+						getUrlHasError: true,
+					});
+
+					const elementCard = wrapper.findComponent({
+						ref: "collaborativetextEditorElement",
+					});
+
+					await elementCard.trigger(`keydown.${key}`);
+
+					expect(window.open).toHaveBeenCalledTimes(1);
+					expect(windowMock.location).not.toBe(
+						`${CollaborativeTextEditorParentType.ContentElement}/${element.id}`
+					);
+				}
+			);
+
+			it.each(["up", "down"])(
+				"should not emit 'move-keyboard:edit' when arrow key %s is pressed",
+				async (key) => {
+					const { wrapper } = setup({
+						isEditMode: false,
+					});
+
+					const elementCard = wrapper.findComponent({
+						ref: "collaborativetextEditorElement",
+					});
+
+					await elementCard.trigger(`keydown.${key}`);
+
+					expect(wrapper.emitted("move-keyboard:edit")).toBeUndefined();
+				}
+			);
+		});
+
 		describe("when element is clicked", () => {
 			describe("when getUrl returns successful", () => {
 				it("should call getUrl", async () => {
@@ -157,6 +230,45 @@ describe("CollaborativeTextEditorElement", () => {
 	});
 
 	describe("when component is in edit-mode", () => {
+		describe("when element is focused", () => {
+			it.each(["up", "down"])(
+				"should emit 'move-keyboard:edit' when arrow key %s is pressed",
+				async (key) => {
+					const { wrapper } = setup({
+						isEditMode: true,
+					});
+
+					const elementCard = wrapper.findComponent({
+						ref: "collaborativetextEditorElement",
+					});
+
+					await elementCard.trigger(`keydown.${key}`);
+
+					expect(wrapper.emitted("move-keyboard:edit")).toBeTruthy();
+				}
+			);
+
+			it.each(["enter", "space"])(
+				"should open element in new tab when %s is pressed",
+				async (key) => {
+					const { wrapper, windowMock, element } = setup({
+						isEditMode: true,
+					});
+
+					const elementCard = wrapper.findComponent({
+						ref: "collaborativetextEditorElement",
+					});
+
+					await elementCard.trigger(`keydown.${key}`);
+
+					expect(window.open).toHaveBeenCalledTimes(1);
+					expect(windowMock.location).toBe(
+						`${CollaborativeTextEditorParentType.ContentElement}/${element.id}`
+					);
+				}
+			);
+		});
+
 		it("should render BoardMenu element", async () => {
 			const { wrapper } = setup({
 				isEditMode: true,
