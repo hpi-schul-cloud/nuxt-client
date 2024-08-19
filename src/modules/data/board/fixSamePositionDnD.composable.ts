594e27a84442a70903e55ecee5929e56d631721e
Murat Merdoglu <64781656+muratmerdoglu-dp@users.noreply.github.com>
2024-08-19 15:50:02 +0200

    BC-7808 - Fix unexpected cloned card element (#3353)
    BC-7808 - implement key changing to force the component to re-rendering

---
+++ src/modules/data/board/fixSamePositionDnD.composable.ts
@@ -0,0 +1,29 @@
+import { reactive } from "vue";
+
+const renderKeyList = reactive<Record<string, number>>({});
+
+const getMaxRenderKey = () => {
+	return Object.values(renderKeyList).reduce((a, b) => Math.max(a, b), 0);
+};
+
+/*
+ * This artificial generating renderKey forces a component to re-render by changing the element key.
+ * It is used to fix issue described on https://ticketsystem.dbildungscloud.de/browse/BC-7806
+ *
+ * This solution is based on the article on https://michaelnthiessen.com/force-re-render/
+ */
+export const useForceRender = (id: string) => {
+	const generateRenderKey = () => {
+		renderKeyList[id] = getMaxRenderKey() + 1;
+	};
+
+	const getRenderKey = (): number => {
+		if (!renderKeyList[id]) generateRenderKey();
+		return renderKeyList[id];
+	};
+
+	return {
+		generateRenderKey,
+		getRenderKey,
+	};
+};
