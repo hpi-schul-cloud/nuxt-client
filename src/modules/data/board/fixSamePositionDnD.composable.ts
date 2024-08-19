e04a5660e437eb6e07cb62bcce1079589342a3e5
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-08-19 11:55:37 +0200

    add comments on this implementation

--- src/modules/data/board/fixSamePositionDnD.composable.ts
+++ src/modules/data/board/fixSamePositionDnD.composable.ts
@@ -6,6 +6,12 @@ const getMaxRenderKey = () => {
 	return Object.values(renderKeyList).reduce((a, b) => Math.max(a, b), 0);
 };
 
+/*
+ * This artificial generating renderKey forces a component to re-render by changing the element key.
+ * It is used to fix issue described on https://ticketsystem.dbildungscloud.de/browse/BC-7806
+ *
+ * This solution is based on the article on https://michaelnthiessen.com/force-re-render/
+ */
 export const useForceRender = (id: string) => {
 	const generateRenderKey = () => {
 		renderKeyList[id] = getMaxRenderKey() + 1;
