31501da320ce29674dbb67f1713fc4c4df214702
wolfganggreschus <wolfgang@greschus.de>
2024-08-22 09:51:22 +0200

    Renamed rooms folder

--- src/pages/course-room/tools/RoomExternalToolsSection.vue
+++ src/pages/course-room/tools/RoomExternalToolsSection.vue
@@ -53,7 +53,7 @@
 <script setup lang="ts">
 import VCustomDialog from "@/components/organisms/vCustomDialog.vue";
 import RoomExternalToolCard from "@/components/rooms/RoomExternalToolCard.vue";
-import RoomExternalToolsErrorDialog from "@/pages/rooms/tools/RoomExternalToolsErrorDialog.vue";
+import RoomExternalToolsErrorDialog from "@/pages/course-room/tools/RoomExternalToolsErrorDialog.vue";
 import { ToolContextType } from "@/serverApi/v3";
 import AuthModule from "@/store/auth";
 import { AUTH_MODULE_KEY, injectStrict } from "@/utils/inject";
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -245,7 +245,7 @@ export const routes: Readonly<RouteRecordRaw[]> = [
 	},
 	{
 		path: "/rooms/courses-list",
-		component: () => import("@/pages/rooms/CourseRoomList.page.vue"),
+		component: () => import("@/pages/course-room/CourseRoomList.page.vue"),
 		name: "course-room-list",
 	},
 	{
@@ -255,14 +255,14 @@ export const routes: Readonly<RouteRecordRaw[]> = [
 	},
 	{
 		path: "/rooms/courses-overview",
-		component: () => import("@/pages/rooms/CourseRoomOverview.page.vue"),
+		component: () => import("@/pages/course-room/CourseRoomOverview.page.vue"),
 		name: "course-room-overview",
 	},
 	{
 		// TODO BC-7822, BC-7823 target this route at new room details page
 		// and decide on that page which sub-component (page) has to be rendered
 		path: `/rooms/:id(${REGEX_ID})`,
-		component: () => import("@/pages/rooms/CourseRoomDetails.page.vue"),
+		component: () => import("@/pages/course-room/CourseRoomDetails.page.vue"),
 		name: "rooms-id",
 	},
 	{
