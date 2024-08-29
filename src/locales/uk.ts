3c32db4ecde28eb8aa93ad0a91beb9b9d879788d
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-08-29 13:26:10 +0200

    change text

--- src/locales/uk.ts
+++ src/locales/uk.ts
@@ -1731,7 +1731,7 @@ export default {
 	"utils.adminFilter.placeholder.date.until": "... і 03.03.2020",
 	"feature.media-shelf.title": "Полиця для медіа",
 	"feature.media-shelf.emptyState":
-		"Жоден носій не активовано для використання.",
+		"Наразі немає медіафайлів, доступних для використання",
 	"feature.media-shelf.createLine": "Додайте лінію",
 	"feature.media-shelf.line.title": "лінія",
 	"feature.media-shelf.availableLine.title": "Доступні медіа",
