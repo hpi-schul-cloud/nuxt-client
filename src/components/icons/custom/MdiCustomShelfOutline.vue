49dba7e91823da2ca79cfe45e87fe9160be1c497
Marvin Öhlerking <103562092+MarvinOehlerkingCap@users.noreply.github.com>
2024-08-30 14:47:19 +0200

    N21-2088 Change media shelf line view icon (#3383)

---
+++ src/components/icons/custom/MdiCustomShelfOutline.vue
@@ -0,0 +1,13 @@
+<template>
+	<svg
+		class="v-icon__svg"
+		role="img"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 28 24"
+	>
+		<path
+			d="M10.0899 15H18.1618V7H10.0899M12.1079 9H16.1439V13H12.1079M0 15H8.07193V7H0M2.01798 9H6.05395V13H2.01798"
+		/>
+		<path d="M19.9281 15H28V7H19.9281M21.9461 9H25.982V13H21.9461" />
+	</svg>
+</template>
