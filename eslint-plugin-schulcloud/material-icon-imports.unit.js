4040c76b8f31b9e9fd7c47bf70b3e9527dfb6a0a
NFriedo <69233063+NFriedo@users.noreply.github.com>
2024-09-03 10:16:32 +0200

    add custom rules for material icon imports

---
+++ eslint-plugin-schulcloud/material-icon-imports.unit.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { RuleTester } = require("eslint");
+const rule = require("./material-icon-imports.js");
+
+const ruleTester = new RuleTester({
+	parserOptions: { ecmaVersion: 2020, sourceType: "module" },
+});
+
+ruleTester.run("material-icon-imports", rule, {
+	valid: [
+		{
+			code: `import { mdiCheck } from "@icons/material";`,
+		},
+		{
+			code: `import { useI18n } from "vue-i18n";`,
+		},
+	],
+	invalid: [
+		{
+			code: `import { mdiCheck } from "@mdi/js";`,
+			errors: [{ messageId: "noDirectIconImport" }],
+			output: `import { mdiCheck } from "@icons/material";`,
+		},
+		{
+			code: `import { mdiAlert } from "@/components/icons/material";`,
+			errors: [{ messageId: "noDirectIconImport" }],
+			output: `import { mdiAlert } from "@icons/material";`,
+		},
+		{
+			code: `import { mdiCheck } from "some-other-path";`,
+			errors: [{ messageId: "noDirectIconImport" }],
+			output: `import { mdiCheck } from "@icons/material";`,
+		},
+		{
+			code: `import { mdiCheck } from "../icons/material";`,
+			errors: [{ messageId: "noDirectIconImport" }],
+			output: `import { mdiCheck } from "@icons/material";`,
+		},
+	],
+});
