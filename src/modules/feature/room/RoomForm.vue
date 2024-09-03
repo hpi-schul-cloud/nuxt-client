8593bffc294b208aa8257ad503f2710e3a7d14c8
Odalys Adam <odalys.adam@dataport.de>
2024-09-03 18:47:47 +0200

    merge stashed changes

--- src/modules/feature/room/RoomForm.vue
+++ src/modules/feature/room/RoomForm.vue
@@ -1,10 +1,27 @@
-<template lang="">
+<template>
 	<div>
-		<RoomColorPicker
-			:selected-color="roomColor"
-			@update:selected-color="onColorSelect($event)"
-		/>
-		Hallo
+		<VTextField v-model="roomTitle" label="Name des Raumes" class="mb-8" />
+		<div class="mb-8">
+			Farbe
+			<RoomColorPicker
+				:selected-color="roomColor"
+				@update:selected-color="onColorSelect($event)"
+			/>
+		</div>
+		<div class="mb-8">
+			Zeitraum
+			<div class="d-flex flex-fill">
+				<DatePicker class="flex-1-1 mr-4" />
+				<DatePicker class="flex-1-1 ml-4" />
+			</div>
+		</div>
+		<div class="d-flex">
+			<VSpacer />
+			<VBtn variant="text" class="mr-4">{{ $t("common.actions.cancel") }}</VBtn>
+			<VBtn variant="flat" color="primary">
+				{{ $t("common.actions.save") }}
+			</VBtn>
+		</div>
 	</div>
 </template>
 
@@ -12,7 +29,9 @@
 import { ref } from "vue";
 import { RoomColorEnum } from "./RoomColorPicker/types";
 import RoomColorPicker from "./RoomColorPicker/RoomColorPicker.vue";
+import { DatePicker } from "@ui-date-time-picker";
 
+const roomTitle = ref("");
 const roomColor = ref(RoomColorEnum.BLUE_GREY);
 
 const onColorSelect = (color: RoomColorEnum) => {
