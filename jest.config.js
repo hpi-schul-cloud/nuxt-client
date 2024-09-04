df4e5fb1147f063456f21225fdf6e65a3836b96f
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2024-09-04 11:44:17 +0200

    add icons to jest config

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,7 @@ const config = {
 		"^@page-(.*)$": "<rootDir>/src/modules/page/$1",
 		"^@ui-(.*)$": "<rootDir>/src/modules/ui/$1",
 		"^@util-(.*)$": "<rootDir>/src/modules/util/$1",
+		"^@icons(.*)$": "<rootDir>/src/components/icons/$1",
 		"^@/(.*)$": "<rootDir>/src/$1",
 		"^@@/(.*)$": "<rootDir>/$1",
 	},
