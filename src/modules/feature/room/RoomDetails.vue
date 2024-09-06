836f4a77ce99c940eae3e110b5df90733d574316
Odalys Adam <odalys.adam@dataport.de>
2024-09-06 14:34:15 +0200

    KebabMenu for RoomDetails

--- src/modules/feature/room/RoomDetails.vue
+++ src/modules/feature/room/RoomDetails.vue
@@ -2,25 +2,68 @@
 	<template v-if="room">
 		<DefaultWireframe max-width="short" :breadcrumbs="breadcrumbs">
 			<template #header>
-				<h1 class="text-h3 py-2 mb-4">{{ room.title }}</h1>
+				<div class="d-flex align-items-center">
+					<h1 class="text-h3 pl-2 margin-bottom">{{ room.title }}</h1>
+					<KebabMenu class="mx-2" data-testid="room-menu">
+						<VListItem
+							role="menuitem"
+							:to="`/rooms/${room.id}/edit`"
+							data-testid="room-action-edit"
+							aria-label="Raum bearbeiten"
+						>
+							<template v-slot:prepend>
+								<VIcon :icon="mdiPencilOutline" />
+							</template>
+							<VListItemTitle>
+								{{ $t("common.actions.edit") }}
+							</VListItemTitle>
+						</VListItem>
+						<VListItem
+							role="menuitem"
+							data-testid="room-action-delete"
+							aria-label="Raum löschen"
+							@click="onDelete"
+						>
+							<template v-slot:prepend>
+								<VIcon :icon="mdiTrashCanOutline" />
+							</template>
+							<VListItemTitle>
+								{{ $t("common.actions.delete") }}
+							</VListItemTitle>
+						</VListItem>
+					</KebabMenu>
+				</div>
 			</template>
 			<div>[TODO - RoomDetails content]</div>
+			<ConfirmationDialog />
 		</DefaultWireframe>
 	</template>
 </template>
 
 <script setup lang="ts">
-import { Room } from "@/types/room/Room";
 import { computed, ComputedRef, PropType } from "vue";
+import { useI18n } from "vue-i18n";
 import { Breadcrumb } from "@/components/templates/default-wireframe.types";
 import DefaultWireframe from "@/components/templates/DefaultWireframe.vue";
-import { useI18n } from "vue-i18n";
-
-const { t } = useI18n();
+import { ConfirmationDialog } from "@ui-confirmation-dialog";
+import { Room } from "@/types/room/Room";
+import { KebabMenu } from "@ui-kebab-menu";
+import {
+	mdiPencilOutline,
+	mdiTrashCanOutline,
+} from "@/components/icons/material";
+import { useRoomsState } from "@data-room";
+import { useDeleteConfirmationDialog } from "@ui-confirmation-dialog";
+import { useRouter } from "vue-router";
 
 const props = defineProps({
 	room: { type: Object as PropType<Room | undefined> },
 });
+
+const { t } = useI18n();
+const router = useRouter();
+const { deleteRoom } = useRoomsState();
+const { askDeleteConfirmation } = useDeleteConfirmationDialog();
 
 const room = computed(() => props.room);
 
@@ -39,4 +82,25 @@ const breadcrumbs: ComputedRef<Breadcrumb[]> = computed(() => {
 	}
 	return [];
 });
+
+const onDelete = async () => {
+	if (!room.value) return;
+	const shouldDelete = await askDeleteConfirmation(
+		"blub",
+		"components.cardElement.mediaExternalToolElement"
+	);
+
+	if (shouldDelete) {
+		await deleteRoom(room.value.id);
+		router.push({
+			name: "rooms",
+		});
+	}
+};
 </script>
+
+<style scoped>
+.margin-bottom {
+	margin-bottom: var(--space-md);
+}
+</style>
