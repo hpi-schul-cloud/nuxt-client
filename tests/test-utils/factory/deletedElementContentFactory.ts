468a1d985f8d9eed661dbf40855059d03e819322
Gordon Nicholas <gordon.nicholas@capgemini.com>
2024-09-11 16:59:03 +0200

    N21-2089 update api, added new factories for deleted element

---
+++ tests/test-utils/factory/deletedElementContentFactory.ts
@@ -0,0 +1,8 @@
+import { ContentElementType, DeletedElementContent } from "@/serverApi/v3";
+import { Factory } from "fishery";
+
+export const deletedElementContentFactory =
+	Factory.define<DeletedElementContent>(({ sequence }) => ({
+		title: `deleted-element-${sequence}`,
+		deletedElementType: ContentElementType.Deleted,
+	}));
