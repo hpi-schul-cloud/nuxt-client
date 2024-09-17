f566e212f207d9341dacc7e5b47687c274d4cf2a
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-09-17 09:59:49 +0200

    seperate functions in composable file

--- src/modules/data/board/focusPrevios.composable.ts
+++ src/modules/data/board/focusPrevios.composable.ts
@@ -7,62 +7,47 @@ type ParamType = {
 	elementType?: string;
 };
 
-export const useSetFocusPrevios = async (payload: ParamType) => {
+const findPreviousElement = (payload: ParamType) => {
 	const { board } = useBoardStore();
 	const { cards } = useCardStore();
-
-	const findPreviousElement = (payload: ParamType) => {
-		if (!board) return;
-
-		if (payload.level === "column") {
-			const columnIndex = board.columns.findIndex(
-				(column) => column.id === payload.id
-			);
-
-			if (columnIndex <= 0) return board.id;
-			return board.columns[columnIndex - 1].id;
-		}
-
-		if (payload.level === "card") {
-			const columnIndex = board.columns.findIndex(
-				(column) =>
-					column.cards.find((c) => c.cardId === payload.id) !== undefined
-			);
-
-			const cardIndex = board.columns[columnIndex].cards.findIndex(
-				(c) => c.cardId === payload.id
-			);
-
-			if (cardIndex <= 0) return board.columns[columnIndex].id;
-			return board.columns[columnIndex].cards[cardIndex - 1].cardId;
-		}
-
-		if (payload.level === "element") {
-			if (!cards) return;
-			const elements = cards[payload.parentId].elements;
-			if (elements.length === 0) return payload.parentId;
-
-			const elementIndex = elements.findIndex((e) => e.id === payload.id);
-			if (elementIndex <= 0) return payload.parentId;
-			const currentElement = elements[elementIndex];
-			const previousElement = elements[elementIndex - 1];
-
-			if (previousElement.type !== "richText") return previousElement.id;
-
-			if (
-				currentElement.type === "richText" ||
-				previousElement.type === "richText"
-			) {
-				return findPreviousElement({
-					id: elements[elementIndex - 1].id,
-					parentId: payload.parentId,
-					level: "element",
-				});
-			}
-		}
-	};
-
 	if (!board) return;
+	if (payload.level === "column") {
+		const columnIndex = board.columns.findIndex(
+			(column) => column.id === payload.id
+		);
+
+		if (columnIndex <= 0) return board.id;
+		return board.columns[columnIndex - 1].id;
+	}
+
+	if (payload.level === "card") {
+		const columnIndex = board.columns.findIndex(
+			(column) =>
+				column.cards.find((c) => c.cardId === payload.id) !== undefined
+		);
+
+		const cardIndex = board.columns[columnIndex].cards.findIndex(
+			(c) => c.cardId === payload.id
+		);
+
+		if (cardIndex <= 0) return board.columns[columnIndex].id;
+		return board.columns[columnIndex].cards[cardIndex - 1].cardId;
+	}
+
+	if (payload.level === "element") {
+		if (!cards) return;
+		const elements = cards[payload.parentId].elements;
+		if (elements.length === 0) return payload.parentId;
+
+		const elementIndex = elements.findIndex((e) => e.id === payload.id);
+		if (elementIndex <= 0) return payload.parentId;
+		const previousElement = elements[elementIndex - 1];
+
+		return previousElement.id;
+	}
+};
+
+export const useSetFocusPrevios = async (payload: ParamType) => {
 	const { setFocus } = useBoardFocusHandler();
 
 	const previousId = findPreviousElement(payload);
@@ -74,7 +59,5 @@ export const useSetFocusPrevios = async (payload: ParamType) => {
 		`[data-focused-id="${previousId}"]`
 	) as HTMLElement;
 
-	element?.setAttribute("tabindex", "0");
-	setTimeout(() => element.removeAttribute("tabindex"), 1000);
 	element?.focus();
 };
