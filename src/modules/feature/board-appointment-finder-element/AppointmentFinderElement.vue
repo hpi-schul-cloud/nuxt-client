a80d1eaadeab3529a5527d2db4fb9fdbde99def6
dyedwiper <maximilian.becker@dataport.de>
2024-10-02 16:37:33 +0200

    Use iframe for poll and update

--- src/modules/feature/board-appointment-finder-element/AppointmentFinderElement.vue
+++ src/modules/feature/board-appointment-finder-element/AppointmentFinderElement.vue
@@ -9,8 +9,8 @@
 		@keydown.up.down="onKeydownArrow"
 		role="button"
 		:aria-label="ariaLabel"
-		@click="redirectToAppointmentFinderUrl"
-		@keydown.enter.space="redirectToAppointmentFinderUrl"
+		@click="openPoll"
+		@keydown.enter.space="openPoll"
 		@keydown.stop
 	>
 		<ContentElementBar :hasGreyBackground="true">
@@ -30,6 +30,11 @@
 				/>
 			</template>
 		</ContentElementBar>
+		<VDialog v-model="isDialogOpen">
+			<VCard class="appointmentFinderDialog">
+				<iframe :src="iframeUrl" class="appointmentFinderIframe" />
+			</VCard>
+		</VDialog>
 	</v-card>
 </template>
 
@@ -38,7 +43,7 @@ import image from "@/assets/img/appointmentFinder.png";
 import { AppointmentFinderElementResponse } from "@/serverApi/v3";
 import { useBoardFocusHandler } from "@data-board";
 import { ContentElementBar } from "@ui-board";
-import { computed, PropType, ref, toRef } from "vue";
+import { computed, PropType, ref, toRef, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import AppointmentFinderElementMenu from "./components/AppointmentFinderElementMenu.vue";
 
@@ -62,19 +67,37 @@ const { t } = useI18n();
 const appointmentFinderElement = ref<HTMLElement | null>(null);
 const element = toRef(props, "element");
 useBoardFocusHandler(element.value.id, appointmentFinderElement);
+const isDialogOpen = ref(false);
+const iframeUrl = ref("");
 
 const onEdit = () => {
 	const adminId = props.element.content.adminId;
-	const url = `http://localhost:4200/#/admin/dashboard/${adminId}`;
-
-	window.open(url, "_blank");
+	iframeUrl.value = `http://localhost:4200/#/admin/dashboard/${adminId}`;
+	isDialogOpen.value = true;
+	addEventListenerToCloseDialog();
 };
 
-const redirectToAppointmentFinderUrl = async () => {
+const openPoll = async () => {
 	const appointmentFinderId = props.element.content.appointmentFinderId;
-	const url = `http://localhost:4200/#/poll/${appointmentFinderId}`;
+	iframeUrl.value = `http://localhost:4200/#/poll/${appointmentFinderId}`;
+	isDialogOpen.value = true;
+	addEventListenerToCloseDialog();
+};
 
-	window.open(url, "_blank");
+const addEventListenerToCloseDialog = () => {
+	const closeDialog = () => {
+		isDialogOpen.value = false;
+
+		window.removeEventListener("message", closeDialog);
+	};
+
+	window.addEventListener("message", closeDialog);
+
+	watch(isDialogOpen, (isOpen) => {
+		if (!isOpen) {
+			window.removeEventListener("message", closeDialog);
+		}
+	});
 };
 
 const ariaLabel = computed(() => {
@@ -98,5 +121,14 @@ const onMoveDown = () => emit("move-down:edit");
 <style scoped lang="scss">
 .text-editor-card {
 	outline-offset: 1px;
+}
+
+.appointmentFinderDialog {
+	height: 90vh;
+}
+
+.appointmentFinderIframe {
+	width: 100%;
+	height: 100%;
 }
 </style>
