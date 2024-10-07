bca06c610b86fd2702ca65079922bea756b5c13c
wolfganggreschus <wolfgang@greschus.de>
2024-10-07 18:23:51 +0200

    comment added

--- src/components/templates/DefaultWireframe.vue
+++ src/components/templates/DefaultWireframe.vue
@@ -128,6 +128,7 @@ const showBorder = computed(() => {
 <style lang="scss" scoped>
 @import "@/styles/settings.scss";
 
+//TODO: Check if this is still needed
 .wireframe-container h1:first-of-type {
 	margin-bottom: var(--space-md);
 }
