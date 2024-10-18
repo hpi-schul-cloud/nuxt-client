a3bef5ae7a0f8bfbac97cd510c3605679f1ca262
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-10-18 11:34:46 +0200

    add and export mockList in barrel file

--- src/modules/data/room/index.ts
+++ src/modules/data/room/index.ts
@@ -5,3 +5,4 @@ export { useCourseInfoApi } from "./courseInfoApi.composable";
 export { useCourseList } from "./courseList.composable";
 export { useRoomCreateState } from "./RoomCreate.state";
 export { useRoomEditState } from "./RoomEdit.state";
+export { participants } from "./mockParticipantsList";
