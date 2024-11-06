a7938c52e4e6b7c30631ce2d85fcc3b1973ea29a
wolfganggreschus <wolfgang@greschus.de>
2024-11-06 11:51:44 +0100

    Add unit tests for RoomCreate page component

--- src/modules/page/room/RoomCreate.page.unit.ts
+++ src/modules/page/room/RoomCreate.page.unit.ts
@@ -1,1 +1,25 @@
-// TODO tests to be added
+import {
+	createTestingI18n,
+	createTestingVuetify,
+} from "@@/tests/test-utils/setup";
+import { RoomCreatePage } from "@page-room";
+
+describe("@pages/RoomCreate.page.vue", () => {
+	const setup = () => {
+		const wrapper = shallowMount(RoomCreatePage, {
+			global: {
+				plugins: [createTestingVuetify(), createTestingI18n()],
+			},
+		});
+
+		return {
+			wrapper,
+		};
+	};
+
+	it("should be rendered in DOM", () => {
+		const { wrapper } = setup();
+
+		expect(wrapper.vm).toBeDefined();
+	});
+});
