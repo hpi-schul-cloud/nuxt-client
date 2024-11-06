622fc4d75b59dca8cd9e7bdf9f13f0836141351b
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-11-06 13:20:18 +0100

    change brb theming colors

--- src/themes/brb/vuetify.options.ts
+++ src/themes/brb/vuetify.options.ts
@@ -5,15 +5,15 @@ import { type ThemeDefinition } from "vuetify";
 const brbTheme: ThemeDefinition = {
 	dark: false,
 	colors: {
-		primary: "#0a9396",
+		primary: "#2876d0",
 		"primary-darken-1": "#086e71",
 		"primary-lighten": "#e6f4f5",
 		accent: "#e4032e",
-		"on-surface": "#294c5a",
-		"on-background": "#294c5a",
+		"on-surface": "#0f3551",
+		"on-background": "#0f3551",
 		"surface-light": "#f2f4f5",
 		"on-surface-light": "#294c5a",
-		"on-white": "#294c5a",
+		"on-white": "#0f3551",
 	},
 };
 
