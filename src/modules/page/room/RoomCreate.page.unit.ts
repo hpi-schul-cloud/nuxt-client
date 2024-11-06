8616a668f3a8c16ce23a3488edff67a06be8302c
wolfganggreschus <wolfgang@greschus.de>
2024-11-06 15:08:45 +0100

    Add navigation test for RoomCreate page component

--- src/modules/page/room/RoomCreate.page.unit.ts
+++ src/modules/page/room/RoomCreate.page.unit.ts
@@ -3,10 +3,19 @@ import {
 	createTestingVuetify,
 } from "@@/tests/test-utils/setup";
 import { RoomCreatePage } from "@page-room";
+import { useRouter } from "vue-router";
 
+jest.mock("vue-router", () => ({
+	useRouter: jest.fn().mockReturnValue({
+		push: jest.fn(),
+	}),
+}));
+jest.mock<typeof import("@/utils/pageTitle")>("@/utils/pageTitle", () => ({
+	buildPageTitle: (pageTitle) => pageTitle ?? "",
+}));
 describe("@pages/RoomCreate.page.vue", () => {
 	const setup = () => {
-		const wrapper = shallowMount(RoomCreatePage, {
+		const wrapper = mount(RoomCreatePage, {
 			global: {
 				plugins: [createTestingVuetify(), createTestingI18n()],
 			},
@@ -14,12 +23,17 @@ describe("@pages/RoomCreate.page.vue", () => {
 
 		return {
 			wrapper,
+			router: useRouter(),
 		};
 	};
 
 	it("should be rendered in DOM", () => {
 		const { wrapper } = setup();
-
 		expect(wrapper.vm).toBeDefined();
+	});
+	it("should navigate to 'rooms' on cancel", async () => {
+		const { wrapper, router } = setup();
+		(wrapper.vm as any).onCancel();
+		expect(router.push).toHaveBeenCalledWith({ name: "rooms" });
 	});
 });
