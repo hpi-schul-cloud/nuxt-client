b17469cb49ac65affade3f2cf0ade6c3f083e6a3
wolfganggreschus <wolfgang@greschus.de>
2024-11-07 08:48:00 +0100

    Add unit tests for RoomCreate page save functionality

--- src/modules/page/room/RoomCreate.page.unit.ts
+++ src/modules/page/room/RoomCreate.page.unit.ts
@@ -1,9 +1,12 @@
+import { RoomCreateParams } from "@/types/room/Room";
+import { useRoomCreateState } from "@data-room";
 import {
 	createTestingI18n,
 	createTestingVuetify,
 } from "@@/tests/test-utils/setup";
 import { RoomCreatePage } from "@page-room";
 import { useRouter } from "vue-router";
+import { RoomColor } from "@/serverApi/v3";
 
 jest.mock("vue-router", () => ({
 	useRouter: jest.fn().mockReturnValue({
@@ -13,17 +16,38 @@ jest.mock("vue-router", () => ({
 jest.mock<typeof import("@/utils/pageTitle")>("@/utils/pageTitle", () => ({
 	buildPageTitle: (pageTitle) => pageTitle ?? "",
 }));
+
+jest.mock("@data-room", () => ({
+	useRoomCreateState: jest.fn().mockReturnValue({
+		createRoom: jest.fn().mockResolvedValue({
+			id: "123",
+			name: "test",
+			color: "blue",
+		}),
+	}),
+	roomData: {},
+}));
+
+const roomParams: RoomCreateParams = {
+	name: "test",
+	color: RoomColor.Blue,
+};
+
 describe("@pages/RoomCreate.page.vue", () => {
 	const setup = () => {
 		const wrapper = mount(RoomCreatePage, {
+			shallow: true,
 			global: {
 				plugins: [createTestingVuetify(), createTestingI18n()],
 			},
 		});
 
+		const { createRoom } = useRoomCreateState();
+
 		return {
 			wrapper,
 			router: useRouter(),
+			createRoom,
 		};
 	};
 
@@ -35,5 +59,23 @@ describe("@pages/RoomCreate.page.vue", () => {
 		const { wrapper, router } = setup();
 		wrapper.vm.onCancel();
 		expect(router.push).toHaveBeenCalledWith({ name: "rooms" });
+	});
+	it("should call createRoom with correct parameters on save", async () => {
+		const { wrapper, createRoom } = setup();
+		await wrapper.vm.onSave(roomParams);
+		expect(createRoom).toHaveBeenCalledWith(roomParams);
+	});
+	it("should call createRoom with correct parameters on save", async () => {
+		const { wrapper, createRoom } = setup();
+		await wrapper.vm.onSave(roomParams);
+		expect(createRoom).toHaveBeenCalledWith(roomParams);
+	});
+	it("should navigate to 'room-details' with correct room id on save", async () => {
+		const { wrapper, router } = setup();
+		await wrapper.vm.onSave(roomParams);
+		expect(router.push).toHaveBeenCalledWith({
+			name: "room-details",
+			params: { id: "123" },
+		});
 	});
 });
