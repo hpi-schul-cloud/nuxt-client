2b99358e07e846a89424dd20ff8cd49b03343fac
Odalys Adam <odalys.adam@dataport.de>
2024-11-24 14:27:24 +0100

    remove unnecessary error type

--- src/types/room/Room.ts
+++ src/types/room/Room.ts
@@ -12,22 +12,3 @@ export type RoomBoardItem = RoomBoardItemResponse;
 
 export type RoomCreateParams = CreateRoomBodyParams;
 export type RoomUpdateParams = UpdateRoomBodyParams;
-
-export class RoomApiError extends Error {
-	readonly type: string;
-	readonly code: number;
-
-	constructor({
-		message,
-		type,
-		code,
-	}: {
-		message: string;
-		type: string;
-		code: number;
-	}) {
-		super(message);
-		this.type = type;
-		this.code = code;
-	}
-}
