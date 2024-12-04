528e8aeeeed973859fb57beac8309c8b142b4071
NFriedo <69233063+NFriedo@users.noreply.github.com>
2024-12-04 09:41:46 +0100

    WIP: add focus trap remove dialog

--- src/components/organisms/vCustomDialog.vue
+++ src/components/organisms/vCustomDialog.vue
@@ -6,81 +6,85 @@
 		@click:outside="closeDialog"
 		@keydown.esc="closeDialog"
 	>
-		<v-card :ripple="false">
-			<v-card-title data-testid="dialog-title" class="dialog-title px-6 pt-4">
-				<slot name="title" />
-			</v-card-title>
-			<v-card-text class="text--primary">
-				<slot name="content" />
-			</v-card-text>
-			<v-card-actions v-if="hasButtons" class="action-buttons px-6">
-				<div class="button-section button-left">
-					<v-btn
-						v-if="checkButtons('back')"
-						data-testid="dialog-back"
-						variant="outlined"
-						@click="$emit('back')"
-					>
-						{{ $t("common.actions.back") }}
-					</v-btn>
-					<v-btn
-						v-if="checkButtons('edit')"
-						data-testid="dialog-edit"
-						variant="flat"
-						@click="$emit('dialog-edit')"
-					>
-						{{ $t("common.actions.edit") }}
-					</v-btn>
-				</div>
-				<v-spacer />
-				<div class="button-section button-right">
-					<v-btn
-						v-if="checkButtons('cancel')"
-						data-testid="dialog-cancel"
-						class="dialog-closed"
-						variant="text"
-						@click="cancelDialog"
-					>
-						{{ $t("common.actions.cancel") }}
-					</v-btn>
-					<v-btn
-						v-if="checkButtons('confirm')"
-						data-testid="dialog-confirm"
-						class="dialog-confirmed px-6"
-						color="primary"
-						variant="flat"
-						:disabled="confirmBtnDisabled"
-						@click="confirmDialog"
-					>
-						<v-icon v-if="confirmBtnIcon" size="small" class="mr-1">
-							{{ confirmBtnIcon }}
-						</v-icon>
-						{{ $t(confirmBtnTitleKey) }}
-					</v-btn>
-					<v-btn
-						v-if="checkButtons('close')"
-						data-testid="dialog-close"
-						variant="outlined"
-						@click="closeDialog"
-					>
-						{{ $t("common.labels.close") }}
-					</v-btn>
-					<v-btn
-						v-if="checkButtons('next')"
-						data-testid="dialog-next"
-						color="primary"
-						variant="flat"
-						:disabled="nextBtnDisabled"
-						@click="$emit('next')"
-						>{{ $t(nextBtnTitleKey) }}
-					</v-btn>
-				</div>
-			</v-card-actions>
-		</v-card>
+		<!-- ToDo: think about this focus trap if there could be problem to do it here -->
+		<UseFocusTrap>
+			<v-card :ripple="false">
+				<v-card-title data-testid="dialog-title" class="dialog-title px-6 pt-4">
+					<slot name="title" />
+				</v-card-title>
+				<v-card-text class="text--primary">
+					<slot name="content" />
+				</v-card-text>
+				<v-card-actions v-if="hasButtons" class="action-buttons px-6">
+					<div class="button-section button-left">
+						<v-btn
+							v-if="checkButtons('back')"
+							data-testid="dialog-back"
+							variant="outlined"
+							@click="$emit('back')"
+						>
+							{{ $t("common.actions.back") }}
+						</v-btn>
+						<v-btn
+							v-if="checkButtons('edit')"
+							data-testid="dialog-edit"
+							variant="flat"
+							@click="$emit('dialog-edit')"
+						>
+							{{ $t("common.actions.edit") }}
+						</v-btn>
+					</div>
+					<v-spacer />
+					<div class="button-section button-right">
+						<v-btn
+							v-if="checkButtons('cancel')"
+							data-testid="dialog-cancel"
+							class="dialog-closed"
+							variant="text"
+							@click="cancelDialog"
+						>
+							{{ $t("common.actions.cancel") }}
+						</v-btn>
+						<v-btn
+							v-if="checkButtons('confirm')"
+							data-testid="dialog-confirm"
+							class="dialog-confirmed px-6"
+							color="primary"
+							variant="flat"
+							:disabled="confirmBtnDisabled"
+							@click="confirmDialog"
+						>
+							<v-icon v-if="confirmBtnIcon" size="small" class="mr-1">
+								{{ confirmBtnIcon }}
+							</v-icon>
+							{{ $t(confirmBtnTitleKey) }}
+						</v-btn>
+						<v-btn
+							v-if="checkButtons('close')"
+							data-testid="dialog-close"
+							variant="outlined"
+							@click="closeDialog"
+						>
+							{{ $t("common.labels.close") }}
+						</v-btn>
+						<v-btn
+							v-if="checkButtons('next')"
+							data-testid="dialog-next"
+							color="primary"
+							variant="flat"
+							:disabled="nextBtnDisabled"
+							@click="$emit('next')"
+							>{{ $t(nextBtnTitleKey) }}
+						</v-btn>
+					</div>
+				</v-card-actions>
+			</v-card>
+		</UseFocusTrap>
 	</v-dialog>
 </template>
 
 <script setup lang="ts">
+import { UseFocusTrap } from "@vueuse/integrations/useFocusTrap/component";
 import { ModelRef, PropType } from "vue";
 
 const props = defineProps({
