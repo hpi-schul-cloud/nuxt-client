ecc845d66f7b7d51a14f6812695aaf97d2b2ca5c
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-01-10 16:51:45 +0100

    chore: fix translation

--- src/modules/ui/board/BoardMenuActionCopy.vue
+++ src/modules/ui/board/BoardMenuActionCopy.vue
@@ -4,13 +4,15 @@
 		data-testid="board-menu-action-copy"
 		@click="onClick"
 	>
-		{{ $t("common.actions.copy") }}
+		{{ t("common.actions.copy") }}
 	</BoardMenuAction>
 </template>
 
 <script setup lang="ts">
 import { BoardMenuAction } from "@ui-board";
 import { mdiContentCopy } from "@icons/material";
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 
 const emit = defineEmits(["click"]);
 
