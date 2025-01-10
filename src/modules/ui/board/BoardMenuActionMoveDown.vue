2c0a50df599fa684dbf04e31e84728391ce921f1
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-01-10 16:47:15 +0100

    remove click event passing for moveDown-MenuAction

--- src/modules/ui/board/BoardMenuActionMoveDown.vue
+++ src/modules/ui/board/BoardMenuActionMoveDown.vue
@@ -2,18 +2,14 @@
 	<BoardMenuAction
 		:icon="mdiArrowDown"
 		data-testid="board-menu-action-move-down"
-		@click="onClick"
 	>
 		{{ t("components.board.action.moveDown") }}
 	</BoardMenuAction>
 </template>
 
 <script setup lang="ts">
-import { mdiArrowDown } from "@icons/material";
 import { BoardMenuAction } from "@ui-board";
+import { mdiArrowDown } from "@icons/material";
 import { useI18n } from "vue-i18n";
 const { t } = useI18n();
-
-const emit = defineEmits(["click"]);
-const onClick = ($event: Event) => emit("click", $event);
 </script>
