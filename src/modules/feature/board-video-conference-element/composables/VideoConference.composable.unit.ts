3558953ca1cf858ccce4376e1316d09b5ad666f2
MartinSchuhmacher <mschuhmacher@gmx.de>
2025-01-13 14:34:09 +0100

    remove comment

--- src/modules/feature/board-video-conference-element/composables/VideoConference.composable.unit.ts
+++ src/modules/feature/board-video-conference-element/composables/VideoConference.composable.unit.ts
@@ -4,7 +4,6 @@ import * as serverApi from "@/serverApi/v3/api";
 import { useVideoConference } from "./VideoConference.composable";
 import {
 	VideoConferenceInfoResponse,
-	// VideoConferenceInfoResponse,
 	VideoConferenceJoinResponse,
 	VideoConferenceScope,
 	VideoConferenceStateResponse,
