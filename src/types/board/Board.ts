7b0d04b0f9bd2449c0bd6ca1b47dc428d4052ee2
Odalys Adam <odalys.adam@dataport.de>
2025-01-14 09:53:59 +0100

    CommonCartridgeExportModal

--- src/types/board/Board.ts
+++ src/types/board/Board.ts
@@ -1,7 +1,21 @@
 import {
 	BoardResponse as Board,
+	BoardColumnBoardResponse as BoardColumnBoard,
+	BoardElementResponse as BoardElement,
+	BoardElementResponseTypeEnum as BoardElementType,
+	BoardLessonResponse as BoardLesson,
+	BoardTaskResponse as BoardTask,
 	ColumnResponse as BoardColumn,
 	CardSkeletonResponse as BoardSkeletonCard,
 } from "@/serverApi/v3";
 
-export { Board, BoardColumn, BoardSkeletonCard };
+export {
+	Board,
+	BoardColumnBoard,
+	BoardElement,
+	BoardElementType,
+	BoardLesson,
+	BoardTask,
+	BoardColumn,
+	BoardSkeletonCard,
+};
