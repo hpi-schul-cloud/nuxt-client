ef3e648ca1a2f6324bac5c4096b8691b2ae70bc4
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-01-16 16:37:29 +0100

    add options for the visibility options

--- src/modules/data/room/roomMembers/membersVisibleOptions.ts
+++ src/modules/data/room/roomMembers/membersVisibleOptions.ts
@@ -4,109 +4,128 @@ type Options = {
 	disabled: boolean;
 };
 
-type ViewOptions = {
+type PageViewOptions = {
 	"selection-column": Options;
 	"actions-column": Options;
+	"add-member-button"?: Options;
 	"checkbox-in-row"?: Options;
 	"actions-in-row"?: Options;
 };
 
-type RoomRoles = Pick<
-	Record<RoleName, ViewOptions>,
+export type UserRoles =
 	| RoleName.Roomowner
 	| RoleName.Roomadmin
 	| RoleName.Roomeditor
-	| RoleName.Roomviewer
->;
+	| RoleName.Roomviewer;
 
-interface User extends RoomMemberResponse {
-	roleName: Extract<
-		RoleName,
-		| RoleName.Roomowner
-		| RoleName.Roomadmin
-		| RoleName.Roomeditor
-		| RoleName.Roomviewer
-	>;
-	id: string;
-}
+type RoomRoles = Pick<Record<RoleName, PageViewOptions>, UserRoles>;
 
-export const useRoomMemberVisibilityOptions = (currentUserId: string) => {
-	const hasVisibleOption = (
-		user: User,
-		source: keyof ViewOptions,
-		action:
-			| keyof ViewOptions["selection-column"]
-			| keyof ViewOptions["actions-column"]
-			| keyof ViewOptions["checkbox-in-row"]
-			| keyof ViewOptions["actions-in-row"]
+const ROLES: RoomRoles = {
+	[RoleName.Roomowner]: {
+		"selection-column": {
+			show: false,
+			disabled: false,
+		},
+		"actions-column": {
+			show: false,
+			disabled: false,
+		},
+		"add-member-button": {
+			show: false,
+			disabled: false,
+		},
+		"checkbox-in-row": {
+			show: true,
+			disabled: false,
+		},
+		"actions-in-row": {
+			show: true,
+			disabled: false,
+		},
+	},
+	[RoleName.Roomadmin]: {
+		"selection-column": {
+			show: true,
+			disabled: false,
+		},
+		"actions-column": {
+			show: true,
+			disabled: false,
+		},
+		"add-member-button": {
+			show: false,
+			disabled: false,
+		},
+		"checkbox-in-row": {
+			show: true,
+			disabled: false,
+		},
+		"actions-in-row": {
+			show: true,
+			disabled: false,
+		},
+	},
+	[RoleName.Roomeditor]: {
+		"selection-column": {
+			show: false,
+			disabled: false,
+		},
+		"actions-column": {
+			show: false,
+			disabled: false,
+		},
+		"checkbox-in-row": {
+			show: false,
+			disabled: false,
+		},
+		"actions-in-row": {
+			show: false,
+			disabled: false,
+		},
+	},
+	[RoleName.Roomviewer]: {
+		"selection-column": {
+			show: false,
+			disabled: false,
+		},
+		"actions-column": {
+			show: false,
+			disabled: false,
+		},
+		"checkbox-in-row": {
+			show: false,
+			disabled: false,
+		},
+		"actions-in-row": {
+			show: false,
+			disabled: false,
+		},
+	},
+};
+
+export const useRoomMemberVisibilityOptions = () => {
+	const checkPageVisibleOption = (
+		userRole: UserRoles,
+		source: keyof PageViewOptions,
+		action: keyof Options
 	) => {
-		const ROLES: RoomRoles = {
-			[RoleName.Roomowner]: {
-				"selection-column": {
-					show: true,
-					disabled: false,
-				},
-				"actions-column": {
-					show: true,
-					disabled: false,
-				},
-				"checkbox-in-row": {
-					show: true,
-					disabled: user.id === currentUserId,
-				},
-				"actions-in-row": {
-					show: true,
-					disabled: false,
-				},
-			},
-			[RoleName.Roomadmin]: {
-				"selection-column": {
-					show: true,
-					disabled: false,
-				},
-				"actions-column": {
-					show: true,
-					disabled: false,
-				},
-				"checkbox-in-row": {
-					show: true,
-					disabled: user.id === currentUserId,
-				},
-				"actions-in-row": {
-					show: true,
-					disabled: false,
-				},
-			},
-			[RoleName.Roomeditor]: {
-				"selection-column": {
-					show: true,
-					disabled: false,
-				},
-				"actions-column": {
-					show: false,
-					disabled: false,
-				},
-			},
-			[RoleName.Roomviewer]: {
-				"selection-column": {
-					show: false,
-					disabled: false,
-				},
-				"actions-column": {
-					show: false,
-					disabled: false,
-				},
-			},
-		};
-
-		const sourceOption = ROLES[user.roleName]?.[source];
-		if (!action || typeof sourceOption === "boolean") {
-			return sourceOption;
+		if (!ROLES[userRole] || !ROLES[userRole][source]) {
+			return false;
 		}
-		return sourceOption?.[action];
+
+		return ROLES[userRole][source][action];
 	};
 
 	return {
-		hasVisibleOption,
+		checkPageVisibleOption,
 	};
 };
+
+// Usage:
+
+const { checkPageVisibleOption } = useRoomMemberVisibilityOptions();
+
+checkPageVisibleOption(RoleName.Roomowner, "selection-column", "show"); // true
+checkPageVisibleOption(RoleName.Roomowner, "selection-column", "disabled"); // false
+checkPageVisibleOption(RoleName.Roomviewer, "selection-column", "show");
+checkPageVisibleOption(RoleName.Roomviewer, "actions-in-row", "show"); // false
