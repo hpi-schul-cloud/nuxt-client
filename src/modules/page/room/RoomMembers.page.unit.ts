747e0c560e4f9a6f9007b5d012d7927183bbc6d9
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-01-17 09:03:08 +0100

    fix unit tests

--- src/modules/page/room/RoomMembers.page.unit.ts
+++ src/modules/page/room/RoomMembers.page.unit.ts
@@ -204,9 +204,9 @@ describe("RoomMembersPage", () => {
 				await addMemberButton.trigger("click");
 
 				expect(mockRoomMemberCalls.getSchools).toHaveBeenCalled();
-				expect(mockRoomMemberCalls.getPotentialMembers).toHaveBeenCalledWith({
-					role: RoleName.Roomeditor,
-				});
+				expect(mockRoomMemberCalls.getPotentialMembers).toHaveBeenCalledWith(
+					RoleName.Teacher
+				);
 			});
 
 			it("should open Dialog", async () => {
@@ -276,7 +276,10 @@ describe("RoomMembersPage", () => {
 					await dialog.setValue(true);
 					const addMemberComponent = dialog.getComponent(AddMembers);
 
-					await addMemberComponent.vm.$emit("update:role");
+					await addMemberComponent.vm.$emit("update:role", {
+						schoolRole: RoleName.Teacher,
+						schoolId: "school-id",
+					});
 
 					expect(mockRoomMemberCalls.getPotentialMembers).toHaveBeenCalled();
 				});
