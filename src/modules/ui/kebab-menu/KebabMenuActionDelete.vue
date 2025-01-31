db3090f23651e9ff59ffeeb9a1760a0aecc51e38
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-01-31 17:04:34 +0100

    refactored KebabMenuActionDelete to receive scopeLanguageKey for more flexibility

--- src/modules/ui/kebab-menu/KebabMenuActionDelete.vue
+++ src/modules/ui/kebab-menu/KebabMenuActionDelete.vue
@@ -30,29 +30,8 @@ const emit = defineEmits(["click"]);
 
 const { askDeleteConfirmation } = useDeleteConfirmationDialog();
 
-// const languageKeyForScopeType: Record<BoardMenuScope, keyof MessageSchema> = {
-// 	[BoardMenuScope.BOARD]: "components.board",
-// 	[BoardMenuScope.COLUMN]: "components.boardColumn",
-// 	[BoardMenuScope.CARD]: "components.boardCard",
-// 	[BoardMenuScope.COLLABORATIVE_TEXT_EDITOR_ELEMENT]:
-// 		"components.cardElement.collaborativeTextEditorElement",
-// 	[BoardMenuScope.DRAWING_ELEMENT]: "components.cardElement.drawingElement",
-// 	[BoardMenuScope.EXTERNAL_TOOL_ELEMENT]:
-// 		"components.cardElement.externalToolElement",
-// 	[BoardMenuScope.FILE_ELEMENT]: "components.cardElement.fileElement",
-// 	[BoardMenuScope.LINK_ELEMENT]: "components.cardElement.LinkElement",
-// 	[BoardMenuScope.SUBMISSION_ELEMENT]:
-// 		"components.cardElement.submissionElement",
-// 	[BoardMenuScope.DELETED_ELEMENT]: "components.cardElement.deletedElement",
-// 	[BoardMenuScope.MEDIA_EXTERNAL_TOOL_ELEMENT]:
-// 		"components.cardElement.mediaExternalToolElement",
-// 	[BoardMenuScope.VIDEO_CONFERENCE_ELEMENT]:
-// 		"components.cardElement.videoConferenceElement",
-// };
-
 const onClick = (): void => {
 	const promise = askDeleteConfirmation(props.name, props.scopeLanguageKey);
-
 	emit("click", promise);
 };
 </script>
