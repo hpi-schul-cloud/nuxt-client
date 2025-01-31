e0a262064307d4e27f18dc592c49dedcde6cd6e6
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-01-31 17:02:04 +0100

    add new KebabMenuActions

--- src/modules/ui/kebab-menu/KebabMenuActionDelete.vue
+++ src/modules/ui/kebab-menu/KebabMenuActionDelete.vue
@@ -11,16 +11,18 @@
 <script setup lang="ts">
 import type { MessageSchema } from "@/locales/schema";
 import { mdiTrashCanOutline } from "@icons/material";
-import { BoardMenuScope } from "@ui-board";
 import { useDeleteConfirmationDialog } from "@ui-confirmation-dialog";
 import { useI18n } from "vue-i18n";
-import { PropType } from "vue";
 import KebabMenuAction from "./KebabMenuAction.vue";
+import { PropType } from "vue";
 const { t } = useI18n();
 
 const props = defineProps({
 	name: { type: String, required: false },
-	scope: { type: String as PropType<BoardMenuScope>, required: true },
+	scopeLanguageKey: {
+		type: String as PropType<keyof MessageSchema>,
+		required: true,
+	},
 	skipDeleteConfirmation: { type: Boolean, default: () => false },
 });
 
@@ -28,31 +30,28 @@ const emit = defineEmits(["click"]);
 
 const { askDeleteConfirmation } = useDeleteConfirmationDialog();
 
-const languageKeyForScopeType: Record<BoardMenuScope, keyof MessageSchema> = {
-	[BoardMenuScope.BOARD]: "components.board",
-	[BoardMenuScope.COLUMN]: "components.boardColumn",
-	[BoardMenuScope.CARD]: "components.boardCard",
-	[BoardMenuScope.COLLABORATIVE_TEXT_EDITOR_ELEMENT]:
-		"components.cardElement.collaborativeTextEditorElement",
-	[BoardMenuScope.DRAWING_ELEMENT]: "components.cardElement.drawingElement",
-	[BoardMenuScope.EXTERNAL_TOOL_ELEMENT]:
-		"components.cardElement.externalToolElement",
-	[BoardMenuScope.FILE_ELEMENT]: "components.cardElement.fileElement",
-	[BoardMenuScope.LINK_ELEMENT]: "components.cardElement.LinkElement",
-	[BoardMenuScope.SUBMISSION_ELEMENT]:
-		"components.cardElement.submissionElement",
-	[BoardMenuScope.DELETED_ELEMENT]: "components.cardElement.deletedElement",
-	[BoardMenuScope.MEDIA_EXTERNAL_TOOL_ELEMENT]:
-		"components.cardElement.mediaExternalToolElement",
-	[BoardMenuScope.VIDEO_CONFERENCE_ELEMENT]:
-		"components.cardElement.videoConferenceElement",
-};
+// const languageKeyForScopeType: Record<BoardMenuScope, keyof MessageSchema> = {
+// 	[BoardMenuScope.BOARD]: "components.board",
+// 	[BoardMenuScope.COLUMN]: "components.boardColumn",
+// 	[BoardMenuScope.CARD]: "components.boardCard",
+// 	[BoardMenuScope.COLLABORATIVE_TEXT_EDITOR_ELEMENT]:
+// 		"components.cardElement.collaborativeTextEditorElement",
+// 	[BoardMenuScope.DRAWING_ELEMENT]: "components.cardElement.drawingElement",
+// 	[BoardMenuScope.EXTERNAL_TOOL_ELEMENT]:
+// 		"components.cardElement.externalToolElement",
+// 	[BoardMenuScope.FILE_ELEMENT]: "components.cardElement.fileElement",
+// 	[BoardMenuScope.LINK_ELEMENT]: "components.cardElement.LinkElement",
+// 	[BoardMenuScope.SUBMISSION_ELEMENT]:
+// 		"components.cardElement.submissionElement",
+// 	[BoardMenuScope.DELETED_ELEMENT]: "components.cardElement.deletedElement",
+// 	[BoardMenuScope.MEDIA_EXTERNAL_TOOL_ELEMENT]:
+// 		"components.cardElement.mediaExternalToolElement",
+// 	[BoardMenuScope.VIDEO_CONFERENCE_ELEMENT]:
+// 		"components.cardElement.videoConferenceElement",
+// };
 
 const onClick = (): void => {
-	const promise = askDeleteConfirmation(
-		props.name,
-		languageKeyForScopeType[props.scope]
-	);
+	const promise = askDeleteConfirmation(props.name, props.scopeLanguageKey);
 
 	emit("click", promise);
 };
--- src/modules/ui/kebab-menu/KebabMenuActionEditMembers.vue
+++ src/modules/ui/kebab-menu/KebabMenuActionEditMembers.vue
@@ -1,15 +1,15 @@
 <template>
 	<KebabMenuAction
-		:icon="mdiPencilOutline"
-		data-testid="kebab-menu-action-edit"
+		:icon="mdiAccountGroupOutline"
+		data-testid="kebab-menu-action-edit-members"
 	>
-		{{ text ?? t("common.actions.edit") }}
+		{{ text ?? t("pages.rooms.members.manage") }}
 	</KebabMenuAction>
 </template>
 
 <script setup lang="ts">
 import KebabMenuAction from "./KebabMenuAction.vue";
-import { mdiPencilOutline } from "@icons/material";
+import { mdiAccountGroupOutline } from "@icons/material";
 import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 defineProps({
