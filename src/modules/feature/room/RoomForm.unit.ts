7f72753c9b1437caa275b1461bbfd2d78e197a10
Max Bischof <maximilian.bischof@dataport.de>
2025-02-04 19:06:45 +0100

    Add unit test to roomform

--- src/modules/feature/room/RoomForm.unit.ts
+++ src/modules/feature/room/RoomForm.unit.ts
@@ -34,6 +34,26 @@ describe("@feature-room/RoomForm", () => {
 		return { wrapper };
 	};
 
+	describe("when room name contains < followed by a string", () => {
+		const mockRoom: RoomCreateParams = {
+			name: "Room 1",
+			color: RoomColorEnum.Magenta,
+			startDate: "",
+			endDate: "",
+		};
+
+		it("should show error message", async () => {
+			const { wrapper } = setup({ room: mockRoom });
+
+			const textField = wrapper.findComponent({ name: "VTextField" });
+			const input = textField.find("input");
+
+			await input.setValue("<abc");
+
+			expect(wrapper.text()).toContain("common.validation.containsOpeningTag");
+		});
+	});
+
 	describe("when save button is clicked", () => {
 		describe("when room data is invalid", () => {
 			it("should not emit 'save' event", async () => {
