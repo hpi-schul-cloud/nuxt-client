cb4eb9dae26df4f4941470725c6d6f5444b90f87
wolfganggreschus <wolfgang@greschus.de>
2025-02-13 11:53:31 +0100

    Add type definitions for queries, resources and lessons in content.ts

---
+++ src/store/types/content.ts
@@ -0,0 +1,137 @@
+/**
+ *
+ */
+export type Query = {
+	$limit: number;
+	$skip: number;
+	searchQuery: string;
+};
+
+export type Resource = {
+	access: string[];
+	aspects: string[];
+	collection: any;
+	commentCount: number | null;
+	content: object;
+	createdAt: any;
+	createdBy: object;
+	downloadUrl: string | null;
+	iconURL: string | null;
+	isDirectory: boolean;
+	license: object;
+	mediatype: string;
+	metadataset: string;
+	mimetype: string;
+	modifiedAt: any;
+	modifiedBy: object;
+	name: string;
+	owner: object;
+	parent: object;
+	preview: object;
+	properties: ResourceProperties;
+	rating: any;
+	ref: object;
+	remote: any;
+	repositoryType: string;
+	size: string;
+	title: string;
+	type: string;
+};
+
+export type ResourceProperties = {
+	"cclom:aggregationlevel": string[];
+	"cclom:format": string[];
+	"cclom:general_description": string[];
+	"cclom:general_keyword": string[];
+	"cclom:location": string[];
+	"cclom:title": string[];
+	"cclom:version": string[];
+	"ccm:commonlicense_cc_version": string[];
+	"ccm:commonlicense_key": string[];
+	"ccm:create_version": string[];
+	"ccm:educontextname": string[];
+	"ccm:hpi_lom_relation": string[];
+	"ccm:hpi_searchable": string[];
+	"ccm:lom_relation": string[];
+	"ccm:objecttype": string[];
+	"ccm:objecttype_DISPLAYNAME": string[];
+	"ccm:original": string[];
+	"ccm:replicationsource": string[];
+	"ccm:replicationsourcehash": string[];
+	"ccm:replicationsourceid": string[];
+	"ccm:replicationsourceuuid": string[];
+	"ccm:taxonid": string[];
+	"ccm:taxonid_DISPLAYNAME": string[];
+	"ccm:userdefined_preview": string[];
+	"ccm:version_comment": string[];
+	"ccm:wwwurl": string[];
+	"cm:autoVersion": string[];
+	"cm:autoVersionOnUpdateProps": string[];
+	"cm:automaticUpdate": string[];
+	"cm:created": string[];
+	"cm:created_LONG": string[];
+	"cm:creator": string[];
+	"cm:edu_forcemetadataset": string[];
+	"cm:edu_metadataset": string[];
+	"cm:initialVersion": string[];
+	"cm:modified": string[];
+	"cm:modified_LONG": string[];
+	"cm:modifier": string[];
+	"cm:name": string[];
+	"cm:versionLabel": string[];
+	"sys:locale": string[];
+	"sys:node-dbid": string[];
+	"sys:node-uuid": string[];
+	"sys:store-identifier": string[];
+	"sys:store-protocol": string[];
+	"virtual:primaryparent_nodeid": string[];
+};
+
+export type Resources = {
+	total: number;
+	limit: number;
+	skip: number;
+	data: Resource[];
+	pagination: undefined;
+};
+
+export type Elements = {
+	data: any[];
+	limit: number;
+	skip: number;
+	total: number;
+	pagination: undefined;
+};
+
+export type Lesson = {
+	contents: any[];
+	courseId: string;
+	createdAt: string;
+	date: string;
+	hidden: boolean;
+	isCopyFrom: any;
+	materialIds: string[];
+	name: string;
+	position: number;
+	time: string;
+	updatedAt: string;
+	_id: string;
+};
+
+export type Lessons = {
+	data: Lesson[];
+	limit: number;
+	skip: number;
+	total: number;
+};
+
+export type AddToLessonQuery = {
+	lessonId: string;
+	event: object;
+	material: {
+		client: string;
+		merlinReference: string;
+		title: string;
+		url: string;
+	};
+};
