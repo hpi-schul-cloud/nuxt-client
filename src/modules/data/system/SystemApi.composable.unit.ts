04c2a305bf44e3192296c67595580f9c94a32b7d
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-02-13 14:22:07 +0100

    update external logout

--- src/modules/data/system/SystemApi.composable.unit.ts
+++ src/modules/data/system/SystemApi.composable.unit.ts
@@ -1,3 +1,4 @@
+import { OauthConfigResponse } from "./../../../serverApi/v3/api";
 import * as serverApi from "@/serverApi/v3/api";
 import { PublicSystemResponse, SystemsApiInterface } from "@/serverApi/v3/api";
 import { createMock, DeepMocked } from "@golevelup/ts-jest";
@@ -58,6 +59,7 @@ describe("SystemApi.composable", () => {
 				expect(result).toEqual({
 					id: system.id,
 					displayName: system.displayName,
+					hasEndSessionEndpoint: false,
 				});
 			});
 		});
@@ -80,6 +82,40 @@ describe("SystemApi.composable", () => {
 				expect(useErrorHandlerMock.handleError).toHaveBeenCalledWith(error, {
 					404: undefined,
 					500: undefined,
+				});
+			});
+		});
+	});
+
+	describe("getSystem with OAuthConfig", () => {
+		const setup = () => {
+			const system: PublicSystemResponse = {
+				id: "systemId",
+				displayName: "displayName",
+				oauthConfig: {
+					endSessionEndpoint: "endSessionEndpoint",
+				} as OauthConfigResponse,
+			};
+
+			systemApi.systemControllerGetSystem.mockResolvedValue(
+				mockApiResponse({ data: system })
+			);
+
+			return {
+				system,
+			};
+		};
+
+		describe("when the api call succeeds", () => {
+			it("should return a system", async () => {
+				const { system } = setup();
+
+				const result = await useSystemApi().getSystem("systemId");
+
+				expect(result).toEqual({
+					id: system.id,
+					displayName: system.displayName,
+					hasEndSessionEndpoint: true,
 				});
 			});
 		});
