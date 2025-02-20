0e665fa70c237da1098bfcd3f5a209458343b277
Odalys Adam <odalys.adam@dataport.de>
2025-02-20 11:31:30 +0100

    adjust type import to not cause error in CommonCartridgeExportModal type assertion

--- src/types/board/Board.ts
+++ src/types/board/Board.ts
@@ -2,12 +2,12 @@ import type {
 	BoardResponse as Board,
 	BoardColumnBoardResponse as BoardColumnBoard,
 	BoardElementResponse as BoardElement,
-	BoardElementResponseTypeEnum as BoardElementType,
 	BoardLessonResponse as BoardLesson,
 	BoardTaskResponse as BoardTask,
 	ColumnResponse as BoardColumn,
 	CardSkeletonResponse as BoardSkeletonCard,
 } from "@/serverApi/v3";
+import { BoardElementResponseTypeEnum as BoardElementType } from "@/serverApi/v3";
 
 export {
 	Board,
