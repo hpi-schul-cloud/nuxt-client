327dcfdc522de3c0e5f7b109d86dc363d0717b1e
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-02-27 15:33:51 +0100

    chore: fix failing unit tests for BoardPermission.composable

--- src/modules/data/board/BoardPermissions.composable.unit.ts
+++ src/modules/data/board/BoardPermissions.composable.unit.ts
@@ -1,18 +1,22 @@
 import AuthModule from "@/store/auth";
 import { useBoardPermissions } from "./BoardPermissions.composable";
 import { createModuleMocks } from "@@/tests/test-utils/mock-store-module";
+import { mountComposable } from "@@/tests/test-utils";
+import { createTestingI18n } from "@@/tests/test-utils/setup";
 
 describe("BoardPermissions.composable", () => {
 	let authModuleMock: jest.Mocked<AuthModule>;
 	beforeEach(() => {
 		authModuleMock = createModuleMocks(AuthModule, {
 			getUserPermissions: ["course_edit", "course_create", "course_remove"],
+			getUserRoles: ["teacher"],
 		});
 	});
 
-	// trouble with authModule mocking
 	it("should set user permissions to boardPermission", async () => {
-		const permissions = useBoardPermissions();
+		const permissions = mountComposable(() => useBoardPermissions(), {
+			global: { plugins: [createTestingI18n()] },
+		});
 
 		expect(authModuleMock).toBeDefined();
 		expect(permissions).toBeDefined();
