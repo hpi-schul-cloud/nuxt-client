0b612a254ca080c1ab4248723ae213a2fb2e9adb
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-03-03 13:21:55 +0100

    BC-9069 - dependency updates @vueuse packages (#3576)
    * update @vueuse/integrations from v12.0.0 to 12.7.0
    * update @vueuse/components from v10.11.0 to 12.7.0
    * update @vueuse/core from v10.11.0 to 12.7.0
    
    * remove useBreakpoints from vueuse and use vuetifys useDisplay() instead

--- src/modules/util/device-detection/index.ts
+++ src/modules/util/device-detection/index.ts
@@ -1,4 +1,3 @@
-import { useVuetifyBreakpoints } from "./VuetifyBreakpoints.composable";
 import { useTouchDetection } from "./TouchDetection.composable";
 
-export { useVuetifyBreakpoints, useTouchDetection };
+export { useTouchDetection };
