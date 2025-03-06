777344ca364d9717c7d3a0857c0c04ea414fdeff
dyedwiper <maximilian.becker@dataport.de>
2025-03-06 17:24:04 +0100

    Reduce height of add-card-button

--- src/modules/feature/board/board/BoardAddCardButton.vue
+++ src/modules/feature/board/board/BoardAddCardButton.vue
@@ -1,7 +1,6 @@
 <template>
 	<div
-		class="d-flex w-100 justify-center button-background pb-4 pt-2 text-center sticky"
-		style="height: 4rem"
+		class="d-flex w-100 justify-center button-background pb-2 pt-2 text-center sticky"
 		ref="sticky"
 	>
 		<VBtn
