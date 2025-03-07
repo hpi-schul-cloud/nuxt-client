18077533d79b34a8864e7b29f15f814698556186
wolfganggreschus <wolfgang@greschus.de>
2025-03-07 15:42:01 +0100

    Extend logger class with info and warn methods; add timestamps and prevent console output in the production environment

--- src/modules/util/logger/logger.ts
+++ src/modules/util/logger/logger.ts
@@ -1,11 +1,33 @@
 /* eslint-disable no-console */
+const isProduction = process.env.NODE_ENV === "production";
+
 class Logger {
-	static log(...message: unknown[]) {
-		console.log(...message);
+	static info(message: unknown[], ...optionalParams: unknown[]) {
+		if (!isProduction) {
+			console.log(
+				`[INFO] ${new Date().toISOString()} -`,
+				message,
+				...optionalParams
+			);
+		}
 	}
 
-	static error(...message: unknown[]) {
-		console.error(message);
+	static warn(message: unknown[], ...optionalParams: unknown[]) {
+		if (!isProduction) {
+			console.warn(
+				`[WARN] ${new Date().toISOString()} -`,
+				message,
+				...optionalParams
+			);
+		}
+	}
+
+	static error(message: unknown[], ...optionalParams: unknown[]) {
+		console.error(
+			`[ERROR] ${new Date().toISOString()} -`,
+			message,
+			...optionalParams
+		);
 	}
 }
 
