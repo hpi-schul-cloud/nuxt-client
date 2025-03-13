6e988494e119668d35d17adadeb3bd0c92fca01d
dyedwiper <maximilian.becker@dataport.de>
2025-03-13 16:33:59 +0100

    Fix drag and drop for new columns

--- src/modules/feature/board/board/BoardColumnGhost.vue
+++ src/modules/feature/board/board/BoardColumnGhost.vue
@@ -1,5 +1,5 @@
 <template>
-	<div ref="ghostColumnRef" :class="{ 'pl-4 pr-6': !isListBoard }">
+	<div ref="ghostColumnRef" :class="{ 'h-100 pl-4 pr-6': !isListBoard }">
 		<BoardSectionCreationHeader
 			:label="title"
 			:isColumnActive="isColumnHovered"
@@ -9,7 +9,7 @@
 		<div
 			:style="{ 'min-width': colWidth + 'px' }"
 			class="column-drag-handle grow-transition"
-			:class="{ 'mr-4': !isListBoard }"
+			:class="{ 'mr-4 sortable-container': !isListBoard }"
 		>
 			<Sortable
 				:list="[]"
@@ -30,7 +30,7 @@
 					forceFallback: true,
 					bubbleScroll: true,
 				}"
-				:class="ghostColumnStyle"
+				:class="ghostColumnClasses"
 			/>
 		</div>
 	</div>
@@ -59,7 +59,7 @@ const colWidth = computed<number>(() =>
 
 const onAddColumn = () => emit("create:column");
 
-const ghostColumnStyle = computed(() => {
+const ghostColumnClasses = computed(() => {
 	const classes = ["d-flex", "flex-row", "flex-shrink-1"];
 	if (!props.isListBoard) {
 		classes.push("column-container", "ml-n4", "pl-2");
@@ -83,12 +83,16 @@ const title = computed(() =>
 	transition: min-width 200ms;
 }
 
+/* Subtracted is the height of the column-header. */
+.sortable-container {
+	height: calc(100% - 77px);
+}
+
 /**
  * This rule extends the droppable area of columns.
  * Without this rule cards have to be placed closely below the last card in a column to be added.
 */
 .column-container {
-	min-height: 70vh;
 	height: 100%;
 	padding-bottom: 50px;
 }
