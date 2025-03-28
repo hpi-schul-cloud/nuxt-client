26373e97819dbf3be02d3826234bf04aae115856
dyedwiper <maximilian.becker@dataport.de>
2025-03-28 11:17:21 +0100

    Update scrollbar styles of media-board

--- src/modules/feature/media-shelf/MediaBoard.vue
+++ src/modules/feature/media-shelf/MediaBoard.vue
@@ -137,30 +137,19 @@ const onLineDragEnd = async (event: SortableEvent) => {
 	overflow: auto hidden;
 }
 
-/* height */
-.scrollable-line::-webkit-scrollbar {
-	height: 8px;
-}
-
-/* Track */
 .scrollable-line::-webkit-scrollbar-track {
 	background: transparent;
-	border: none;
 }
 
-/* Handle */
 .scrollable-line::-webkit-scrollbar-thumb {
 	background-color: transparent;
-	border-radius: 5px;
 }
 
 .line-drag-handle:hover .scrollable-line::-webkit-scrollbar-thumb {
 	background-color: rgba(var(--v-theme-on-surface), 0.6);
-	border-radius: 5px;
 }
 
-/* Handle on hover */
-.scrollable-line::-webkit-scrollbar-thumb:hover {
-	background: rgba(var(--v-theme-on-surface), 0.8) !important;
+.line-drag-handle:hover .scrollable-line::-webkit-scrollbar-thumb:hover {
+	background: rgba(var(--v-theme-on-surface), 0.8);
 }
 </style>
