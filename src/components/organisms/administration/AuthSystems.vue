34b5fa1ef747436795f4cdf104c5f4996d9e3413
MBergCap <111343628+MBergCap@users.noreply.github.com>
2025-04-07 16:24:19 +0200

    N21-1794 add data-testid (#3617)

--- src/components/organisms/administration/AuthSystems.vue
+++ src/components/organisms/administration/AuthSystems.vue
@@ -48,12 +48,13 @@
 				</thead>
 				<tbody>
 					<tr v-for="system in systems" :key="system.id">
-						<td>{{ system.alias }}</td>
-						<td>{{ system.type }}</td>
+						<td data-testid="system-table-alias">{{ system.alias }}</td>
+						<td data-testid="system-table-type">{{ system.type }}</td>
 						<td v-if="customLoginLinkEnabled">
 							<v-text-field
 								v-if="isLoginSystem(system)"
 								:id="`school-login-link-${system.id}`"
+								data-testid="system-table-login-link"
 								:model-value="generateLoginLink(system)"
 								class="school-login-link"
 								:color="getCopyStatus(system.id) ? 'success' : 'primary'"
@@ -84,6 +85,7 @@
 								class="edit-system-btn"
 								icon
 								variant="text"
+								data-testid="system-table-button-edit"
 								:to="redirectTo(system)"
 								:aria-label="ariaLabels(system).edit"
 							>
@@ -94,6 +96,7 @@
 								class="delete-system-btn"
 								icon
 								variant="text"
+								data-testid="system-table-button-delete"
 								:aria-label="ariaLabels(system).delete"
 								@click.stop="openConfirmDeleteDialog(system.id)"
 							>
