0673c66e4f0f2f1de4b3e4c1a74dfad7dda119ff
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-04-16 11:20:14 +0200

    fix change Role test, adjust factory for room members

--- tests/test-utils/factory/room/roomMembersFactory.ts
+++ tests/test-utils/factory/room/roomMembersFactory.ts
@@ -2,21 +2,17 @@ import { Factory } from "fishery";
 import { RoleName, SchoolForExternalInviteResponse } from "@/serverApi/v3";
 import { RoomMember } from "@data-room";
 
-export const roomMemberFactory = (
-	roomRoleName = RoleName.Roomowner,
-	schoolRoleNames = [RoleName.Teacher]
-) =>
-	Factory.define<RoomMember>(({ sequence }) => ({
-		userId: `member${sequence}`,
-		firstName: `firstName${sequence}`,
-		lastName: `lastName${sequence}`,
-		roomRoleName,
-		schoolRoleNames,
-		schoolName: "Paul-Gerhardt-Gymnasium",
-		displayRoomRole: `displayRoomRole${sequence}`,
-		displaySchoolRole: `displaySchoolRole${sequence}`,
-		isSelectable: true,
-	}));
+export const roomMemberFactory = Factory.define<RoomMember>(({ sequence }) => ({
+	userId: `member${sequence}`,
+	firstName: `firstName${sequence}`,
+	lastName: `lastName${sequence}`,
+	roomRoleName: RoleName.Roomadmin,
+	schoolRoleNames: [RoleName.Teacher],
+	schoolName: "Paul-Gerhardt-Gymnasium",
+	displayRoomRole: `displayRoomRole${sequence}`,
+	displaySchoolRole: `displaySchoolRole${sequence}`,
+	isSelectable: true,
+}));
 
 export const roomMemberListFactory = Factory.define<RoomMember>(
 	({ sequence }) => ({
