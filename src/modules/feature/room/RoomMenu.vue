1b14be10edde0ffed4c7eebf00d6685f2dbcf9c2
MartinSchuhmacher <mschuhmacher@gmx.de>
2025-04-23 23:08:40 +0200

    handing computed language key to the menu action component

--- src/modules/feature/room/RoomMenu.vue
+++ src/modules/feature/room/RoomMenu.vue
@@ -7,7 +7,7 @@
 		<KebabMenuActionEdit v-if="canEditRoom" @click="() => $emit('room:edit')" />
 		<KebabMenuActionRoomMembers
 			v-if="canViewRoom"
-			:can-add-room-members="canAddRoomMembers"
+			:members-info-text="membersInfoText"
 			@click="() => $emit('room:manage-members')"
 		/>
 		<KebabMenuActionDelete
@@ -31,6 +31,7 @@ import {
 } from "@ui-kebab-menu";
 import { useRoomAuthorization } from "@feature-room";
 import { useI18n } from "vue-i18n";
+import { computed } from "vue";
 
 const { t } = useI18n();
 
@@ -53,4 +54,10 @@ const onDeleteRoom = async (confirmation: Promise<boolean>) => {
 
 const { canAddRoomMembers, canEditRoom, canDeleteRoom, canViewRoom } =
 	useRoomAuthorization();
+
+const membersInfoText = computed(() =>
+	canAddRoomMembers.value
+		? t("pages.rooms.members.manage")
+		: t("pages.rooms.members.view")
+);
 </script>
--- src/modules/ui/kebab-menu/KebabMenuActionRoomMembers.vue
+++ src/modules/ui/kebab-menu/KebabMenuActionRoomMembers.vue
@@ -10,17 +10,8 @@
 <script setup lang="ts">
 import KebabMenuAction from "./KebabMenuAction.vue";
 import { mdiAccountGroupOutline } from "@icons/material";
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
-const { t } = useI18n();
 
-const props = defineProps({
-	canAddRoomMembers: { type: Boolean, required: true, default: false },
+defineProps({
+	membersInfoText: { type: String, required: true },
 });
-
-const membersInfoText = computed(() =>
-	props.canAddRoomMembers
-		? t("pages.rooms.members.manage")
-		: t("pages.rooms.members.view")
-);
 </script>
