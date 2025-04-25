9ec4d74f882d12e587439f00d3bdc94f36ffb182
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-04-25 11:19:19 +0200

    add menu to show the datepicker

--- src/modules/feature/room/RoomMembers/InviteMembers.vue
+++ src/modules/feature/room/RoomMembers/InviteMembers.vue
@@ -1,89 +1,91 @@
 <template>
-	<div @click.stop>
-		<v-card ref="inviteMembersContent">
-			<template #title>
-				<h2 class="text-h4 mt-2">
-					{{ t("pages.rooms.members.inviteMember.title") }}
-				</h2>
-			</template>
+	<v-card ref="inviteMembersContent">
+		<template #title>
+			<h2 class="text-h4 mt-2">
+				{{ t("pages.rooms.members.inviteMember.title") }}
+			</h2>
+		</template>
 
-			<template #text>
-				<p>
-					Im nächsten Schritt wird ein Link erstellt, mit dem der Raum betreten
-					werden kann.
-				</p>
-				<InfoAlert
-					>Lernende anderer Schulen müssen immer durch eine aufsichtsführende
-					Lernbegleitung ihrer Schule zum Raum hinzugefügt werden.</InfoAlert
-				>
+		<template #text>
+			<p>
+				Im nächsten Schritt wird ein Link erstellt, mit dem der Raum betreten
+				werden kann.
+			</p>
+			<InfoAlert
+				>Lernende anderer Schulen müssen immer durch eine aufsichtsführende
+				Lernbegleitung ihrer Schule zum Raum hinzugefügt werden.</InfoAlert
+			>
 
-				<div @click.stop>
+			<div>
+				<v-text-field
+					label="Beschreibung (optional)"
+					hint="Wird in der Link-Übersicht angezeigt"
+					persistent-hint
+				/>
+				<v-checkbox
+					label="Link nur für Nutzende der folgenden Schule gültig [Schulname (hier entstehen wohl mindestens 2 Zeilen)]"
+					hide-details
+				/>
+				<v-checkbox label="Link auch für Lernende gültig" hide-details />
+
+				<div class="d-flex align-center justify-start">
+					<v-checkbox id="checkDate" hide-details />
+					<label for="checkDate">Link läuft ab am</label>
 					<v-text-field
-						label="Beschreibung (optional)"
-						hint="Wird in der Link-Übersicht angezeigt"
-						persistent-hint
+						v-date-input-mask
+						:placeholder="t('common.placeholder.dateformat')"
+						style="max-width: 100px"
+						class="me-4"
 					/>
-					<v-checkbox
-						label="Link nur für Nutzende der folgenden Schule gültig [Schulname (hier entstehen wohl mindestens 2 Zeilen)]"
-						hide-details
-					/>
-					<v-checkbox label="Link auch für Lernende gültig" hide-details />
+					<v-menu
+						transition="scale-transition"
+						location="top"
+						:close-on-content-click="false"
+						:open-on-click="true"
+					>
+						<template #activator="{ props }">
+							<v-btn v-bind="props" variant="flat">
+								<v-icon :icon="mdiCalendar" />
+							</v-btn>
+						</template>
 
-					<div class="d-flex align-center justify-start" @click.stop>
-						<v-checkbox id="checkDate" hide-details />
-						<label for="checkDate">Link läuft ab am</label>
-						<v-text-field
-							v-date-input-mask
-							:placeholder="t('common.placeholder.dateformat')"
-							style="max-width: 100px"
-							class="me-4"
+						<v-date-picker
+							v-model="dateObject"
+							@update:model-value="onDateUpdate"
 						/>
-						<v-menu transition="scale-transition" location="top" @click.stop>
-							<template #activator="{ props }">
-								<v-btn v-bind="props" variant="flat">
-									<v-icon :icon="mdiCalendar" />
-								</v-btn>
-							</template>
-
-							<v-date-picker
-								v-model="dateObject"
-								@update:model-value="onDateUpdate"
-								@click.stop
-							/>
-						</v-menu>
-					</div>
-
-					<v-checkbox
-						label="Betreten des Raums nur nach Bestätigung möglich (weitere Informationen)"
-						hide-details
-					/>
+					</v-menu>
 				</div>
-			</template>
 
-			<template #actions>
-				<v-spacer />
-				<div class="mr-4 mb-3">
-					<v-btn
-						ref="cancelButton"
-						class="ms-auto mr-2"
-						color="primary"
-						:text="t('common.actions.cancel')"
-						data-testid="add-participant-cancel-btn"
-						@click="onClose"
-					/>
-					<v-btn
-						ref="addButton"
-						class="ms-auto"
-						color="primary"
-						variant="flat"
-						:text="t('common.actions.add')"
-						data-testid="add-participant-save-btn"
-						@click="onInviteMembers"
-					/>
-				</div>
-			</template>
-		</v-card>
-	</div>
+				<v-checkbox
+					label="Betreten des Raums nur nach Bestätigung möglich (weitere Informationen)"
+					hide-details
+				/>
+			</div>
+		</template>
+
+		<template #actions>
+			<v-spacer />
+			<div class="mr-4 mb-3">
+				<v-btn
+					ref="cancelButton"
+					class="ms-auto mr-2"
+					color="primary"
+					:text="t('common.actions.cancel')"
+					data-testid="add-participant-cancel-btn"
+					@click="onClose"
+				/>
+				<v-btn
+					ref="addButton"
+					class="ms-auto"
+					color="primary"
+					variant="flat"
+					:text="t('common.actions.add')"
+					data-testid="add-participant-save-btn"
+					@click="onInviteMembers"
+				/>
+			</div>
+		</template>
+	</v-card>
 </template>
 
 <script setup lang="ts">
