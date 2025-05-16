634d9079d861916c22b0202b127677f3487d5880
Max Bischof <maximilian.bischof@dataport.de>
2025-05-16 10:51:10 +0200

    Remove unecessary code in kebabmenuaction

--- src/modules/ui/kebab-menu/KebabMenuAction.vue
+++ src/modules/ui/kebab-menu/KebabMenuAction.vue
@@ -4,7 +4,6 @@
 		role="menuitem"
 		:aria-label="ariaLabel"
 		:disabled="disabled"
-		@click="onClick"
 	>
 		<template #prepend>
 			<VIcon :icon="icon" data-testid="kebab-menu-action-icon" />
@@ -19,20 +18,10 @@
 import { PropType } from "vue";
 import { IconProps } from "vuetify";
 
-const props = defineProps({
+defineProps({
 	icon: { type: String as PropType<IconProps["icon"]>, default: undefined },
 	dataTestId: { type: String as PropType<string | null>, default: null },
 	ariaLabel: { type: String as PropType<string | null>, default: null },
 	disabled: { type: Boolean as PropType<boolean>, default: false },
 });
-
-const emit = defineEmits(["click"]);
-
-const onClick = () => {
-	if (props.disabled) {
-		return;
-	}
-
-	emit("click");
-};
 </script>
