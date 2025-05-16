f3ee035dead35faefdef69525765875a7bedf0db
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-05-16 14:56:35 +0200

    BC-8758 - remove unused ConfirmationDialog import and component from Folder.vue

--- src/modules/feature/folder/Folder.vue
+++ src/modules/feature/folder/Folder.vue
@@ -22,7 +22,6 @@
 			@update:name="onUpdateName"
 		/>
 	</DefaultWireframe>
-	<ConfirmationDialog />
 	<input
 		ref="fileInput"
 		type="file"
@@ -42,7 +41,6 @@ import { useBoardApi, useSharedBoardPageInformation } from "@data-board";
 import { useFileStorageApi } from "@data-file";
 import { useFolderState } from "@data-folder";
 import { mdiPlus } from "@icons/material";
-import { ConfirmationDialog } from "@ui-confirmation-dialog";
 import { computed, onMounted, ref, toRef, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import FileTable from "./file-table/FileTable.vue";
