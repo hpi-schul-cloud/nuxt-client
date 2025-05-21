99e14d115a8760dfd944de67c2de95bee9324d09
Hussam Kayed <hussam.kayed@capgemini.com>
2025-05-21 10:07:12 +0200

    EW-1215: change call again to use CC import

--- src/store/common-cartridge-import.ts
+++ src/store/common-cartridge-import.ts
@@ -59,7 +59,7 @@ export default class CommonCartridgeImportModule extends VuexModule {
 		}
 
 		try {
-			await this.coursesApi.courseControllerImportCourse(file);
+			await this.commonCartridgeApi.commonCartridgeControllerImportCourse(file);
 			this.setIsSuccess(true);
 		} catch {
 			this.setIsSuccess(false);
