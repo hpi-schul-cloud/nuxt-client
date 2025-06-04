39fbb6168866339bc9bee14efafdbf13fbf956ac
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-06-04 13:10:47 +0200

    code review

--- src/modules/feature/folder/Folder.unit.ts
+++ src/modules/feature/folder/Folder.unit.ts
@@ -32,34 +32,33 @@ describe("Folder.vue", () => {
 	const buildUploadStatsTranslation = (uploaded: string, total: string) => {
 		return `${uploaded} von ${total} Dateien hochgeladen`;
 	};
+	const setupWrapper = () => {
+		const parentId = "123";
+		const wrapper = mount(Folder, {
+			global: {
+				plugins: [
+					createTestingVuetify(),
+					createTestingI18n({
+						messages: {
+							en: {
+								"pages.folder.uploadstats": buildUploadStatsTranslation(
+									"{uploaded}",
+									"{total}"
+								),
+							},
+						},
+					}),
+				],
+			},
+			props: {
+				folderId: parentId,
+			},
+		});
+
+		return { wrapper, parentId };
+	};
 
 	describe("when user has board edit permission", () => {
-		const setupWrapper = () => {
-			const parentId = "123";
-			const wrapper = mount(Folder, {
-				global: {
-					plugins: [
-						createTestingVuetify(),
-						createTestingI18n({
-							messages: {
-								en: {
-									"pages.folder.uploadstats": buildUploadStatsTranslation(
-										"{uploaded}",
-										"{total}"
-									),
-								},
-							},
-						}),
-					],
-				},
-				props: {
-					folderId: parentId,
-				},
-			});
-
-			return { wrapper, parentId };
-		};
-
 		describe("when folder contains no files", () => {
 			describe("when component is loaded", () => {
 				const setup = async () => {
@@ -1521,32 +1520,6 @@ describe("Folder.vue", () => {
 	});
 
 	describe("when user has not board edit permission", () => {
-		const setupWrapper = () => {
-			const parentId = "123";
-			const wrapper = mount(Folder, {
-				global: {
-					plugins: [
-						createTestingVuetify(),
-						createTestingI18n({
-							messages: {
-								en: {
-									"pages.folder.uploadstats": buildUploadStatsTranslation(
-										"{uploaded}",
-										"{total}"
-									),
-								},
-							},
-						}),
-					],
-				},
-				props: {
-					folderId: parentId,
-				},
-			});
-
-			return { wrapper };
-		};
-
 		describe("check visibility of the folder menu and fab button", () => {
 			const setup = async () => {
 				const folderStateMock =
