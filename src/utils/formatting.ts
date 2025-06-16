3032178b1e600545fe185ec6745aa9b253ab0143
Odalys Adam <odalys.adam@dataport.de>
2025-06-16 11:19:17 +0200

    utils/formatting in typescript

---
+++ src/utils/formatting.ts
@@ -0,0 +1,9 @@
+export const upperCaseFirstChar = (text: string) =>
+	text.charAt(0).toUpperCase() + text.slice(1);
+
+export const toKebabCase = (text: string) =>
+	text
+		.replace(/([\w])(?=[A-Z])/g, (match) => {
+			return match[0] + "-";
+		})
+		.toLowerCase();
