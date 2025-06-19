c0c69d0197d2f22d6ed6234780bf35988a2bc078
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-06-19 09:10:21 +0200

    refactor FileContentElement.vue

--- src/modules/feature/board-file-element/FileContentElement.vue
+++ src/modules/feature/board-file-element/FileContentElement.vue
@@ -54,7 +54,7 @@
 	</v-card>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { FileRecordParentType, PreviewWidth } from "@/fileStorageApi/v3";
 import { FileElementResponse } from "@/serverApi/v3";
 import {
@@ -70,180 +70,135 @@ import {
 	KebabMenuActionMoveDown,
 	KebabMenuActionMoveUp,
 } from "@ui-kebab-menu";
-import {
-	computed,
-	defineComponent,
-	onMounted,
-	PropType,
-	ref,
-	toRef,
-	watch,
-} from "vue";
+import { computed, onMounted, PropType, ref, toRef, watch } from "vue";
 import { useFileAlerts } from "./content/alert/useFileAlerts.composable";
 import FileContent from "./content/FileContent.vue";
 import { FileAlert } from "./shared/types/FileAlert.enum";
 import FileUpload from "./upload/FileUpload.vue";
 
-export default defineComponent({
-	name: "FileContentElement",
-	components: {
-		FileUpload,
-		FileContent,
-		BoardMenu,
-		KebabMenuActionMoveUp,
-		KebabMenuActionMoveDown,
-		KebabMenuActionDelete,
-	},
-	props: {
-		element: { type: Object as PropType<FileElementResponse>, required: true },
-		isEditMode: { type: Boolean, required: true },
-		isNotFirstElement: { type: Boolean, requried: false },
-		isNotLastElement: { type: Boolean, requried: false },
-		columnIndex: { type: Number, required: true },
-		rowIndex: { type: Number, required: true },
-		elementIndex: { type: Number, required: true },
-	},
-	emits: [
-		"delete:element",
-		"move-down:edit",
-		"move-up:edit",
-		"move-keyboard:edit",
-	],
-	setup(props, { emit }) {
-		const fileContentElement = ref(null);
-		const isLoadingFileRecord = ref(true);
-
-		const element = toRef(props, "element");
-		useBoardFocusHandler(element.value.id, fileContentElement);
-
-		const { modelValue } = useContentElementState(props);
-		const { fetchFiles, upload, getFileRecordsByParentId } =
-			useFileStorageApi();
-
-		const fileRecord = computed(
-			() => getFileRecordsByParentId(element.value.id)[0]
-		);
-
-		const { alerts, addAlert } = useFileAlerts(fileRecord);
-
-		const isUploading = computed(() => {
-			return fileRecord.value?.isUploading;
-		});
-
-		const fileProperties = computed(() => {
-			if (fileRecord.value === undefined) {
-				return;
-			}
-
-			const previewUrl = isPreviewPossible(fileRecord.value.previewStatus)
-				? convertDownloadToPreviewUrl(fileRecord.value.url, PreviewWidth._500)
-				: undefined;
-
-			return {
-				size: fileRecord.value.size,
-				name: fileRecord.value.name,
-				url: fileRecord.value.url,
-				previewUrl,
-				previewStatus: fileRecord.value.previewStatus,
-				isDownloadAllowed: isScanStatusBlocked(
-					fileRecord.value.securityCheckStatus
-				),
-				mimeType: fileRecord.value.mimeType,
-				element: props.element,
-			};
-		});
-
-		const hasFileRecord = computed(() => {
-			return fileRecord.value !== undefined;
-		});
-
-		const isOutlined = computed(() => {
-			const { isEditMode } = props;
-			const isUploadingInViewMode =
-				fileRecord.value?.id !== undefined && !isEditMode && !isUploading.value;
-
-			return isUploadingInViewMode || isEditMode;
-		});
-
-		watch(element.value, async () => {
-			isLoadingFileRecord.value = true;
-			await fetchFiles(element.value.id, FileRecordParentType.BOARDNODES);
-			isLoadingFileRecord.value = false;
-		});
-
-		onMounted(async () => {
-			await fetchFiles(element.value.id, FileRecordParentType.BOARDNODES);
-			isLoadingFileRecord.value = false;
-		});
-
-		const onKeydownArrow = (event: KeyboardEvent) => {
-			if (props.isEditMode) {
-				event.preventDefault();
-				emit("move-keyboard:edit", event);
-			}
-		};
-
-		const onUploadFile = async (file: File): Promise<void> => {
-			try {
-				await upload(file, element.value.id, FileRecordParentType.BOARDNODES);
-				element.value.content.caption = " ";
-			} catch {
-				emit("delete:element", element.value.id);
-			}
-		};
-
-		const onFetchFile = async (): Promise<void> => {
-			await fetchFiles(element.value.id, FileRecordParentType.BOARDNODES);
-		};
-
-		const onUpdateAlternativeText = (value: string) => {
-			modelValue.value.alternativeText = value;
-		};
-
-		const onUpdateCaption = (value: string) => {
-			modelValue.value.caption = value;
-		};
-
-		const onAddAlert = (alert: FileAlert) => {
-			addAlert(alert);
-		};
-
-		const onDelete = async (confirmation: Promise<boolean>) => {
-			const shouldDelete = await confirmation;
-			if (shouldDelete) {
-				emit("delete:element", element.value.id);
-			}
-		};
-
-		const onMoveUp = () => emit("move-up:edit");
-		const onMoveDown = () => emit("move-down:edit");
-
-		return {
-			fileContentElement,
-			fileProperties,
-			fileRecord,
-			hasFileRecord,
-			isOutlined,
-			modelValue,
-			alerts,
-			isUploading,
-			onKeydownArrow,
-			onUploadFile,
-			onFetchFile,
-			onUpdateAlternativeText,
-			onUpdateCaption,
-			onAddAlert,
-			onDelete,
-			onMoveUp,
-			onMoveDown,
-		};
-	},
-	computed: {
-		BoardMenuScope() {
-			return BoardMenuScope;
-		},
-	},
+const props = defineProps({
+	element: { type: Object as PropType<FileElementResponse>, required: true },
+	isEditMode: { type: Boolean, required: true },
+	isNotFirstElement: { type: Boolean, required: false },
+	isNotLastElement: { type: Boolean, required: false },
+	columnIndex: { type: Number, required: true },
+	rowIndex: { type: Number, required: true },
+	elementIndex: { type: Number, required: true },
 });
+
+const emit = defineEmits<{
+	(e: "delete:element", elementId: string): void;
+	(e: "move-down:edit"): void;
+	(e: "move-up:edit"): void;
+	(e: "move-keyboard:edit", event: KeyboardEvent): void;
+}>();
+
+const fileContentElement = ref(null);
+const isLoadingFileRecord = ref(true);
+
+const element = toRef(props, "element");
+useBoardFocusHandler(element.value.id, fileContentElement);
+
+const { modelValue } = useContentElementState(props);
+const { fetchFiles, upload, getFileRecordsByParentId } = useFileStorageApi();
+
+const fileRecord = computed(
+	() => getFileRecordsByParentId(element.value.id)[0]
+);
+
+const { alerts, addAlert } = useFileAlerts(fileRecord);
+
+const isUploading = computed(() => {
+	return fileRecord.value?.isUploading;
+});
+
+const fileProperties = computed(() => {
+	if (fileRecord.value === undefined) {
+		return;
+	}
+
+	const previewUrl = isPreviewPossible(fileRecord.value.previewStatus)
+		? convertDownloadToPreviewUrl(fileRecord.value.url, PreviewWidth._500)
+		: undefined;
+
+	return {
+		size: fileRecord.value.size,
+		name: fileRecord.value.name,
+		url: fileRecord.value.url,
+		previewUrl,
+		previewStatus: fileRecord.value.previewStatus,
+		isDownloadAllowed: isScanStatusBlocked(
+			fileRecord.value.securityCheckStatus
+		),
+		mimeType: fileRecord.value.mimeType,
+		element: props.element,
+	};
+});
+
+const hasFileRecord = computed(() => {
+	return fileRecord.value !== undefined;
+});
+
+const isOutlined = computed(() => {
+	const { isEditMode } = props;
+	const isUploadingInViewMode =
+		fileRecord.value?.id !== undefined && !isEditMode && !isUploading.value;
+
+	return isUploadingInViewMode || isEditMode;
+});
+
+watch(element.value, async () => {
+	isLoadingFileRecord.value = true;
+	await fetchFiles(element.value.id, FileRecordParentType.BOARDNODES);
+	isLoadingFileRecord.value = false;
+});
+
+onMounted(async () => {
+	await fetchFiles(element.value.id, FileRecordParentType.BOARDNODES);
+	isLoadingFileRecord.value = false;
+});
+
+const onKeydownArrow = (event: KeyboardEvent) => {
+	if (props.isEditMode) {
+		event.preventDefault();
+		emit("move-keyboard:edit", event);
+	}
+};
+
+const onUploadFile = async (file: File): Promise<void> => {
+	try {
+		await upload(file, element.value.id, FileRecordParentType.BOARDNODES);
+		element.value.content.caption = " ";
+	} catch {
+		emit("delete:element", element.value.id);
+	}
+};
+
+const onFetchFile = async (): Promise<void> => {
+	await fetchFiles(element.value.id, FileRecordParentType.BOARDNODES);
+};
+
+const onUpdateAlternativeText = (value: string) => {
+	modelValue.value.alternativeText = value;
+};
+
+const onUpdateCaption = (value: string) => {
+	modelValue.value.caption = value;
+};
+
+const onAddAlert = (alert: FileAlert) => {
+	addAlert(alert);
+};
+
+const onDelete = async (confirmation: Promise<boolean>) => {
+	const shouldDelete = await confirmation;
+	if (shouldDelete) {
+		emit("delete:element", element.value.id);
+	}
+};
+
+const onMoveUp = () => emit("move-up:edit");
+const onMoveDown = () => emit("move-down:edit");
 </script>
 <style lang="scss" scoped>
 /* show focus indicatator properly on all browsers */
