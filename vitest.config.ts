480548c3a2a65ee4a857592186a9716360f4a3b3
Odalys Adam <odalys.adam@dataport.de>
2025-06-19 12:18:58 +0200

    install vitest & change scripts

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config.ts";
+
+export default mergeConfig(
+	viteConfig,
+	defineConfig({
+		test: {
+			globals: true,
+		},
+	})
+);
