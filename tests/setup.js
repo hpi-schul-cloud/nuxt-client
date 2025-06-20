87c5a02417d4c6954f02365b154f343c25d4f311
Odalys Adam <odalys.adam@dataport.de>
2025-06-20 16:44:11 +0200

    define visualViewport

--- tests/setup.js
+++ tests/setup.js
@@ -57,3 +57,18 @@ Object.defineProperty(window, "matchMedia", {
 		},
 	}),
 });
+
+Object.defineProperty(window, "visualViewport", {
+	value: {
+		width: 1024,
+		height: 768,
+		scale: 1,
+		offsetLeft: 0,
+		offsetTop: 0,
+		pageLeft: 0,
+		pageTop: 0,
+		addEventListener: () => {},
+		removeEventListener: () => {},
+	},
+	writable: true,
+});
