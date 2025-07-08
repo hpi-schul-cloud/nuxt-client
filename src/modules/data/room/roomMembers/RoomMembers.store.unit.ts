08e885c101c5bde9e61fc84e2c72bf38faee83d3
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-07-08 13:27:59 +0200

    add unit test case for RoomMembers.store file

--- src/modules/data/room/roomMembers/RoomMembers.store.unit.ts
+++ src/modules/data/room/roomMembers/RoomMembers.store.unit.ts
@@ -414,6 +414,38 @@ describe("useRoomMembers", () => {
 
 			expect(consoleErrorSpy).toHaveBeenCalledWith(error);
 		});
+
+		describe("when the current user is a student", () => {
+			it("should not fetch the school list", async () => {
+				const currentUser = meResponseFactory.build({
+					roles: [{ id: "student-id", name: RoleName.Student }],
+				});
+				authModule.setMe(currentUser);
+
+				const { roomMembersStore } = setup();
+
+				const roomMembers = roomMemberFactory.buildList(2, {
+					roomRoleName: RoleName.Roomviewer,
+				});
+
+				roomMembers[0].schoolRoleNames = [RoleName.Student];
+				roomMembers[0].userId = currentUser.user.id;
+
+				roomMembersStore.roomMembers = [...roomMembers];
+
+				const schoolList = roomMembersStore.schools;
+
+				await roomMembersStore.loadSchoolList();
+				expect(
+					schoolApiMock.schoolControllerGetSchoolList
+				).not.toHaveBeenCalled();
+				expect(schoolList).toHaveLength(1);
+				expect(schoolList[0]).toStrictEqual({
+					id: "school-id",
+					name: "Paul-Gerhardt-Gymnasium",
+				});
+			});
+		});
 	});
 
 	describe("addMembers", () => {
