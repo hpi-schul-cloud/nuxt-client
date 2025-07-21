0134264be2265b3ec60c94bda5b8d35b11a78b8c
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-07-21 15:45:07 +0200

    add postinstall check for win32/x64

--- scripts/installRollupBinaries.js
+++ scripts/installRollupBinaries.js
@@ -8,6 +8,8 @@ if (platform === "linux" && arch === "x64") {
 	execSync("npm install @rollup/rollup-linux-x64-musl --no-save");
 } else if (platform === "darwin" && arch === "arm64") {
 	execSync("npm install @rollup/rollup-darwin-arm64 --no-save");
+} else if (platform === "win32" && arch === "x64") {
+	execSync("npm install @rollup/rollup-win32-x64 --no-save");
 } else {
 	// eslint-disable-next-line no-console
 	console.log(`No native binary needed for ${platform}-${arch}`);
