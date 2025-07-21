1e6777c1fe5cf57b4d00d896a330dc5a59b33cf8
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-07-21 15:36:20 +0200

    attempt to ensure rollup native binaries get installed

---
+++ scripts/installRollupBinaries.js
@@ -0,0 +1,14 @@
+import os from "os";
+import { execSync } from "child_process";
+
+const platform = os.platform();
+const arch = os.arch();
+
+if (platform === "linux" && arch === "x64") {
+	execSync("npm install @rollup/rollup-linux-x64-musl --no-save");
+} else if (platform === "darwin" && arch === "arm64") {
+	execSync("npm install @rollup/rollup-darwin-arm64 --no-save");
+} else {
+	// eslint-disable-next-line no-console
+	console.log(`No native binary needed for ${platform}-${arch}`);
+}
