0f42d1fd2ace4c0e4bffbe600ae35466d28b5f60
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-07-25 09:31:16 +0200

    add test page and test

---
+++ src/pages/Test.page.vue
@@ -0,0 +1,22 @@
+<!-- TODO REMOVE THIS PAGE -->
+<template>
+	<div>
+		<h1 data-testid="greeting">{{ greeting }}</h1>
+		<VBtn data-testid="increment" @click="increment">Increment</VBtn>
+		<p data-testid="count">Count: {{ count }}</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const count = ref(0);
+
+function increment() {
+	count.value++;
+}
+
+const greeting = computed(() =>
+	count.value === 0 ? "Hello, World!" : "You clicked!"
+);
+</script>
