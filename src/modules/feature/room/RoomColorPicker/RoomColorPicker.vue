2e89db5dbe513148f51fb4d946d77b96b309c25c
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-08-14 16:03:31 +0200

    RoomColorPicker: remove extra swatch component to avoid duplication and confusing emit

--- src/modules/feature/room/RoomColorPicker/RoomColorPicker.vue
+++ src/modules/feature/room/RoomColorPicker/RoomColorPicker.vue
@@ -1,27 +1,39 @@
 <template>
-	<label id="room-color-label" class="d-flex mb-2">
-		{{ t("common.words.color") }}
-	</label>
-	<div
-		class="d-flex flex-wrap"
-		role="radiogroup"
-		aria-labelledby="room-color-label"
-	>
-		<RoomColorPickerSwatch
-			v-for="swatchColor in RoomColor"
-			:key="swatchColor"
-			:color="swatchColor"
-			:is-selected="swatchColor === currentColor"
-			@update:color="onUpdateColor(swatchColor)"
-		/>
+	<div role="radiogroup" aria-labelledby="room-color-label">
+		<!-- Todo: clarify with UX if this should be a div or h2 -->
+		<div id="room-color-label" class="d-flex mb-2">
+			{{ t("common.words.color") }}
+		</div>
+		<div class="d-flex flex-wrap">
+			<VBtn
+				v-for="swatchColor in RoomColor"
+				:key="swatchColor"
+				class="rounded-circle elevation-3 ma-1 d-flex justify-center align-items-center"
+				:class="`room-color--${swatchColor}`"
+				height="40px"
+				width="40px"
+				min-width="40px"
+				:data-testid="`color-swatch-${swatchColor}`"
+				role="radio"
+				:aria-label="ariaLabel"
+				:aria-checked="isSelected(swatchColor)"
+				@click="onUpdateColor(swatchColor)"
+			>
+				<VIcon
+					v-if="isSelected(swatchColor)"
+					:icon="mdiCheckCircleOutline"
+					color="white"
+				/>
+			</VBtn>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { PropType } from "vue";
-import RoomColorPickerSwatch from "./RoomColorPickerSwatch.vue";
+import { computed, PropType } from "vue";
 import { RoomColor } from "@/types/room/Room";
 import { useI18n } from "vue-i18n";
+import { mdiCheckCircleOutline } from "@icons/material";
 
 const currentColor = defineModel("color", {
 	type: String as PropType<RoomColor>,
@@ -33,4 +45,13 @@ const { t } = useI18n();
 const onUpdateColor = (color: RoomColor) => {
 	currentColor.value = color;
 };
+
+const isSelected = (color: RoomColor) => {
+	return color === currentColor.value;
+};
+
+const ariaLabel = computed(() => {
+	const translatedColor = t(`common.words.color.${currentColor.value}`);
+	return `${t("common.words.color")} ${translatedColor}`;
+});
 </script>
