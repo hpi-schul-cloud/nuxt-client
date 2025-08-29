db2d7f53e4a556bdbc6ce1aea555f4c511d95aa8
Sergej Hoffmann <97111299+SevenWaysDP@users.noreply.github.com>
2025-08-29 13:29:44 +0200

    BC-10436 - Konfiguration der Office-Dokumente (#3852)

--- src/modules/page/collabora/CollaboraPostMessageApi.composable.unit.ts
+++ src/modules/page/collabora/CollaboraPostMessageApi.composable.unit.ts
@@ -5,7 +5,10 @@ import { mountComposable } from "@@/tests/test-utils";
 import { createModuleMocks } from "@@/tests/test-utils/mock-store-module";
 import { createTestingI18n } from "@@/tests/test-utils/setup";
 import { describe, expect, it } from "vitest";
-import { useCollaboraPostMessageApi } from "./CollaboraPostMessageApi.composable";
+import {
+	CollaboraEvents,
+	useCollaboraPostMessageApi,
+} from "./CollaboraPostMessageApi.composable";
 
 describe("useCollaboraMessage", () => {
 	const notifierModuleMock = createModuleMocks(NotifierModule);
@@ -146,5 +149,95 @@ describe("useCollaboraMessage", () => {
 		const { documentHasUnsavedChanges } = setupMountComposable();
 
 		expect(documentHasUnsavedChanges).toHaveProperty("value");
+	});
+
+	describe("handleLoadingStatusUpdate", () => {
+		const notifierModuleMock = createModuleMocks(NotifierModule);
+
+		const setupMountComposable = () => {
+			const targetOrigin = "https://collabora.example.com";
+
+			const iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+
+			const { setupPostMessageAPI } = mountComposable(
+				() => useCollaboraPostMessageApi(),
+				{
+					global: {
+						plugins: [createTestingI18n()],
+						provide: {
+							[NOTIFIER_MODULE_KEY as symbol]: notifierModuleMock,
+						},
+					},
+				}
+			);
+
+			setupPostMessageAPI(iframe, targetOrigin);
+
+			return { setupPostMessageAPI, iframe, targetOrigin };
+		};
+
+		it("should handle App_LoadingStatus:Initialized correctly", () => {
+			const { iframe, targetOrigin } = setupMountComposable();
+
+			const validMsg = JSON.stringify({
+				MessageId: CollaboraEvents.APP_LOADING_STATUS,
+				Values: { Status: "Initialized" },
+			});
+
+			const expectedMsg = {
+				MessageId: CollaboraEvents.HOST_POSTMESSAGE_READY,
+				SendTime: expect.any(Number),
+				Values: undefined,
+			};
+
+			const spy = vi.spyOn(iframe.contentWindow as Window, "postMessage");
+			window.dispatchEvent(new MessageEvent("message", { data: validMsg }));
+
+			expect(spy).toHaveBeenCalledWith(expectedMsg, targetOrigin);
+
+			document.body.removeChild(iframe);
+		});
+
+		it("should handle App_LoadingStatus:Document_Loaded correctly", () => {
+			const { iframe, targetOrigin } = setupMountComposable();
+
+			const validMsg = JSON.stringify({
+				MessageId: CollaboraEvents.APP_LOADING_STATUS,
+				Values: { Status: "Document_Loaded" },
+			});
+
+			const expectedMsg = {
+				MessageId: CollaboraEvents.REMOVE_BUTTON,
+				SendTime: expect.any(Number),
+				Values: undefined,
+			};
+
+			const spy = vi.spyOn(iframe.contentWindow as Window, "postMessage");
+			window.dispatchEvent(new MessageEvent("message", { data: validMsg }));
+
+			expect(spy).toHaveBeenNthCalledWith(
+				1,
+				{ ...expectedMsg, Values: { id: "feedback-button" } },
+				targetOrigin
+			);
+			expect(spy).toHaveBeenNthCalledWith(
+				2,
+				{ ...expectedMsg, Values: { id: "about-button" } },
+				targetOrigin
+			);
+			expect(spy).toHaveBeenNthCalledWith(
+				3,
+				{ ...expectedMsg, Values: { id: "latestupdates" } },
+				targetOrigin
+			);
+			expect(spy).toHaveBeenNthCalledWith(
+				4,
+				{ ...expectedMsg, Values: { id: "signature-button" } },
+				targetOrigin
+			);
+
+			document.body.removeChild(iframe);
+		});
 	});
 });
