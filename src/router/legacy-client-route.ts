8d87ff042e01f3e37ab22b50a6ca33786fa76070
Igor Richter <igor.richter@capgemini.com>
2025-09-10 15:14:02 +0200

    desperate attempt to see the homepage

--- src/router/legacy-client-route.ts
+++ src/router/legacy-client-route.ts
@@ -2,7 +2,7 @@ import { isServer } from "./server-route.mjs";
 import { isVueClient } from "./vue-client-route.mjs";
 
 const isLegacyClient = (path: string) => {
-	return !(isServer(path) || isVueClient(path));
+	return !(isServer(path) || isVueClient(path) || path === "/");
 };
 
 export { isLegacyClient };
