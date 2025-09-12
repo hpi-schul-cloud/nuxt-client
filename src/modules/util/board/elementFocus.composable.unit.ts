d7976ad8fd60e3d377a6a77608cab939d920ede0
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-09-12 16:19:43 +0200

    add unit tests

--- src/modules/util/board/elementFocus.composable.unit.ts
+++ src/modules/util/board/elementFocus.composable.unit.ts
@@ -1,33 +1,36 @@
 import { createMock } from "@golevelup/ts-vitest";
 import { useElementFocus } from "./elementFocus.composable";
+import { logger } from "@util-logger";
 
 describe("elementFocus.composable", () => {
 	beforeEach(() => {
 		vi.useFakeTimers();
 		vi.clearAllMocks();
 	});
-	describe("when there is a node to focus", () => {
-		const setup = () => {
-			Object.defineProperty(window, "location", {
-				get: () =>
-					createMock<Location>({
-						hash: "#card-12345",
-					}),
-			});
 
-			const domElementMock = createMock<HTMLElement>();
-			const querySelectorSpy = vi.spyOn(document, "querySelector");
-			querySelectorSpy.mockReturnValueOnce(domElementMock);
+	const setup = () => {
+		Object.defineProperty(window, "location", {
+			get: () =>
+				createMock<Location>({
+					hash: "#card-12345",
+				}),
+		});
 
-			return {
-				domElementMock,
-			};
+		const domElementMock = createMock<HTMLElement>();
+		const querySelectorSpy = vi.spyOn(document, "querySelector");
+		querySelectorSpy.mockReturnValueOnce(domElementMock);
+		const { focusNodeFromHash } = useElementFocus();
+
+		return {
+			domElementMock,
+			focusNodeFromHash,
 		};
-
+	};
+	describe("when there is a node to focus", () => {
 		it("should scroll to and focus the element", () => {
-			const { domElementMock } = setup();
+			const { domElementMock, focusNodeFromHash } = setup();
 
-			useElementFocus().focusNodeFromHash();
+			focusNodeFromHash();
 			vi.runAllTimers();
 
 			expect(domElementMock.scrollIntoView).toHaveBeenCalledWith({
@@ -35,6 +38,36 @@ describe("elementFocus.composable", () => {
 				inline: "center",
 			});
 			expect(domElementMock.focus).toHaveBeenCalled();
+		});
+	});
+
+	describe("when hash is empty", () => {
+		it("should do nothing if hash is empty", async () => {
+			Object.defineProperty(window, "location", {
+				get: () => createMock<Location>({ hash: "" }),
+			});
+
+			const { focusNodeFromHash } = setup();
+			await expect(focusNodeFromHash()).resolves.toBeUndefined();
+		});
+	});
+
+	describe("when element is not found after attempts", () => {
+		it("should log error if element not found after attempts", async () => {
+			Object.defineProperty(window, "location", {
+				get: () => createMock<Location>({ hash: "#not-found" }),
+			});
+
+			const querySelectorSpy = vi.spyOn(document, "querySelector");
+			querySelectorSpy.mockReturnValue(null);
+
+			const loggerErrorSpy = vi.spyOn(logger, "error");
+
+			const { focusNodeFromHash } = useElementFocus();
+			const promise = focusNodeFromHash();
+			vi.advanceTimersByTime(20000);
+			await promise;
+			expect(loggerErrorSpy).toHaveBeenCalled();
 		});
 	});
 });
