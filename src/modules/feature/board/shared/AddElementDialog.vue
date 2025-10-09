758e2846f1052162cf4ae1b6339106493d1dfc47
Max Bischof <maximilian.bischof@dataport.de>
2025-10-09 13:24:16 +0200

    Refactor element type selection to use a combined options array and sort it alphabetically

--- src/modules/feature/board/shared/AddElementDialog.vue
+++ src/modules/feature/board/shared/AddElementDialog.vue
@@ -10,15 +10,7 @@
 			</VCardTitle>
 			<VCardText class="d-flex flex-row flex-wrap align-center">
 				<ExtendedIconBtn
-					v-for="(item, key) in staticElementTypeOptions"
-					:key="key"
-					:data-testid="item.testId"
-					:icon="item.icon"
-					:label="item.label"
-					@click.stop="item.action"
-				/>
-				<ExtendedIconBtn
-					v-for="(item, key) in dynamicElementTypeOptions"
+					v-for="(item, key) in elementTypeOptions"
 					:key="key"
 					:data-testid="item.testId"
 					:icon="item.icon"
@@ -45,20 +37,14 @@ import { computed, ComputedRef } from "vue";
 import { useI18n } from "vue-i18n";
 import { useSharedElementTypeSelection } from "./SharedElementTypeSelection.composable";
 
-const {
-	isDialogOpen,
-	isDialogLoading,
-	closeDialog,
-	staticElementTypeOptions,
-	dynamicElementTypeOptions,
-} = useSharedElementTypeSelection();
+const { isDialogOpen, isDialogLoading, closeDialog, elementTypeOptions } =
+	useSharedElementTypeSelection();
 
 const { t } = useI18n();
 
 const dialogWidth: ComputedRef<number> = computed(() => {
-	const totalOptions =
-		staticElementTypeOptions.value.length +
-		dynamicElementTypeOptions.value.length;
+	const totalOptions = elementTypeOptions.value.length;
+
 	return totalOptions >= 3 ? 426 : 320;
 });
 </script>
