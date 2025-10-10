d2051a9280066d66060e00039fb4dd7fbe76859f
Christian Darsow <hello@darsow.io>
2025-10-10 11:26:08 +0200

    Adjust tests to new structure, add negative test

--- src/modules/feature/board-file-element/content/inputs/alternative-text/AlternativeText.unit.ts
+++ src/modules/feature/board-file-element/content/inputs/alternative-text/AlternativeText.unit.ts
@@ -1,8 +1,9 @@
 import AlternativeText from "./AlternativeText.vue";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
+import { InputWrapperWithCheckmark } from "@ui-input";
 import { mount } from "@vue/test-utils";
 import { nextTick } from "vue";
-import { VTextarea } from "vuetify/components";
+import { VTextField } from "vuetify/components";
 
 describe("AlternativeText", () => {
 	const mountSetup = () => {
@@ -15,35 +16,46 @@ describe("AlternativeText", () => {
 
 		return {
 			wrapper,
+			alternativeText,
 		};
 	};
 
 	it("should be found in dom", () => {
 		const { wrapper } = mountSetup();
 
-		const fileContentElement = wrapper.findComponent(AlternativeText);
+		const alternativeText = wrapper.findComponent(AlternativeText);
 
-		expect(fileContentElement.exists()).toBe(true);
+		expect(alternativeText.exists()).toBe(true);
 	});
 
-	it("should emit update:alternativeText if text changes", async () => {
+	it("should emit update:alternativeText on confirm", async () => {
 		const { wrapper } = mountSetup();
 
-		const textarea = wrapper.findComponent(VTextarea);
+		const inputWrapperWithCheckmark = wrapper.findComponent(InputWrapperWithCheckmark);
+		const textField = wrapper.findComponent(VTextField);
 		const newText = "new text";
-		await textarea.setValue(newText);
-		await nextTick();
+		await textField.setValue(newText);
+		inputWrapperWithCheckmark.vm.$emit("confirm");
 
 		expect(wrapper.emitted("update:alternativeText")).toHaveLength(1);
 		expect(wrapper.emitted("update:alternativeText")?.[0][0]).toBe(newText);
 	});
 
+	it("should pass the alternativeText prop to the text field", async () => {
+		const { wrapper, alternativeText } = mountSetup();
+
+		const textField = wrapper.findComponent(VTextField);
+		await nextTick();
+		const modelValue = textField.props("modelValue");
+
+		expect(modelValue).toBe(alternativeText);
+	});
+
 	it("should have a hint translation", async () => {
 		const { wrapper } = mountSetup();
 
-		const textarea = wrapper.findComponent(VTextarea);
-
-		const hint = textarea.props("hint");
+		const textField = wrapper.findComponent(VTextField);
+		const hint = textField.props("hint");
 
 		expect(hint).toBe("components.cardElement.fileElement.altDescription");
 	});
@@ -51,10 +63,23 @@ describe("AlternativeText", () => {
 	it("should have a label translation", async () => {
 		const { wrapper } = mountSetup();
 
-		const textarea = wrapper.findComponent(VTextarea);
-
-		const label = textarea.props("label");
+		const textField = wrapper.findComponent(VTextField);
+		const label = textField.props("label");
 
 		expect(label).toBe("components.cardElement.fileElement.alternativeText");
+	});
+
+	describe("when a value containing a < directly followed by a string is entered", () => {
+		it("should not emit update:alternativeText", async () => {
+			const { wrapper } = mountSetup();
+
+			const inputWrapperWithCheckmark = wrapper.findComponent(InputWrapperWithCheckmark);
+			const textField = wrapper.findComponent(VTextField);
+			const newText = "<abc123";
+			await textField.setValue(newText);
+			inputWrapperWithCheckmark.vm.$emit("confirm");
+
+			expect(wrapper.emitted("update:alternativeText")).toBeUndefined();
+		});
 	});
 });
