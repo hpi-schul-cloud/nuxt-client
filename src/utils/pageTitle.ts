c9d98cd7bd710f119496acbabb6c8649d17121db
Odalys Adam <odalys.adam@dataport.de>
2025-10-13 16:38:49 +0200

    simplify pageTitle.ts

--- src/utils/pageTitle.ts
+++ src/utils/pageTitle.ts
@@ -3,17 +3,5 @@ import { useEnvConfig } from "@data-env";
 export function buildPageTitle(pageTitle?: string, parentTitle?: string): string {
 	const instanceTitle = useEnvConfig().value.SC_TITLE;
 
-	if (pageTitle && !parentTitle) {
-		return `${pageTitle} - ${instanceTitle}`;
-	}
-
-	if (!pageTitle && parentTitle) {
-		return `${parentTitle} - ${instanceTitle}`;
-	}
-
-	if (pageTitle && parentTitle) {
-		return `${pageTitle} - ${parentTitle} - ${instanceTitle}`;
-	}
-
-	return instanceTitle;
+	return [pageTitle, parentTitle, instanceTitle].filter(Boolean).join(" - ");
 }
