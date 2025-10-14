5f3b9dcfdc682c2a506983caf631464e3fe8fde4
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-10-14 09:26:58 +0200

    implement pageTitle reactively changed

--- src/pages/registration-external-members/RegistrationExternalMembers.page.vue
+++ src/pages/registration-external-members/RegistrationExternalMembers.page.vue
@@ -1,4 +1,5 @@
 <template>
+	<h1 ref="registrationTitle" tabindex="0">{{ t("pages.registrationExternalMembers.steps.registration.title") }}</h1>
 	<Registration :selected-language="language" @update:selected-language="setSelectedLanguage" />
 </template>
 
@@ -6,13 +7,14 @@
 import { buildPageTitle } from "@/utils/pageTitle";
 import { Registration } from "@feature-room";
 import { useTitle } from "@vueuse/core";
-import { onMounted, Ref, ref } from "vue";
+import { computed, onMounted, Ref, ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 const i18n = useI18n();
 const { t } = i18n;
 
 const language: Ref<string | undefined> = ref(undefined);
+const registrationTitle = ref<HTMLElement | null>(null);
 
 const getCookieValue = (): string | undefined => {
 	const match = document.cookie.match(new RegExp(`(?:^|;\\s*)USER_LANG=([^;]*)`));
@@ -22,7 +24,6 @@ const getCookieValue = (): string | undefined => {
 const setCookie = (lang = "de") => {
 	const expires = new Date(Date.now() + 60 * 60 * 24 * 365 * 1000).toUTCString();
 	document.cookie = `USER_LANG=${lang}; path=/; expires=${expires}; SameSite=Lax`;
-	i18n.locale.value = lang;
 };
 
 const setSelectedLanguage = (value: string) => {
@@ -31,11 +32,12 @@ const setSelectedLanguage = (value: string) => {
 	i18n.locale.value = value;
 };
 
-const pageTitle = buildPageTitle(t("pages.registrationExternalMembers.pageTitle"));
+const pageTitle = computed(() => buildPageTitle(t("pages.registrationExternalMembers.pageTitle")));
 useTitle(pageTitle);
 
 onMounted(() => {
 	language.value = getCookieValue() || "de";
 	i18n.locale.value = language.value;
+	registrationTitle.value?.focus();
 });
 </script>
