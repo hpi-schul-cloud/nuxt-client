b227caf5febbde65c7efd98686bc8b7e6dbe854c
Christian Darsow <hello@darsow.io>
2025-10-29 10:11:51 +0100

    Adjust general chip component

---
+++ src/modules/ui/chip/BaseChip.vue
@@ -0,0 +1,23 @@
+<template>
+	<VChip :color="color" :prepend-icon="icon" size="small" variant="tonal">
+		<slot />
+	</VChip>
+</template>
+
+<script setup lang="ts">
+import { PropType } from "vue";
+import { IconProps } from "vuetify";
+
+defineProps({
+	color: {
+		type: String as PropType<"error" | "warning" | "info">,
+		required: false,
+		default: undefined,
+	},
+	icon: {
+		type: String as PropType<IconProps["icon"]>,
+		required: false,
+		default: undefined,
+	},
+});
+</script>
