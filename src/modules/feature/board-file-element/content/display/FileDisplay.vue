b9bc8a30cb950062b1c17b46c9f7dea8e7787a97
Johannes Lueder <johannes.lueder@dataport.de>
2025-10-29 15:07:27 +0100

    revert

--- src/modules/feature/board-file-element/content/display/FileDisplay.vue
+++ src/modules/feature/board-file-element/content/display/FileDisplay.vue
@@ -2,7 +2,7 @@
 	<PdfDisplay
 		v-if="hasPdfMimeType && fileProperties.previewUrl"
 		:src="fileProperties.url"
-		:preview-src="fileProperties.url"
+		:preview-src="fileProperties.previewUrl"
 		:name="fileProperties.name"
 		:is-edit-mode="isEditMode"
 		:element="fileProperties.element"
