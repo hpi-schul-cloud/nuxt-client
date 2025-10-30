8857f30d4d7f25c5a1b1d5e1eda8507497c932dc
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-10-30 12:16:07 +0100

    add more tests

--- src/modules/ui/date-time-picker/DatePicker.unit.ts
+++ src/modules/ui/date-time-picker/DatePicker.unit.ts
@@ -4,7 +4,7 @@ import { flushPromises, mount } from "@vue/test-utils";
 import { VDatePicker, VMenu, VTextField } from "vuetify/components";
 
 describe("DatePicker", () => {
-	const setup = (options?: Partial<{ date: string; required: boolean }>) => {
+	const setup = (options?: Partial<{ date: string; required: boolean; ariaLabel: string; label: string }>) => {
 		const wrapper = mount(DatePicker, {
 			attachTo: document.body,
 			global: {
@@ -17,9 +17,10 @@ describe("DatePicker", () => {
 			props: {
 				date: options?.date || undefined,
 				required: options?.required || true,
+				ariaLabel: options?.ariaLabel || undefined,
+				label: options?.label || undefined,
 			},
 		});
-
 		const textField = wrapper.findComponent(VTextField);
 
 		return { wrapper, textField };
@@ -38,94 +39,129 @@ describe("DatePicker", () => {
 		expect(textField.exists()).toBe(true);
 	});
 
-	it("should emit update:date event valid input", async () => {
-		const { wrapper, textField } = setup();
+	describe("validation", () => {
+		describe("when input is valid", () => {
+			it("should emit update:date event", async () => {
+				const { wrapper, textField } = setup();
 
-		await textField.trigger("click");
-		const menu = wrapper.getComponent(VMenu);
-		const datePicker = menu.getComponent(VDatePicker);
+				await textField.trigger("click");
 
-		const selectedDate = new Date(2024, 0, 1);
-		vi.setSystemTime(selectedDate);
+				const datePicker = wrapper.getComponent(VMenu).getComponent(VDatePicker);
+				const selectedDate = new Date(2024, 0, 1);
+				vi.setSystemTime(selectedDate);
 
-		await datePicker.setValue(selectedDate);
-		await flushPromises();
+				await datePicker.setValue(selectedDate);
+				await flushPromises();
 
-		expect(wrapper.emitted("update:date")).toHaveLength(1);
-		expect(wrapper.emitted("update:date")![0]).toEqual([selectedDate.toISOString()]);
+				expect(wrapper.emitted("update:date")).toHaveLength(1);
+				expect(wrapper.emitted("update:date")![0]).toEqual([selectedDate.toISOString()]);
+			});
+
+			it("should emit update:date with null if input is empty and not required", async () => {
+				const { wrapper, textField } = setup({ required: false });
+
+				await textField.setValue("");
+				await flushPromises();
+
+				const updateDateEvent = wrapper.emitted("update:date");
+				expect(updateDateEvent).toEqual([[null]]);
+			});
+		});
+
+		describe("when input is invalid", () => {
+			describe("if required and value is empty", () => {
+				it("should emit error event", async () => {
+					const { wrapper, textField } = setup();
+
+					await textField.setValue("");
+					await flushPromises();
+
+					const errorEvent = wrapper.emitted("error");
+					expect(errorEvent).toHaveLength(1);
+				});
+
+				it("should emit update:date event with null value", async () => {
+					const { wrapper, textField } = setup();
+
+					await textField.setValue("");
+					await flushPromises();
+
+					const updateDateEvent = wrapper.emitted("update:date");
+					expect(updateDateEvent).toEqual([[null]]);
+				});
+
+				it("should display error message for required date", async () => {
+					const { textField } = setup();
+
+					await textField.setValue("");
+					await flushPromises();
+
+					expect(textField.text()).toBe("components.datePicker.validation.required");
+				});
+			});
+
+			describe("when date is invalid", () => {
+				it("should emit error event", async () => {
+					const { wrapper, textField } = setup();
+
+					await textField.setValue("55.55.5555");
+					await flushPromises();
+
+					const errorEvent = wrapper.emitted("error");
+					expect(errorEvent).toBeDefined();
+					expect(errorEvent).toHaveLength(1);
+				});
+
+				it("should emit update:date event with null value", async () => {
+					const { wrapper, textField } = setup();
+
+					await textField.setValue("");
+					await flushPromises();
+
+					const updateDateEvent = wrapper.emitted("update:date");
+					const updateDateEventValue = updateDateEvent?.[0][0];
+
+					expect(updateDateEvent).toBeDefined();
+					expect(updateDateEvent!.length).toBeGreaterThan(0);
+					expect(updateDateEventValue).toBe(null);
+				});
+
+				it("should display error message for invalid date format", async () => {
+					const { textField } = setup();
+
+					await textField.setValue("22");
+					await flushPromises();
+
+					expect(textField.text()).toBe("components.datePicker.validation.format");
+				});
+			});
+		});
 	});
 
-	describe("when required prop is set & value is left empty", () => {
-		it("should emit error event", async () => {
-			const { wrapper, textField } = setup();
+	describe("aria label", () => {
+		it("should set aria-label to aria label prop if provided", () => {
+			const ariaLabel = "Date Picker Aria ariaLabel";
+			const { textField } = setup({ ariaLabel });
+			const input = textField.find("input");
 
-			await textField.setValue("");
-			await flushPromises();
-
-			const errorEvent = wrapper.emitted("error");
-			expect(errorEvent).toHaveLength(1);
+			expect(input.attributes("aria-label")).toBe(`${ariaLabel} (common.placeholder.dateformat)`);
 		});
 
-		it("should emit update:date event with null value", async () => {
-			const { wrapper, textField } = setup();
+		it("should set aria-label to label prop if aria label prop not provided", () => {
+			const label = "Date Picker Label";
+			const { textField } = setup({ label });
+			const input = textField.find("input");
 
-			await textField.setValue("");
-			await flushPromises();
-
-			const updateDateEvent = wrapper.emitted("update:date");
-			const updateDateEventValue = updateDateEvent?.[0][0];
-
-			expect(updateDateEvent).toBeDefined();
-			expect(updateDateEvent!.length).toBeGreaterThan(0);
-			expect(updateDateEventValue).toBe(null);
+			expect(input.attributes("aria-label")).toBe(`${label} (common.placeholder.dateformat)`);
 		});
 
-		it("should display error message for required date", async () => {
-			vi.useFakeTimers();
+		it("should set aria-label to default if neither aria label nor label prop provided", () => {
 			const { textField } = setup();
+			const input = textField.find("input");
 
-			await textField.setValue("");
-			await flushPromises();
-
-			expect(textField.text()).toBe("components.datePicker.validation.required");
+			expect(input.attributes("aria-label")).toBe(`common.labels.date (common.placeholder.dateformat)`);
 		});
 	});
 
-	describe("when date is invalid", () => {
-		it("should emit error event", async () => {
-			const { wrapper, textField } = setup();
-
-			await textField.setValue("55.55.5555");
-			await flushPromises();
-
-			const errorEvent = wrapper.emitted("error");
-			expect(wrapper.emitted("error")).toBeDefined();
-			expect(errorEvent).toHaveLength(1);
-		});
-
-		it("should emit update:date event with null value", async () => {
-			const { wrapper, textField } = setup();
-
-			await textField.setValue("");
-			await flushPromises();
-
-			const updateDateEvent = wrapper.emitted("update:date");
-			const updateDateEventValue = updateDateEvent?.[0][0];
-
-			expect(updateDateEvent).toBeDefined();
-			expect(updateDateEvent!.length).toBeGreaterThan(0);
-			expect(updateDateEventValue).toBe(null);
-		});
-
-		it("should display error message for invalid date format", async () => {
-			const { textField } = setup();
-
-			await textField.setValue("22");
-			await flushPromises();
-
-			expect(textField.text()).toBe("components.datePicker.validation.format");
-		});
-
-		// TODO: add more tests, e.g for keyboard navigation, when valid date, watchers
-	});
+	// TODO: add more tests, e.g for keyboard navigation, when valid date, watchers, open menu
 });
