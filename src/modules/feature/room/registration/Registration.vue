39d3fce852e90d37ad59d0a2fbeb9fe675a55dac
Sebastian Mueller <sebastian.mueller@dataport.de>
2025-11-03 10:47:25 +0100

    move code to separate component

--- src/modules/feature/room/registration/Registration.vue
+++ src/modules/feature/room/registration/Registration.vue
@@ -16,20 +16,7 @@
 							:selected-language="lang"
 							@update:selected-language="onUpdateSelectedLanguage"
 						/>
-						<div v-else-if="step.value === 2">
-							<i18n-t keypath="pages.registrationExternalMembers.steps.welcome.description" scope="global">
-								<template #dataProtection>
-									<strong>
-										{{ t("pages.registrationExternalMembers.steps.welcome.dataProtection") }}
-									</strong>
-								</template>
-								<template #tos>
-									<strong>
-										{{ t("pages.registrationExternalMembers.steps.welcome.tos") }}
-									</strong>
-								</template>
-							</i18n-t>
-						</div>
+						<Welcome v-else-if="step.value === 2" />
 					</VStepperWindowItem>
 				</template>
 			</VStepperWindow>
@@ -58,6 +45,7 @@
 
 <script setup lang="ts">
 import LanguageSelection from "./steps/LanguageSelection.vue";
+import Welcome from "./steps/Welcome.vue";
 import { LanguageType } from "@/serverApi/v3";
 import { useRegistration } from "@data-room";
 import { computed, onMounted, ref } from "vue";
---
+++ src/modules/feature/room/registration/steps/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<i18n-t keypath="pages.registrationExternalMembers.steps.welcome.description" scope="global">
+			<template #dataProtection>
+				<strong>
+					{{ t("pages.registrationExternalMembers.steps.welcome.dataProtection") }}
+				</strong>
+			</template>
+			<template #tos>
+				<strong>
+					{{ t("pages.registrationExternalMembers.steps.welcome.tos") }}
+				</strong>
+			</template>
+		</i18n-t>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
