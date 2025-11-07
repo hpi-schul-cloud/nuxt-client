99a6c012b2f3119600fe7bf81e98f1196198a6e6
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-11-07 10:59:53 +0100

    chore: check isExpert -- this check will be deleted after BE provides spesific permission

--- src/modules/data/room/roomAuthorization.composable.ts
+++ src/modules/data/room/roomAuthorization.composable.ts
@@ -7,6 +7,7 @@ import { computed } from "vue";
 export const useRoomAuthorization = () => {
 	const { room } = storeToRefs(useRoomDetailsStore());
 	const { hasPermission } = useAppStore();
+	const { isExpert } = storeToRefs(useAppStore());
 	const roomPermissions = computed(() => room.value?.permissions ?? []);
 
 	const checkRoomPermission = (permission: Permission) => roomPermissions.value.includes(permission);
@@ -24,7 +25,7 @@ export const useRoomAuthorization = () => {
 	const canEditRoomContent = computed(() => checkRoomPermission(Permission.RoomEditContent));
 	const canLeaveRoom = computed(() => checkRoomPermission(Permission.RoomLeaveRoom));
 	const canRemoveRoomMembers = computed(() => checkRoomPermission(Permission.RoomRemoveMembers));
-	const canViewRoom = computed(() => checkRoomPermission(Permission.RoomListContent));
+	const canViewRoom = computed(() => checkRoomPermission(Permission.RoomListContent) && !isExpert.value);
 	const canListDrafts = computed(() => checkRoomPermission(Permission.RoomListDrafts));
 	const canManageVideoconferences = computed(() => checkRoomPermission(Permission.RoomManageVideoconferences));
 	const canSeeAllStudents = hasPermission(Permission.StudentList);
