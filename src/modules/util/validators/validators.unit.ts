7e779f438725f20c1752f99785af1b06a00d1520
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-11-07 14:05:37 +0100

    add units tests for hasSpecialCharacter validator

--- src/modules/util/validators/validators.unit.ts
+++ src/modules/util/validators/validators.unit.ts
@@ -182,14 +182,21 @@ describe("util-validators", () => {
 	});
 
 	describe("hasSpecialCharacter", () => {
-		it("should accept string with at least one special character", () => {
+		const specialChars = "!§$%&/()=?\\;:,.#+*~-".split("");
+
+		it.each(specialChars)("should accept string with at least one special character", (specialChar) => {
 			const isValid = hasSpecialCharacter(ERROR);
-			expect(isValid("Abcdef!")).toBe(true);
+			expect(isValid(`Abcdef${specialChar}`)).toBe(true);
 		});
 
 		it("should not accept string without special character", () => {
 			const isValid = hasSpecialCharacter(ERROR);
 			expect(isValid("ABCDEF")).toBe(ERROR);
+		});
+
+		it("should not accept special characters that are not allowed", () => {
+			const isValid = hasSpecialCharacter(ERROR);
+			expect(isValid("Abcdef€")).toBe(ERROR);
 		});
 	});
 });
