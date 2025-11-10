f8687c4b1a22622ad3527e3de62e5fcf3ab8a0ef
Johannes Lueder <johannes.lueder@dataport.de>
2025-11-10 10:05:26 +0100

    unit tests removal of old files room grid dont need to mock data-room

--- src/modules/feature/room/RoomGrid.unit.ts
+++ src/modules/feature/room/RoomGrid.unit.ts
@@ -2,13 +2,14 @@ import RoomGrid from "./RoomGrid.vue";
 import RoomGridItem from "./RoomGridItem.vue";
 import { roomItemFactory } from "@@/tests/test-utils";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
+import { createTestingPinia } from "@pinia/testing";
 import { mount, RouterLinkStub } from "@vue/test-utils";
+import { setActivePinia } from "pinia";
 import { ComponentProps } from "vue-component-type-helpers";
-
-vi.mock("@data-room");
 
 describe("@feature-room/RoomGrid", () => {
 	const setup = (props?: ComponentProps<typeof RoomGrid>) => {
+		setActivePinia(createTestingPinia());
 		const wrapper = mount(RoomGrid, {
 			global: {
 				plugins: [createTestingVuetify(), createTestingI18n()],
