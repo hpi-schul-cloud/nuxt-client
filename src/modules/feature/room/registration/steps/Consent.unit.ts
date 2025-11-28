e6b7824163f737e5d34879e0ef894e7837336f0d
Murat Merdoglu <murat.merdoglu@dataport.de>
2025-11-28 16:46:23 +0100

    chore: increase coverage

--- src/modules/feature/room/registration/steps/Consent.unit.ts
+++ src/modules/feature/room/registration/steps/Consent.unit.ts
@@ -1,5 +1,5 @@
 import Consent from "./Consent.vue";
-import { LanguageType } from "@/serverApi/v3";
+import { LanguageType, SchulcloudTheme } from "@/serverApi/v3";
 import { createTestEnvStore } from "@@/tests/test-utils";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
 import { useRegistration } from "@data-room";
@@ -15,10 +15,9 @@ const useRegistrationMock = vi.mocked(useRegistration);
 describe("Consent.vue", () => {
 	beforeEach(() => {
 		setActivePinia(createTestingPinia());
-		createTestEnvStore();
 	});
 
-	const setup = () => {
+	const setup = (theme?: SchulcloudTheme) => {
 		useRegistrationMock.mockReturnValue({
 			selectedLanguage: ref(LanguageType.De),
 			password: ref(""),
@@ -32,6 +31,9 @@ describe("Consent.vue", () => {
 			createAccount: vi.fn(),
 			hasCreatingAccountError: ref(false),
 			userData: ref({ name: "Max", surname: "Mustermann", email: "max@mustermann.com" }),
+		});
+		createTestEnvStore({
+			SC_THEME: theme ?? SchulcloudTheme.Default,
 		});
 		const wrapper = mount(Consent, {
 			global: {
@@ -57,59 +59,73 @@ describe("Consent.vue", () => {
 		});
 
 		describe("translation keys", () => {
-			it("should contain all necessary translation keys", () => {
-				const { wrapper } = setup();
-				const translationKeys = [
-					"pages.registrationExternalMembers.steps.declarationOfConsent.firstParagraph",
-					"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.consent",
-					"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.consent.subtext",
-					"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.termsOfUse",
-				];
-
+			it.each([
+				{
+					theme: SchulcloudTheme.Default,
+					translationKeys: [
+						"pages.registrationExternalMembers.steps.declarationOfConsent.firstParagraph",
+						"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.consent",
+						"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.consent.subtext",
+						"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.termsOfUse",
+					],
+					description: "should contain all necessary translation keys",
+				},
+				{
+					theme: SchulcloudTheme.Brb,
+					translationKeys: [
+						"pages.registrationExternalMembers.steps.declarationOfConsent.firstParagraph",
+						"pages.registrationExternalMembers.steps.declarationOfConsentBRB.checkbox.consent",
+						"pages.registrationExternalMembers.steps.declarationOfConsentBRB.checkbox.consent.subtext",
+						"pages.registrationExternalMembers.steps.declarationOfConsent.checkbox.termsOfUse",
+					],
+					description: "should contain all necessary translation keys when instance is BRB",
+				},
+			])("$description", ({ theme, translationKeys }) => {
+				const { wrapper } = setup(theme);
 				translationKeys.forEach((key) => {
 					expect(wrapper.html()).toContain(key);
 				});
 			});
 		});
-	});
 
-	describe("privacy policy checkbox", () => {
-		it("should render privacy policy checkbox", () => {
-			const { wrapper } = setup();
-			const privacyPolicyCheckbox = getCheckbpoxByTestId(wrapper, "privacy-policy-checkbox");
+		describe("privacy policy checkbox", () => {
+			it("should render privacy policy checkbox", () => {
+				const { wrapper } = setup();
+				const privacyPolicyCheckbox = getCheckbpoxByTestId(wrapper, "privacy-policy-checkbox");
 
-			expect(privacyPolicyCheckbox.props("modelValue")).toBe(false);
+				expect(privacyPolicyCheckbox.props("modelValue")).toBe(false);
+			});
+
+			it("should show validation error when checkboxes are not checked", async () => {
+				const { wrapper } = setup();
+				const privacyPolicyCheckbox = getCheckbpoxByTestId(wrapper, "privacy-policy-checkbox");
+
+				await privacyPolicyCheckbox.setValue(true);
+				await privacyPolicyCheckbox.setValue(false);
+				await privacyPolicyCheckbox.trigger("blur");
+
+				expect(privacyPolicyCheckbox.text()).toContain("");
+			});
 		});
 
-		it("should show validation error when checkboxes are not checked", async () => {
-			const { wrapper } = setup();
-			const privacyPolicyCheckbox = getCheckbpoxByTestId(wrapper, "privacy-policy-checkbox");
+		describe("terms of use checkbox", () => {
+			it("should render terms of use checkbox", () => {
+				const { wrapper } = setup();
+				const termsOfUseCheckbox = getCheckbpoxByTestId(wrapper, "terms-of-use-checkbox");
 
-			await privacyPolicyCheckbox.setValue(true);
-			await privacyPolicyCheckbox.setValue(false);
-			await privacyPolicyCheckbox.trigger("blur");
+				expect(termsOfUseCheckbox.props("modelValue")).toBe(false);
+			});
 
-			expect(privacyPolicyCheckbox.text()).toContain("");
-		});
-	});
+			it("should show validation error when checkboxes are not checked", async () => {
+				const { wrapper } = setup();
+				const termsOfUseCheckbox = getCheckbpoxByTestId(wrapper, "terms-of-use-checkbox");
 
-	describe("terms of use checkbox", () => {
-		it("should render terms of use checkbox", () => {
-			const { wrapper } = setup();
-			const termsOfUseCheckbox = getCheckbpoxByTestId(wrapper, "terms-of-use-checkbox");
+				await termsOfUseCheckbox.setValue(true);
+				await termsOfUseCheckbox.setValue(false);
+				await termsOfUseCheckbox.trigger("blur");
 
-			expect(termsOfUseCheckbox.props("modelValue")).toBe(false);
-		});
-
-		it("should show validation error when checkboxes are not checked", async () => {
-			const { wrapper } = setup();
-			const termsOfUseCheckbox = getCheckbpoxByTestId(wrapper, "terms-of-use-checkbox");
-
-			await termsOfUseCheckbox.setValue(true);
-			await termsOfUseCheckbox.setValue(false);
-			await termsOfUseCheckbox.trigger("blur");
-
-			expect(termsOfUseCheckbox.text()).toContain("");
+				expect(termsOfUseCheckbox.text()).toContain("");
+			});
 		});
 	});
 });
