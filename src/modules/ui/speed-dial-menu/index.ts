f813c4e2968a5392aa87c66a819304a9c4d09dc6
Odalys Adam <odalys.adam@dataport.de>
2025-12-08 13:40:08 +0100

    adjust exports

--- src/modules/ui/speed-dial-menu/index.ts
+++ src/modules/ui/speed-dial-menu/index.ts
@@ -1,5 +1,5 @@
 import SpeedDialMenu from "./SpeedDialMenu.vue";
 import SpeedDialMenuAction from "./SpeedDialMenuAction.vue";
-import { FabAction } from "./types";
+export * from "./types";
 
-export { type FabAction, SpeedDialMenu, SpeedDialMenuAction };
+export { SpeedDialMenu, SpeedDialMenuAction };
