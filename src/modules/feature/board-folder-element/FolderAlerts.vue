24f41444688a3c9b30014d084b93acd0a26946ad
Christian Darsow <hello@darsow.io>
2025-12-15 14:54:55 +0100

    Add folder stats alerts fallback

---
+++ src/modules/feature/board-folder-element/FolderAlerts.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<WarningAlert v-if="alerts.includes(FolderAlert.FILE_STORAGE_ERROR)">
+			{{ t("Infos zum Dateiordner sind gerade nicht verfügbar. Lade den Bereich neu.") }}
+		</WarningAlert>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { FolderAlert } from "./FolderAlert.enum";
+import { WarningAlert } from "@ui-alert";
+import { useI18n } from "vue-i18n";
+
+type Props = {
+	alerts: FolderAlert[];
+};
+
+defineProps<Props>();
+
+const { t } = useI18n();
+</script>
