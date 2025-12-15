fb13cecbd67f843c48734e338587d12209c52818
Odalys Adam <odalys.adam@dataport.de>
2025-12-15 23:11:53 +0100

    add identifier for data-testids

--- src/modules/ui/dialog/Dialog.vue
+++ src/modules/ui/dialog/Dialog.vue
@@ -1,10 +1,14 @@
 <template>
-	<!-- TODO: identifier prop to add to data-testids? -->
-	<VDialog v-model="isOpen" data-testid="dialog" :max-width="480" :aria-labelledby="`dialog-${uid}-title`">
+	<VDialog
+		v-model="isOpen"
+		:data-testid="attrs['data-testid'] ?? 'dialog'"
+		:max-width="480"
+		:aria-labelledby="`dialog-${uid}-title`"
+	>
 		<UseFocusTrap>
 			<VCard :loading>
 				<template #title>
-					<h2 :id="`dialog-${uid}-title`" class="ma-0 dialog-title" data-testid="dialog-title">
+					<h2 :id="`dialog-${uid}-title`" class="ma-0 dialog-title" :data-testid="`${identifier}-dialog-title`">
 						{{ title }}
 					</h2>
 				</template>
@@ -18,16 +22,14 @@
 						<VSpacer />
 						<div class="d-flex ga-2">
 							<VBtn
-								data-testid="dialog-cancel"
+								:data-testid="`${identifier}-dialog-cancel`"
 								:disabled="areActionsDisabled"
-								:aria-label="t('common.actions.cancel')"
 								variant="text"
 								:text="t('common.actions.cancel')"
 								@click="onCancel"
 							/>
 							<VBtn
-								data-testid="dialog-confirm"
-								:aria-label="t('common.actions.confirm')"
+								:data-testid="`${identifier}-dialog-confirm`"
 								class="px-6"
 								color="primary"
 								variant="flat"
@@ -46,16 +48,17 @@
 <script setup lang="ts">
 import { useUid } from "@/utils/uid";
 import { UseFocusTrap } from "@vueuse/integrations/useFocusTrap/component";
-import { computed } from "vue";
+import { computed, useAttrs } from "vue";
 import { useI18n } from "vue-i18n";
 import { VBtn, VCard, VDialog, VSpacer } from "vuetify/lib/components/index";
 
 const props = defineProps({
+	identifier: { type: String, required: false, default: undefined },
 	title: { type: String, required: true },
 	loading: { type: Boolean, default: false },
 	areActionsDisabled: { type: Boolean, default: false },
 	confirmBtnDisabled: { type: Boolean, required: false, default: false },
-	confirmBtnLangKey: { type: String, required: false, default: "" },
+	confirmBtnLangKey: { type: String, required: false, default: undefined },
 });
 
 const emit = defineEmits(["cancel", "confirm"]);
@@ -67,6 +70,7 @@ const isOpen = defineModel({
 
 const { t } = useI18n();
 const { uid } = useUid();
+const attrs = useAttrs();
 
 const confirmBtnLangKey = computed(() =>
 	props.confirmBtnLangKey ? props.confirmBtnLangKey : "common.actions.confirm"
@@ -75,6 +79,7 @@ const confirmBtnLangKey = computed(() =>
 const onCancel = () => {
 	emit("cancel");
 };
+
 const onConfirm = () => {
 	emit("confirm");
 };
