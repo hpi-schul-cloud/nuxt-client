89b2e28e5281a91c6b050fca7722687ad175f43e
Murat Merdoglu <murat.merdoglu@dataport.de>
2026-01-14 11:33:56 +0100

    add unit tests

--- src/modules/feature/room/roomMembers/dialogs/add-external-person-dialog/StepEmail.unit.ts
+++ src/modules/feature/room/roomMembers/dialogs/add-external-person-dialog/StepEmail.unit.ts
@@ -1,5 +1,6 @@
 import StepEmail from "./StepEmail.vue";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
+import { ErrorAlert } from "@ui-alert";
 import { flushPromises, mount, VueWrapper } from "@vue/test-utils";
 import { VTextField } from "vuetify/components";
 
@@ -9,7 +10,7 @@ describe("StepEmail", () => {
 	const setup = () => {
 		wrapper = mount(StepEmail, {
 			attachTo: document.body,
-			props: { email: "" },
+			props: { email: "", hasError: false },
 			global: {
 				plugins: [createTestingVuetify(), createTestingI18n()],
 			},
@@ -100,6 +101,41 @@ describe("StepEmail", () => {
 				await clickAddButton();
 
 				expect(emailInput.text()).not.toContain("pages.rooms.members.dialog.addExternalPerson.label.email.error");
+			});
+		});
+
+		describe("when hasError prop is true", () => {
+			it("should display error message", async () => {
+				const { wrapper } = setup();
+				const errorComponentBefore = wrapper.findComponent(ErrorAlert);
+				expect(errorComponentBefore.exists()).toBe(false);
+				await wrapper.setProps({ hasError: true });
+				const errorComponent = wrapper.findComponent(ErrorAlert);
+
+				expect(errorComponent.exists()).toBe(true);
+				expect(wrapper.text()).toContain(
+					"pages.rooms.members.dialog.addExternalPerson.steps.email.error.userNotExternal"
+				);
+				const addButton = wrapper.findComponent('[data-testid="add-external-person-add-email-btn"]');
+				const cancelButton = wrapper.findComponent('[data-testid="add-external-person-cancel-btn"]');
+				const closeButton = wrapper.findComponent('[data-testid="add-external-person-close-btn"]');
+
+				expect(addButton.exists()).toBe(false);
+				expect(cancelButton.exists()).toBe(false);
+				expect(closeButton.exists()).toBe(true);
+			});
+
+			it("should display close button and hide add and cancel buttons", async () => {
+				const { wrapper } = setup();
+				await wrapper.setProps({ hasError: true });
+
+				const addButton = wrapper.findComponent('[data-testid="add-external-person-add-email-btn"]');
+				const cancelButton = wrapper.findComponent('[data-testid="add-external-person-cancel-btn"]');
+				const closeButton = wrapper.findComponent('[data-testid="add-external-person-close-btn"]');
+
+				expect(addButton.exists()).toBe(false);
+				expect(cancelButton.exists()).toBe(false);
+				expect(closeButton.exists()).toBe(true);
 			});
 		});
 	});
