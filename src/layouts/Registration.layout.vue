8de4d8e5c5dac09d10c2ce3e90a8f0e324d7eb65
Murat Merdoglu <murat.merdoglu@dataport.de>
2026-01-19 14:26:26 +0100

    remove navigation bar and adjust vuetify classes

--- src/layouts/Registration.layout.vue
+++ src/layouts/Registration.layout.vue
@@ -1,21 +1,33 @@
 <template>
-	<div class="page">
-		<header class="topbar" data-testid="registration-layout-top-bar">
-			<NavigationBar :img="Logo" :hide-buttons="true" />
-		</header>
-		<VMain :class="isExtraSmallDevice ? 'small-wrapper' : 'wrapper'">
-			<ApplicationError>
-				<AlertContainer />
-			</ApplicationError>
+	<VToolbar color="white" density="compact" elevation="2">
+		<VContainer class="d-flex align-center pa-0" :style="isExtraSmallDevice ? 'justify-content: center;' : ''">
+			<a href="/">
+				<img
+					:src="logo"
+					class="ma-0"
+					alt="Schulcloud Logo"
+					:style="isExtraSmallDevice ? 'display: block; margin: 0 auto;' : ''"
+				/>
+			</a>
+		</VContainer>
+	</VToolbar>
+	<VMain>
+		<ApplicationError>
+			<AlertContainer />
+		</ApplicationError>
+		<VContainer
+			class="main-container"
+			:style="isExtraSmallDevice ? 'width: 100%; margin-left: 0; margin-right: 0;' : ''"
+		>
 			<slot />
-		</VMain>
-		<TheFooter />
-	</div>
+		</VContainer>
+	</VMain>
+
+	<TheFooter />
 </template>
 
 <script setup lang="ts">
-import Logo from "@/assets/img/logo/logo-image-mono.svg";
-import NavigationBar from "@/components/legacy/NavigationBar.vue";
+import logo from "@/assets/img/logo/logo-image-mono.svg";
 import TheFooter from "@/components/legacy/TheFooter.vue";
 import { AlertContainer, ApplicationError } from "@ui-layout";
 import { computed } from "vue";
@@ -26,67 +38,9 @@ const isExtraSmallDevice = computed(() => xs.value);
 </script>
 
 <style lang="scss" scoped>
-.page {
-	display: grid;
-	grid-template-areas:
-		"side top"
-		"side content"
-		"side footer";
-	grid-template-rows: auto 1fr auto;
-	grid-template-columns: 0 1fr;
-	max-width: 100%;
-	min-height: 100vh;
-}
-
-.topbar {
-	grid-area: top;
-}
-
-.footer {
-	grid-area: footer;
-}
-
-.small-wrapper {
-	grid-area: content;
-	min-height: 100vh;
-	padding: 16px;
-}
-
-.wrapper {
-	grid-area: content;
-	min-height: calc(80vh - var(--legacy-topbar-height));
-	padding-right: 15px;
-	padding-left: 15px;
-	margin: 24px auto;
-}
-
-@media (min-width: 576px) {
-	.wrapper {
-		width: 540px;
-		max-width: 100%;
-	}
-}
-
-@media (min-width: 768px) {
-	.wrapper {
-		width: 720px;
-		max-width: 100%;
-	}
-}
-
-@media (min-width: 992px) {
-	.wrapper {
-		width: 960px;
-		max-width: 100%;
-	}
-}
-
-@media (min-width: 1200px) {
-	.wrapper {
-		align-items: center;
-		justify-content: space-between;
-		width: 1140px;
-		max-width: 100%;
-	}
+.main-container {
+	width: 80%;
+	margin-left: auto;
+	margin-right: auto;
 }
 </style>
