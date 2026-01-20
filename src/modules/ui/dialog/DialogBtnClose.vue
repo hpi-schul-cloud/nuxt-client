ab284349e75e7ccbe484a07844e04fa2eba56394
Odalys Adam <odalys.adam@dataport.de>
2026-01-20 19:07:17 +0100

    add basic dialog btn components

---
+++ src/modules/ui/dialog/DialogBtnClose.vue
@@ -0,0 +1,21 @@
+<template>
+	<VBtn
+		:data-testid="`${identifier}-close`"
+		variant="outlined"
+		:text="t('common.labels.close')"
+		@click="emit('click')"
+	/>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+defineProps({
+	identifier: { type: String, default: "dialog" },
+});
+
+const emit = defineEmits<{
+	(e: "click"): void;
+}>();
+</script>
---
+++ src/modules/ui/dialog/DialogBtnConfirm.vue
@@ -0,0 +1,27 @@
+<template>
+	<VBtn
+		:data-testid="`${identifier}-confirm`"
+		class="px-6"
+		color="primary"
+		variant="flat"
+		:text="t(textLangKey)"
+		:disabled="disabled"
+		@click="() => emit('click')"
+	/>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+defineProps({
+	identifier: { type: String, default: "dialog" },
+	textLangKey: { type: String, default: "common.actions.confirm" },
+	disabled: { type: Boolean, default: false },
+});
+
+const emit = defineEmits<{
+	(e: "click"): void;
+}>();
+
+const { t } = useI18n();
+</script>
