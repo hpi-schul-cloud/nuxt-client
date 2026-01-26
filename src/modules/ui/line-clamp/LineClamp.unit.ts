76fcd51dc8b7c2cee9b3ec3f07795c4a50c3f1ce
Christian Darsow <hello@darsow.io>
2026-01-26 10:25:10 +0100

    Add test for observer stopping

--- src/modules/ui/line-clamp/LineClamp.unit.ts
+++ src/modules/ui/line-clamp/LineClamp.unit.ts
@@ -1,8 +1,24 @@
 import LineClamp from "./LineClamp.vue";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
+import { createMock, DeepMocked } from "@golevelup/ts-vitest";
 import { mount } from "@vue/test-utils";
+import { useMutationObserver } from "@vueuse/core";
+
+vi.mock("@vueuse/core");
+const mockedMutationObserver = vi.mocked(useMutationObserver);
 
 describe("LineClamp", () => {
+	let mockedMutationObserverActions: DeepMocked<ReturnType<typeof useMutationObserver>>;
+	const mockStop = vi.fn();
+
+	beforeEach(() => {
+		mockedMutationObserverActions = createMock<ReturnType<typeof useMutationObserver>>({
+			stop: mockStop,
+		});
+
+		mockedMutationObserver.mockReturnValue(mockedMutationObserverActions);
+	});
+
 	const setup = (defaultSlotContent: string) => {
 		const icon = "mdi-test";
 		const wrapper = mount(LineClamp, {
@@ -25,5 +41,14 @@ describe("LineClamp", () => {
 		const { wrapper } = setup(defaultSlotContent);
 
 		expect(wrapper.text()).toBe(defaultSlotContent);
+	});
+
+	it("stops the mutation observer on component unmount", () => {
+		const defaultSlotContent = "Test content";
+		const { wrapper } = setup(defaultSlotContent);
+
+		wrapper.unmount();
+
+		expect(mockStop).toHaveBeenCalled();
 	});
 });
