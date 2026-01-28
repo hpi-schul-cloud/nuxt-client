c85455f46a868f5dc43d98e3144a76df1c3b6b3a
Christian Darsow <hello@darsow.io>
2026-01-28 14:07:51 +0100

    Use proper UTC function

--- src/plugins/datetime.js
+++ src/plugins/datetime.js
@@ -155,8 +155,7 @@ export const inputDateFormat = (date) => {
  */
 export const printDate = (date) => {
 	if (date) {
-		const tz = fromUTC(date).tz();
-		const result = tz.format(DATETIME_FORMAT.date);
+		const result = fromUTC(date).format(DATETIME_FORMAT.date);
 		return result;
 	}
 	return null;
