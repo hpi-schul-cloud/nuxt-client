42996ac2a22785fd1405e101f620badb34c2e8ba
Johannes Lueder <johannes.lueder@dataport.de>
2026-01-29 13:47:31 +0100

    unit tests

--- src/modules/ui/dialog/SvsDialog.unit.ts
+++ src/modules/ui/dialog/SvsDialog.unit.ts
@@ -1,25 +1,31 @@
 import SvsDialog from "./SvsDialog.vue";
+import SvsDialogBtnCancel from "./SvsDialogBtnCancel.vue";
+import SvsDialogBtnConfirm from "./SvsDialogBtnConfirm.vue";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
 import { createTestingPinia } from "@pinia/testing";
 import { mount } from "@vue/test-utils";
 import { setActivePinia } from "pinia";
+import { nextTick } from "vue";
 import { VCard, VCardTitle, VDialog } from "vuetify/components";
 
-describe("Dialog", () => {
+describe("SvsDialog", () => {
 	beforeEach(() => {
 		setActivePinia(createTestingPinia());
 	});
 
-	const setup = (isOpened = true) => {
+	const setup = (props: { isOpen?: boolean; noCancel?: boolean; noConfirm?: boolean; noActions?: boolean } = {}) => {
 		const title = "title of dialog";
 		const slotContent = "<div>content</div>";
 		const confirmBtnLangKey = "language.key";
 
 		const wrapper = mount(SvsDialog, {
 			props: {
-				modelValue: isOpened,
+				modelValue: props.isOpen ?? true,
 				title,
 				confirmBtnLangKey,
+				noActions: props.noActions ?? false,
+				noConfirm: props.noConfirm ?? false,
+				noCancel: props.noCancel ?? false,
 			},
 			slots: {
 				content: slotContent,
@@ -40,6 +46,38 @@ describe("Dialog", () => {
 		it("should render title", () => {
 			const { title, dialog } = setup();
 			expect(dialog.findComponent(VCardTitle).text()).toContain(title);
+		});
+
+		it("should close dialog when confirm button is clicked", async () => {
+			const { wrapper, dialog } = setup();
+			wrapper.findComponent(SvsDialogBtnConfirm).vm.$emit("click");
+			await nextTick();
+			expect(dialog.props().modelValue).toBe(false);
+		});
+
+		it("should close dialog when cancel button is clicked", async () => {
+			const { wrapper, dialog } = setup();
+			wrapper.findComponent(SvsDialogBtnCancel).vm.$emit("click");
+			await nextTick();
+			expect(dialog.props().modelValue).toBe(false);
+		});
+
+		it("should not render cancel button, when no-cancel prop is true", () => {
+			const { wrapper } = setup({ noCancel: true, noConfirm: false });
+			expect(wrapper.findComponent(SvsDialogBtnCancel).exists()).toBe(false);
+			expect(wrapper.findComponent(SvsDialogBtnConfirm).exists()).toBe(true);
+		});
+
+		it("should not render confirm button, when no-confirm prop is true", () => {
+			const { wrapper } = setup({ noCancel: true, noConfirm: false });
+			expect(wrapper.findComponent(SvsDialogBtnCancel).exists()).toBe(false);
+			expect(wrapper.findComponent(SvsDialogBtnConfirm).exists()).toBe(true);
+		});
+
+		it("should neither render confirm and cancel button when no-action prop is true", () => {
+			const { wrapper } = setup({ noActions: true });
+			expect(wrapper.findComponent(SvsDialogBtnCancel).exists()).toBe(false);
+			expect(wrapper.findComponent(SvsDialogBtnConfirm).exists()).toBe(false);
 		});
 
 		describe("when cancel button is clicked", () => {
@@ -74,7 +112,7 @@ describe("Dialog", () => {
 
 	describe("when the dialog is closed", () => {
 		it("should not render card", () => {
-			const { card } = setup(false);
+			const { card } = setup({ isOpen: false });
 			expect(card.exists()).toBe(false);
 		});
 	});
