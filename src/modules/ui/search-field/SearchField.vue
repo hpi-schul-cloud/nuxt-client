56708abbcabbdb3535094d330f02393ac695e967
Odalys Adam <odalys.adam@dataport.de>
2026-02-02 22:45:28 +0100

    add SearchField component

---
+++ src/modules/ui/search-field/SearchField.vue
@@ -0,0 +1,19 @@
+<template>
+	<VTextField v-model="searchQuery" :label="t('common.labels.search')" :prepend-inner-icon="mdiMagnify" clearable />
+</template>
+
+<script setup lang="ts">
+import { mdiMagnify } from "@icons/material";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
+defineProps<{
+	hideLabel?: boolean;
+}>();
+
+const searchQuery = defineModel({
+	type: String,
+	default: "",
+});
+</script>
---
+++ src/modules/ui/search-field/index.ts
@@ -0,0 +1,2 @@
+import SearchField from "./SearchField.vue";
+export { SearchField };
