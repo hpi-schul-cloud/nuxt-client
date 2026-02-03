4b080960c94aaebdb68825378563bee5d12af265
Odalys Adam <odalys.adam@dataport.de>
2026-02-03 17:37:05 +0100

    adjust searchfield component wip

--- src/modules/ui/search-field/SearchField.vue
+++ src/modules/ui/search-field/SearchField.vue
@@ -1,12 +1,38 @@
 <template>
-	<VTextField v-model="searchQuery" :label="t('common.labels.search')" :prepend-inner-icon="mdiMagnify" clearable />
+	<VTextField
+		v-model="searchQuery"
+		:prepend-inner-icon="mdiMagnify"
+		variant="solo-filled"
+		density="compact"
+		flat
+		hide-details
+		clearable
+		:aria-label="ariaLabel"
+		:data-testid="testId"
+	/>
+	<!-- <VTextField
+			v-model="search"
+			density="compact"
+			flat
+			hide-details
+			mobile-breakpoint="sm"
+			single-line
+			variant="solo-filled"
+			:class="{ 'order-1 w-100 mt-2': isExtraSmallDisplay }"
+			:label="t('common.labels.search')"
+			:prepend-inner-icon="mdiMagnify"
+			:aria-label="t('pages.rooms.members.filter')"
+			data-testid="table-search"
+		/> -->
 </template>
 
 <script setup lang="ts">
 import { mdiMagnify } from "@icons/material";
+import { useAttrs } from "vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
+const attrs = useAttrs();
 
 defineProps<{
 	hideLabel?: boolean;
@@ -16,4 +42,7 @@ const searchQuery = defineModel({
 	type: String,
 	default: "",
 });
+
+const ariaLabel = attrs["aria-label"] || t("common.labels.search");
+const testId = attrs["data-testid"] || "search-field";
 </script>
