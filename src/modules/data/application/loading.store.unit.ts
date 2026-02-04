9fd392395f527fc2d3a375b62b726e246bc421b8
NFriedo <69233063+NFriedo@users.noreply.github.com>
2026-02-04 16:14:08 +0100

    add tests for loading store

--- src/modules/data/application/loading.store.unit.ts
+++ src/modules/data/application/loading.store.unit.ts
@@ -1,81 +1,35 @@
-import LoadingStateModule from "@/store/loading-state";
-import { LoadingStatePayload } from "@/store/types/loading-state-payload";
+import { useLoadingStore } from "./loading.store";
+import { createTestingPinia } from "@pinia/testing";
+import { setActivePinia } from "pinia";
 
-describe("loading-state store", () => {
-	describe("actions", () => {
-		describe("open", () => {
-			it("should set default values for omitted options", () => {
-				const loadingStateModule = new LoadingStateModule({});
-				const setLoadingStateMock = vi.spyOn(loadingStateModule, "setLoadingState");
-				const payload: LoadingStatePayload = { text: "Test Text" };
-
-				loadingStateModule.open(payload);
-
-				const expectedPayload: LoadingStatePayload = {
-					...payload,
-					isPersistent: true,
-					hasOverlay: true,
-				};
-
-				expect(setLoadingStateMock).toHaveBeenCalledWith(expectedPayload);
-			});
-
-			it("should overwrite default values with given options", () => {
-				const loadingStateModule = new LoadingStateModule({});
-				const setLoadingStateMock = vi.spyOn(loadingStateModule, "setLoadingState");
-
-				const payload: LoadingStatePayload = {
-					text: "Test Text",
-					hasOverlay: false,
-					isPersistent: false,
-				};
-
-				loadingStateModule.open(payload);
-
-				expect(setLoadingStateMock).toHaveBeenCalledWith(payload);
-			});
-		});
-
-		describe("close", () => {
-			it("should change loading state", () => {
-				const loadingStateModule = new LoadingStateModule({});
-				loadingStateModule.open({
-					text: "Test Text",
-					hasOverlay: true,
-					isPersistent: false,
-				});
-				expect(loadingStateModule.getIsOpen).toBe(true);
-				loadingStateModule.close();
-				expect(loadingStateModule.getIsOpen).toBe(false);
-			});
-		});
+describe("useLoadingStore", () => {
+	beforeEach(() => {
+		setActivePinia(createTestingPinia({ stubActions: false }));
 	});
 
-	describe("getters", () => {
-		describe("getIsOpen", () => {
-			it("should initially return false", () => {
-				const loadingStateModule = new LoadingStateModule({});
-				expect(loadingStateModule.getIsOpen).toBe(false);
-			});
+	it("should have initial state", () => {
+		const store = useLoadingStore();
 
-			it("should return true after opening the dialog", () => {
-				const loadingStateModule = new LoadingStateModule({});
-				loadingStateModule.open({ text: "Test" });
-				expect(loadingStateModule.getIsOpen).toBe(true);
-			});
+		expect(store.isLoading).toBe(false);
+		expect(store.loadingText).toBe("");
+	});
+
+	describe("setLoadingState", () => {
+		it("should set loading state to true with text", () => {
+			const store = useLoadingStore();
+			const text = "Loading...";
+			store.setLoadingState(true, text);
+
+			expect(store.isLoading).toBe(true);
+			expect(store.loadingText).toBe(text);
 		});
 
-		describe("getLoadingState", () => {
-			it("should return given loading state", () => {
-				const payload: LoadingStatePayload = {
-					text: "Test Text",
-					hasOverlay: false,
-					isPersistent: false,
-				};
-				const loadingStateModule = new LoadingStateModule({});
-				loadingStateModule.open(payload);
-				expect(loadingStateModule.getLoadingState).toEqual(payload);
-			});
+		it("should set loading state to false", () => {
+			const store = useLoadingStore();
+			store.setLoadingState(true, "");
+			store.setLoadingState(false);
+
+			expect(store.isLoading).toBe(false);
 		});
 	});
 });
