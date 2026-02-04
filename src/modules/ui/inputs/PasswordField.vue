22904e10a89d78a0dacf3e029c3cb45e9c01b0ff
Odalys Adam <odalys.adam@dataport.de>
2026-02-04 23:12:45 +0100

    add Visibility toggle to PasswordField component

--- src/modules/ui/inputs/PasswordField.vue
+++ src/modules/ui/inputs/PasswordField.vue
@@ -1,12 +1,28 @@
 <template>
-	<VTextField v-model="password" type="password" :prepend-inner-icon="mdiLockOutline" validate-on="blur" />
+	<VTextField
+		v-model="password"
+		:type="fieldType"
+		:prepend-inner-icon="mdiLockOutline"
+		:append-inner-icon="visibilityIcon"
+		validate-on="blur"
+		@click:append-inner="toggleVisibility"
+	/>
 </template>
 
 <script setup lang="ts">
-import { mdiLockOutline } from "@icons/material";
+import { mdiEyeOffOutline, mdiEyeOutline, mdiLockOutline } from "@icons/material";
+import { computed, ref } from "vue";
 
 const password = defineModel({
 	type: String,
 	default: "",
 });
+
+const isHidden = ref(true);
+const visibilityIcon = computed(() => (isHidden.value ? mdiEyeOffOutline : mdiEyeOutline));
+const fieldType = computed(() => (isHidden.value ? "password" : "text"));
+
+const toggleVisibility = () => {
+	isHidden.value = !isHidden.value;
+};
 </script>
