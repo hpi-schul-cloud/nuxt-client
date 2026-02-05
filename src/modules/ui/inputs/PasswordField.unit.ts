4d26b10b868f5d7f4079050191e4fb58ddf7d0b9
Odalys Adam <odalys.adam@dataport.de>
2026-02-05 14:24:25 +0100

    add PasswordField tests

---
+++ src/modules/ui/inputs/PasswordField.unit.ts
@@ -0,0 +1,39 @@
+import PasswordField from "./PasswordField.vue";
+import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
+import { nextTick } from "vue";
+import { VTextField } from "vuetify/components";
+
+describe("@ui-inputs/PasswordField", () => {
+	const setup = (options = {}) => {
+		const wrapper = mount(PasswordField, {
+			global: {
+				plugins: [createTestingVuetify(), createTestingI18n()],
+			},
+			...options,
+		});
+
+		return wrapper;
+	};
+
+	it("should initially be of type password", () => {
+		const wrapper = setup();
+		const input = wrapper.find("input");
+
+		expect(input.attributes("type")).toBe("password");
+	});
+
+	it("should toggle visibility when append icon is clicked", async () => {
+		const wrapper = setup();
+		const input = wrapper.find("input");
+
+		wrapper.getComponent(VTextField).vm.$emit("click:append-inner");
+		await nextTick();
+
+		expect(input.attributes("type")).toBe("text");
+
+		wrapper.getComponent(VTextField).vm.$emit("click:append-inner");
+		await nextTick();
+
+		expect(input.attributes("type")).toBe("password");
+	});
+});
