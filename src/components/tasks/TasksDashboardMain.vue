11c7bf387e2645045b96b2b4035bedcb408a73ff
Odalys Adam <odalys.adam@dataport.de>
2026-02-05 20:00:33 +0100

    apply SearchField style to Task filter

--- src/components/tasks/TasksDashboardMain.vue
+++ src/components/tasks/TasksDashboardMain.vue
@@ -48,12 +48,13 @@
 				closable-chips
 				multiple
 				clearable
-				variant="solo"
-				rounded
+				variant="solo-filled"
+				flat
 				chips
 				data-testid="courseFilter"
 				item-title="text"
 				item-value="value"
+				:prepend-inner-icon="mdiMagnify"
 				:menu-props="{ closeOnContentClick: false, zIndex: 30 }"
 				:items="getSortedCoursesFilters"
 				:label="$t('pages.tasks.labels.filter')"
@@ -88,6 +89,7 @@ import {
 	mdiCheck,
 	mdiCheckCircleOutline,
 	mdiFormatListChecks,
+	mdiMagnify,
 	mdiPlaylistEdit,
 	mdiPlus,
 } from "@icons/material";
@@ -119,7 +121,7 @@ export default {
 		},
 	},
 	data() {
-		return { mdiPlus, mdiCheck };
+		return { mdiPlus, mdiCheck, mdiMagnify };
 	},
 	computed: {
 		hasTasks() {
