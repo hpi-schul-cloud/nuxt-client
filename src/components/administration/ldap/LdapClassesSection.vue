ad4b863c8d167211238b72e0402b5323d399a5c9
Odalys Adam <odalys.adam@dataport.de>
2026-02-05 09:07:23 +0100

    LdapClassesSection in script setup with Vuetify validation

--- src/components/administration/ldap/LdapClassesSection.vue
+++ src/components/administration/ldap/LdapClassesSection.vue
@@ -1,53 +1,53 @@
 <template>
 	<div>
 		<h2 class="my-10">
-			{{ $t("pages.administration.ldap.classes.title") }}
+			{{ t("pages.administration.ldap.classes.title") }}
 		</h2>
-		<VSwitch v-model="checked" :label="$t('pages.administration.ldap.classes.activate.import')" :true-icon="mdiCheck" />
+		<VSwitch v-model="checked" :label="t('pages.administration.ldap.classes.activate.import')" :true-icon="mdiCheck" />
 		<p class="my-10">
-			{{ $t("pages.administration.ldap.classes.path.subtitle") }}
+			{{ t("pages.administration.ldap.classes.path.subtitle") }}
 		</p>
 		<VTextField
 			:model-value="classPathValue"
 			data-testid="ldapDataClassesPath"
 			:disabled="checked === false"
 			class="mt-8"
-			:placeholder="$t('pages.administration.ldap.classes.path.title')"
-			:label="$t('pages.administration.ldap.classes.path.title')"
-			:hint="$t('pages.administration.ldap.classes.path.info')"
+			:placeholder="t('pages.administration.ldap.classes.path.title')"
+			:label="t('pages.administration.ldap.classes.path.title')"
+			:hint="t('pages.administration.ldap.classes.path.info')"
 			:prepend-inner-icon="mdiFileTreeOutline"
 			validate-on="blur"
-			:validation-messages="classPathValidationMessage"
+			:rules="[rules.required, rules.isValidLdapPath]"
 			datatest-id="ldapDataClassesclassPath"
-			@update:model-value="$emit('update:modelValue', { ...modelValue, classPath: $event })"
+			@update:model-value="emit('update:modelValue', { ...modelValue, classPath: $event })"
 		/>
 		<p class="my-10">
-			{{ $t("pages.administration.ldap.classes.hint") }}
+			{{ t("pages.administration.ldap.classes.hint") }}
 		</p>
 		<VTextField
 			:model-value="modelValue.nameAttribute"
 			data-testid="ldapDataClassesNameAttribute"
 			:disabled="checked === false"
 			class="mt-8"
-			:label="$t('pages.administration.ldap.classes.notice.title')"
+			:label="t('pages.administration.ldap.classes.notice.title')"
 			:prepend-inner-icon="mdiAccountEye"
 			validate-on="blur"
-			:validation-messages="classesValidationMessage"
+			:rules="[rules.required]"
 			datatest-id="ldapDataClassesNameAttribute"
-			@update:model-value="$emit('update:modelValue', { ...modelValue, nameAttribute: $event })"
+			@update:model-value="emit('update:modelValue', { ...modelValue, nameAttribute: $event })"
 		/>
 		<VTextField
 			:model-value="modelValue.participantAttribute"
 			data-testid="ldapDataClassesNameparticipantAttribute"
 			:disabled="checked === false"
 			class="mt-8"
-			:label="$t('pages.administration.ldap.classes.participant.title')"
+			:label="t('pages.administration.ldap.classes.participant.title')"
 			:prepend-inner-icon="mdiAccountGroupOutline"
 			validate-on="blur"
-			:validation-messages="classesValidationMessage"
+			:rules="[rules.required]"
 			datatest-id="ldapDataClassesParticipantsAttribute"
 			@update:model-value="
-				$emit('update:modelValue', {
+				emit('update:modelValue', {
 					...modelValue,
 					participantAttribute: $event,
 				})
@@ -56,91 +56,62 @@
 	</div>
 </template>
 
-<script>
-import { ldapPathRegexValidator } from "@/utils/ldapConstants";
+<script setup lang="ts">
 import { mdiAccountEye, mdiAccountGroupOutline, mdiCheck, mdiFileTreeOutline } from "@icons/material";
-import useVuelidate from "@vuelidate/core";
-import { required } from "@vuelidate/validators";
-import { defineComponent } from "vue";
+import { isRequired, isValidLdapPath } from "@util-validators";
+import { computed, onBeforeMount, ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 
-export default defineComponent({
-	props: {
-		modelValue: {
-			type: Object,
-			default() {
-				return {};
-			},
-		},
-		validate: {
-			type: Boolean,
-		},
-	},
-	emits: ["update:modelValue", "update:errors", "update:inputs"],
-	setup() {
-		return { v$: useVuelidate() };
-	},
-	data() {
-		return {
-			checked: false,
-			classesValidationMessage: [{ key: "required", message: this.$t("common.validation.required") }],
-			classPathValidationMessage: [
-				{
-					key: "ldapPathRegexValidator",
-					message: this.$t("pages.administration.ldapEdit.validation.path"),
-				},
-				{ key: "required", message: this.$t("common.validation.required") },
-			],
-			mdiAccountGroupOutline,
-			mdiCheck,
-			mdiFileTreeOutline,
-			mdiAccountEye,
-		};
-	},
-	computed: {
-		classPathValue() {
-			if (this.checked === false) {
-				return "";
-			}
-			return this.modelValue.classPath;
-		},
-		classPathChanged() {
-			return this.modelValue.classPath;
-		},
-	},
-	watch: {
-		validate: function () {
-			this.v$.$touch();
-			this.$emit("update:errors", this.v$.$invalid, "classes");
-		},
-		checked: function () {
-			this.$emit("update:errors", this.v$.$invalid, "classes");
-			if (this.checked === false) {
-				this.$emit("update:inputs");
-			}
-		},
-		classPathChanged: function () {
-			this.checked = !!this.modelValue.classPath;
-		},
-	},
-	beforeMount() {
-		if (this.modelValue.classPath) this.checked = true;
-	},
-	validations() {
-		if (this.checked === true) {
-			return {
-				modelValue: {
-					classPath: {
-						required,
-						ldapPathRegexValidator: ldapPathRegexValidator,
-					},
-					nameAttribute: { required },
-					participantAttribute: { required },
-				},
-			};
-		}
-		return {
-			modelValue: {},
-		};
-	},
+type LdapClassesModel = {
+	classPath?: string;
+	nameAttribute?: string;
+	participantAttribute?: string;
+};
+
+const props = defineProps<{
+	modelValue: LdapClassesModel;
+	validate?: boolean;
+}>();
+
+const emit = defineEmits<{
+	(e: "update:modelValue", value: LdapClassesModel): void;
+	(e: "update:errors", value: boolean, section: string): void;
+	(e: "update:inputs"): void;
+}>();
+
+const { t } = useI18n();
+
+onBeforeMount(() => {
+	if (props.modelValue.classPath) checked.value = true;
 });
+
+const checked = ref(false);
+const classPathValue = computed(() => (checked.value ? props.modelValue.classPath : ""));
+const classPathChanged = computed(() => props.modelValue.classPath);
+
+const rules = computed(() => ({
+	required: isRequired(t("common.validation.required")),
+	isValidLdapPath: isValidLdapPath(t("pages.administration.ldapEdit.validation.path")),
+}));
+
+watch(checked, () => {
+	// emit("update:errors", this.v$.$invalid, "classes");
+	if (checked.value === false) {
+		emit("update:inputs");
+	}
+});
+
+watch(classPathChanged, () => {
+	checked.value = !!props.modelValue.classPath;
+});
+
+watch(
+	() => props.validate,
+	(newVal) => {
+		if (newVal) {
+			// TODO: figure out what it does and refactor to work with Vuetify validation
+			// emit("update:errors", this.v$.$invalid, "classes");
+		}
+	}
+);
 </script>
