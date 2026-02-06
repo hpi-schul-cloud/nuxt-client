85e3466b9e679aa90aba7b3b90ffc8948c2bbbb5
Murat Merdoglu <murat.merdoglu@dataport.de>
2026-02-06 15:21:49 +0100

    add unit tests for the composable

---
+++ src/modules/data/application/status-alerts.composable.unit.ts
@@ -0,0 +1,88 @@
+import { useStatusAlerts } from "./status-alerts.composable";
+import * as serverApi from "@/serverApi/v3/api";
+import { mockStatusAlerts } from "@@/tests/test-utils/mockStatusAlerts";
+import { DeepMocked } from "@golevelup/ts-vitest";
+
+type StatusAlert = {
+	title: string;
+	text: string;
+	status: "info" | "done" | "danger";
+	origin: {
+		page: string;
+		message_id: number;
+	};
+	timestamp: string;
+	url: string;
+	createdAt: string;
+};
+
+let alertApi: DeepMocked<serverApi.AlertApiInterface>;
+
+describe("status alerts composable", () => {
+	beforeEach(() => {
+		alertApi = {
+			alertControllerFind: vi.fn(() => ({
+				data: {
+					data: mockStatusAlerts,
+				},
+			})),
+		} as unknown as DeepMocked<serverApi.AlertApiInterface>;
+		vi.spyOn(serverApi, "AlertApiFactory").mockReturnValue(alertApi);
+	});
+
+	describe("fetchStatusAlerts", () => {
+		it("should call api and set the alerts corectly", async () => {
+			const { fetchStatusAlerts, getStatusAlerts } = useStatusAlerts();
+			await fetchStatusAlerts();
+
+			expect(alertApi.alertControllerFind).toHaveBeenCalledOnce();
+			expect(getStatusAlerts.value).toStrictEqual(mockStatusAlerts);
+		});
+
+		it("should handle exception", async () => {
+			const error = { status: 418, statusText: "I'm a teapot" };
+			alertApi.alertControllerFind.mockRejectedValueOnce(error);
+			const { fetchStatusAlerts, businessError } = useStatusAlerts();
+			await fetchStatusAlerts();
+			expect(businessError.value).toStrictEqual(error);
+		});
+	});
+
+	describe("setStatusAlerts", () => {
+		it("should set the status alerts", () => {
+			const { setStatusAlerts, getStatusAlerts } = useStatusAlerts();
+
+			expect(getStatusAlerts.value).toEqual([]);
+			setStatusAlerts(mockStatusAlerts as StatusAlert[]);
+			expect(getStatusAlerts.value).toStrictEqual(mockStatusAlerts);
+		});
+	});
+
+	describe("businessError", () => {
+		it("should set the business error", () => {
+			const { setBusinessError, businessError } = useStatusAlerts();
+			const error = { statusCode: "500", message: "Internal Server Error" };
+			expect(businessError.value).toEqual({ statusCode: "", message: "" });
+			setBusinessError(error);
+			expect(businessError.value).toStrictEqual(error);
+		});
+
+		it("should reset the business error", () => {
+			const { setBusinessError, resetBusinessError, businessError } = useStatusAlerts();
+			const error = { statusCode: "500", message: "Internal Server Error" };
+			setBusinessError(error);
+			expect(businessError.value).toStrictEqual(error);
+			resetBusinessError();
+			expect(businessError.value).toEqual({ statusCode: "", message: "" });
+		});
+	});
+
+	describe("status", () => {
+		it("should set the status", () => {
+			const { setStatus, status } = useStatusAlerts();
+			expect(status.value).toEqual("");
+			setStatus("pending");
+			expect(status.value).toEqual("pending");
+		});
+	});
+});
