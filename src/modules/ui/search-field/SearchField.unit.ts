632fd8d0dde59446d3fa6bfde50d9667cc231393
Odalys Adam <odalys.adam@dataport.de>
2026-02-06 11:41:07 +0100

    remove unnecessary test

--- src/modules/ui/search-field/SearchField.unit.ts
+++ src/modules/ui/search-field/SearchField.unit.ts
@@ -1,7 +1,5 @@
 import SearchField from "./SearchField.vue";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
-import { mdiMagnify } from "@icons/material";
-import { VTextField } from "vuetify/components";
 
 describe("@ui-search-field", () => {
 	const setup = (options = {}) => {
@@ -18,10 +16,5 @@ describe("@ui-search-field", () => {
 	it("should render correctly", () => {
 		const wrapper = setup();
 		expect(wrapper.exists()).toBe(true);
-	});
-
-	it("should have mdiMagnify as prepend-inner-icon", () => {
-		const wrapper = setup();
-		expect(wrapper.findComponent(VTextField).props("prependInnerIcon")).toBe(mdiMagnify);
 	});
 });
