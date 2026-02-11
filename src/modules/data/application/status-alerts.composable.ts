167e2e362beb3e3f35f0a1d444a125a3260b08a1
Murat Merdoglu <64781656+muratmerdoglu-dp@users.noreply.github.com>
2026-02-11 13:38:31 +0100

    BC-11202- Migration StatusAlertsModule's Vuex Store to Composable (#4071)
    BC-11202- Migration StatusAlertsModule's Vuex Store to Composable

---
+++ src/modules/data/application/status-alerts.composable.ts
@@ -0,0 +1,22 @@
+import { useSafeAxiosTask } from "@/composables/async-tasks.composable";
+import { AlertApiFactory } from "@/serverApi/v3";
+import { StatusAlert } from "@/store/types/status-alert";
+import { $axios } from "@/utils/api";
+import { readonly, ref } from "vue";
+
+export const useStatusAlerts = () => {
+	const { execute, status } = useSafeAxiosTask();
+	const statusAlerts = ref<StatusAlert[]>([]);
+	const alertApi = AlertApiFactory(undefined, "v3", $axios);
+
+	const fetchStatusAlerts = async () => {
+		const { result } = await execute(() => alertApi.alertControllerFind());
+		statusAlerts.value = (result?.data?.data as StatusAlert[]) || [];
+	};
+
+	return {
+		status: readonly(status),
+		statusAlerts: readonly(statusAlerts),
+		fetchStatusAlerts,
+	};
+};
