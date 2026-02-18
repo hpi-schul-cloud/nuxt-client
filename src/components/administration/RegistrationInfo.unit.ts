562e7ce5e07ac24616a6fdc10a7559c792a99fb6
wolfganggreschus <wolfgang@greschus.de>
2026-02-18 11:39:14 +0100

    Refactor tests in RegistrationInfo.unit.ts for improved clarity and structure

--- src/components/administration/RegistrationInfo.unit.ts
+++ src/components/administration/RegistrationInfo.unit.ts
@@ -2,13 +2,11 @@ import RegistrationInfo from "./RegistrationInfo.vue";
 import { createTestEnvStore } from "@@/tests/test-utils";
 import { createTestingI18n, createTestingVuetify } from "@@/tests/test-utils/setup";
 import { createTestingPinia } from "@pinia/testing";
-import { mount, VueWrapper } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import { setActivePinia } from "pinia";
-import { VBtn } from "vuetify/components";
+import { VBtn, VCardTitle } from "vuetify/components";
 
 describe("RegistrationInfo", () => {
-	let wrapper: VueWrapper;
-
 	const setup = async (props = {}, envConfig = {}) => {
 		setActivePinia(createTestingPinia());
 		createTestEnvStore({
@@ -16,8 +14,7 @@ describe("RegistrationInfo", () => {
 			...envConfig,
 		});
 
-		wrapper = mount(RegistrationInfo, {
-			attachTo: document.body,
+		const wrapper = mount(RegistrationInfo, {
 			global: {
 				plugins: [createTestingVuetify(), createTestingI18n()],
 			},
@@ -27,20 +24,20 @@ describe("RegistrationInfo", () => {
 		const button = wrapper.findComponent(VBtn);
 		await button.trigger("click");
 
-		return { wrapper };
+		const title = wrapper.findComponent(VCardTitle);
+
+		return { wrapper, title };
 	};
 
-	afterEach(() => {
-		wrapper?.unmount();
-		vi.clearAllMocks();
-	});
+	//afterEach(() => {
+	//	wrapper?.unmount();
+	//	vi.clearAllMocks();
+	//});
 
 	describe("when consent is necessary", () => {
 		it("should render consent headline", async () => {
-			await setup();
-			const title = document.querySelector('[data-testid="infobox-title"]');
-
-			expect(title?.textContent).toContain("pages.administration.students.infobox.headline");
+			const { title } = await setup();
+			expect(title.text()).toContain("pages.administration.students.infobox.headline");
 		});
 
 		it("should render default consent content when showExternalText is false", async () => {
