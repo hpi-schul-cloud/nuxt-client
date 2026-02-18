e5dae7328a011a9ccce8b315e72616278bad0876
Johannes Lueder <johannes.lueder@dataport.de>
2026-02-18 08:45:05 +0100

    updated first bunch of dependencies -- minor updates, patches and eslint

--- src/pages/administration/StudentOverview.page.vue
+++ src/pages/administration/StudentOverview.page.vue
@@ -297,7 +297,7 @@ export default defineComponent({
 			return useEnvConfig().value.ADMIN_TABLES_DISPLAY_CONSENT_COLUMN;
 		},
 		filteredActions() {
-			let editedActions = this.tableActions;
+			let editedActions;
 
 			// filter actions by permissions
 			editedActions = this.tableActions.filter((action) =>
