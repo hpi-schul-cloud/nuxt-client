b05d6086794099aaf0bdfe11a544cd184aa2f140
Odalys Adam <odalys.adam@dataport.de>
2026-03-02 19:51:56 +0100

    use vuetify validation in FormCreateUser

--- src/components/administration/FormCreateUser.vue
+++ src/components/administration/FormCreateUser.vue
@@ -1,58 +1,57 @@
 <template>
-	<form>
-		<v-text-field
+	<VForm ref="userForm" @submit.prevent.stop="onSubmit">
+		<VTextField
 			v-model="userData.firstName"
-			:label="$t('common.labels.firstName')"
-			:hint="$t('common.placeholder.firstName')"
+			:label="t('common.labels.firstName')"
+			:hint="t('common.placeholder.firstName')"
 			required
-			:error-messages="getErrorMessages(v$.firstName)"
+			:rules="[isRequired()]"
+			validate-on="blur"
 			data-testid="input_create-user_firstname"
-			@blur="v$.firstName.$touch"
 		/>
-		<v-text-field
+		<VTextField
 			v-model="userData.lastName"
-			:label="$t('common.labels.lastName')"
-			:hint="$t('common.placeholder.lastName')"
+			:label="t('common.labels.lastName')"
+			:hint="t('common.placeholder.lastName')"
 			required
-			:error-messages="getErrorMessages(v$.lastName)"
+			:rules="[isRequired()]"
+			validate-on="blur"
 			data-testid="input_create-user_lastname"
-			@blur="v$.lastName.$touch"
 		/>
-		<v-text-field
+		<VTextField
 			v-model="userData.email"
-			:label="$t('common.labels.email')"
-			:hint="$t('common.placeholder.email')"
+			:label="t('common.labels.email')"
+			:hint="t('common.placeholder.email')"
 			required
-			:error-messages="getErrorMessages(v$.email)"
+			:rules="[isRequired(), isValidEmail()]"
+			validate-on="blur"
 			data-testid="input_create-user_email"
-			@blur="v$.email.$touch"
 		/>
 		<slot name="inputs" />
-
 		<slot name="errors" />
-		<v-btn
-			color="primary"
-			variant="flat"
-			class="w-100 mt-6"
-			data-testid="button_create-user_submit"
-			@click.prevent="onSubmit"
-		>
-			{{ $t("common.actions.add") }}
-		</v-btn>
-		<v-btn class="w-100 mt-6" variant="text" data-testid="button_create-user_abort" @click.prevent="$router.go(-1)">
-			{{ $t("common.actions.back") }}
-		</v-btn>
-	</form>
+		<VBtn color="primary" variant="flat" type="submit" class="w-100 mt-6" data-testid="button_create-user_submit">
+			{{ t("common.actions.add") }}
+		</VBtn>
+		<VBtn class="w-100 mt-6" variant="text" data-testid="button_create-user_abort" @click.prevent="router.go(-1)">
+			{{ t("common.actions.back") }}
+		</VBtn>
+	</VForm>
 </template>
 
-<script setup>
-import { useVuelidate } from "@vuelidate/core";
-import { email, required } from "@vuelidate/validators";
-import { reactive, unref } from "vue";
+<script setup lang="ts">
+import { isRequired, isValidEmail } from "@util-validators";
+import { reactive, unref, useTemplateRef } from "vue";
 import { useI18n } from "vue-i18n";
+import { useRouter } from "vue-router";
 
-const emit = defineEmits(["create-user"]);
+const emit = defineEmits<{
+	(e: "create-user", payload: typeof userData): void;
+}>();
+
 const { t } = useI18n();
+const router = useRouter();
+
+const userForm = useTemplateRef("userForm");
 
 const userData = reactive({
 	firstName: "",
@@ -60,21 +59,10 @@ const userData = reactive({
 	email: "",
 });
 
-const validations = {
-	firstName: { required },
-	lastName: { required },
-	email: { required, email },
-};
-
-const v$ = useVuelidate(validations, userData);
-
-const getErrorMessages = (validationModel) => {
-	const messages = validationModel.$errors.map((e) => t(`common.validation.${e.$validator}`));
-	return messages;
-};
-
 const onSubmit = async () => {
-	const isValid = await unref(v$).$validate();
+	if (!userForm.value) return;
+
+	const isValid = (await userForm.value.validate()).valid;
 
 	if (isValid) {
 		emit("create-user", unref(userData));
