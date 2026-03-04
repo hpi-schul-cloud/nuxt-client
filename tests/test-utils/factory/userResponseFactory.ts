bb78bb113f93c69bf7ee77214797306fb8ebddc1
NFriedo <69233063+NFriedo@users.noreply.github.com>
2026-03-04 14:37:41 +0100

    refactor: teacher overview page unit tests to typescript

--- tests/test-utils/factory/userResponseFactory.ts
+++ tests/test-utils/factory/userResponseFactory.ts
@@ -2,13 +2,29 @@ import { RoleName, UserResponse } from "@/serverApi/v3";
 import { UserCreatingData } from "@data-users";
 import { Factory } from "fishery";
 
-export const userResponseFactory = Factory.define<Partial<UserResponse>>(({ sequence }) => ({
+export const userResponseFactory = Factory.define<UserResponse>(({ sequence }) => ({
 	_id: `id-${sequence}`,
 	firstName: `firstName${sequence}`,
 	lastName: `lastName${sequence}`,
 	email: `email${sequence}@example.com`,
 	createdAt: new Date().toISOString(),
 	birthday: new Date(1990, 0, 1).toISOString(),
+	classes: [],
+	consentStatus: "ok",
+	preferences: {},
+	consent: {
+		userConsent: {
+			form: "digital",
+			privacyConsent: true,
+			termsOfUseConsent: true,
+			dateOfPrivacyConsent: new Date().toISOString(),
+			dateOfTermsOfUseConsent: new Date().toISOString(),
+		},
+		parentConsents: [],
+	},
+	importHash: `importHash-${sequence}`,
+	lastLoginSystemChange: new Date().toISOString(),
+	outdatedSince: "",
 }));
 
 export const userCreationDataFactory = Factory.define<UserCreatingData>(({ sequence }) => ({
