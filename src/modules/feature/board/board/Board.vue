10d390fcbdd18433ba0cdc9963c9199cbf9d6291
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2026-03-16 15:40:40 +0100

    BC-11465 - restore check for prefered tools (#4118)

--- src/modules/feature/board/board/Board.vue
+++ src/modules/feature/board/board/Board.vue
@@ -323,7 +323,10 @@ onMounted(async () => {
 	resetPageInformation();
 	useBoardInactivity();
 
-	cardStore.loadPreferredTools(ToolContextType.BoardElement);
+	if (allowedOperations.value.createExternalToolElement) {
+		await cardStore.loadPreferredTools(ToolContextType.BOARD_ELEMENT);
+	}
+
 	await boardStore.fetchBoardRequest({ boardId: props.boardId });
 
 	focusNodeFromHash();
