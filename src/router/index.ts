710289346726b3ea3c16b4f88fe341c782016440
Johannes Lueder <johannes.lueder@dataport.de>
2026-03-25 13:26:37 +0100

    scrollbehavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,12 @@ const router = createRouter({
 	linkActiveClass: "nuxt-link-active",
 	linkExactActiveClass: "nuxt-link-exact-active",
 	routes,
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition;
+		}
+		return { top: 0 };
+	},
 });
 
 router.beforeEach(clearApplicationErrorGuard);
