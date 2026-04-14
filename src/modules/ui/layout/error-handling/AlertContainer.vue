3caf66b78da8822682b19e0ec84428071d150260
wolfganggreschus <wolfgang@greschus.de>
2026-04-14 14:36:22 +0200

    fix: remove redundant comments regarding z-index in AlertContainer.vue

--- src/modules/ui/layout/error-handling/AlertContainer.vue
+++ src/modules/ui/layout/error-handling/AlertContainer.vue
@@ -76,7 +76,6 @@ const getNotificationText = (text: string) => (i18nKeyExists(text) ? t(text) : t
 .alert-wrapper {
 	position: fixed;
 	right: 0;
-	// The z-index is required because it would otherwise be hidden by the element with the class .wireframe-menu-container, which has a z-index of 2000
 	z-index: 80;
 	overflow: visible;
 }
@@ -86,7 +85,6 @@ const getNotificationText = (text: string) => (i18nKeyExists(text) ? t(text) : t
 	right: 0;
 	bottom: 5vh;
 	left: 0;
-	// The z-index is required because it would otherwise be hidden by the element with the class .wireframe-menu-container, which has a z-index of 2000
 	z-index: 80;
 	overflow: visible;
 }
