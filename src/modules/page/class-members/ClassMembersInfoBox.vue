62aecb780a3ca1567709707a9ebb37174422d094
dyedwiper <maximilian.becker@dataport.de>
2026-04-17 15:16:47 +0200

    Switch ClassMembersInfoBox to script setup

--- src/modules/page/class-members/ClassMembersInfoBox.vue
+++ src/modules/page/class-members/ClassMembersInfoBox.vue
@@ -26,56 +26,41 @@
 		</div>
 	</div>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 import { System, useSystemApi } from "@data-system";
-import { computed, ComputedRef, defineComponent, onMounted, Ref, ref, watch } from "vue";
+import { computed, ComputedRef, onMounted, Ref, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
-export default defineComponent({
-	props: {
-		systemId: {
-			type: String,
-			default: "",
-		},
-	},
-	setup(props) {
-		const { t } = useI18n();
-		const { getSystem } = useSystemApi();
+const { systemId = "" } = defineProps<{ systemId?: string }>();
 
-		const system: Ref<System | undefined> = ref();
+const { t } = useI18n();
+const { getSystem } = useSystemApi();
 
-		const infoBoxTextListItems = [
-			t("pages.classMembers.infoBox.text.listItem.first"),
-			t("pages.classMembers.infoBox.text.listItem.second"),
-			t("pages.classMembers.infoBox.text.listItem.third"),
-			t("pages.classMembers.infoBox.text.listItem.last"),
-		];
+const system: Ref<System | undefined> = ref();
 
-		onMounted(async () => {
-			if (props.systemId) {
-				system.value = await getSystem(props.systemId);
-			}
-		});
+const infoBoxTextListItems = [
+	t("pages.classMembers.infoBox.text.listItem.first"),
+	t("pages.classMembers.infoBox.text.listItem.second"),
+	t("pages.classMembers.infoBox.text.listItem.third"),
+	t("pages.classMembers.infoBox.text.listItem.last"),
+];
 
-		watch(
-			() => props.systemId,
-			async (value, oldValue) => {
-				if (value && value !== oldValue) {
-					system.value = await getSystem(props.systemId);
-				}
-			}
-		);
-
-		const hasSystem: ComputedRef<boolean> = computed(() => !!system.value);
-
-		const systemName: ComputedRef<string> = computed(() => system.value?.displayName ?? "");
-
-		return {
-			hasSystem,
-			systemName,
-			infoBoxTextListItems,
-			t,
-		};
-	},
+onMounted(async () => {
+	if (systemId) {
+		system.value = await getSystem(systemId);
+	}
 });
+
+watch(
+	() => systemId,
+	async (value, oldValue) => {
+		if (value && value !== oldValue) {
+			system.value = await getSystem(systemId);
+		}
+	}
+);
+
+const hasSystem: ComputedRef<boolean> = computed(() => !!system.value);
+
+const systemName: ComputedRef<string> = computed(() => system.value?.displayName ?? "");
 </script>
