76e842cbb5005309f3b450855467c04ec15a7a94
bergatco <129839305+bergatco@users.noreply.github.com>
2026-05-19 10:28:08 +0200

    BC-8038 - Add download functionality to H5P element (#4183)

--- src/modules/feature/board-h5p-element/H5pElementMenu.vue
+++ src/modules/feature/board-h5p-element/H5pElementMenu.vue
@@ -9,13 +9,16 @@
 		<KebabMenuAction :icon="mdiCogOutline" @click="onEdit">
 			{{ t("common.labels.settings") }}
 		</KebabMenuAction>
+		<KebabMenuAction v-if="hasLinkedContent" :icon="mdiTrayArrowDown" @click="onDownload">
+			{{ t("components.board.action.download") }}
+		</KebabMenuAction>
 		<KebabMenuActionDelete :name="displayName" @click="onDelete" />
 	</BoardMenu>
 </template>
 
 <script setup lang="ts">
 import { askDeletionForType } from "@/utils/confirmation-dialog.utils";
-import { mdiCogOutline } from "@icons/material";
+import { mdiCogOutline, mdiTrayArrowDown } from "@icons/material";
 import { BoardMenu, BoardMenuScope } from "@ui-board";
 import { KebabMenuAction, KebabMenuActionDelete, KebabMenuActionMoveDown, KebabMenuActionMoveUp } from "@ui-kebab-menu";
 import { useI18n } from "vue-i18n";
@@ -29,11 +32,18 @@ defineProps<{
 	columnIndex: number;
 	rowIndex: number;
 	elementIndex: number;
+	hasLinkedContent: boolean;
 }>();
 
-const emit = defineEmits(["edit:element", "delete:element", "move-down:element", "move-up:element"]);
+const emit = defineEmits([
+	"edit:element",
+	"delete:element",
+	"move-down:element",
+	"move-up:element",
+	"download:content",
+]);
 const onEdit = () => emit("edit:element");
-
+const onDownload = () => emit("download:content");
 const onDelete = async () => {
 	const shouldDelete = await askDeletionForType("components.cardElement.h5pElement");
 	if (shouldDelete) {
