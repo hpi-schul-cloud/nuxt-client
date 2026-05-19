d1fefc303c59172cc9132f311bd7122d0231cf62
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2026-05-19 15:28:00 +0200

    add dialog contracts

--- src/modules/feature/dialog/dialog-contracts.ts
+++ src/modules/feature/dialog/dialog-contracts.ts
@@ -1,6 +1,33 @@
+import { ShareOptions } from "../share/types";
+import { ContentItemTypeEnum } from "@/types/enum/content-item-type.enum";
+import { ShareTokenBodyParamsParentType, ShareTokenInfoResponse } from "@api-server";
+import { ImportDestination, ImportDestinationItem, ImportDestinationType } from "@feature-import";
+
 export type ConfirmationOptions = {
 	title: string;
 	message?: string;
 	messageType?: "warning" | "info";
 	confirmBtnKey?: string;
+};
+export type ConfirmationDialogProps = { options: ConfirmationOptions };
+
+export type CopyDialogProps = { copyItemType: ContentItemTypeEnum };
+
+export type ImportDialogProps = {
+	shareTokenInfo: ShareTokenInfoResponse;
+	availableDestinations: ImportDestinationItem[];
+	destinationType: Extract<ImportDestinationType, "room" | "course">;
+};
+export type ImportDialogResult = { newName: string; destination?: ImportDestination };
+
+export type ImportCardDialogProps = {
+	shareTokenInfo: ShareTokenInfoResponse;
+	availableDestinations: { id: string; name: string }[];
+	destinationType: Extract<ImportDestinationType, "column">;
+};
+export type ImportCardDialogResult = { newName: string; destination?: { id: string; name: string } };
+
+export type ShareDialogProps = {
+	shareItemType: ShareTokenBodyParamsParentType;
+	onConfirm: (options: ShareOptions) => Promise<string>;
 };
--- src/modules/feature/dialog/dialog-registry.ts
+++ src/modules/feature/dialog/dialog-registry.ts
@@ -1,46 +1,27 @@
-import { ConfirmationOptions } from "./dialog-contracts";
+import type {
+	ConfirmationDialogProps,
+	CopyDialogProps,
+	ImportCardDialogProps,
+	ImportCardDialogResult,
+	ImportDialogProps,
+	ImportDialogResult,
+	ShareDialogProps,
+} from "./dialog-contracts";
 import { defineDialog } from "./dialog-types";
+import ConfirmationDialog from "./dialogs/ConfirmationDialog.vue";
 import ConfirmDialog from "./dialogs/ConfirmDialog.vue";
 import PromptDialog from "./dialogs/PromptDialog.vue";
-import { ContentItemTypeEnum } from "@/types/enum/content-item-type.enum";
-import { ShareTokenBodyParamsParentType, ShareTokenInfoResponse } from "@api-server";
 import { CopyDialog } from "@feature-copy";
-import {
-	ImportCardDialog,
-	ImportDestination,
-	ImportDestinationItem,
-	ImportDestinationType,
-	ImportDialog,
-} from "@feature-import";
-import type { ShareOptions } from "@feature-share";
+import { ImportCardDialog, ImportDialog } from "@feature-import";
 import { ShareDialog } from "@feature-share";
 
 export const dialogRegistry = {
+	confirmation: defineDialog<ConfirmationDialogProps, boolean>(ConfirmationDialog),
+	copy: defineDialog<CopyDialogProps, boolean>(CopyDialog),
+	import: defineDialog<ImportDialogProps, ImportDialogResult>(ImportDialog),
+	importCard: defineDialog<ImportCardDialogProps, ImportCardDialogResult>(ImportCardDialog),
+	share: defineDialog<ShareDialogProps, void>(ShareDialog),
+	// examples
 	confirm: defineDialog<{ title: string; message: string }, boolean>(ConfirmDialog),
 	prompt: defineDialog<{ title: string; placeholder?: string; initialValue?: string }, string>(PromptDialog),
-	confirmation: defineDialog<ConfirmationOptions, boolean>(ConfirmDialog),
-	copy: defineDialog<{ copyItemType: ContentItemTypeEnum }, boolean>(CopyDialog),
-	import: defineDialog<
-		{
-			shareTokenInfo: ShareTokenInfoResponse;
-			availableDestinations: ImportDestinationItem[];
-			destinationType: Extract<ImportDestinationType, "room" | "course">;
-		},
-		{ newName: string; destination?: ImportDestination }
-	>(ImportDialog),
-	importCard: defineDialog<
-		{
-			shareTokenInfo: ShareTokenInfoResponse;
-			availableDestinations: { id: string; name: string }[];
-			destinationType: Extract<ImportDestinationType, "column">;
-		},
-		{ newName: string; destination?: ImportDestination }
-	>(ImportCardDialog),
-	share: defineDialog<
-		{
-			shareItemType: ShareTokenBodyParamsParentType;
-			onConfirm: (options: ShareOptions) => Promise<string>;
-		},
-		void
-	>(ShareDialog),
 };
