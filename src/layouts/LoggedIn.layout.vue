fc3cd02b8f6680ac0d5a022c93090c8490b16824
johannes-dataport <johannes.lueder@dataport.de>
2026-06-05 09:16:50 +0200

    BC-11720 - toast query utils introduced again due to legacy client query params (#4252)
    * toast query utils introduced again due to legacy client necessacity

--- src/layouts/LoggedIn.layout.vue
+++ src/layouts/LoggedIn.layout.vue
@@ -16,12 +16,14 @@
 </template>
 
 <script setup lang="ts">
+import { notifyFromQueryParams } from "@/utils/toast-query.utils";
 import { useNotificationListenerStore } from "@data-notification";
 import { AutoLogoutWarning } from "@feature-auto-logout";
 import { AlertContainer, ApplicationError, Sidebar, Topbar } from "@ui-layout";
 import { SkipLink } from "@ui-skip-link";
 import { useStorage } from "@vueuse/core";
 import { computed, onMounted, onUnmounted, watch } from "vue";
+import { useRouter } from "vue-router";
 import { useDisplay } from "vuetify";
 
 const { lgAndUp } = useDisplay();
@@ -29,6 +31,12 @@ const { lgAndUp } = useDisplay();
 const isDesktop = computed(() => lgAndUp.value);
 
 const sidebarExpanded = useStorage("sidebarExpanded", isDesktop.value);
+
+useRouter()
+	.isReady()
+	.then(() => {
+		notifyFromQueryParams();
+	});
 
 watch(isDesktop, () => {
 	sidebarExpanded.value = lgAndUp.value;
