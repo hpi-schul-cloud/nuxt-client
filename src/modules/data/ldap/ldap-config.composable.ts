6fcfa2cca7c129b893522f82b24476fac3586baa
NFriedo <69233063+NFriedo@users.noreply.github.com>
2026-06-05 10:51:54 +0200

    migrate vuex ldap config module to composable

---
+++ src/modules/data/ldap/ldap-config.composable.ts
@@ -0,0 +1,187 @@
+import { unchangedPassword } from "../../../utils/ldapConstants";
+import { useSafeAxiosTask } from "@/composables/async-tasks.composable";
+import { $axios } from "@/utils/api";
+import { notifyError } from "@data-app";
+import { ref } from "vue";
+
+type LdapFormData = {
+	url: string;
+	basisPath: string;
+	searchUser: string;
+	searchUserPassword: string | undefined;
+	userPath: string;
+	groupOption: string;
+	firstName: string;
+	familyName: string;
+	email: string;
+	uid: string;
+	uuid: string;
+	member: string;
+	student: string;
+	teacher: string;
+	admin: string;
+	user: string;
+	classPath: string | undefined;
+	nameAttribute: string | undefined;
+	participantAttribute: string | undefined;
+};
+
+const formatClientData = (data: LdapFormData) => ({
+	url: data.url,
+	rootPath: data.basisPath,
+	searchUser: data.searchUser,
+	searchUserPassword: data.searchUserPassword,
+	providerOptions: {
+		userPathAdditions: data.userPath,
+		classPathAdditions: data.classPath,
+		roleType: data.groupOption,
+		userAttributeNameMapping: {
+			givenName: data.firstName,
+			sn: data.familyName,
+			uuid: data.uuid,
+			uid: data.uid,
+			mail: data.email,
+			role: data.member,
+		},
+		roleAttributeNameMapping: {
+			roleStudent: data.student,
+			roleTeacher: data.teacher,
+			roleAdmin: data.admin,
+			roleNoSc: data.user,
+		},
+		classAttributeNameMapping: {
+			description: data.nameAttribute,
+			uniqueMember: data.participantAttribute,
+		},
+	},
+});
+
+export const useLdapConfig = () => {
+	const initialLdapConfig: LdapFormData = {
+		url: "",
+		basisPath: "",
+		searchUser: "",
+		searchUserPassword: "",
+		userPath: "",
+		groupOption: "group",
+		firstName: "",
+		familyName: "",
+		email: "",
+		uid: "",
+		uuid: "",
+		member: "memberOf",
+		student: "",
+		teacher: "",
+		admin: "",
+		user: "",
+		classPath: "",
+		nameAttribute: "",
+		participantAttribute: "",
+	};
+	const ldapConfig = ref<LdapFormData>({ ...initialLdapConfig });
+	const verified = ref({});
+	const submitted = ref({});
+	const temp = ref<Partial<LdapFormData>>({});
+
+	const { execute, status } = useSafeAxiosTask();
+
+	const getLdapConfig = async (id: string) => {
+		const { result, success, error } = await execute(() => $axios.get(`/v1/ldap-config/${id}`));
+
+		if (success) {
+			const { providerOptions } = result.data;
+			const { userAttributeNameMapping, roleAttributeNameMapping, classAttributeNameMapping } = providerOptions;
+
+			ldapConfig.value = {
+				url: result.data.url,
+				basisPath: result.data.rootPath,
+				searchUser: result.data.searchUser,
+				searchUserPassword: unchangedPassword,
+				userPath: providerOptions.userPathAdditions,
+				firstName: userAttributeNameMapping.givenName,
+				familyName: userAttributeNameMapping.sn,
+				email: userAttributeNameMapping.mail,
+				uid: userAttributeNameMapping.uid,
+				uuid: userAttributeNameMapping.uuid,
+				groupOption: providerOptions.roleType,
+				member: userAttributeNameMapping.role,
+				student: roleAttributeNameMapping.roleStudent,
+				teacher: roleAttributeNameMapping.roleTeacher,
+				admin: roleAttributeNameMapping.roleAdmin,
+				user: roleAttributeNameMapping.roleNoSc,
+				classPath: providerOptions.classPathAdditions,
+				nameAttribute: classAttributeNameMapping.description,
+				participantAttribute: classAttributeNameMapping.uniqueMember,
+			};
+		} else {
+			notifyError(String(error));
+		}
+	};
+
+	const verifyData = async (payload: LdapFormData) => {
+		const { result, success, error } = await execute(() =>
+			$axios.post("/v1/ldap-config?verifyOnly=true", formatClientData(payload))
+		);
+		if (success) {
+			temp.value = payload;
+			verified.value = result.data;
+		} else {
+			notifyError(String(error));
+		}
+	};
+
+	const verifyExisting = async (systemId: string, payload: LdapFormData) => {
+		const { result, success, error } = await execute(() =>
+			$axios.patch(`/v1/ldap-config/${systemId}?verifyOnly=true`, formatClientData(payload))
+		);
+		if (success) {
+			if (!payload.searchUserPassword) {
+				payload.searchUserPassword = unchangedPassword;
+			}
+			temp.value = payload;
+			verified.value = result.data;
+		} else {
+			notifyError(String(error));
+		}
+	};
+
+	const submitData = async (payload: LdapFormData) => {
+		const { result, success, error } = await execute(() =>
+			$axios.post("/v1/ldap-config?verifyOnly=false&activate=true", formatClientData(payload))
+		);
+		if (success) {
+			submitted.value = result.data;
+		} else {
+			notifyError(String(error));
+		}
+	};
+
+	const patchData = async (payload: LdapFormData, systemId: string) => {
+		const { result, success, error } = await execute(() =>
+			$axios.patch(`/v1/ldap-config/${systemId}?verifyOnly=false&activate=true`, formatClientData(payload))
+		);
+		if (success) {
+			submitted.value = result.data;
+		} else {
+			notifyError(String(error));
+		}
+	};
+
+	const resetLdapConfig = () => {
+		ldapConfig.value = { ...initialLdapConfig };
+	};
+
+	return {
+		ldapConfig,
+		verified,
+		submitted,
+		temp,
+		getLdapConfig,
+		resetLdapConfig,
+		verifyData,
+		verifyExisting,
+		submitData,
+		patchData,
+		status,
+	};
+};
