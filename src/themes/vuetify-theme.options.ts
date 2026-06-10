44369a8a3006e8b1e161430784b814956e5b54ed
Odalys Adam <odalys.adam@dataport.de>
2026-06-10 11:32:08 +0200

    correct hex code

--- src/themes/vuetify-theme.options.ts
+++ src/themes/vuetify-theme.options.ts
@@ -7,7 +7,7 @@ const federalStateTheme: ThemeDefinition = {
 	colors: {
 		primary: "#2370CB",
 		"primary-darken-1": "#1e599c",
-		"primary-darken-2": "#1a4d75",
+		"primary-darken-2": "#00489a",
 		"primary-lighten": "#e9f1fa",
 		"on-surface": "#0f3551",
 		"on-background": "#0f3551",
