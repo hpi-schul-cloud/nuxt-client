252b32fba393bcebf193f530a433c13053ab77d7
Max Bischof <maximilian.bischof@dataport.de>
2026-06-23 11:48:10 +0200

    fix: update OfficeDocumentType enum to use simplified document types

--- src/generated/fileStorageApi/v3/models/office-document-type.ts
+++ src/generated/fileStorageApi/v3/models/office-document-type.ts
@@ -20,9 +20,9 @@
  * @enum {string}
  */
 export enum OfficeDocumentType {
-    WORDPROCESSINGML_DOCUMENT = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    SPREADSHEETML_SHEET = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    PRESENTATIONML_PRESENTATION = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+    DOCX = 'docx',
+    XLSX = 'xlsx',
+    PPTX = 'pptx'
 }
 
 
