083ac37d20a2705ef3718f88df5db72b4299a0fd
odalys-dataport <82401838+odalys-dataport@users.noreply.github.com>
2026-07-06 09:41:33 +0200

    BC-11400 - Add card above (#4286)
    Adds card menu entry to add a card above the current card.

--- src/modules/data/board/boardActions/boardRestApi.composable.unit.ts
+++ src/modules/data/board/boardActions/boardRestApi.composable.unit.ts
@@ -99,15 +99,19 @@ describe("boardRestApi", () => {
 			const { boardStore } = setup();
 			const { createCardRequest } = useBoardRestApi();
 			const columnId = boardStore.board!.columns[0].id;
+			const position = 1;
 
 			const newCard = cardResponseFactory.build();
 			mockedBoardApiCalls.createCardCall.mockResolvedValue(newCard);
 
-			await createCardRequest({ columnId: columnId });
+			await createCardRequest({ columnId: columnId, position });
+
+			expect(mockedBoardApiCalls.createCardCall).toHaveBeenCalledWith(columnId, position);
 
 			expect(boardStore.createCardSuccess).toHaveBeenCalledWith({
 				newCard: newCard,
 				columnId: columnId,
+				position,
 				isOwnAction: true,
 			});
 		});
