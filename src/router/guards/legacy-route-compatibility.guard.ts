3701b57a3ebab15871d053518dfb499fce871eaf
Murat Merdoglu <murat.merdoglu@dataport.de>
2026-07-27 11:04:08 +0200

    refactor legacyCompatibilityGuard

--- src/router/guards/legacy-route-compatibility.guard.ts
+++ src/router/guards/legacy-route-compatibility.guard.ts
@@ -2,10 +2,14 @@ import { isLegacyClient } from "../legacy-client-route.js";
 import { NavigationGuard, RouteLocationNormalized } from "vue-router";
 
 export const legacyCompatibilityGuard: NavigationGuard = (to: RouteLocationNormalized) => {
-	if (isLegacyClient(to.path)) {
-		window.location.assign(to.path);
-		return false;
-	} else {
+	if (!isLegacyClient(to.path)) {
 		return true;
 	}
+
+	if (window.location.pathname === to.path) {
+		return true;
+	}
+
+	window.location.assign(to.fullPath);
+	return false;
 };
