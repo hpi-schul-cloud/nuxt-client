b064445489be799d07e8ea288199763e86a3e1c1
Murat Merdoglu <murat.merdoglu@dataport.de>
2026-08-06 15:04:11 +0200

    add unit tests for url utils file

--- src/modules/feature/board-link-element/util/url.util.unit.ts
+++ src/modules/feature/board-link-element/util/url.util.unit.ts
@@ -1,4 +1,5 @@
-import { ensureProtocolIncluded } from "./url.util";
+import { ensureProtocolIncluded, isBoardCardLink } from "./url.util";
+import { ObjectIdMock } from "@@/tests/test-utils";
 
 describe("url.util", () => {
 	describe("ensureProtocolIncluded", () => {
@@ -16,6 +17,40 @@ describe("url.util", () => {
 				const result = ensureProtocolIncluded(url);
 				expect(result.indexOf("https://")).toEqual(0);
 			});
+		});
+	});
+
+	describe("isBoardCardLink", () => {
+		const host = "dbildungscloud.test";
+		const boardId = ObjectIdMock();
+		const cardId = ObjectIdMock();
+
+		it("should return true for a card link on the same host", () => {
+			const url = `https://${host}/boards/${boardId}#card-${cardId}`;
+
+			expect(isBoardCardLink(url, host)).toBe(true);
+		});
+
+		it("should return false for a card link on a different host", () => {
+			const url = `https://other.host/boards/${boardId}#card-${cardId}`;
+
+			expect(isBoardCardLink(url, host)).toBe(false);
+		});
+
+		it("should return false when the hash is not a card hash", () => {
+			const url = `https://${host}/boards/${boardId}#column-${cardId}`;
+
+			expect(isBoardCardLink(url, host)).toBe(false);
+		});
+
+		it("should return false when the path is not a board path", () => {
+			const url = `https://${host}/rooms/${boardId}#card-${cardId}`;
+
+			expect(isBoardCardLink(url, host)).toBe(false);
+		});
+
+		it("should return false for an invalid url", () => {
+			expect(isBoardCardLink("not-a-url", host)).toBe(false);
 		});
 	});
 });
--- tests/test-utils/ObjectIdMock.ts
+++ tests/test-utils/ObjectIdMock.ts
@@ -1,5 +1,5 @@
 export const ObjectIdMock = () => {
-	const charSet = "abcdefgh0123456789";
+	const charSet = "abcdef0123456789";
 	let randomString = "";
 	for (let i = 0; i < 24; i++) {
 		const randomPoz = Math.floor(Math.random() * charSet.length);
