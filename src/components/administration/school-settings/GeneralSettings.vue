a94a00ba90257fbeb268c4a05cf4ead88fe15b2d
wolfganggreschus <wolfgang.greschus@dataport.de>
2026-09-10 08:44:40 +0200

    Add ‘persistent-hint’ to the school name rules in the general settings

--- src/components/administration/school-settings/GeneralSettings.vue
+++ src/components/administration/school-settings/GeneralSettings.vue
@@ -14,6 +14,7 @@
 							? t('pages.administration.school.index.generalSettings.schoolDataReadOnlyHint')
 							: undefined
 					"
+					persistent-hint
 					data-testid="school-name"
 					:rules="[validateOnOpeningTag]"
 				/>
