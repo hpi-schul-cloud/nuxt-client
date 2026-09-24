bd0fbe5674e8e124d16f93a598ff080675637a22
Murat Merdoglu <murat.merdoglu@dataport.de>
2026-09-24 10:46:29 +0200

    remove sidebar team menu item

--- src/modules/ui/layout/sidebar/SidebarItems.composable.ts
+++ src/modules/ui/layout/sidebar/SidebarItems.composable.ts
@@ -3,7 +3,6 @@ import { Permission, SchulcloudTheme } from "@api-server";
 import { useEnvConfig } from "@data-env";
 import { useFilePaths } from "@data-file";
 import {
-	mdiAccountGroupOutline,
 	mdiAccountSupervisorCircleOutline,
 	mdiApplicationBracketsOutline,
 	mdiBookshelf,
@@ -43,13 +42,6 @@ export const useSidebarItems = () => {
 			testId: "sidebar-courses",
 		},
 		{
-			title: "global.sidebar.item.teams",
-			href: "/teams",
-			icon: mdiAccountGroupOutline,
-			feature: "FEATURE_TEAMS_ENABLED",
-			testId: "sidebar-teams",
-		},
-		{
 			title: "global.sidebar.item.tasks",
 			to: "/tasks",
 			icon: mdiFormatListChecks,
@@ -70,12 +62,6 @@ export const useSidebarItems = () => {
 					title: "global.sidebar.item.courses",
 					href: "/files/courses/",
 					testId: "sidebar-files-coursefiles",
-				},
-				{
-					title: "global.sidebar.item.teams",
-					href: "/files/teams/",
-					feature: "FEATURE_TEAMS_ENABLED",
-					testId: "sidebar-files-teamfiles",
 				},
 				{
 					title: "global.sidebar.item.filesShared",
